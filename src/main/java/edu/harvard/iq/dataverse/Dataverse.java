22283d444db2369a28a0f219614e1a24a327c4ac
Gustavo Durand <gdurand@iq.harvard.edu>
2013-11-04 15:47:50 -0500

    formatting cleanup

--- src/main/java/edu/harvard/iq/dataverse/Dataverse.java
+++ src/main/java/edu/harvard/iq/dataverse/Dataverse.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package edu.harvard.iq.dataverse;
 
 import java.io.Serializable;
@@ -23,30 +22,32 @@ import org.hibernate.validator.constraints.NotBlank;
  */
 @Entity
 public class Dataverse implements Serializable {
+
     private static final long serialVersionUID = 1L;
     @Id
     //@GeneratedValue(strategy = GenerationType.AUTO)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
-    @NotBlank (message = "The World Champion Red Sox DEMAND a name!")  
+
+    @NotBlank(message = "The World Champion Red Sox DEMAND a name!")
     private String name;
-    
-    @NotBlank (message = "Please enter an alias.") 
-    @Size (max=16,message = "Alias must be at most 16 characters.")
-    @Pattern(regexp = "[a-zA-Z0-9\\_\\-]*", message="Found an illegal character(s). Valid characters are a-Z, 0-9, '_', and '-'.")
+
+    @NotBlank(message = "Please enter an alias.")
+    @Size(max = 16, message = "Alias must be at most 16 characters.")
+    @Pattern(regexp = "[a-zA-Z0-9\\_\\-]*", message = "Found an illegal character(s). Valid characters are a-Z, 0-9, '_', and '-'.")
     private String alias;
 
-    @Size (max=1000, message = "Description must be at most 1000 characters.") 
+    // #VALIDATION: page defines maxlength in input:textarea component
+    @Size(max = 1000, message = "Description must be at most 1000 characters.")
     private String description;
-    
-    @NotBlank (message = "Please enter a valid email address.") 
-    @Email(message = "Please enter a valid email address.") 
+
+    @NotBlank(message = "Please enter a valid email address.")
+    @Email(message = "Please enter a valid email address.")
     private String contactEmail;
 
     private String affiliation;
-    
-    @ManyToOne  
+
+    @ManyToOne
     private Dataverse owner;
 
     public Long getId() {
@@ -55,27 +56,56 @@ public class Dataverse implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
-    }    
-    
-    public String getName() {return name;}
-    public void setName(String name) {this.name = name;}
+    }
 
-    public String getAlias() {return alias;}
-    public void setAlias(String alias) {this.alias = alias;}
+    public String getName() {
+        return name;
+    }
 
-    public String getDescription() {return description;}
-    public void setDescription(String description) {this.description = description;}
-    
-    public String getContactEmail() {return contactEmail;}
-    public void setContactEmail(String contactEmail) {this.contactEmail = contactEmail;}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public String getAffiliation() {return affiliation;}
-    public void setAffiliation(String affiliation) {this.affiliation = affiliation;}
+    public String getAlias() {
+        return alias;
+    }
 
-    public Dataverse getOwner() {return owner;}
-    public void setOwner(Dataverse owner) {this.owner = owner;}
-    
-    
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getAffiliation() {
+        return affiliation;
+    }
+
+    public void setAffiliation(String affiliation) {
+        this.affiliation = affiliation;
+    }
+
+    public Dataverse getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Dataverse owner) {
+        this.owner = owner;
+    }
+
     @Override
     public int hashCode() {
         int hash = 0;
@@ -100,5 +130,4 @@ public class Dataverse implements Serializable {
     public String toString() {
         return "edu.harvard.iq.dataverse.Dataverse[ id=" + id + " ]";
     }
-    
 }
