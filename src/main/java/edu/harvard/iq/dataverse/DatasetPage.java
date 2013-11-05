716195bf32ad1a7f788659294b942606d016cdde
Gustavo Durand <gdurand@iq.harvard.edu>
2013-11-05 13:16:44 -0500

    new DatasetPage backing bean

---
+++ src/main/java/edu/harvard/iq/dataverse/DatasetPage.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.harvard.iq.dataverse;
+
+import javax.ejb.EJB;
+import javax.ejb.EJBException;
+import javax.faces.event.ActionEvent;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import javax.persistence.NoResultException;
+
+/**
+ *
+ * @author gdurand
+ */
+@ViewScoped
+@Named("DatasetPage")
+public class DatasetPage implements java.io.Serializable {
+
+    @EJB
+    DatasetServiceBean datasetService;
+    @EJB
+    DataverseServiceBean dataverseService;
+
+    private Dataset dataset = new Dataset();
+    private boolean editMode = false;
+    private Long ownerId;
+
+    public Dataset getDataset() {
+        return dataset;
+    }
+
+    public void setDataset(Dataset dataset) {
+        this.dataset = dataset;
+    }
+
+    public boolean isEditMode() {
+        return editMode;
+    }
+
+    public void setEditMode(boolean editMode) {
+        this.editMode = editMode;
+    }
+
+    public Long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public void init() {
+        if (dataset.getId() != null) { // view mode for a dataset           
+            dataset = datasetService.find(dataset.getId());
+
+        } else if (ownerId != null) { // create mode for a new child dataset
+            editMode = true;
+            dataset.setOwner(dataverseService.find(ownerId));
+
+        } else {
+            throw new RuntimeException("On Dataset page without id or ownerid."); // improve error handling
+        }
+    }
+
+    public void edit(ActionEvent e) {
+        editMode = true;
+    }
+
+    public void save(ActionEvent e) {
+        dataset = datasetService.save(dataset);
+        editMode = false;
+    }
+
+    public void cancel(ActionEvent e) {
+        dataset = datasetService.find(dataset.getId()); // reset dv values
+        editMode = false;
+    }
+}
