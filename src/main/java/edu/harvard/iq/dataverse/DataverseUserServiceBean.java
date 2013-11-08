69fd143f3c642e3232ad42d379ab9962e6601375
xyang02 <xyang02@hmdc.harvard.edu>
2013-11-08 17:15:04 -0500

    Added Dataverse User service bean.

---
+++ src/main/java/edu/harvard/iq/dataverse/DataverseUserServiceBean.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.harvard.iq.dataverse;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author xyang
+ */
+@Stateless
+@Named
+public class DataverseUserServiceBean {
+
+    @PersistenceContext(unitName = "VDCNet-ejbPU")
+    private EntityManager em;
+        
+    public DataverseUser save(DataverseUser dataverseUser) {
+         return em.merge(dataverseUser);
+    }   
+}
