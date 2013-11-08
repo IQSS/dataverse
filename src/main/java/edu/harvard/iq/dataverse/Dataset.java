b89385eddf0ed4d21549bd30692405b7bf5af4a6
Gustavo Durand <gdurand@iq.harvard.edu>
2013-11-08 00:05:56 -0500

    added mapped by to files relationship

--- src/main/java/edu/harvard/iq/dataverse/Dataset.java
+++ src/main/java/edu/harvard/iq/dataverse/Dataset.java
@@ -12,6 +12,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.Size;
@@ -48,9 +49,10 @@ public class Dataset implements Serializable {
     private String description;
     
     @ManyToOne
+    @JoinColumn(nullable=false)     
     private Dataverse owner;
     
-    @OneToMany
+    @OneToMany (mappedBy = "dataset")
     private List<DataFile> files = new ArrayList();
 
     public Long getId() {
