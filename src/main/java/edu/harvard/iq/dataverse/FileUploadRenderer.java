a6b2f71190716cff71f5c2046f5a5f7533e86e7f
Gustavo Durand <gdurand@iq.harvard.edu>
2013-11-07 20:25:05 -0500

    initial (fixes an issue with PrimeFaces FileUploadRender implementation)

---
+++ src/main/java/edu/harvard/iq/dataverse/FileUploadRenderer.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.harvard.iq.dataverse;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author gdurand
+ */
+public class FileUploadRenderer extends org.primefaces.component.fileupload.FileUploadRenderer{
+    @Override
+    public void decode(FacesContext context, UIComponent component) {
+        if (context.getExternalContext().getRequestContentType().toLowerCase().startsWith("multipart/")) {
+            super.decode(context, component);
+        }
+    }
+
+}
