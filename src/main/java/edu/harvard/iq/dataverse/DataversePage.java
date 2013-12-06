dce5e37684e34e35013255a9280e0b274c4fc513
mheppler <mheppler@vpn11-238.fas.harvard.edu>
2013-12-06 16:39:17 -0500

    Cleaned up the UI for creating and editing a dataverse. Added static place holders for Create Account and Log In, in the header.

--- src/main/java/edu/harvard/iq/dataverse/DataversePage.java
+++ src/main/java/edu/harvard/iq/dataverse/DataversePage.java
@@ -8,6 +8,8 @@ package edu.harvard.iq.dataverse;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.EJBException;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
@@ -55,20 +57,23 @@ public class DataversePage implements java.io.Serializable {
     }
 
     public void init() {
+        
+        // FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"Create Root Dataverse", " - To get started, you need to create your root dataverse."));  
+        
         if (dataverse.getId() != null) { // view mode for a dataverse           
             dataverse = dataverseService.find(dataverse.getId());
             ownerId = dataverse.getOwner() != null ? dataverse.getOwner().getId() : null;
-
         } else if (ownerId != null) { // create mode for a new child dataverse
             editMode = true;
             dataverse.setOwner(dataverseService.find(ownerId));
-
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"Create New Dataverse", " - Create a new dataverse that will be a child dataverse of the parent you clicked from."));
         } else { // view mode for root dataverse (or create root dataverse)
             try {
                 dataverse = dataverseService.findRootDataverse();
             } catch (EJBException e) {
                 if (e.getCause() instanceof NoResultException) {
                     editMode = true;
+                    FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"Create Root Dataverse", " - To get started, you need to create your root dataverse."));
                 } else {
                     throw e;
                 }
@@ -85,6 +90,7 @@ public class DataversePage implements java.io.Serializable {
 
     public void edit(ActionEvent e) {
         editMode = true;
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"Edit Dataverse", " - Edit your dataverse and click Save."));
     }
 
     public void save(ActionEvent e) {
