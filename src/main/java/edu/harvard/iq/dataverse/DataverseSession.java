ae9a95fa6f33f8963b7569f462bc7a8a4f308f96
Gustavo Durand <gdurand@iq.harvard.edu>
2013-12-09 13:48:13 -0500

    initial dataverseSession oject

---
+++ src/main/java/edu/harvard/iq/dataverse/DataverseSession.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.harvard.iq.dataverse;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+//import javax.faces.bean.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author gdurand
+ */
+@Named
+@SessionScoped
+public class DataverseSession implements Serializable{
+    
+DataverseUser user;
+
+    public DataverseUser getUser() {
+        return user;
+    }
+
+    public void setUser(DataverseUser user) {
+        this.user = user;
+    }
+
+
+    
+}
