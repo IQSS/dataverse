082629d8392629d014e2e7d6b0d2075335540335
Philip Durbin <philip_durbin@harvard.edu>
2013-12-09 12:38:44 -0500

    switch to dynamic fields in Solr for now (faster iteration)

---
+++ src/main/java/edu/harvard/iq/dataverse/api/SearchFields.java
@@ -0,0 +1,18 @@
+package edu.harvard.iq.dataverse.api;
+
+public class SearchFields {
+
+    // standard fields from example/solr/collection1/conf/schema.xml
+    public static final String ID = "id";
+    public static final String NAME = "name";
+    public static final String DESCRIPTION = "description";
+    public static final String TITLE = "title";
+    // dynamic fields (for now) http://wiki.apache.org/solr/SchemaXml#Dynamic_fields
+    // *_s for String
+    // *_l for Long
+    // etc.
+    public static final String ENTITY_ID = "entityid_l";
+    public static final String TYPE = "type_s";
+    public static final String AFFILIATION = "affiliation_s";
+
+}
