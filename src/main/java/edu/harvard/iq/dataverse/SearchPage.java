51b0f5a7d5dd1eaf1647f91f7bce595580b35686
Philip Durbin <philip_durbin@harvard.edu>
2013-12-13 10:45:20 -0500

    remove search type "users" (decision not to index them)

--- src/main/java/edu/harvard/iq/dataverse/SearchPage.java
+++ src/main/java/edu/harvard/iq/dataverse/SearchPage.java
@@ -17,7 +17,6 @@ public class SearchPage implements java.io.Serializable {
     private String query;
     private List<Dataverse> dataverses = new ArrayList();
     private List<Dataset> datasets = new ArrayList<>();
-    private List<DataverseUser> dataverseUsers = new ArrayList<>();
     private List<DataFile> dataFiles = new ArrayList<>();
     private List<String> facets = new ArrayList<>();
     private List<String> spelling_alternatives = new ArrayList<>();
@@ -79,15 +78,9 @@ public class SearchPage implements java.io.Serializable {
         /**
          * @todo fill this in with real search results
          */
-        dataverseUsers = new ArrayList();
         dataFiles = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
             Long id = new Long(String.valueOf(i));
-
-            DataverseUser dvUser = new DataverseUser();
-            dvUser.setId(id);
-            dvUser.setUserName("user" + i);
-            dataverseUsers.add(dvUser);
 
             DataFile dataFile = new DataFile();
             dataFile.setId(id);
@@ -119,14 +112,6 @@ public class SearchPage implements java.io.Serializable {
 
     public void setDatasets(List<Dataset> datasets) {
         this.datasets = datasets;
-    }
-
-    public List<DataverseUser> getDataverseUsers() {
-        return dataverseUsers;
-    }
-
-    public void setDataverseUsers(List<DataverseUser> dataverseUsers) {
-        this.dataverseUsers = dataverseUsers;
     }
 
     public List<DataFile> getDataFiles() {
