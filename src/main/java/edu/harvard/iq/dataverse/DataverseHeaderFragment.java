8c6c311db8ee895416ea34cd6957f25b68e1c4e5
Gustavo Durand <gdurand@iq.harvard.edu>
2014-02-12 17:21:30 -0500

    cleanup of breadcrumb code

--- src/main/java/edu/harvard/iq/dataverse/DataverseHeaderFragment.java
+++ src/main/java/edu/harvard/iq/dataverse/DataverseHeaderFragment.java
@@ -5,6 +5,7 @@
  */
 package edu.harvard.iq.dataverse;
 
+import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.view.ViewScoped;
@@ -20,25 +21,35 @@ import org.primefaces.model.TreeNode;
 @Named
 public class DataverseHeaderFragment implements java.io.Serializable {
 
-    
     @EJB
-    DataverseServiceBean dataverseService;    
+    DataverseServiceBean dataverseService;
 
-    
-    public TreeNode getDataverseTree(Dataverse dataverse) { 
-        if (dataverse == null) {return null;}
-        return getDataverseTree(dataverse, new DefaultTreeNode("Root",null));
-    }   
+    public List<Dataverse> getDataverses(Dataverse dataverse) {
+        List dataverses = new ArrayList();
+        if (dataverse != null) {
+            dataverses.addAll(dataverse.getOwners());
+            dataverses.add(dataverse);
+        } else {
+            dataverses.add(dataverseService.findRootDataverse());
+        }
+        return dataverses;
+    }
 
-    public TreeNode getDataverseTree(Dataverse dataverse, TreeNode root) {
-        
+    public TreeNode getDataverseTree(Dataverse dataverse) {
+        if (dataverse == null) { // the primefaces component seems to call this with dataverse == null for some reason
+            return null;
+        }
+        return getDataverseNode(dataverse, new DefaultTreeNode("Root", null), true);
+    }
+
+    private TreeNode getDataverseNode(Dataverse dataverse, TreeNode root, boolean expand) {
         TreeNode dataverseNode = new DefaultTreeNode(dataverse, root);
-
+        dataverseNode.setExpanded(expand);
         List<Dataverse> childDataversesOfCurrentDataverse = dataverseService.findByOwnerId(dataverse.getId());
         for (Dataverse child : childDataversesOfCurrentDataverse) {
-            getDataverseTree(child, dataverseNode);
-        }   
-      
+            getDataverseNode(child, dataverseNode, false);
+        }
+
         return root;
     }
 
