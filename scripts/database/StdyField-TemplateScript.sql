40764e6e4f266b54ac63f81d3481404a9f97feaf
Leonid Andreev <leonid@hmdc.harvard.edu>
2014-02-13 19:12:15 -0500

    Added variable type configuration to the reference data.

--- scripts/database/StdyField-TemplateScript.sql
+++ scripts/database/StdyField-TemplateScript.sql
@@ -411,3 +411,26 @@ INSERT INTO dvobject_metadatablock(
         INSERT INTO dvobject_metadatablock(
             dataverse_id, metadatablocks_id)
     VALUES (1, 5);
+
+INSERT INTO variableformattype (id, "name") VALUES (1,'numeric');
+INSERT INTO variableformattype (id, "name") VALUES (2,'character');
+
+INSERT INTO variableintervaltype (id, "name") VALUES (1, 'discrete');
+INSERT INTO variableintervaltype (id, "name") VALUES (2, 'continuous');
+INSERT INTO variableintervaltype (id, "name") VALUES (3, 'nominal');
+INSERT INTO variableintervaltype (id, "name") VALUES (4, 'dichotomous');
+
+INSERT INTO summarystatistictype (id, "name") VALUES (1, 'mean');
+INSERT INTO summarystatistictype (id, "name") VALUES (2, 'medn');
+INSERT INTO summarystatistictype (id, "name") VALUES (3, 'mode');
+INSERT INTO summarystatistictype (id, "name") VALUES (4, 'min');
+INSERT INTO summarystatistictype (id, "name") VALUES (5, 'max');
+INSERT INTO summarystatistictype (id, "name") VALUES (6, 'stdev');
+INSERT INTO summarystatistictype (id, "name") VALUES (7, 'vald');
+INSERT INTO summarystatistictype (id, "name") VALUES (8, 'invd');
+
+INSERT INTO variablerangetype (id, "name") VALUES (1, 'min');
+INSERT INTO variablerangetype (id, "name") VALUES (2, 'max');
+INSERT INTO variablerangetype (id, "name") VALUES (3, 'min');
+INSERT INTO variablerangetype (id, "name") VALUES (4, 'max');
+INSERT INTO variablerangetype (id, "name") VALUES (5, 'point');
