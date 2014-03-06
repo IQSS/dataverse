7e1dcbc7d7b009f60e16dc68250edad321bba803
Gustavo Durand <gdurand@iq.harvard.edu>
2014-03-05 19:43:51 -0500

    initial checkin

---
+++ src/main/java/edu/harvard/iq/dataverse/MetadataBlockConverter.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.harvard.iq.dataverse;
+
+import javax.ejb.EJB;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+/**
+ *
+ * @author xyang
+ */
+@FacesConverter("metadataBlockConverter")
+public class MetadataBlockConverter implements Converter {
+
+    @EJB
+    DataverseServiceBean dataverseService;
+
+    public Object getAsObject(FacesContext facesContext, UIComponent component, String submittedValue) {
+        System.out.println("getAsObject: " + submittedValue);
+        MetadataBlock mdb = dataverseService.findMDB(new Long(submittedValue));
+        System.out.println("getAsObject: " + mdb);
+        return mdb;
+    }
+
+    public String getAsString(FacesContext facesContext, UIComponent component, Object value) {
+         System.out.println("getAsString: " + value);
+        if (value == null || value.equals("")) {
+            return "";
+        } else {
+            return ((MetadataBlock) value).getId().toString();
+        }
+    }
+}
