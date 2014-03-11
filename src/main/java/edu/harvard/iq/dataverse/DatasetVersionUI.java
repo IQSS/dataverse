b1548d75696494febe54349f3abea1e4612c012d
Gustavo Durand <gdurand@iq.harvard.edu>
2014-03-11 16:45:54 -0400

    cleanup for handling metadata as generic

--- src/main/java/edu/harvard/iq/dataverse/DatasetVersionUI.java
+++ src/main/java/edu/harvard/iq/dataverse/DatasetVersionUI.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package edu.harvard.iq.dataverse;
 
 import java.util.ArrayList;
@@ -22,18 +21,20 @@ import javax.faces.model.SelectItem;
  * @author skraffmiller
  */
 public class DatasetVersionUI {
+
     @EJB
-    DataverseServiceBean dataverseService; 
+    DataverseServiceBean dataverseService;
+
     public DatasetVersionUI() {
     }
-    
-    private Map<MetadataBlock, List> metadataBlockValues = new HashMap(); 
-    
-    public List<Map.Entry<MetadataBlock, List>> getMetadataBlockValues() { 
-       Set<Map.Entry<MetadataBlock, List>> metadataSet = metadataBlockValues.entrySet();
+
+    private Map<MetadataBlock, List> metadataBlockValues = new HashMap();
+
+    public List<Map.Entry<MetadataBlock, List>> getMetadataBlockValues() {
+        Set<Map.Entry<MetadataBlock, List>> metadataSet = metadataBlockValues.entrySet();
         return new ArrayList<>(metadataSet);
     }
-    
+
     public DatasetVersionUI(DatasetVersion datasetVersion) {
         /*takes in the values of a dataset version 
          and apportions them into lists for 
@@ -42,11 +43,11 @@ public class DatasetVersionUI {
         setDatasetVersion(datasetVersion);
         this.setDatasetAuthors(new ArrayList());
         this.setDatasetKeywords(new ArrayList());
-        this.setDatasetRelPublications(new ArrayList()); 
+        this.setDatasetRelPublications(new ArrayList());
         this.setSubjects(new ArrayList());
 
+        // loop through vaues to get fields for view mode
         for (DatasetFieldValue dsfv : datasetVersion.getDatasetFieldValues()) {
-            //Special Fields for 'above the fold'
             if (dsfv.getDatasetField().getName().equals(DatasetFieldConstant.title)) {
                 setTitle(dsfv);
             } else if (dsfv.getDatasetField().getName().equals(DatasetFieldConstant.descriptionText)) {
@@ -70,13 +71,10 @@ public class DatasetVersionUI {
                 this.getDatasetKeywords().add(dsfv);
             } else if (dsfv.getDatasetField().getName().equals(DatasetFieldConstant.subject)) {
                 this.getSubjects().add(dsfv.getStrValue());
-            } else if (dsfv.getDatasetField().isShowAboveFold()) {   
-                //General fields Above Fold
-                this.getAboveFoldGeneralValues().add(dsfv);                                
-            } 
-        //Special handling for Related Materials
+            }
+        //Special handling for Related Publications
         /* Treated as below the tabs for editing, but must get first value for display above tabs    
-        */
+             */
             if (dsfv.getDatasetField().getName().equals(DatasetFieldConstant.publication) && this.datasetRelPublications.isEmpty()) {
                 Collection childVals = dsfv.getChildDatasetFieldValues();
                 DatasetRelPublication datasetRelPublication = new DatasetRelPublication();
@@ -100,85 +98,100 @@ public class DatasetVersionUI {
                         }
                     }
                 }
-                this.getDatasetRelPublications().add(datasetRelPublication);  
+                this.getDatasetRelPublications().add(datasetRelPublication);
             }
             //Get notes text for display
-            if (dsfv.getDatasetField().getName().equals(DatasetFieldConstant.notesText)){
+            if (dsfv.getDatasetField().getName().equals(DatasetFieldConstant.notesText)) {
                 this.setNotesText(dsfv.getStrValue());
             }
         }
-     setMetadataValueBlocks(datasetVersion);
-     loadSubjectControlledVocabulary();
+        setMetadataValueBlocks(datasetVersion);
     }
-    
-    private Dataset getDataset(){
+
+    private Dataset getDataset() {
         return this.datasetVersion.getDataset();
     }
-    
+
     private DatasetVersion datasetVersion;
+
     public DatasetVersion getDatasetVersion() {
         return datasetVersion;
     }
+
     public void setDatasetVersion(DatasetVersion datasetVersion) {
         this.datasetVersion = datasetVersion;
     }
 
     private DatasetFieldValue title;
+
     public DatasetFieldValue getTitle() {
         return title;
     }
+
     public void setTitle(DatasetFieldValue title) {
         this.title = title;
     }
 
-    private List <String> subjects = new ArrayList();
-    public List<String>  getSubjects() {
+    private List<String> subjects = new ArrayList();
+
+    public List<String> getSubjects() {
         return subjects;
     }
-    public void setSubjects(List <String> subjects) {
+
+    public void setSubjects(List<String> subjects) {
         this.subjects = subjects;
     }
 
-    private List <SelectItem> subjectControlledVocabulary = new ArrayList();
-    public List<SelectItem>  getSubjectControlledVocabulary() {
+    private List<SelectItem> subjectControlledVocabulary = new ArrayList();
+
+    public List<SelectItem> getSubjectControlledVocabulary() {
         return subjectControlledVocabulary;
     }
-    public void setSubjectControlledVocabulary(List <SelectItem> subjectControlledVocabulary) {
+
+    public void setSubjectControlledVocabulary(List<SelectItem> subjectControlledVocabulary) {
         this.subjectControlledVocabulary = subjectControlledVocabulary;
     }
-    
+
     private List<DatasetAuthor> datasetAuthors = new ArrayList();
+
     public List<DatasetAuthor> getDatasetAuthors() {
         return datasetAuthors;
     }
+
     public void setDatasetAuthors(List<DatasetAuthor> datasetAuthors) {
         this.datasetAuthors = datasetAuthors;
     }
-    
+
     private DatasetFieldValue description;
+
     public DatasetFieldValue getDescription() {
         return description;
     }
+
     public void setDescription(DatasetFieldValue description) {
         this.description = description;
     }
-    
+
     private List<DatasetFieldValue> datasetKeywords = new ArrayList();
+
     public List<DatasetFieldValue> getDatasetKeywords() {
         return datasetKeywords;
     }
+
     public void setDatasetKeywords(List<DatasetFieldValue> datasetKeywords) {
         this.datasetKeywords = datasetKeywords;
-    } 
-    
+    }
+
     private List<DatasetRelPublication> datasetRelPublications;
+
     public List<DatasetRelPublication> getDatasetRelPublications() {
         return datasetRelPublications;
     }
+
     public void setDatasetRelPublications(List<DatasetRelPublication> datasetRelPublications) {
         this.datasetRelPublications = datasetRelPublications;
-    } 
-    
+    }
+
     private String notesText;
 
     public String getNotesText() {
@@ -188,27 +201,20 @@ public class DatasetVersionUI {
     public void setNotesText(String notesText) {
         this.notesText = notesText;
     }
-    
-    public String getRelPublicationCitation(){
-        if(!this.datasetRelPublications.isEmpty()){
+
+    public String getRelPublicationCitation() {
+        if (!this.datasetRelPublications.isEmpty()) {
             return this.getDatasetRelPublications().get(0).getText();
         } else {
             return "";
         }
     }
-    
+
     public String getUNF() {
         //todo get UNF to calculate and display here.
         return "";
     }
-    
-    private List<DatasetFieldValue> aboveFoldGeneralValues = new ArrayList();
-    public List<DatasetFieldValue> getAboveFoldGeneralValues() {
-        return aboveFoldGeneralValues;
-    }
-    public void setAboveFoldGeneralValues(List<DatasetFieldValue> aboveFoldGeneralValues) {
-        this.aboveFoldGeneralValues = aboveFoldGeneralValues;
-    }    
+
 
     //TODO - make sure getCitation works
     private String getYearForCitation(String dateString) {
@@ -248,14 +254,14 @@ public class DatasetVersionUI {
                 str += getYearForCitation(getProductionDate());
             }
         }
-        
+
         if (!StringUtil.isEmpty(getTitle().getStrValue())) {
             if (!StringUtil.isEmpty(str)) {
                 str += ", ";
             }
             str += "\"" + getTitle().getStrValue() + "\"";
         }
-        
+
         if (!StringUtil.isEmpty(dataset.getIdentifier())) {
             if (!StringUtil.isEmpty(str)) {
                 str += ", ";
@@ -266,40 +272,39 @@ public class DatasetVersionUI {
                 str += dataset.getPersistentURL();
             }
         }
-        
+
         //Get root dataverse name for Citation
-         Dataverse root = getDatasetVersion().getDataset().getOwner();
-         while (root.getOwner() != null){
-             root = root.getOwner();
-         }
-         String rootDataverseName = root.getName();
+        Dataverse root = getDatasetVersion().getDataset().getOwner();
+        while (root.getOwner() != null) {
+            root = root.getOwner();
+        }
+        String rootDataverseName = root.getName();
         if (!StringUtil.isEmpty(rootDataverseName)) {
             if (!StringUtil.isEmpty(str)) {
                 str += ", ";
             }
             str += " " + rootDataverseName + " [Publisher] ";
         }
-        
-                
+
         if (this.getDatasetVersion().getVersionNumber() != null) {
             str += " V" + this.getDatasetVersion().getVersionNumber();
             str += " [Version]";
         }
         /*UNF is not calculated yet
-        if (!StringUtil.isEmpty(getUNF())) {
-            if (!StringUtil.isEmpty(str)) {
-                str += " ";
-            }
-            str += getUNF();
-        }
-        String distributorNames = getDistributorNames();
-        if (distributorNames.trim().length() > 0) {
-            str += " " + distributorNames;
-            str += " [Distributor]";
-        }*/
+         if (!StringUtil.isEmpty(getUNF())) {
+         if (!StringUtil.isEmpty(str)) {
+         str += " ";
+         }
+         str += getUNF();
+         }
+         String distributorNames = getDistributorNames();
+         if (distributorNames.trim().length() > 0) {
+         str += " " + distributorNames;
+         str += " [Distributor]";
+         }*/
         return str;
     }
-    
+
     public String getAuthorsStr() {
         return getAuthorsStr(true);
     }
@@ -310,8 +315,8 @@ public class DatasetVersionUI {
             if (str.trim().length() > 1) {
                 str += "; ";
             }
-            if (sa.getName() != null && !StringUtil.isEmpty(sa.getName().getStrValue())){
-                str += sa.getName().getStrValue(); 
+            if (sa.getName() != null && !StringUtil.isEmpty(sa.getName().getStrValue())) {
+                str += sa.getName().getStrValue();
             }
             if (affiliation) {
                 if (sa.getAffiliation() != null && !StringUtil.isEmpty(sa.getAffiliation().getStrValue())) {
@@ -321,85 +326,69 @@ public class DatasetVersionUI {
         }
         return str;
     }
-    
+
     public String getKeywordsStr() {
         String str = "";
         for (DatasetFieldValue sa : this.getDatasetKeywords()) {
             if (str.trim().length() > 1) {
                 str += "; ";
             }
-            if (sa.getStrValue() != null && sa.getStrValue().toString() != null && !sa.getStrValue().toString().trim().isEmpty() ){
-                 str += sa.getStrValue().toString().trim(); 
+            if (sa.getStrValue() != null && sa.getStrValue().toString() != null && !sa.getStrValue().toString().trim().isEmpty()) {
+                str += sa.getStrValue().toString().trim();
             }
         }
         return str;
     }
-    
+
     public String getSubjectStr() {
         String str = "";
         for (String sa : this.getSubjects()) {
             if (str.trim().length() > 1) {
                 str += "; ";
             }
-            if (sa != null && sa.toString() != null && !sa.toString().trim().isEmpty() ){
-                 str += sa.toString().trim(); 
+            if (sa != null && sa.toString() != null && !sa.toString().trim().isEmpty()) {
+                str += sa.toString().trim();
             }
         }
         return str;
     }
 
-    public String getProductionDate() {        
-        for (DatasetFieldValue dsfv : datasetVersion.getDatasetFieldValues()){
-            if(dsfv.getDatasetField().getName().equals(DatasetFieldConstant.productionDate)){
-                 return dsfv.getStrValue();
+    public String getProductionDate() {
+        for (DatasetFieldValue dsfv : datasetVersion.getDatasetFieldValues()) {
+            if (dsfv.getDatasetField().getName().equals(DatasetFieldConstant.productionDate)) {
+                return dsfv.getStrValue();
             }
         }
         return "";
     }
 
     public String getDistributionDate() {
-         for (DatasetFieldValue dsfv : datasetVersion.getDatasetFieldValues()){
-            if(dsfv.getDatasetField().getName().equals(DatasetFieldConstant.distributionDate)){
-                 return dsfv.getStrValue();
+        for (DatasetFieldValue dsfv : datasetVersion.getDatasetFieldValues()) {
+            if (dsfv.getDatasetField().getName().equals(DatasetFieldConstant.distributionDate)) {
+                return dsfv.getStrValue();
             }
         }
         return "";
     }
-    
+
     public void setMetadataValueBlocks(DatasetVersion datasetVersion) {
         metadataBlockValues.clear();
         for (MetadataBlock mdb : this.datasetVersion.getDataset().getOwner().getMetadataBlocks()) {
             List addList = new ArrayList();
-            if (!mdb.isShowOnCreate()) {
-                for (DatasetFieldValue dsfv : datasetVersion.getDatasetFieldValues()) {
-                    if (!dsfv.getDatasetField().isShowAboveFold() && dsfv.getDatasetField().getMetadataBlock().equals(mdb)) {
-                        addList.add(dsfv);
-                    }
+            for (DatasetFieldValue dsfv : datasetVersion.getDatasetFieldValues()) {
+                if (dsfv.getDatasetField().getMetadataBlock().equals(mdb)) {
+                    addList.add(dsfv);
                 }
-                Collections.sort(addList, new Comparator<DatasetFieldValue>() {
-                    public int compare(DatasetFieldValue d1, DatasetFieldValue d2) {
-                        int a = d1.getDatasetField().getDisplayOrder();
-                        int b = d2.getDatasetField().getDisplayOrder();
-                        return Integer.valueOf(a).compareTo(Integer.valueOf(b));
-                    }
-                });
-                metadataBlockValues.put(mdb, addList);
             }
+            Collections.sort(addList, new Comparator<DatasetFieldValue>() {
+                public int compare(DatasetFieldValue d1, DatasetFieldValue d2) {
+                    int a = d1.getDatasetField().getDisplayOrder();
+                    int b = d2.getDatasetField().getDisplayOrder();
+                    return Integer.valueOf(a).compareTo(Integer.valueOf(b));
+                }
+            });
+            metadataBlockValues.put(mdb, addList);
         }
     }
-    
-    private void loadSubjectControlledVocabulary(){        
-       for (DatasetFieldValue dsfv : datasetVersion.getDatasetFieldValues()){
-            if(dsfv.getDatasetField().getName().equals(DatasetFieldConstant.subject)){
-                 this.subjectControlledVocabulary.clear();
-                 for (ControlledVocabularyValue value : dsfv.getDatasetField().getControlledVocabularyValues()){
-                     SelectItem item = new SelectItem();
-                     item.setValue(value.getStrValue());
-                     item.setLabel(value.getStrValue());
-                     this.subjectControlledVocabulary.add(item);
-                 }
-                 break; //only do once if multiple values
-            }
-        }               
-    }
+
 }
