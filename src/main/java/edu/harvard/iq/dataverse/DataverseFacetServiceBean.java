bc216404bf95b83beeb50f61790beeb8b767346f
xyang02 <xyang02@hmdc.harvard.edu>
2014-03-13 17:54:13 -0400

    Added selecting domain search facets feature (#3498).

---
+++ src/main/java/edu/harvard/iq/dataverse/DataverseFacetServiceBean.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.harvard.iq.dataverse;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author xyang
+ */
+@Stateless
+@Named
+public class DataverseFacetServiceBean {
+    
+    @PersistenceContext(unitName = "VDCNet-ejbPU")
+    private EntityManager em;
+    
+    public List<DataverseFacet> findByDataverseId(Long dataverseId) {
+        Query query = em.createQuery("select object(o) from DataverseFacet as o where o.dataverse.id = :dataverseId order by o.displayOrder");
+        query.setParameter("dataverseId", dataverseId);
+        return query.getResultList();
+    }
+    public List<DataverseFacet> findByRootDataverse() {
+        return em.createQuery("select object(o) from DataverseFacet as o where o.dataverse.id = 1 order by o.displayOrder").getResultList();
+    }
+
+    public void delete(DataverseFacet dataverseFacet) {
+        em.remove(em.merge(dataverseFacet));
+    }
+    
+    public void create(int diplayOrder, Long datasetFieldId, Long dataverseId) {
+        DataverseFacet dataverseFacet = new DataverseFacet();
+        
+        dataverseFacet.setDisplayOrder(diplayOrder);
+        
+        DatasetField datasetField = (DatasetField)em.find(DatasetField.class,datasetFieldId);
+        dataverseFacet.setDatasetField(datasetField);
+        
+        Dataverse dataverse = (Dataverse)em.find(Dataverse.class,dataverseId);
+        dataverseFacet.setDataverse(dataverse);
+        
+        dataverse.getDataverseFacets().add(dataverseFacet);
+        em.persist(dataverseFacet);
+    }
+}
+
