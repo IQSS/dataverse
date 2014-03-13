b751a71f5e11d27d0a63bda2bba699239971527c
Gustavo Durand <gdurand@iq.harvard.edu>
2014-03-13 17:29:18 -0400

    script calls load for all the blocks we have right now

--- scripts/api/datasetfields.sh
+++ scripts/api/datasetfields.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-curl http://localhost:8080/api/datasetfield/showtsv -X POST --data-binary @doc/Sphinx/source/User/metadata/general.tsv -H 'Content-type: text/tab-separated-values'
+curl http://localhost:8080/api/datasetfield/load -X POST --data-binary @data/metadatablocks/general.tsv -H 'Content-type: text/tab-separated-values'
+curl http://localhost:8080/api/datasetfield/load -X POST --data-binary @data/metadatablocks/social_science.tsv -H 'Content-type: text/tab-separated-values'
+curl http://localhost:8080/api/datasetfield/load -X POST --data-binary @data/metadatablocks/astrophysics.tsv -H 'Content-type: text/tab-separated-values'
+curl http://localhost:8080/api/datasetfield/load -X POST --data-binary @data/metadatablocks/biomedical.tsv -H 'Content-type: text/tab-separated-values'
