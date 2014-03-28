2bdd0a77c1ba4ab1bc1f744e84f84c81bd25bfc2
sekmiller <skraffmiller@hmdc.harvard.edu>
2014-03-28 15:31:45 -0400

    Update service bean to use save instead of command

--- src/main/java/edu/harvard/iq/dataverse/DatasetServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/DatasetServiceBean.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package edu.harvard.iq.dataverse;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
@@ -32,15 +32,75 @@ public class DatasetServiceBean {
     @PersistenceContext(unitName = "VDCNet-ejbPU")
     private EntityManager em;
 
-    public Dataset save(Dataset dataset) {
+    public Dataset CreateDatasetCommand(Dataset dataset) {
+        return save(dataset);
+    }
 
-        em.merge(dataset.getVersions().get(0));
+    public Dataset UpdateDatasetCommand(Dataset dataset) {
+        return save(dataset);
+    }
+
+    public void saveDatasetAPI(Dataset dataset) {
+        //Called by depricated method on API
+        //Should put into command?
+        save(dataset);
+    }
+
+    public Dataset save(Dataset dataset) {
+        em.merge(removeBlankRows(dataset.getVersions().get(0)));
         Dataset savedDataset = em.merge(dataset);
-        String indexingResult = indexService.indexDataset(savedDataset);
-        logger.info("during dataset save, indexing result was: " + indexingResult);
+        //TODO - Commented out indexing because 
+        //Release dataset fails
+        //String indexingResult = indexService.indexDataset(savedDataset);
+        System.out.print("after indexing saved...");
+        //logger.info("during dataset save, indexing result was: " + indexingResult);
         return savedDataset;
     }
-    
+
+    private DatasetVersion removeBlankRows(DatasetVersion version) {
+        //Trim spaces from any input values
+        //add any blank records to a "to Remove" list"
+
+        List<Integer> toRemoveIndex = new ArrayList();
+        int index = 0;
+
+        for (DatasetFieldValue dsfv : version.getDatasetFieldValues()) {
+            //Only want to drop if never saved previously
+            // if in DB the blank will blank out the existing value
+            if (dsfv.getId() == null) {
+                if (dsfv.getStrValue() != null) {
+                    dsfv.setStrValue(dsfv.getStrValue().trim());
+                }
+
+                //Single recs and child recs (with no controlled vocab)
+                if ((!dsfv.getDatasetField().isHasChildren() && !dsfv.getDatasetField().isControlledVocabulary()) && (dsfv.getStrValue() == null || dsfv.getStrValue().trim().isEmpty())) {
+                    toRemoveIndex.add(index);
+                }
+                //parent recs where all kids are empty.
+                if (dsfv.getDatasetField().isHasChildren() && dsfv.isChildEmpty()) {
+                    toRemoveIndex.add(index);
+                }
+                //controlled vocab recs where all kids are empty.
+                if (dsfv.getDatasetField().isControlledVocabulary() && (dsfv.getControlledVocabularyValues() == null || dsfv.getControlledVocabularyValues().isEmpty())) {
+                    toRemoveIndex.add(index);
+                }
+
+            }
+            index++;
+        }
+        //Actually do the remove here
+        // the adjustment takes into account the prior 
+        //blank fields which have been removed.
+        int adjustment = 0;
+        if (!toRemoveIndex.isEmpty()) {
+            for (Integer dsfvRI : toRemoveIndex) {
+                version.getDatasetFieldValues().remove(dsfvRI.intValue() - adjustment);
+                adjustment++;
+            }
+        }
+        return version;
+    }
+
     public Dataset removeRecs(Dataset dataset, List<DatasetFieldValue> toDelete) {
         for (DatasetFieldValue dsfv : toDelete) {
             deleteVal(dsfv);
@@ -53,7 +113,6 @@ public class DatasetServiceBean {
             DatasetFieldValue p = em.find(DatasetFieldValue.class, dsfv.getId());
             em.remove(p);
         }
-
     }
 
     public Dataset find(Object pk) {
@@ -61,9 +120,9 @@ public class DatasetServiceBean {
     }
 
     public List<Dataset> findByOwnerId(Long ownerId) {
-         Query query = em.createQuery("select object(o) from Dataset as o where o.owner.id =:ownerId order by o.id");
-         query.setParameter("ownerId", ownerId);
-         return query.getResultList();
+        Query query = em.createQuery("select object(o) from Dataset as o where o.owner.id =:ownerId order by o.id");
+        query.setParameter("ownerId", ownerId);
+        return query.getResultList();
     }
 
     public List<Dataset> findAll() {
@@ -74,20 +133,22 @@ public class DatasetServiceBean {
         coll.remove(elem);
         em.remove(elem);
     }
-     public void removeCollectionElement(List list,int index) {
-        System.out.println("index is "+index+", list size is "+list.size());
+
+    public void removeCollectionElement(List list, int index) {
+        System.out.println("index is " + index + ", list size is " + list.size());
         em.remove(list.get(index));
         list.remove(index);
     }
+
     public void removeCollectionElement(Iterator iter, Object elem) {
         iter.remove();
         em.remove(elem);
     }
 
-    public String getDatasetVersionTitle(DatasetVersion version){
+    public String getDatasetVersionTitle(DatasetVersion version) {
         Long id = version.getId();
         Query query = em.createQuery("select v.strValue from DatasetFieldValue as v, DatasetVersion as dv, DatasetField as dsf where dsf.name ='title'"
-                 + " and dsf.id = v.datasetField.id and dv.id =:id ");
+                + " and dsf.id = v.datasetField.id and dv.id =:id ");
         query.setParameter("id", id);
         return (String) query.getSingleResult();
     }
