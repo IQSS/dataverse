56663e07fa4023a42b72dde37052a670e43a1efd
Gustavo Durand <gdurand@iq.harvard.edu>
2014-03-31 17:52:02 -0400

    renamed DatasetField to DatasetFieldType; renamed DatasetFieldValue to DatasetField; renamed DatasetFieldValueValue to DatasetFieldValue

---
+++ src/main/java/edu/harvard/iq/dataverse/DatasetFieldType.java
@@ -0,0 +1,389 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.harvard.iq.dataverse;
+
+import java.util.Collection;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Logger;
+import javax.faces.model.SelectItem;
+import javax.persistence.*;
+
+/**
+ *
+ * @author Stephen Kraffmiller
+ */
+@Entity
+public class DatasetFieldType implements Serializable, Comparable<DatasetFieldType> {
+
+    private static final Logger logger = Logger.getLogger(DatasetFieldType.class.getCanonicalName());
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name = "name", columnDefinition = "TEXT")
+    private String name;    // This is the internal, DDI-like name, no spaces, etc.
+    @Column(name = "title", columnDefinition = "TEXT")
+    private String title;   // A longer, human-friendlier name - punctuation allowed
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description; // A user-friendly Description; will be used for 
+    // mouse-overs, etc. 
+    private String fieldType;
+    private boolean allowControlledVocabulary;
+    private String watermark;
+
+    @OneToMany(mappedBy = "datasetField")
+    private Set<DataverseFacet> dataverseFacets;
+    
+
+    @Transient
+    private String searchValue;
+    
+    @Transient
+    private List<String> listValues;
+
+    public DatasetFieldType() {
+    }
+
+    private int displayOrder;
+
+    public int getDisplayOrder() {
+        return this.displayOrder;
+    }
+
+    public void setDisplayOrder(int displayOrder) {
+        this.displayOrder = displayOrder;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public boolean isAllowControlledVocabulary() {
+        return allowControlledVocabulary;
+    }
+
+    public void setAllowControlledVocabulary(boolean allowControlledVocabulary) {
+        this.allowControlledVocabulary = allowControlledVocabulary;
+    }
+
+    private boolean allowMultiples;
+
+    public boolean isAllowMultiples() {
+        return this.allowMultiples;
+    }
+
+    public void setAllowMultiples(boolean allowMultiples) {
+        this.allowMultiples = allowMultiples;
+    }
+
+    public String getFieldType() {
+        return fieldType;
+    }
+
+    public void setFieldType(String fieldType) {
+        this.fieldType = fieldType;
+    }
+    
+    public String getWatermark() {
+        return watermark;
+    }
+
+    public void setWatermark(String watermark) {
+        this.watermark = watermark;
+    }
+    private boolean facetable;
+
+    public boolean isFacetable() {
+        return facetable;
+    }
+
+    public void setFacetable(boolean facetable) {
+        this.facetable = facetable;
+    }
+
+    private boolean displayOnCreate;
+
+    public boolean isDisplayOnCreate() {
+        return displayOnCreate;
+    }
+
+    public void setDisplayOnCreate(boolean displayOnCreate) {
+        this.displayOnCreate = displayOnCreate;
+    }
+    
+    public boolean isControlledVocabulary() {
+        return controlledVocabularyValues != null && !controlledVocabularyValues.isEmpty();
+    }
+
+    @ManyToOne(cascade = CascadeType.MERGE)
+    private MetadataBlock metadataBlock;
+
+    public MetadataBlock getMetadataBlock() {
+        return metadataBlock;
+    }
+
+    public void setMetadataBlock(MetadataBlock metadataBlock) {
+        this.metadataBlock = metadataBlock;
+    }
+    
+   @OneToMany(mappedBy = "datasetField", cascade = {CascadeType.REMOVE, CascadeType.MERGE, CascadeType.PERSIST})
+   @OrderBy("displayOrder ASC")
+    private Collection<ControlledVocabularyValue> controlledVocabularyValues;
+
+    public Collection<ControlledVocabularyValue> getControlledVocabularyValues() {
+        return this.controlledVocabularyValues;
+    }
+
+    public void setControlledVocabularyValues(Collection<ControlledVocabularyValue> controlledVocabularyValues) {
+        this.controlledVocabularyValues = controlledVocabularyValues;
+    }
+       
+
+    @OneToMany(mappedBy = "parentDatasetFieldType", cascade = {CascadeType.REMOVE, CascadeType.MERGE, CascadeType.PERSIST})
+    @OrderBy("displayOrder ASC")
+    private Collection<DatasetFieldType> childDatasetFieldTypes;
+
+    public Collection<DatasetFieldType> getChildDatasetFieldTypes() {
+        return this.childDatasetFieldTypes;
+    }
+
+    public void setChildDatasetFieldTypes(Collection<DatasetFieldType> childDatasetFieldTypes) {
+        this.childDatasetFieldTypes = childDatasetFieldTypes;
+    }
+
+    @ManyToOne(cascade = CascadeType.MERGE)
+    private DatasetFieldType parentDatasetFieldType;
+
+    public DatasetFieldType getParentDatasetFieldType() {
+        return parentDatasetFieldType;
+    }
+
+    public void setParentDatasetFieldType(DatasetFieldType parentDatasetFieldType) {
+        this.parentDatasetFieldType = parentDatasetFieldType;
+    }
+
+
+    public Set<DataverseFacet> getDataverseFacets() {
+        return dataverseFacets;
+    }
+
+    public void setDataverseFacets(Set<DataverseFacet> dataverseFacets) {
+        this.dataverseFacets = dataverseFacets;
+    }
+
+    public String getSearchValue() {
+        return searchValue;
+    }
+
+    public void setSearchValue(String searchValue) {
+        this.searchValue = searchValue;
+    }
+
+    public List<String> getListValues() {
+        return listValues;
+    }
+
+    public void setListValues(List<String> listValues) {
+        this.listValues = listValues;
+    }
+    
+    private boolean required;
+
+    public boolean isRequired() {
+        return this.required;
+    }
+
+    public void setRequired(boolean required) {
+        this.required = required;
+    }
+
+
+    private boolean advancedSearchFieldType;
+
+    public boolean isAdvancedSearchFieldType() {
+        return this.advancedSearchFieldType;
+    }
+
+    public void setAdvancedSearchFieldType(boolean advancedSearchFieldType) {
+        this.advancedSearchFieldType = advancedSearchFieldType;
+    }
+
+    public boolean isPrimitive() {
+        return this.childDatasetFieldTypes.isEmpty();
+    }
+    
+    public boolean isCompound() {
+         return !this.childDatasetFieldTypes.isEmpty();       
+    }
+    
+    public boolean isChild() {
+        return this.parentDatasetFieldType != null;        
+    }    
+    
+    public boolean isSubField() {
+        return this.parentDatasetFieldType != null;        
+    }
+    
+    
+    public boolean isHasChildren() {
+        return !this.childDatasetFieldTypes.isEmpty();
+    }
+
+    public boolean isHasParent() {
+        return this.parentDatasetFieldType != null;
+    }
+
+    public int hashCode() {
+        int hash = 0;
+        hash += (this.id != null ? this.id.hashCode() : 0);
+        return hash;
+    }
+
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof DatasetFieldType)) {
+            return false;
+        }
+        DatasetFieldType other = (DatasetFieldType) object;
+        if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @OneToMany(mappedBy = "datasetFieldType", cascade = {CascadeType.REMOVE, CascadeType.MERGE, CascadeType.PERSIST})
+    private List<DatasetField> datasetFields;
+
+    public List<DatasetField> getDatasetFields() {
+        return datasetFields;
+    }
+
+    public void setDatasetFields(List<DatasetField> datasetFieldValues) {
+        this.datasetFields = datasetFieldValues;
+    }
+
+    @OneToMany(mappedBy = "datasetField", cascade = {CascadeType.REMOVE, CascadeType.MERGE, CascadeType.PERSIST})
+    private List<DatasetFieldDefaultValue> datasetFieldDefaultValues;
+
+    public List<DatasetFieldDefaultValue> getDatasetFieldDefaultValues() {
+        return datasetFieldDefaultValues;
+    }
+
+    public void setDatasetFieldDefaultValues(List<DatasetFieldDefaultValue> datasetFieldDefaultValues) {
+        this.datasetFieldDefaultValues = datasetFieldDefaultValues;
+    }
+
+    @Override
+    public int compareTo(DatasetFieldType o) {
+        return Integer.compare(this.getDisplayOrder(), (o.getDisplayOrder()));
+    }
+    
+    public String getDisplayName() {
+        if (isHasParent() && !parentDatasetFieldType.getTitle().equals(title)) {
+        return parentDatasetFieldType.getTitle() + " " + title;
+        } else {
+            return title;
+        }
+    }
+
+    public SolrField getSolrField() {
+        SolrField.SolrType solrType2 = SolrField.SolrType.TEXT_GENERAL;
+        if (fieldType != null) {
+            /**
+             * @todo: make this an enum
+             */
+            if (fieldType.equals("textBox")) {
+                solrType2 = SolrField.SolrType.TEXT_GENERAL;
+            } else if (fieldType.equals("text")) {
+                solrType2 = SolrField.SolrType.TEXT_GENERAL;
+            } else if (fieldType.equals("date")) {
+                solrType2 = SolrField.SolrType.INTEGER;
+            } else if (fieldType.equals("email")) {
+                solrType2 = SolrField.SolrType.TEXT_GENERAL;
+            } else if (fieldType.equals("url")) {
+                solrType2 = SolrField.SolrType.TEXT_GENERAL;
+            } else {
+                /**
+                 * @todo: what should we do with types we don't expect?
+                 */
+                solrType2 = SolrField.SolrType.TEXT_GENERAL;
+            }
+
+            Boolean parentAllowsMultiplesBoolean = false;
+            if (isHasParent()) {
+                if (getParentDatasetFieldType() != null) {
+                    DatasetFieldType parent = getParentDatasetFieldType();
+                    parentAllowsMultiplesBoolean = parent.isAllowMultiples();
+                }
+            }
+
+            // http://stackoverflow.com/questions/5800762/what-is-the-use-of-multivalued-field-type-in-solr
+            boolean makeSolrFieldMultivalued = false;
+
+            if (solrType2 == SolrField.SolrType.TEXT_GENERAL) {
+                if (allowMultiples || parentAllowsMultiplesBoolean) {
+                    makeSolrFieldMultivalued = true;
+//                    logger.info(name + " allows multiples, Solr field will be made multvalued: " + makeSolrFieldMultivalued);
+                } else {
+                    makeSolrFieldMultivalued = false;
+//                    logger.info(name + " does not allow multiples, Solr field will be made multvalued: " + makeSolrFieldMultivalued);
+                }
+            } else {
+                makeSolrFieldMultivalued = false;
+//                logger.info(name + " only converting _s (String) fields to multiple, Solr field will be made multvalued: " + makeSolrFieldMultivalued);
+            }
+
+            return new SolrField(name, solrType2, makeSolrFieldMultivalued, facetable);
+
+        } else {
+            /**
+             * @todo: clean this up
+             */
+            String oddValue = name + getTmpNullFieldTypeIdentifier();
+            boolean makeSolrFieldMultivalued = false;
+            SolrField solrField2 = new SolrField(oddValue, solrType2, makeSolrFieldMultivalued, facetable);
+            return solrField2;
+        }
+    }
+
+    // help us identify fields that have null fieldType values
+    public String getTmpNullFieldTypeIdentifier() {
+        return "NullFieldType_s";
+    }
+}
