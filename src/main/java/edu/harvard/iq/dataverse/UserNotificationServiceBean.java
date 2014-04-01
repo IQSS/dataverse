5f91ef0d256420263daec4e92e8d3124d9a9ce9b
xyang02 <xyang02@hmdc.harvard.edu>
2014-04-01 11:28:40 -0400

    Added generating notifications to user account page functionality. Notifications will be saved in db and displayed as either read or unread and can be removed in account page. Added link to account page in dataverse header. Show the # of notifications next to the user name. Generate notification when user creates a dataverse. Removed phone field from user account.  (#3513, #3531)

--- src/main/java/edu/harvard/iq/dataverse/UserNotificationServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/UserNotificationServiceBean.java
@@ -6,6 +6,7 @@
 
 package edu.harvard.iq.dataverse;
 
+import java.sql.Timestamp;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Named;
@@ -24,8 +25,34 @@ public class UserNotificationServiceBean {
     private EntityManager em;
     
     public List<UserNotification> findByUser(Long userId) {
-        Query query = em.createQuery("select object(o) from UserNotification as o where o.user.id =:userId");
+        Query query = em.createQuery("select object(o) from UserNotification as o where o.user.id =:userId order by o.sendDate desc");
         query.setParameter("userId", userId);
         return query.getResultList();
+    }
+    
+    public List<UserNotification> findUnreadByUser(Long userId) {
+        Query query = em.createQuery("select object(o) from UserNotification as o where o.user.id =:userId and o.readNotification = 'false' order by o.sendDate desc");
+        query.setParameter("userId", userId);
+        return query.getResultList();
+    }    
+    
+    public UserNotification find(Object pk) {
+        return (UserNotification) em.find(UserNotification.class, pk);
+    }
+
+    public UserNotification save(UserNotification userNotification) {
+        return em.merge(userNotification);
+    }
+    
+    public void delete(UserNotification userNotification) {
+        em.remove(em.merge(userNotification));
+    }
+    
+    public void sendNotification(String notification, DataverseUser dataverseUser, Timestamp sendDate) {
+        UserNotification userNotification = new UserNotification();
+        userNotification.setNotification(notification);
+        userNotification.setUser(dataverseUser);
+        userNotification.setSendDate(sendDate);
+        save(userNotification);
     }
 }
