c2294814013b938c34ccb9dce0b553836aa2e662
xyang02 <xyang02@hmdc.harvard.edu>
2014-04-11 10:32:35 -0400

    Implemented email system for notifications. (#3833)

--- src/main/java/edu/harvard/iq/dataverse/UserNotificationServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/UserNotificationServiceBean.java
@@ -35,7 +35,12 @@ public class UserNotificationServiceBean {
         Query query = em.createQuery("select object(o) from UserNotification as o where o.user.id =:userId and o.readNotification = 'false' order by o.sendDate desc");
         query.setParameter("userId", userId);
         return query.getResultList();
-    }    
+    }
+    
+    public List<UserNotification> findUnemailed() {
+        Query query = em.createQuery("select object(o) from UserNotification as o where o.readNotification = 'false' and o.emailed = 'false'");
+        return query.getResultList();
+    }
     
     public UserNotification find(Object pk) {
         return (UserNotification) em.find(UserNotification.class, pk);
