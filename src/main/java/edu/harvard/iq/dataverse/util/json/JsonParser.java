794a11fedf8c9a8996b89296aaf5c4313f98eb4c
Michael Bar-Sinai <mich.barsinai@gmail.com>
2014-04-24 14:27:23 -0400

    More work on dataset parsing from JSON

--- src/main/java/edu/harvard/iq/dataverse/util/json/JsonParser.java
+++ src/main/java/edu/harvard/iq/dataverse/util/json/JsonParser.java
@@ -1,14 +1,23 @@
 package edu.harvard.iq.dataverse.util.json;
 
 import edu.harvard.iq.dataverse.ControlledVocabularyValue;
+import edu.harvard.iq.dataverse.DatasetAuthor;
 import edu.harvard.iq.dataverse.DatasetField;
 import edu.harvard.iq.dataverse.DatasetFieldCompoundValue;
 import edu.harvard.iq.dataverse.DatasetFieldServiceBean;
 import edu.harvard.iq.dataverse.DatasetFieldType;
 import edu.harvard.iq.dataverse.DatasetFieldValue;
+import edu.harvard.iq.dataverse.DatasetVersion;
+import edu.harvard.iq.dataverse.MetadataBlockServiceBean;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.json.JsonString;
@@ -19,13 +28,85 @@ import javax.json.JsonString;
  */
 public class JsonParser {
     
+    private final DateFormat dateFormat = new SimpleDateFormat( JsonPrinter.TIME_FORMAT_STRING );
+    
     DatasetFieldServiceBean datasetFieldSvc;
+    MetadataBlockServiceBean blockService;
 
-    public JsonParser(DatasetFieldServiceBean datasetFieldSvc) {
+    public JsonParser(DatasetFieldServiceBean datasetFieldSvc, MetadataBlockServiceBean blockService) {
         this.datasetFieldSvc = datasetFieldSvc;
+        this.blockService = blockService;
+    }
+    
+    public DatasetVersion parseDatasetVersion( JsonObject obj ) throws JsonParseException {
+        try {
+            DatasetVersion dsv = new DatasetVersion();
+            
+            dsv.setArchiveNote( obj.getString("archiveNote", null) );
+            dsv.setDeaccessionLink( obj.getString("deaccessionLink", null) );
+            dsv.setVersion( parseLong(obj.getString("version")) );
+            dsv.setVersionNumber( parseLong(obj.getString("versionNumber")) );
+            dsv.setMinorVersionNumber( parseLong(obj.getString("minorVersionNumber")) );
+            dsv.setId( parseLong(obj.getString("id")) );
+            
+            String versionStateStr = obj.getString("versionState");
+            if ( versionStateStr != null ) {
+                dsv.setVersionState( DatasetVersion.VersionState.valueOf(versionStateStr) );
+            }
+            
+            dsv.setReleaseTime( parseDate(obj.getString("releaseDate")) );
+            dsv.setLastUpdateTime( parseDate(obj.getString("lastUpdateTime")) );
+            dsv.setCreateTime( parseDate(obj.getString("createTime")) );
+            dsv.setArchiveTime( parseDate(obj.getString("archiveTime")) );
+            
+            dsv.setDatasetFields( parseMetadataBlocks(obj.getJsonObject("metadataBlocks")) );
+            
+            // parse authors
+            JsonArray authorsJson = obj.getJsonArray("authors");
+            List<DatasetAuthor> authors = new ArrayList<>( authorsJson.size() );
+            for ( JsonObject authorJson : authorsJson.getValuesAs(JsonObject.class) ) {
+                DatasetAuthor author = new DatasetAuthor();
+                author.setAffiliation( parseField( authorJson.getJsonObject("affiliation")) );
+                author.setIdType( authorJson.getString("idType") );
+                author.setIdValue( authorJson.getString("idValue"));
+                author.setDisplayOrder( parsePrimitiveInt(authorJson.getString("displayOrder"), 0) );
+                author.setName( parseField( authorJson.getJsonObject("name")) );
+                
+                authors.add( author );
+                author.setDatasetVersion(dsv);
+            }
+            dsv.setDatasetAuthors(authors);
+            
+            // parse distributors
+            // CONTPOINT 
+            
+            return dsv;
+            
+        } catch (ParseException ex) {
+            throw new JsonParseException("Error parsing date:" + ex.getMessage(), ex);
+        } catch ( NumberFormatException ex ) {
+            throw new JsonParseException("Error parsing number:" + ex.getMessage(), ex);
+        }
+    }
+    
+    public List<DatasetField> parseMetadataBlocks( JsonObject json ) throws JsonParseException {
+        Set<String> keys = json.keySet();
+        List<DatasetField> fields = new LinkedList<>();
+        
+        for ( String blockName : keys ) {
+            blockService.findByName(blockName);
+            JsonObject blockJson = json.getJsonObject(blockName);
+            JsonArray fieldsJson = blockJson.getJsonArray("fields");
+            for ( JsonObject fieldJson : fieldsJson.getValuesAs(JsonObject.class) ) {
+                fields.add( parseField(fieldJson) );
+            }
+        }
+        
+        return fields;
     }
     
     public DatasetField parseField( JsonObject json ) throws JsonParseException {
+        if ( json == null ) return null;
         DatasetField ret = new DatasetField();
         DatasetFieldType type = datasetFieldSvc.findByName(json.getString("typeName"));
         if ( type == null ) {
@@ -134,6 +215,18 @@ public class JsonParser {
             }
             return Collections.singletonList(cvv);
         }
+    }
+    
+    Date parseDate( String str ) throws ParseException {
+        return str==null ? null : dateFormat.parse(str);
+    }
+    
+    Long parseLong( String str ) throws NumberFormatException {
+        return (str==null) ? null : Long.valueOf(str);
+    }
+    
+    int parsePrimitiveInt( String str, int defaultValue ) {
+        return str==null ? defaultValue : Integer.parseInt(str);
     }
     
 }
