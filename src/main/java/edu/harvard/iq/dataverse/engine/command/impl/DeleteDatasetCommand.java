84d73b8b68ee567cb5dc68b27f729f1ab63830f9
Michael Bar-Sinai <mich.barsinai@gmail.com>
2014-05-05 14:17:56 -0400

    Added a command to destroy a dataset. Not connected to any UI at the moment (related to #3921)

--- src/main/java/edu/harvard/iq/dataverse/engine/command/impl/DeleteDatasetCommand.java
+++ src/main/java/edu/harvard/iq/dataverse/engine/command/impl/DeleteDatasetCommand.java
@@ -1,31 +1,24 @@
 package edu.harvard.iq.dataverse.engine.command.impl;
 
-import edu.harvard.iq.dataverse.DataFile;
 import edu.harvard.iq.dataverse.Dataset;
-import edu.harvard.iq.dataverse.DatasetVersion;
-import edu.harvard.iq.dataverse.DataverseRole;
 import edu.harvard.iq.dataverse.DataverseUser;
-import edu.harvard.iq.dataverse.RoleAssignment;
 import edu.harvard.iq.dataverse.engine.Permission;
-import edu.harvard.iq.dataverse.engine.command.AbstractVoidCommand;
 import edu.harvard.iq.dataverse.engine.command.CommandContext;
 import edu.harvard.iq.dataverse.engine.command.RequiredPermissions;
 import edu.harvard.iq.dataverse.engine.command.exception.CommandException;
 import edu.harvard.iq.dataverse.engine.command.exception.IllegalCommandException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Deletes a data set.
  * @author michael
  */
 @RequiredPermissions( Permission.DestructiveEdit )
-public class DeleteDatasetCommand extends AbstractVoidCommand {
+public class DeleteDatasetCommand extends DestroyDataverseCommand {
 	
 	private final Dataset doomed;
 
 	public DeleteDatasetCommand(Dataset doomed, DataverseUser aUser) {
-		super(aUser, doomed.getOwner());
+		super(doomed, aUser);
 		this.doomed = doomed;
 	}
 	
@@ -34,33 +27,7 @@ public class DeleteDatasetCommand extends AbstractVoidCommand {
 		if ( doomed.isReleased() ) {
 			throw new IllegalCommandException("Cannot delete a released dataset", this);
 		}
-		
-		final Dataset managedDoomed = ctxt.em().merge(doomed);
-		
-        // ASSIGNMENTS
-		for ( RoleAssignment ra : ctxt.roles().directRoleAssignments(doomed) ) {
-			ctxt.em().remove(ra);
-		}
-		// ROLES
-		for ( DataverseRole ra : ctxt.roles().findByOwnerId(doomed.getId()) ) {
-			ctxt.em().remove(ra);
-		}
-        
-		// files
-		for ( DataFile df : managedDoomed.getFiles() ) {
-			ctxt.engine().submit( new DeleteDataFileCommand(df, getUser(), managedDoomed.getOwner()) );
-		}
-		
-		// versions
-		for ( DatasetVersion ver : managedDoomed.getVersions() ) {
-			Logger.getLogger(DeleteDatasetCommand.class.getName()).log(Level.INFO, "deleting " + ver );
-			DatasetVersion managed = ctxt.em().merge(ver);
-			Logger.getLogger(DeleteDatasetCommand.class.getName()).log(Level.INFO, " - Managed: " + managed );
-			ctxt.em().remove( managed );
-		}
-		
-		// dataset
-		ctxt.em().remove(managedDoomed);
+		super.executeImpl(ctxt);
 	}
 	
 }
