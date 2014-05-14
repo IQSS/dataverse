bba4e6e026f4aa07acfd8bfc049a973e4c001221
Philip Durbin <philip_durbin@harvard.edu>
2014-05-14 16:37:14 -0400

    stub out SWORD file upload (many todo's) #3385

---
+++ src/main/java/edu/harvard/iq/dataverse/api/datadeposit/SWORDv2MediaResourceServlet.java
@@ -0,0 +1,53 @@
+package edu.harvard.iq.dataverse.api.datadeposit;
+
+import java.io.IOException;
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.swordapp.server.MediaResourceAPI;
+import org.swordapp.server.servlets.SwordServlet;
+
+public class SWORDv2MediaResourceServlet extends SwordServlet {
+
+    @Inject
+    MediaResourceManagerImpl mediaResourceManagerImpl;
+    protected MediaResourceAPI api;
+
+    public void init() throws ServletException {
+        super.init();
+
+        // load the api
+        this.api = new MediaResourceAPI(mediaResourceManagerImpl, this.config);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        this.api.get(req, resp);
+    }
+
+    @Override
+    protected void doHead(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        this.api.head(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        this.api.post(req, resp);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        this.api.put(req, resp);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        this.api.delete(req, resp);
+    }
+}
