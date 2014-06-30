d339c337ba65d64abfc25e87cdb2ccd8b9e09629
Philip Durbin <philip_durbin@harvard.edu>
2014-06-30 13:50:57 -0400

    build dataverse war in vagrant if need be

--- scripts/vagrant/install-dataverse.sh
+++ scripts/vagrant/install-dataverse.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+WAR=/dataverse/target/dataverse-4.0.war
+if [ ! -f $WAR ]; then
+  echo "no war file found... building"
+  su $SUDO_USER -s /bin/sh -c "cd /dataverse && mvn package"
+fi
 cd /dataverse/scripts/installer
 ./install --hostname localhost --gfdir /home/glassfish/glassfish4 -y --force
