b7ca08bf2b64092632f2c24d2e560e363cc35380
Jian <jian881219@gmail.com>
2014-07-30 10:56:13 +0000

    Delete tmpscript.sh

--- scripts/api/tmpscript.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-curl http://localhost.localdomain/api/datasetfield/load -X POST --data-binary @data/metadatablocks/citation.tsv -H "Content-type: text/tab-separated-values"
-curl http://localhost.localdomain/api/datasetfield/load -X POST --data-binary @data/metadatablocks/social_science.tsv -H "Content-type: text/tab-separated-values"
-curl http://localhost.localdomain/api/datasetfield/load -X POST --data-binary @data/metadatablocks/astrophysics.tsv -H "Content-type: text/tab-separated-values"
-curl http://localhost.localdomain/api/datasetfield/load -X POST --data-binary @data/metadatablocks/biomedical.tsv -H "Content-type: text/tab-separated-values"
