c8203b10764dcaf7a8141d6fb0edf32832cd6339
Philip Durbin <philip_durbin@harvard.edu>
2014-08-04 16:42:09 -0400

    added more user group classes

--- doc/Architecture/auth-classes.sh
+++ doc/Architecture/auth-classes.sh
@@ -2,5 +2,5 @@
 FILES=`ls -1 src/main/java/edu/harvard/iq/dataverse/authorization/*.java | grep -v Main.java`;
 UML='doc/Architecture/auth-classes.uml'
 javadoc -private -quiet -J-DdestinationFile=$UML -J-DcreatePackages=false -J-DshowPublicMethods=true -J-DshowPublicConstructors=false -J-DshowPublicFields=true -doclet de.mallox.doclet.PlantUMLDoclet -docletpath ~/bin/plantUmlDoclet.jar -sourcepath . $FILES
-java -jar ~/bin/plantuml.jar -graphvizdot ~/.homebrew/bin/dot -t png $UML
-java -jar ~/bin/plantuml.jar -graphvizdot ~/.homebrew/bin/dot -t svg $UML
+java -jar ~/bin/plantuml.jar -graphvizdot ~/.homebrew/bin/dot -tpng $UML
+java -jar ~/bin/plantuml.jar -graphvizdot ~/.homebrew/bin/dot -tsvg $UML
