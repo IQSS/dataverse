3fc46e9bf561d3ecf31275134e74af5ae3c600ff
Raman Prasad <raman_prasad@harvard.edu>
2014-08-08 20:18:51 -0400

    Almost there with IngestServiceShapefileHelper.java

--- src/main/java/edu/harvard/iq/dataverse/ingest/IngestServiceShapefileHelper.java
+++ src/main/java/edu/harvard/iq/dataverse/ingest/IngestServiceShapefileHelper.java
@@ -7,17 +7,27 @@
 package edu.harvard.iq.dataverse.ingest;
 
 import edu.harvard.iq.dataverse.DataFile;
+import edu.harvard.iq.dataverse.DataFileServiceBean;
+import edu.harvard.iq.dataverse.Dataset;
+import edu.harvard.iq.dataverse.DatasetVersion;
+import edu.harvard.iq.dataverse.FileMetadata;
+import edu.harvard.iq.dataverse.util.FileUtil;
 import edu.harvard.iq.dataverse.util.ShapefileHandler;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+import javax.ejb.EJB;
 
 /**
  *  Used by the IngestServiceBean to redistribute a zipped Shapefile*
@@ -29,6 +39,9 @@ import java.util.zip.ZipInputStream;
  */
 public class IngestServiceShapefileHelper {
     
+    @EJB
+    DataFileServiceBean fileService; 
+
     /*
         code to add to IngestServiceBean
             File rezipFolder = this.getShapefileUnzipTempDirectory();
@@ -40,11 +53,22 @@ public class IngestServiceShapefileHelper {
     private static final Logger logger = Logger.getLogger(IngestServiceShapefileHelper.class.getCanonicalName());
 
     private ShapefileHandler shpHandler;
+    private DatasetVersion datasetVersion;
+    List<DataFile> dataFileList = null;
+    private File zippedShapefile;
+    private File rezipFolder;
 
-    IngestServiceShapefileHelper(File toFile, File rezipFolder) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+  
     
+    public List<DataFile> getDataFileList(){
+        if (this.dataFileList==null){
+            return null;
+        }
+        if (this.dataFileList.size()==0){
+            return null;
+        }
+        return this.dataFileList;
+    }
     
     private boolean isValidFile(File fileObject){
         
@@ -75,108 +99,217 @@ public class IngestServiceShapefileHelper {
     /*
         Constructor that accepts a file object
     */
-    public void IngestServiceShapefileHelper(File zippedShapefile, File rezipFolder){
+   IngestServiceShapefileHelper(File zippedShapefile, File rezipFolder, DatasetVersion version){
         
         if ((!isValidFile(zippedShapefile))||(!isValidFolder(rezipFolder))){
             return;
         }
-        this.processFile(zippedShapefile, rezipFolder);
+        this.zippedShapefile = zippedShapefile;
+        this.rezipFolder = rezipFolder;
+        this.datasetVersion = version;
+
+        //this.processFile(zippedShapefile, rezipFolder);
 
     }
     
-    private boolean processFile(File zippedShapefile, File rezipFolder){
-         if ((!isValidFile(zippedShapefile))||(!isValidFolder(rezipFolder))){
-            return false;
-        }
-
-        
-        FileInputStream shp_file_input_stream;
-        try {
-            shp_file_input_stream = new FileInputStream(zippedShapefile);
+   private FileInputStream getFileInputStream(File fileObject){
+       if (fileObject==null){
+           return null;
+       }
+       try {
+            return new FileInputStream(fileObject);
         } catch (FileNotFoundException ex) {
-            logger.severe("Failed to convert File to a FileInputStream");
+            logger.severe("Failed to create FileInputStream from File: " + fileObject.getAbsolutePath());
+            return null;
+        }
+   }
+   
+   private void closeFileInputStream(FileInputStream fis){
+       if (fis==null){
+           return;
+       }
+        try {
+            fis.close();            
+        } catch (IOException ex) {
+            logger.info("Failed to close FileInputStream");
+        }
+   }
+   
+    public boolean processFile() {
+       
+        if (this.datasetVersion==null){
+            logger.severe("datasetVersion is null!!");
+            return false;
+        }
+       if ((!isValidFile(this.zippedShapefile))||(!isValidFolder(this.rezipFolder))){
             return false;
         }
         
-        this.shpHandler = new ShapefileHandler(shp_file_input_stream);
+       // (1) Use the ShapefileHandler to the .zip for a shapefile
+       //
+        FileInputStream shpfileInputStream = this.getFileInputStream(zippedShapefile);
+        if (shpfileInputStream==null){
+            return false;
+        }
+        
+        this.shpHandler = new ShapefileHandler(shpfileInputStream);
         if (!shpHandler.containsShapefile()){
             logger.severe("Shapefile was incorrectly detected upon Ingest (FileUtil) and passed here");
-
             return false;
         }
+
+        this.closeFileInputStream(shpfileInputStream);
         
+        //  (2) Rezip the shapefile pieces
+        logger.info("rezipFolder: " + rezipFolder.getAbsolutePath());
+        shpfileInputStream = this.getFileInputStream(zippedShapefile);
+        if (shpfileInputStream==null){
+            return false;
+        }
+
         boolean rezip_success;
         try {
-            rezip_success = shpHandler.rezipShapefileSets(shp_file_input_stream, rezipFolder);
+            rezip_success = shpHandler.rezipShapefileSets(shpfileInputStream, rezipFolder);
         } catch (IOException ex) {
             logger.severe("Shapefile was not correctly unpacked/repacked");
+            logger.severe("shpHandler message: " + shpHandler.errorMessage);
             return false;
         }
         
-        // Create DataFiles
+        this.closeFileInputStream(shpfileInputStream);
         
-     
+        if (!rezip_success){
+            return false;
+        }
+        return createDataFiles(rezipFolder);
         
-        return true;
-}
- /*  
-    private List<DataFile> scratch_code(File tempFile){
-        
-           ZipInputStream unZippedIn = null; 
-            ZipEntry zipEntry = null; 
-            try {
-                unZippedIn = new ZipInputStream(new FileInputStream(tempFile));
-
-                if (unZippedIn == null) {
-                    return null;
-                }
-                while ((zipEntry = unZippedIn.getNextEntry()) != null) {
-                    // Note that some zip entries may be directories - we 
-                    // simply skip them:
-                    
-                    if (!zipEntry.isDirectory()) {
-
-                        String fileEntryName = zipEntry.getName();
-
-                        if (fileEntryName != null && !fileEntryName.equals("")) {
-
-                            fileEntryName = fileEntryName.replaceFirst("^.*[\\/]", "");
-
-                            // Check if it's a "fake" file - a zip archive entry 
-                            // created for a MacOS X filesystem element: (these 
-                            // start with "._")
-                            if (!fileEntryName.startsWith("._")) {
-                                // OK, this seems like an OK file entry - we'll try 
-                                // to read it and create a DataFile with it:
-
-                                DataFile datafile = createSingleDataFile(version, unZippedIn, fileEntryName, "application/octet-stream");
-                                // TODO: 
-                                // Need to try to identify mime types for the individual 
-                                // files inside the ZIP archive! -- L.A.
-                                
-                                if (datafile != null) {
-                                    datafiles.add(datafile);
-                                }
-                            }
-                        }
-                    }
-                    unZippedIn.closeEntry(); 
-                }
-                
-            } catch (IOException ioex) {
-                // just clear the datafiles list and let 
-                // ingest default to creating a single DataFile out
-                // of the unzipped file. 
-                datafiles.clear();
-            } finally {
-                if (unZippedIn != null) {
-                    try {unZippedIn.close();} catch (Exception zEx) {}
-                }
-            }
-            if (datafiles.size() > 0) {
-                return datafiles;
-            }
-            return null;
+    }
+    
+    /*
+        Note: This creates DataFile objects, the files themselves already exist!
     */
+    private boolean createDataFiles(File rezipFolder){
+        if (!isValidFolder(rezipFolder)){
+            return false;
+        }
+        
+        // Initialize dataFileList
+        this.dataFileList = new ArrayList<DataFile>(); 
+
+        for (File singleFile : rezipFolder.listFiles()){
+            if (!singleFile.isFile()){
+                continue; // not a file
+            }
+            DataFile datafile = createSingleDataFile(singleFile);
+            if (!(datafile==null)){
+                   this.dataFileList.add(datafile);
+            }           
+        }
+        
+        return false;
+    }
+    
+    /*
+        Note: This creates a DataFile object and a related FileMetadata object
+        -> The files themselves already exist!
+    */
+    private DataFile createSingleDataFile(File inputFile){
+        if (!this.isValidFile(inputFile)){
+            logger.warning("createSingleDataFile: fileObject was not a file.");
+            return null;
+        }
+        
+        // (1) Get file name
+        String fileName = inputFile.getName();
+        if (fileName==null){
+           logger.warning("Unusual: inputFile.getName() returned null");
+            return null;
+        }
+        
+        // (2) Determine file type
+        String contentType;
+        try {
+            contentType = FileUtil.determineFileType(inputFile, fileName);
+        } catch (IOException ex) {
+            logger.fine("FileUtil.determineFileType failed for file with name: " + fileName);
+            contentType = null;
+        }
+        
+        if ((contentType==null)||(contentType=="")){
+            contentType = "application/octet-stream";
+        }
+        
+        // (3) - Start making a DataFile
+        DataFile datafile = new DataFile(contentType);
+        
+
+        // Set the data file owner
+        Dataset dataFileOwner = this.datasetVersion.getDataset();
+        if (dataFileOwner == null){
+          logger.severe("method call failed: this.datasetVersion.getDataset()");
+          return null;
+        }        
+        datafile.setOwner(dataFileOwner);
+        
+        // (3a) - Start making the FileMetadata
+        FileMetadata fileMetadata= new FileMetadata();
+        
+        // set the FileMetadata label
+        fileMetadata.setLabel(fileName);
+        fileMetadata.setDataFile(datafile);
+        
+        // Add file fileMetadata to the new datafile
+        datafile.getFileMetadatas().add(fileMetadata);
+
+        // Add file fileMetadata to the datasetVersion
+        if (this.datasetVersion.getFileMetadatas() == null) {
+            this.datasetVersion.setFileMetadatas(new ArrayList());
+        }
+        this.datasetVersion.getFileMetadatas().add(fileMetadata);
+
+        // Add the datasetVersion to the fileMetadata
+        fileMetadata.setDatasetVersion(this.datasetVersion);
+        
+        // Add the data file to the datasetVersion
+        this.datasetVersion.getDataset().getFiles().add(datafile);
+
+        // Generate Storage Identifier
+        fileService.generateStorageIdentifier(datafile);
+        
+        return datafile;
+/*
+        BufferedOutputStream outputStream = null;
+
+            // Once again, at this point we are dealing with *temp*
+            // files only; these are always stored on the local filesystem, 
+            // so we are using FileInput/Output Streams to read and write
+            // these directly, instead of going through the Data Access 
+            // framework. 
+            //      -- L.A.
+
+        try {
+            outputStream = new BufferedOutputStream(new FileOutputStream(getFilesTempDirectory() + "/" + datafile.getFileSystemName()));
+
+            byte[] dataBuffer = new byte[8192];
+            int i = 0;
+
+            while ((i = inputStream.read(dataBuffer)) > 0) {
+                outputStream.write(dataBuffer, 0, i);
+                outputStream.flush();
+            }
+        } catch (IOException ioex) {
+            datafile = null; 
+        } finally {
+            try {
+                outputStream.close();
+            } catch (IOException ioex) {}
+        }
+
+        
+        // End: Read in the actual file contents
+        
+        return datafile;
+        */
+    };
     
 }
