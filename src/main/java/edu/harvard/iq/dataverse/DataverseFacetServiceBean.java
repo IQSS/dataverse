a2f98d5a0d403df29971eb9332e87f7269f827e3
Michael Bar-Sinai <mich.barsinai@gmail.com>
2014-08-19 00:58:59 +0300

    Added a sample cache for the facets

--- src/main/java/edu/harvard/iq/dataverse/DataverseFacetServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/DataverseFacetServiceBean.java
@@ -6,7 +6,11 @@
 
 package edu.harvard.iq.dataverse;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
@@ -16,31 +20,65 @@ import javax.persistence.Query;
 /**
  *
  * @author xyang
+ * @author Michael Bar-Sinai
  */
 @Stateless
 @Named
 public class DataverseFacetServiceBean {
     
+    private static final LinkedHashMap<Long, List<DataverseFacet>> cache = new LinkedHashMap<>(10, 0.75f, true);
+    private static final ReentrantLock cacheLock = new ReentrantLock();
+    private static final int MAX_CACHE_SIZE = 100;
+    
+    private static final Logger logger = Logger.getLogger(DataverseFacetServiceBean.class.getName());
+    
     @PersistenceContext(unitName = "VDCNet-ejbPU")
     private EntityManager em;
     
     public List<DataverseFacet> findByDataverseId(Long dataverseId) {
-        Query query = em.createQuery("select object(o) from DataverseFacet as o where o.dataverse.id = :dataverseId order by o.displayOrder");
-        query.setParameter("dataverseId", dataverseId);
-        return query.getResultList();
-    }
-    public List<DataverseFacet> findByRootDataverse() {
-        return em.createQuery("select object(o) from DataverseFacet as o where o.dataverse.id = 1 order by o.displayOrder").getResultList();
+        List<DataverseFacet> res = null;
+        try {
+            cacheLock.lock();
+            res = cache.get(dataverseId);
+            
+        } finally { cacheLock.unlock(); }
+
+        if ( res == null ) {
+            logger.log(Level.INFO, "Cache miss on dataverse {0}", dataverseId);
+            Query query = em.createNamedQuery("DataverseFacet.findByDataverseId", DataverseFacet.class);
+            query.setParameter("dataverseId", dataverseId);
+            res = query.getResultList();
+            try {
+                cacheLock.lock();
+                cache.put(dataverseId, res);
+                
+                if ( cache.size() > MAX_CACHE_SIZE ) {
+                    cache.remove( cache.entrySet().iterator().next().getKey() );
+                }
+
+            } finally { cacheLock.unlock(); }
+        } else {
+            logger.log(Level.INFO, "Cache hit on dataverse {0}", dataverseId);
+        }
+
+        return res; 
     }
 
     public void delete(DataverseFacet dataverseFacet) {
         em.remove(em.merge(dataverseFacet));
+        invalidateCache();
     }
     
 	public void deleteFacetsFor( Dataverse d ) {
 		em.createNamedQuery("DataverseFacet.removeByOwnerId")
 			.setParameter("ownerId", d.getId())
 				.executeUpdate();
+        try {
+            cacheLock.lock();
+            cache.remove(d.getId());
+        } finally {
+            cacheLock.unlock();
+        }
 	}
 	
     public void create(int diplayOrder, Long datasetFieldId, Long dataverseId) {
@@ -56,6 +94,15 @@ public class DataverseFacetServiceBean {
         
         dataverse.getDataverseFacets().add(dataverseFacet);
         em.persist(dataverseFacet);
+    }
+    
+    public void invalidateCache() {
+        try {
+            cacheLock.lock();
+            cache.clear();
+        } finally {
+            cacheLock.unlock();
+        }
     }
 }
 
