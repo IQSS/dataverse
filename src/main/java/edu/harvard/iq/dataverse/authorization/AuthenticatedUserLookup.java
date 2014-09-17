fe8e41a484fcf02fff498c297633bdfe287fc754
Raman Prasad <raman_prasad@harvard.edu>
2014-09-17 16:09:14 -0400

    updated authenticatedUser attribute to be unique

--- src/main/java/edu/harvard/iq/dataverse/authorization/AuthenticatedUserLookup.java
+++ src/main/java/edu/harvard/iq/dataverse/authorization/AuthenticatedUserLookup.java
@@ -9,6 +9,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToOne;
 
 /**
  * A somewhat glorified key-value pair, persisted in the database.
@@ -32,8 +33,8 @@ public class AuthenticatedUserLookup implements Serializable {
     @EmbeddedId
     AuthenticatedUserLookupId id;
 
-    @ManyToOne( cascade = {CascadeType.PERSIST, CascadeType.MERGE} )
-    @JoinColumn(nullable = false)
+    @OneToOne( cascade = {CascadeType.PERSIST, CascadeType.MERGE} )
+    @JoinColumn(unique=true, nullable=false)
     private AuthenticatedUser authenticatedUser;
 
     public AuthenticatedUserLookup(String persistentUserIdFromIdp, String idp) {
