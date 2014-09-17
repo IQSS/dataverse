0d11ac2e4c746e892db2c9148117179cc892ae9a
Gustavo Durand <gdurand@iq.harvard.edu>
2014-09-17 12:22:50 -0400

    updated for auth merge

--- src/main/java/edu/harvard/iq/dataverse/engine/command/impl/UpdateDateverseTemplateCommand.java
+++ src/main/java/edu/harvard/iq/dataverse/engine/command/impl/UpdateDateverseTemplateCommand.java
@@ -1,9 +1,9 @@
 package edu.harvard.iq.dataverse.engine.command.impl;
 
 import edu.harvard.iq.dataverse.Dataverse;
-import edu.harvard.iq.dataverse.DataverseUser;
+import edu.harvard.iq.dataverse.authorization.users.User;
 import edu.harvard.iq.dataverse.Template;
-import edu.harvard.iq.dataverse.engine.Permission;
+import edu.harvard.iq.dataverse.authorization.Permission;
 import edu.harvard.iq.dataverse.engine.command.AbstractCommand;
 import edu.harvard.iq.dataverse.engine.command.CommandContext;
 import edu.harvard.iq.dataverse.engine.command.RequiredPermissions;
@@ -19,7 +19,7 @@ public class UpdateDateverseTemplateCommand extends AbstractCommand<Dataverse> {
     private final Dataverse editedDv;
     private final Template template;
 
-    public UpdateDateverseTemplateCommand(Dataverse editedDv, Template template, DataverseUser aUser) {
+    public UpdateDateverseTemplateCommand(Dataverse editedDv, Template template, User aUser) {
         super(aUser, editedDv);
         this.editedDv = editedDv;
         this.template = template;
