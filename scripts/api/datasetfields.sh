3abff415b8450391a5f028727398d3d810a19064
Eleni Castro <posixeleni@gmail.com>
2014-10-20 14:44:45 -0400

    Metadata changes to fieldType and new GSD block
    For fieldType changes see #614.
    These will require us to manually change in the DB and then have Phil
    do a schema.xml change.
    
    For new GSD Block addition see: #268

--- scripts/api/datasetfields.sh
+++ scripts/api/datasetfields.sh
@@ -4,3 +4,4 @@ curl http://localhost:8080/api/datasetfield/load -X POST --data-binary @data/met
 curl http://localhost:8080/api/datasetfield/load -X POST --data-binary @data/metadatablocks/social_science.tsv -H "Content-type: text/tab-separated-values"
 curl http://localhost:8080/api/datasetfield/load -X POST --data-binary @data/metadatablocks/astrophysics.tsv -H "Content-type: text/tab-separated-values"
 curl http://localhost:8080/api/datasetfield/load -X POST --data-binary @data/metadatablocks/biomedical.tsv -H "Content-type: text/tab-separated-values"
+curl http://localhost:8080/api/datasetfield/load -X POST --data-binary @data/metadatablocks/customGSD.tsv -H "Content-type: text/tab-separated-values"
