71402f93f0dcdd9a7ad0b1ba551f88a90757eb53
Gustavo Durand <gdurand@iq.harvard.edu>
2014-11-13 17:44:12 -0500

    change creator to be AuthenticatedUser

--- src/main/java/edu/harvard/iq/dataverse/DvObject.java
+++ src/main/java/edu/harvard/iq/dataverse/DvObject.java
@@ -1,6 +1,6 @@
 package edu.harvard.iq.dataverse;
 
-import edu.harvard.iq.dataverse.authorization.providers.builtin.BuiltinUser;
+import edu.harvard.iq.dataverse.authorization.users.AuthenticatedUser;
 import java.sql.Timestamp;
 import java.util.Objects;
 import javax.persistence.*;
@@ -59,7 +59,7 @@ public abstract class DvObject implements java.io.Serializable {
     private Timestamp createDate;
 
     @ManyToOne
-    private BuiltinUser creator;
+    private AuthenticatedUser creator;
 
 
     public interface Visitor<T> {
@@ -124,11 +124,11 @@ public abstract class DvObject implements java.io.Serializable {
         this.createDate = createDate;
     }
 
-    public BuiltinUser getCreator() {
+    public AuthenticatedUser getCreator() {
         return creator;
     }
 
-    public void setCreator(BuiltinUser creator) {
+    public void setCreator(AuthenticatedUser creator) {
         this.creator = creator;
     }
     
