61194fbb91d4efe7f49e5bfa98d81829cabbfc74
Gustavo Durand <gdurand@iq.harvard.edu>
2014-11-14 13:47:28 -0500

    fixed comment

--- scripts/api/setup-builtin-roles.sh
+++ scripts/api/setup-builtin-roles.sh
@@ -1,6 +1,6 @@
 SERVER=http://localhost:8080/api
 
-# Setup the authentication providers
+# Setup the builtin roles
 echo "Setting up admin role"
 curl -H "Content-type:application/json" -d @data/role-admin.json http://localhost:8080/api/s/roles/
 
