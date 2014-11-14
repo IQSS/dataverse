453056cf037ae8297cf5618528183de386bfcc7c
Gustavo Durand <gdurand@iq.harvard.edu>
2014-11-14 15:27:55 -0500

    moved jq check to beginning

--- scripts/api/setup-users.sh
+++ scripts/api/setup-users.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -f
+command -v jq >/dev/null 2>&1 || { echo >&2 "jq required, but it's not installed. On mac, use brew (http://brew.sh) to install it. Aborting."; exit 1; }
+
 SERVER=http://localhost:8080/api
 echo Setting up users on $SERVER
 echo ==============================================
@@ -21,7 +23,6 @@ echo
 curl -s -H "Content-type:application/json" -X POST -d @data/userNick.json "$SERVER/users?password=nick&key=burrito"
 echo
 
-command -v jq >/dev/null 2>&1 || { echo >&2 "jq required, but it's not installed. On mac, use brew (http://brew.sh) to install it. Aborting."; exit 1; }
 echo reporting API keys
 peteKey=$(echo $peteResp | jq .data.apiToken | tr -d \")
 echo :result: Pete\'s key is: $peteKey
