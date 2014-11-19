50f8014f7fc0c0ac51f36442dc31a6893ee934ca
Philip Durbin <philip_durbin@harvard.edu>
2014-11-19 09:00:53 -0500

    local dev setup fixes obviated #1120 #1121

--- scripts/database/homebrew/fix2.sql
+++
@@ -1,4 +0,0 @@
-UPDATE dataverse SET authority = '10.5072/FK2', protocol = 'doi';
-UPDATE dataverse SET doiprovider = 'EZID';
-UPDATE dataverse SET doishouldercharacter = '/';
-UPDATE dataset SET doishouldercharacter = '/';
--- scripts/database/homebrew/run-fix.sql
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-~/.homebrew/bin/psql dataverse_db -f $HOME/NetBeansProjects/dataverse/scripts/database/homebrew/fix.sql
