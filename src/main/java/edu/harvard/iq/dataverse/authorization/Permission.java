57ebe6bd3fad5f2db1d9bc6a175627d97c1e48cf
Gustavo Durand <gdurand@iq.harvard.edu>
2014-11-19 19:26:54 -0500

    removed the old permissions

--- src/main/java/edu/harvard/iq/dataverse/authorization/Permission.java
+++ src/main/java/edu/harvard/iq/dataverse/authorization/Permission.java
@@ -19,33 +19,7 @@ import java.util.Set;
  * @author michael
  */
 public enum Permission implements java.io.Serializable {
-    // @todo: REMOVE THESE, we are keeping old permissions until clean up is done to not break the build
-	Discover("See and search content", DvObject.class),
-    Download("Download the file", DataFile.class),
-    AccessUnpublishedContent("Access unpublished content", DvObject.class),
-	AccessRestrictedMetadata("Access metadata marked as\"restricted\"", DvObject.class),
-	UndoableEdit("Edits that do not cause data loss", DvObject.class),
-	DestructiveEdit("Edits that cannot be reversed, such as deleting data", DvObject.class),
-	EditMetadata("Edit the metadata of objects", DvObject.class),
-    //AddDataverse("Add a dataverse within another dataverse", Dataverse.class),
-    //AddDataset("Add a dataset to a dataverse", Dataverse.class),
-    AddDatasetVersion("Add a version to a dataset", Dataset.class),
-    //DeleteDatasetDraft("Delete dataset draft", Dataset.class),
-    //DeaccessionDataset("Deaccession Dataset", Dataset.class),
-	ChooseTemplate("Choose metadata template for dataverses and datasets", DvObjectContainer.class),
-	ChooseFacets("Choose facets", Dataverse.class),
-	Publish("Release a dataverse or a dataset", Dataverse.class, Dataset.class),
-	Style("Customize the appearance of objects", Dataverse.class, Dataset.class),
-	CreateRole("Create new Roles", Dataset.class, Dataverse.class),
-	AssignRole("Assign roles to users and groups", Dataset.class, Dataverse.class),
-	DeleteRole("Delete Roles", Dataset.class, Dataverse.class),
-	Tracking("Manage guestbook, download statistics, etc.", Dataverse.class),
-    CreateTemplates("Create new templates", DvObjectContainer.class ),
-    RestrictFile("Restrict File Access", DataFile.class),
-    RestrictMetadata("Mark metadata as restricted", DvObject.class),
-    //RequestAccess("Request Access to an object", DvObject.class),    
     
-
     // Create
     AddDataverse("Add a dataverse within another dataverse", Dataverse.class),
     AddDataset("Add a dataset to a dataverse", Dataverse.class),
@@ -57,7 +31,7 @@ public enum Permission implements java.io.Serializable {
     DownloadFile("Download a file", DataFile.class),
     RequestAccess("Request access to an file", DataFile.class),
     // Update
-    EditDataverse("Edit a dataverse's metadata, facets, customization, ", Dataverse.class),
+    EditDataverse("Edit a dataverse's metadata, facets, customization, and templates ", Dataverse.class),
     EditDataset("Edit a dataset's metadata", Dataset.class),
     ManageDataversePermissions("Manage permissions for a dataverse", Dataverse.class),
     ManageDatasetPermissions("Manage permissions for a dataset", Dataset.class), 
