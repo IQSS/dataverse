10d9e964a595b626569350e74945f47a214d2a34
Gustavo Durand <gdurand@iq.harvard.edu>
2014-11-25 15:54:31 -0500

    added new roles to setup

--- scripts/api/setup-builtin-roles.sh
+++ scripts/api/setup-builtin-roles.sh
@@ -8,3 +8,27 @@ echo
 echo "Setting up file downloader role"
 curl -H "Content-type:application/json" -d @data/role-filedownloader.json http://localhost:8080/api/s/roles/
 echo
+
+echo "Setting up full contributor role"
+curl -H "Content-type:application/json" -d @data/role-fullContributor.json http://localhost:8080/api/s/roles/
+echo
+
+echo "Setting up dv contributor role"
+curl -H "Content-type:application/json" -d @data/role-dvContributor.json http://localhost:8080/api/s/roles/
+echo
+
+echo "Setting up ds contributor role"
+curl -H "Content-type:application/json" -d @data/role-dsContributor.json http://localhost:8080/api/s/roles/
+echo
+
+echo "Setting up editor role"
+curl -H "Content-type:application/json" -d @data/role-editor.json http://localhost:8080/api/s/roles/
+echo
+
+echo "Setting up manager role"
+curl -H "Content-type:application/json" -d @data/role-manager.json http://localhost:8080/api/s/roles/
+echo
+
+echo "Setting up curator role"
+curl -H "Content-type:application/json" -d @data/role-curator.json http://localhost:8080/api/s/roles/
+echo
