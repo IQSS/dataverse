5e4b2b66c9ef00aae27a574a0a329f12f6ae2127
Michael Heppler <mheppler@hmdc.harvard.edu>
2014-11-25 16:55:46 -0500

    Moved popup javascript hack for small browser windows off the dataverse page and into dv_rebind_bootstrap_ui (that really needs to be renamed), so that it can be used on te permissions-manage page popups for create/edit roles. [ref #1054]

--- src/main/webapp/resources/js/dv_rebind_bootstrap_ui.js
+++ src/main/webapp/resources/js/dv_rebind_bootstrap_ui.js
@@ -182,3 +182,41 @@ function post_differences(){
        var dialogScroll = dialogHeight - dialogHeader;
        $('div[id$="detailsBlocks"] .ui-dialog-content').css('height', dialogScroll);
 }
+
+function handleResizeDialog(dialog) {
+                        var el = $('div[id$="' + dialog + '"]');
+                        var doc = $('body');
+                        var win = $(window);
+                        var elPos = '';
+                        var bodyHeight = '';
+                        var bodyWidth = '';
+                        // position:fixed is maybe cool, but it makes the dialog not scrollable on browser level, even if document is big enough
+                        if (el.height() > win.height()) {
+                            bodyHeight = el.height() + 'px';
+                            elPos = 'absolute';
+                        }   
+                        if (el.width() > win.width()) {
+                            bodyWidth = el.width() + 'px';
+                            elPos = 'absolute';
+                        }
+                        el.css('position', elPos);
+                        doc.css('width', bodyWidth);
+                        doc.css('height', bodyHeight);
+                        var pos = el.offset();
+                        if (pos.top + el.height() > doc.height())
+                            pos.top = doc.height() - el.height();
+                        if (pos.left + el.width() > doc.width())
+                            pos.left = doc.width() - el.width();
+                        var offsetX = 0;
+                        var offsetY = 0;
+                        if (elPos != 'absolute') {
+                            offsetX = $(window).scrollLeft();
+                            offsetY = $(window).scrollTop();
+                        }
+                        // scroll fix for position fixed
+                        if (pos.left < offsetX)
+                            pos.left = offsetX;
+                        if (pos.top < offsetY)
+                            pos.top = offsetY;
+                        el.offset(pos);
+                    }
