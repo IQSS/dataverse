2d2f4cdeff7f5b32bfe7793c398008a044a9376b
posixeleni <eleni@Elenis-MacBook-Pro.local>
2014-11-26 12:49:00 -0500

    Added Journal Metadata Block
    See ticket #1166

--- scripts/api/setup-datasetfields.sh
+++ scripts/api/setup-datasetfields.sh
@@ -4,4 +4,5 @@ curl http://localhost:8080/api/datasetfield/load -X POST --data-binary @data/met
 curl http://localhost:8080/api/datasetfield/load -X POST --data-binary @data/metadatablocks/social_science.tsv -H "Content-type: text/tab-separated-values"
 curl http://localhost:8080/api/datasetfield/load -X POST --data-binary @data/metadatablocks/astrophysics.tsv -H "Content-type: text/tab-separated-values"
 curl http://localhost:8080/api/datasetfield/load -X POST --data-binary @data/metadatablocks/biomedical.tsv -H "Content-type: text/tab-separated-values"
+curl http://localhost:8080/api/datasetfield/load -X POST --data-binary @data/metadatablocks/journals.tsv -H "Content-type: text/tab-separated-values"
 curl http://localhost:8080/api/datasetfield/load -X POST --data-binary @data/metadatablocks/customGSD.tsv -H "Content-type: text/tab-separated-values"
