cb8acd0e01822aadd06eff674b7cd2b4f54fc38a
Leonid Andreev <leonid@hmdc.harvard.edu>
2014-11-30 21:24:10 -0500

    Removed all the datavariable-related objects from reference data, as they are no longer necessary. Yippee! The only thing we have left in there is that experimental DCTERMS import map.

--- scripts/database/reference_data.sql
+++ scripts/database/reference_data.sql
@@ -1,26 +1,3 @@
-INSERT INTO variableformattype (id, "name") VALUES (1,'numeric');
-INSERT INTO variableformattype (id, "name") VALUES (2,'character');
-
-INSERT INTO variableintervaltype (id, "name") VALUES (1, 'discrete');
-INSERT INTO variableintervaltype (id, "name") VALUES (2, 'continuous');
-INSERT INTO variableintervaltype (id, "name") VALUES (3, 'nominal');
-INSERT INTO variableintervaltype (id, "name") VALUES (4, 'dichotomous');
-
-INSERT INTO summarystatistictype (id, "name") VALUES (1, 'mean');
-INSERT INTO summarystatistictype (id, "name") VALUES (2, 'medn');
-INSERT INTO summarystatistictype (id, "name") VALUES (3, 'mode');
-INSERT INTO summarystatistictype (id, "name") VALUES (4, 'min');
-INSERT INTO summarystatistictype (id, "name") VALUES (5, 'max');
-INSERT INTO summarystatistictype (id, "name") VALUES (6, 'stdev');
-INSERT INTO summarystatistictype (id, "name") VALUES (7, 'vald');
-INSERT INTO summarystatistictype (id, "name") VALUES (8, 'invd');
-
-INSERT INTO variablerangetype (id, "name") VALUES (1, 'min');
-INSERT INTO variablerangetype (id, "name") VALUES (2, 'max');
-INSERT INTO variablerangetype (id, "name") VALUES (3, 'min');
-INSERT INTO variablerangetype (id, "name") VALUES (4, 'max');
-INSERT INTO variablerangetype (id, "name") VALUES (5, 'point');
-
 -- using http://dublincore.org/schemas/xmls/qdc/dcterms.xsd because at http://dublincore.org/schemas/xmls/ it's the schema location for http://purl.org/dc/terms/ which is referenced in http://swordapp.github.io/SWORDv2-Profile/SWORDProfile.html
 INSERT INTO foreignmetadataformatmapping(id, name, startelement, displayName, schemalocation) VALUES (1, 'http://purl.org/dc/terms/', 'entry', 'dcterms: DCMI Metadata Terms', 'http://dublincore.org/schemas/xmls/qdc/dcterms.xsd');
 INSERT INTO foreignmetadatafieldmapping (id, foreignfieldxpath, datasetfieldname, isattribute, parentfieldmapping_id, foreignmetadataformatmapping_id) VALUES (1, ':title', 'title', FALSE, NULL, 1 );
