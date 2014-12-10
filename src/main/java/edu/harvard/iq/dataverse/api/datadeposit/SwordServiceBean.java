47e3fbf64dfe80bd2c6d8db8f638c0e69e854912
Gustavo Durand <gdurand@iq.harvard.edu>
2014-12-10 18:18:26 -0500

    fixed addDatasetContact method

--- src/main/java/edu/harvard/iq/dataverse/api/datadeposit/SwordServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/api/datadeposit/SwordServiceBean.java
@@ -30,21 +30,24 @@ public class SwordServiceBean {
      * the dataverse that will own the dataset.
      */
     public void addDatasetContact(DatasetVersion newDatasetVersion) {
-        DatasetField emailDatasetField = new DatasetField();
         DatasetFieldType emailDatasetFieldType = datasetFieldService.findByNameOpt(DatasetFieldConstant.datasetContact);
-        List<DatasetFieldValue> values = new ArrayList<>();
-        values.add(new DatasetFieldValue(emailDatasetField, newDatasetVersion.getDataset().getOwner().getContactEmail()));
-        emailDatasetField.setDatasetFieldValues(values);
-        emailDatasetField.setDatasetFieldType(emailDatasetFieldType);
-        List<DatasetField> currentFields = newDatasetVersion.getDatasetFields();
-        currentFields.add(emailDatasetField);
-        newDatasetVersion.setDatasetFields(currentFields);
+        DatasetField emailDatasetField = DatasetField.createNewEmptyDatasetField(emailDatasetFieldType, newDatasetVersion);
+
+        for (DatasetField childField : emailDatasetField.getDatasetFieldCompoundValues().get(0).getChildDatasetFields()) {
+            if (DatasetFieldConstant.datasetContactEmail.equals(childField.getDatasetFieldType().getName())) {
+                // set the value to that of the owning dataverse
+                childField.getSingleValue().setValue(newDatasetVersion.getDataset().getOwner().getContactEmail());
+            }
+        }
+
+        newDatasetVersion.getDatasetFields().add(emailDatasetField);
+
     }
 
     public void addDatasetSubject(DatasetVersion datasetVersion) {
-        DatasetField subjectDatasetField = new DatasetField();
         DatasetFieldType subjectDatasetFieldType = datasetFieldService.findByNameOpt(DatasetFieldConstant.subject);
-        subjectDatasetField.setDatasetFieldType(subjectDatasetFieldType);
+        DatasetField subjectDatasetField = DatasetField.createNewEmptyDatasetField(subjectDatasetFieldType, datasetVersion);
+
         /**
          * @todo Rather than hard coding "Other" here, inherit the Subject from
          * the parent dataverse, when it's available once
@@ -58,9 +61,8 @@ public class SwordServiceBean {
             subjectDatasetField.setControlledVocabularyValues(controlledVocabularyValues);
 
             // add the new subject field to the rest of the fields
-            List<DatasetField> currentFields = datasetVersion.getDatasetFields();
-            currentFields.add(subjectDatasetField);
-            datasetVersion.setDatasetFields(currentFields);
+            datasetVersion.getDatasetFields().add(subjectDatasetField);
+
         } else {
             logger.info(subjectDatasetFieldType.getName() + "could not be populated with '" + subjectOther + "': null returned from lookup.");
         }
