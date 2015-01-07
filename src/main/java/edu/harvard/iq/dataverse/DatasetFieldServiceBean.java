9fc5af5648c97e67af85fb10d42b2ec050115716
Gustavo Durand <gdurand@iq.harvard.edu>
2015-01-07 15:49:04 -0500

    modified picklist ot have a dropdown to filter by metadatablock

--- src/main/java/edu/harvard/iq/dataverse/DatasetFieldServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/DatasetFieldServiceBean.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package edu.harvard.iq.dataverse;
 
 import java.util.List;
@@ -25,16 +24,23 @@ public class DatasetFieldServiceBean implements java.io.Serializable {
 
     @PersistenceContext(unitName = "VDCNet-ejbPU")
     private EntityManager em;
-    
+
     private static final String NAME_QUERY = "SELECT dsfType from DatasetFieldType dsfType where dsfType.name= :fieldName";
- 
+
     public List<DatasetFieldType> findAllAdvancedSearchFieldTypes() {
         return em.createQuery("select object(o) from DatasetFieldType as o where o.advancedSearchFieldType = true and o.title != '' order by o.id").getResultList();
     }
-    
+
     public List<DatasetFieldType> findAllFacetableFieldTypes() {
-        return em.createQuery("select object(o) from DatasetFieldType as o where o.facetable = true and o.title != '' order by o.id").getResultList();
-    } 
+         return em.createNamedQuery("DatasetFieldType.findAllFacetable", DatasetFieldType.class)
+                .getResultList();   
+    }
+
+    public List<DatasetFieldType> findFacetableFieldTypesByMetadataBlock(Long metadataBlockId) {
+        return em.createNamedQuery("DatasetFieldType.findFacetableByMetadaBlock", DatasetFieldType.class)
+                .setParameter("metadataBlockId", metadataBlockId)
+                .getResultList();
+    }
 
     public List<DatasetFieldType> findAllRequiredFields() {
         return em.createQuery("select object(o) from DatasetFieldType as o where o.required = true order by o.id").getResultList();
@@ -50,29 +56,31 @@ public class DatasetFieldServiceBean implements java.io.Serializable {
 
     public DatasetFieldType find(Object pk) {
         return (DatasetFieldType) em.find(DatasetFieldType.class, pk);
-    } 
-    
+    }
+
     public DatasetFieldType findByName(String name) {
-        DatasetFieldType dsfType = (DatasetFieldType) em.createQuery(NAME_QUERY).setParameter("fieldName",name).getSingleResult();
+        DatasetFieldType dsfType = (DatasetFieldType) em.createQuery(NAME_QUERY).setParameter("fieldName", name).getSingleResult();
         return dsfType;
     }
-    
+
     /**
-     * Gets the dataset field type, or returns {@code null}. Does not throw exceptions.
+     * Gets the dataset field type, or returns {@code null}. Does not throw
+     * exceptions.
+     *
      * @param name the name do the field type
      * @return the field type, or {@code null}
-     * @see #findByName(java.lang.String) 
+     * @see #findByName(java.lang.String)
      */
-    public DatasetFieldType findByNameOpt( String name ) {
+    public DatasetFieldType findByNameOpt(String name) {
         try {
             return em.createNamedQuery("DatasetFieldType.findByName", DatasetFieldType.class)
                     .setParameter("name", name)
                     .getSingleResult();
-        } catch ( NoResultException nre ) {
+        } catch (NoResultException nre) {
             return null;
         }
     }
-    
+
     /* 
      * Similar method for looking up foreign metadata field mappings, for metadata
      * imports. for these the uniquness of names isn't guaranteed (i.e., there 
@@ -86,15 +94,16 @@ public class DatasetFieldServiceBean implements java.io.Serializable {
                     .setParameter("formatName", formatName)
                     .setParameter("xPath", pathName)
                     .getSingleResult();
-        } catch ( NoResultException nre ) {
+        } catch (NoResultException nre) {
             return null;
         }
-        
+
         // TODO: cache looked up results.
-    }    
+    }
+
     public ControlledVocabularyValue findControlledVocabularyValue(Object pk) {
         return (ControlledVocabularyValue) em.find(ControlledVocabularyValue.class, pk);
-    }        
+    }
 
     /**
      * @param dsft The DatasetFieldType in which to look up a
@@ -115,24 +124,24 @@ public class DatasetFieldServiceBean implements java.io.Serializable {
             return null;
         }
     }
-    
+
     // return singleton NA Controled Vocabulary Value
     public ControlledVocabularyValue findNAControlledVocabularyValue() {
         TypedQuery<ControlledVocabularyValue> typedQuery = em.createQuery("SELECT OBJECT(o) FROM ControlledVocabularyValue AS o WHERE o.datasetFieldType is null AND o.strValue = :strvalue", ControlledVocabularyValue.class);
         typedQuery.setParameter("strvalue", DatasetField.NA_VALUE);
         return typedQuery.getSingleResult();
-    }    
-    
-    public DatasetFieldType save(DatasetFieldType dsfType) {
-       return em.merge(dsfType);
     }
-    
+
+    public DatasetFieldType save(DatasetFieldType dsfType) {
+        return em.merge(dsfType);
+    }
+
     public MetadataBlock save(MetadataBlock mdb) {
-       return em.merge(mdb);
-    }   
-    
+        return em.merge(mdb);
+    }
+
     public ControlledVocabularyValue save(ControlledVocabularyValue cvv) {
-       return em.merge(cvv);
-    }       
-    
+        return em.merge(cvv);
+    }
+
 }
