e6be40b95c7eeeebc1d551f87c2ba7822149d32b
Philip Durbin <philip_durbin@harvard.edu>
2015-01-14 15:34:23 -0500

    added failing (ignored) test for pagekite IP bug #1281

--- src/test/java/edu/harvard/iq/ip/IpAddressTest.java
+++ src/test/java/edu/harvard/iq/ip/IpAddressTest.java
@@ -5,6 +5,7 @@ import edu.harvard.iq.dataverse.authorization.groups.impl.ipaddress.ip.IpAddress
 import edu.harvard.iq.dataverse.authorization.groups.impl.ipaddress.ip.IPv4Address;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Ignore;
 
 /**
  *
@@ -25,5 +26,15 @@ public class IpAddressTest {
         assertEquals( new IPv6Address(0xa, 0xb, 0xc, 0xd, 0xe, 0xf, 0x0, 0x0),
                       IpAddress.valueOf("a:b:c:d:e:f::"));
     }
-    
+
+    @Ignore
+    @Test
+    public void testValueOfPageKiteIp() {
+        /**
+         * @todo Remove @Ignore and get this passing to fix
+         * https://github.com/IQSS/dataverse/issues/1281
+         */
+        IpAddress.valueOf("::ffff:65.112.10.85");
+    }
+
 }
