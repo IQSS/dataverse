c5a8906e437d8c2f55100f94527dc9f507d05475
Leonid Andreev <leonid@hmdc.harvard.edu>
2015-01-16 11:10:37 -0500

    Missing IngestRequest class.

---
+++ src/main/java/edu/harvard/iq/dataverse/ingest/IngestRequest.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.harvard.iq.dataverse.ingest;
+
+import edu.harvard.iq.dataverse.DataFile;
+import java.io.Serializable;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author Leonid Andreev
+ */
+@Entity
+public class IngestRequest implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    //@ManyToOne
+    //@JoinColumn(nullable=false)
+    
+    @OneToOne(cascade={CascadeType.MERGE,CascadeType.PERSIST})
+    @JoinColumn(name="datafile_id")
+    private DataFile dataFile;
+    
+    private String textEncoding; 
+    
+    private String controlCard;
+    
+    private String labelsFile; 
+    
+    public DataFile getDataFile() {
+        return dataFile;
+    }
+    
+    public void setDataFile(DataFile dataFile) {
+        this.dataFile = dataFile; 
+    }
+    
+    public String getTextEncoding() {
+        return textEncoding;
+    }
+    
+    public void setTextEncoding(String textEncoding) {
+        this.textEncoding = textEncoding; 
+    }
+
+    public String getControlCard() {
+        return controlCard;
+    }
+    
+    public void setControlCard(String controlCard) {
+        this.controlCard = controlCard; 
+    }
+    
+    public String getLabelsFile() {
+        return labelsFile;
+    }
+    
+    public void setLabelsFile(String labelsFile) {
+        this.labelsFile = labelsFile; 
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof IngestRequest)) {
+            return false;
+        }
+        IngestRequest other = (IngestRequest) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "edu.harvard.iq.dataverse.ingest.IngestRequest[ id=" + id + " ]";
+    }
+    
+}
