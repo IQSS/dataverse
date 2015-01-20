11de6ca92e2b2df25c9d5451710d6c4652bc2dca
ellenk <ekraffmiller@hmdc.harvard.edu>
2015-01-20 16:37:19 -0500

    change debug=false

--- src/main/java/edu/harvard/iq/dataverse/SearchIncludeFragment.java
+++ src/main/java/edu/harvard/iq/dataverse/SearchIncludeFragment.java
@@ -90,7 +90,7 @@ public class SearchIncludeFragment implements java.io.Serializable {
     Map<String, String> staticSolrFieldFriendlyNamesBySolrField = new HashMap<>();
     private boolean solrIsDown = false;
     private Map<String, Integer> numberOfFacets = new HashMap<>();
-    private boolean debug = true;
+    private boolean debug = false;
 //    private boolean showUnpublished;
     List<String> filterQueriesDebug = new ArrayList<>();
 //    private Map<String, String> friendlyName = new HashMap<>();
