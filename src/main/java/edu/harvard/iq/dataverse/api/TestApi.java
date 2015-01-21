590dabd344c3eb45d8c74a3f119720da1596c31a
Michael Bar-Sinai <mich.barsinai@gmail.com>
2015-01-21 23:53:30 +0200

    Added an API call to test permissions of a user over a DvObject

--- src/main/java/edu/harvard/iq/dataverse/api/TestApi.java
+++ src/main/java/edu/harvard/iq/dataverse/api/TestApi.java
@@ -1,17 +1,15 @@
 package edu.harvard.iq.dataverse.api;
 
+import edu.harvard.iq.dataverse.DvObject;
 import edu.harvard.iq.dataverse.authorization.RoleAssignee;
-import edu.harvard.iq.dataverse.authorization.groups.impl.ipaddress.IpGroup;
-import edu.harvard.iq.dataverse.authorization.groups.impl.ipaddress.ip.IpAddress;
+import edu.harvard.iq.dataverse.authorization.users.AuthenticatedUser;
 import javax.ejb.Stateless;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 import static edu.harvard.iq.dataverse.util.json.JsonPrinter.*;
-import java.util.Set;
-import javax.json.Json;
-import javax.json.JsonArrayBuilder;
+import javax.ws.rs.QueryParam;
 
 /**
  * An API to test internal models without the need to deal with UI etc.
@@ -20,6 +18,23 @@ import javax.json.JsonArrayBuilder;
 @Stateless
 @Path("test")
 public class TestApi extends AbstractApiBean {
+    
+    @Path("permissions/{dvo}")
+    @GET
+    public Response findPermissonsOn( @PathParam("dvo") String dvo,
+                                      @QueryParam("key") String key ) {
+        DvObject dvObj = findDvo(dvo);
+        if ( dvObj == null ) {
+            return notFound("DvObject " + dvo + " not found");
+        }
+        try {
+            AuthenticatedUser au = findUserOrDie(key);
+            return okResponse( json(permissionSvc.permissionsForUser(au, dvObj)) );
+            
+        } catch ( WrappedResponse wr ) {
+            return wr.getResponse();
+        }
+    }
     
     @Path("assignee/{idtf}")
     @GET
