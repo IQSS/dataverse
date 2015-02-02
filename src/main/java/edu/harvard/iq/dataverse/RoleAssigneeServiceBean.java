faa036bf9866e2206b525912ca3172913cb00917
Gustavo Durand <gdurand@iq.harvard.edu>
2015-02-02 18:21:19 -0500

    add notifications for requested file access

--- src/main/java/edu/harvard/iq/dataverse/RoleAssigneeServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/RoleAssigneeServiceBean.java
@@ -5,7 +5,10 @@ import edu.harvard.iq.dataverse.authorization.RoleAssignee;
 import edu.harvard.iq.dataverse.authorization.groups.GroupServiceBean;
 import edu.harvard.iq.dataverse.authorization.groups.impl.builtin.AllUsers;
 import edu.harvard.iq.dataverse.authorization.groups.impl.builtin.AuthenticatedUsers;
+import edu.harvard.iq.dataverse.authorization.groups.impl.explicit.ExplicitGroup;
+import edu.harvard.iq.dataverse.authorization.users.AuthenticatedUser;
 import edu.harvard.iq.dataverse.authorization.users.GuestUser;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -19,24 +22,25 @@ import javax.persistence.PersistenceContext;
 
 /**
  * The place to obtain {@link RoleAssignee}s, based on their identifiers.
- * 
+ *
  * @author michael
  */
 @Stateless
 @Named
 public class RoleAssigneeServiceBean {
+
     private static final Logger logger = Logger.getLogger(RoleAssigneeServiceBean.class.getName());
     @PersistenceContext(unitName = "VDCNet-ejbPU")
     private EntityManager em;
-    
+
     @EJB
     AuthenticationServiceBean authSvc;
 
     @EJB
     GroupServiceBean groupSvc;
-    
+
     Map<String, RoleAssignee> predefinedRoleAssignees = new TreeMap<>();
-    
+
     @PostConstruct
     void setup() {
         GuestUser gu = new GuestUser();
@@ -44,23 +48,38 @@ public class RoleAssigneeServiceBean {
         predefinedRoleAssignees.put(AuthenticatedUsers.get().getIdentifier(), AuthenticatedUsers.get());
         predefinedRoleAssignees.put(AllUsers.get().getIdentifier(), AllUsers.get());
     }
-    
-    public RoleAssignee getRoleAssignee( String identifier ) {
-        switch ( identifier.charAt(0) ) {
+
+    public RoleAssignee getRoleAssignee(String identifier) {
+        switch (identifier.charAt(0)) {
             case ':':
                 return predefinedRoleAssignees.get(identifier);
             case '@':
                 return authSvc.getAuthenticatedUser(identifier.substring(1));
             case '&':
                 return groupSvc.getGroup(identifier.substring(1));
-            default: 
+            default:
                 throw new IllegalArgumentException("Unsupported assignee identifier '" + identifier + "'");
         }
     }
-    
-    public List<RoleAssignment> getAssignmentsFor( String roleAssigneeIdentifier ) {
+
+    public List<RoleAssignment> getAssignmentsFor(String roleAssigneeIdentifier) {
         return em.createNamedQuery("RoleAssignment.listByAssigneeIdentifier", RoleAssignment.class)
-                 .setParameter("assigneeIdentifier", roleAssigneeIdentifier )
-                 .getResultList();
+                .setParameter("assigneeIdentifier", roleAssigneeIdentifier)
+                .getResultList();
+    }
+
+    public List<AuthenticatedUser> getExplicitUsers(RoleAssignee ra) {
+        List<AuthenticatedUser> explicitUsers = new ArrayList();
+        if (ra instanceof AuthenticatedUser) {
+            explicitUsers.add((AuthenticatedUser) ra);
+        } else if (ra instanceof ExplicitGroup) {
+            ExplicitGroup group = (ExplicitGroup) ra;
+            for (String raIdentifier : group.getContainedRoleAssgineeIdentifiers()) {
+                explicitUsers.addAll(getExplicitUsers(getRoleAssignee(raIdentifier)));
+            }
+        }
+
+        return explicitUsers;
+
     }
 }
