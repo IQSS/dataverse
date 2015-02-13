d3113dbd88fdedea803d154334add450983406a9
Leonid Andreev <leonid@hmdc.harvard.edu>
2015-02-13 15:10:20 -0500

    Framework for identifying file type "classes", for type-specific icons.

--- src/main/java/edu/harvard/iq/dataverse/DataFileServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/DataFileServiceBean.java
@@ -37,9 +37,80 @@ public class DataFileServiceBean implements java.io.Serializable {
     @PersistenceContext(unitName = "VDCNet-ejbPU")
     private EntityManager em;
     
-    private static final String MIME_TYPE_SPSS_POR = "application/x-spss-por";
-    private static final String MIME_TYPE_SPSS_SAV = "application/x-spss-sav";
+    // File type "classes" tags:
+    
+    private static final String FILE_CLASS_AUDIO = "audio";
+    private static final String FILE_CLASS_CODE = "code";
+    private static final String FILE_CLASS_DOCUMENT = "document";
+    private static final String FILE_CLASS_ASTRO = "astro";
+    private static final String FILE_CLASS_IMAGE = "image";
+    private static final String FILE_CLASS_NETWORK = "network";
+    private static final String FILE_CLASS_GEO = "geodata";
+    private static final String FILE_CLASS_TABULAR = "tabular";
+    private static final String FILE_CLASS_VIDEO = "video";
+    private static final String FILE_CLASS_OTHER = "other";
 
+    // Assorted useful mime types:
+    
+    // 3rd-party and/or proprietary tabular data formasts that we know
+    // how to ingest: 
+    
+    private static final String MIME_TYPE_STATA = "application/x-stata";
+    private static final String MIME_TYPE_STATA13 = "application/x-stata-13";
+    private static final String MIME_TYPE_RDATA = "application/x-rlang-transport";
+    private static final String MIME_TYPE_CSV   = "text/csv";
+    private static final String MIME_TYPE_CSV_ALT = "text/comma-separated-values";
+    private static final String MIME_TYPE_XLSX  = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+    private static final String MIME_TYPE_SPSS_SAV = "application/x-spss-sav";
+    private static final String MIME_TYPE_SPSS_POR = "application/x-spss-por";
+    
+    // Tabular data formats we don't know how to ingets, but still recognize
+    // as "tabular data":
+    // TODO: - add more to this list? -- L.A. 4.0 beta13
+    
+    private static final String MIME_TYPE_TAB   = "text/tab-separated-values";
+    private static final String MIME_TYPE_FIXED_FIELD = "text/x-fixed-field";
+    private static final String MIME_TYPE_SAS_TRANSPORT = "application/x-sas-transport";
+    private static final String MIME_TYPE_SAS_SYSTEM = "application/x-sas-system";
+    
+    // The following are the "control card/syntax" formats that we recognize 
+    // as "code":
+    
+    private static final String MIME_TYPE_R_SYNTAX = "application/x-r-syntax";
+    private static final String MIME_TYPE_STATA_SYNTAX = "text/x-stata-syntax";
+    private static final String MIME_TYPE_SPSS_CCARD = "text/x-spss-syntax";
+    private static final String MIME_TYPE_SAS_SYNTAX = "text/x-sas-syntax";
+
+    // The types recognized as "documents":
+    // TODO: there has to be more! -- L.A. 4.0 beta13
+    
+    private static final String MIME_TYPE_PLAIN_TEXT = "text/plain";
+    private static final String MIME_TYPE_DOCUMENT_PDF = "application/pdf";
+    private static final String MIME_TYPE_DOCUMENT_MSWORD = "application/msword";
+    private static final String MIME_TYPE_DOCUMENT_MSEXCEL = "application/vnd.ms-excel";
+    private static final String MIME_TYPE_DOCUMENT_MSWORD_OPENXML = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+    
+    // Supported Astrophysics formats: 
+    // (only FITS at this point)
+    
+    private static final String MIME_TYPE_FITS  = "application/fits";
+    private static final String MIME_TYPE_FITSIMAGE = "image/fits";
+   
+    // Network Data files: 
+    // (only GRAPHML at this point): 
+    
+    private static final String MIME_TYPE_NETWORK_GRAPHML = "text/xml-graphml";
+   
+    // SHAPE file type: 
+    // this is the only supported file type in the GEO DATA class:
+    
+    private static final String MIME_TYPE_GEO_SHAPE = "application/zipped-shapefile";
+    
+    private static final String MIME_TYPE_ZIP   = "application/zip";
+    
+    private static final String MIME_TYPE_UNDETERMINED_DEFAULT = "application/octet-stream";
+    private static final String MIME_TYPE_UNDETERMINED_BINARY = "application/binary";
+    
     public DataFile find(Object pk) {
         return (DataFile) em.find(DataFile.class, pk);
     }   
@@ -162,7 +233,7 @@ public class DataFileServiceBean implements java.io.Serializable {
         // mime type for FITS is "application/fits", and problematic: then
         // the file is identified as an image, and the page will attempt to 
         // generate a preview - which of course is going to fail...
-        if ("image/fits".equalsIgnoreCase(contentType)) {
+        if (MIME_TYPE_FITSIMAGE.equalsIgnoreCase(contentType)) {
             return false;
         }
         // besides most image/* types, we can generate thumbnails for 
@@ -171,7 +242,7 @@ public class DataFileServiceBean implements java.io.Serializable {
         return (contentType != null && 
                 (contentType.startsWith("image/") || 
                 contentType.equalsIgnoreCase("application/pdf") ||
-                contentType.equalsIgnoreCase("application/zipped-shapefile")));
+                contentType.equalsIgnoreCase(MIME_TYPE_GEO_SHAPE)));
     }
     
     /* 
@@ -187,6 +258,21 @@ public class DataFileServiceBean implements java.io.Serializable {
        return ImageThumbConverter.isThumbnailAvailable(file);      
     }
     
+ 
+    public boolean isPreviewAvailable (Long fileId) {
+        if (fileId == null) {
+            return false; 
+        }
+        
+        DataFile file = find(fileId);
+        
+        if (file == null) {
+            return false; 
+        }
+        
+        return isThumbnailAvailable(file); 
+    }
+    
     /* 
      * TODO: 
      * similar method, but for non-default thumbnail sizes:
@@ -196,5 +282,264 @@ public class DataFileServiceBean implements java.io.Serializable {
         return false; 
     }
     
+    public boolean ingestableAsTabular(DataFile dataFile) {
+        /* 
+         * In the final 4.0 we'll be doing real-time checks, going through the 
+         * available plugins and verifying the lists of mime types that they 
+         * can handle. In 4.0 beta, the ingest plugins are still built into the 
+         * main code base, so we can just go through a hard-coded list of mime 
+         * types. -- L.A. 
+         */
+        
+        String mimeType = dataFile.getContentType();
+        
+        if (mimeType == null) {
+            return false;
+        }
+        
+        if (mimeType.equals(MIME_TYPE_STATA)) {
+            return true;
+        } else if (mimeType.equals(MIME_TYPE_STATA13)) {
+            return true;
+        } else if (mimeType.equals(MIME_TYPE_RDATA)) {
+            return true;
+        } else if (mimeType.equals(MIME_TYPE_CSV) || mimeType.equals(MIME_TYPE_CSV_ALT)) {
+            return true;
+        } else if (mimeType.equals(MIME_TYPE_XLSX)) {
+            return true;
+        } else if (mimeType.equals(MIME_TYPE_SPSS_SAV)) {
+            return true;
+        } else if (mimeType.equals(MIME_TYPE_SPSS_POR)) {
+            return true;
+        }
+
+        return false;
+    }
+    
+    /* 
+     * Methods for identifying "classes" (groupings) of files by type:
+    */
+    
+    public String getFileClassById (Long fileId) {
+        DataFile file = find(fileId);
+        
+        if (file == null) {
+            return null; 
+        }
+        
+        return getFileClass(file);
+    }
+    
+    public String getFileClass (DataFile file) {
+        if (isFileClassImage(file)) {
+            return FILE_CLASS_IMAGE;
+        }
+        
+        if (isFileClassVideo(file)) {
+            return FILE_CLASS_VIDEO;
+        }
+        
+        if (isFileClassAudio(file)) {
+            return FILE_CLASS_AUDIO;
+        }
+        
+        if (isFileClassCode(file)) {
+            return FILE_CLASS_CODE;
+        }
+        
+        if (isFileClassDocument(file)) {
+            return FILE_CLASS_DOCUMENT;
+        }
+        
+        if (isFileClassAstro(file)) {
+            return FILE_CLASS_ASTRO;
+        }
+        
+        if (isFileClassNetwork(file)) {
+            return FILE_CLASS_NETWORK;
+        }
+        
+        if (isFileClassGeo(file)) {
+            return FILE_CLASS_GEO;
+        }
+        
+        if (isFileClassTabularData(file)) {
+            return FILE_CLASS_TABULAR;
+        }
+        
+        
+        return FILE_CLASS_OTHER;
+    }
+    
+    
+    
+    public boolean isFileClassImage (DataFile file) {
+        if (file == null) {
+            return false;
+        }
+        
+        String contentType = file.getContentType();
+
+        // Some browsers (Chrome?) seem to identify FITS files as mime
+        // type "image/fits" on upload; this is both incorrect (the official
+        // mime type for FITS is "application/fits", and problematic: then
+        // the file is identified as an image, and the page will attempt to 
+        // generate a preview - which of course is going to fail...
+        
+        if (MIME_TYPE_FITSIMAGE.equalsIgnoreCase(contentType)) {
+            return false;
+        }
+        // besides most image/* types, we can generate thumbnails for 
+        // pdf and "world map" files:
+        
+        return (contentType != null && (contentType.toLowerCase().startsWith("image/")));
+    }
+    
+    public boolean isFileClassAudio (DataFile file) {
+        if (file == null) {
+            return false;
+        }
+        
+        String contentType = file.getContentType();
+        
+        // TODO: 
+        // verify that there are no audio types that don't start with "audio/" - 
+        //  some exotic mp[34]... ?
+        
+        return (contentType != null && (contentType.toLowerCase().startsWith("audio/")));    
+    }
+    
+    public boolean isFileClassCode (DataFile file) {
+        if (file == null) {
+            return false;
+        }
+     
+        String contentType = file.getContentType();
+        
+        // The following are the "control card/syntax" formats that we recognize 
+        // as "code":
+    
+        return (MIME_TYPE_R_SYNTAX.equalsIgnoreCase(contentType)
+            || MIME_TYPE_STATA_SYNTAX.equalsIgnoreCase(contentType) 
+            || MIME_TYPE_SAS_SYNTAX.equalsIgnoreCase(contentType)
+            || MIME_TYPE_SPSS_CCARD.equalsIgnoreCase(contentType));
+        
+    }
+    
+    public boolean isFileClassDocument (DataFile file) {
+        if (file == null) {
+            return false;
+        }
+        
+        // "Documents": PDF, assorted MS docs, etc. 
+        
+        String contentType = file.getContentType();
+        
+        return (MIME_TYPE_PLAIN_TEXT.equalsIgnoreCase(contentType)
+            || MIME_TYPE_DOCUMENT_PDF.equalsIgnoreCase(contentType)
+            || MIME_TYPE_DOCUMENT_MSWORD.equalsIgnoreCase(contentType)
+            || MIME_TYPE_DOCUMENT_MSEXCEL.equalsIgnoreCase(contentType)
+            || MIME_TYPE_DOCUMENT_MSWORD_OPENXML.equalsIgnoreCase(contentType));
+        
+    }
+    
+    public boolean isFileClassAstro (DataFile file) {
+        if (file == null) {
+            return false;
+        }
+        
+        String contentType = file.getContentType();
+       
+        // The only known/supported "Astro" file type is FITS,
+        // so far:
+        
+        return (MIME_TYPE_FITS.equalsIgnoreCase(contentType) || MIME_TYPE_FITSIMAGE.equalsIgnoreCase(contentType));
+        
+    }
+    
+    public boolean isFileClassNetwork (DataFile file) {
+        if (file == null) {
+            return false;
+        }
+        
+        String contentType = file.getContentType();
+       
+        // The only known/supported Network Data type is GRAPHML,
+        // so far:
+        
+        return MIME_TYPE_NETWORK_GRAPHML.equalsIgnoreCase(contentType);
+        
+    }
+    
+    /* 
+     * we don't really need a method for "other" - 
+     * it's "other" if it fails to identify as any specific class... 
+     * (or do we?)
+    public boolean isFileClassOther (DataFile file) {
+        if (file == null) {
+            return false;
+        }
+        
+    }
+    */
+    
+    public boolean isFileClassGeo (DataFile file) {
+        if (file == null) {
+            return false;
+        }
+        
+        String contentType = file.getContentType();
+       
+        // The only known/supported Geo Data type is SHAPE,
+        // so far:
+        
+        return MIME_TYPE_GEO_SHAPE.equalsIgnoreCase(contentType);
+    }
+    
+    public boolean isFileClassTabularData (DataFile file) {
+        if (file == null) {
+            return false;
+        }
+        
+        // "Tabular data" is EITHER an INGESTED tabular data file, i.e.
+        // a file with a DataTable and DataVariables; or a DataFile 
+        // of one of the many known tabular data formats - SPSS, Stata, etc.
+        // that for one reason or another didn't get ingested: 
+        
+        if (file.isTabularData()) {
+            return true; 
+        }
+        
+        // The formats we know how to ingest: 
+        if (ingestableAsTabular(file)) {
+            return true;
+        }
+        
+        String contentType = file.getContentType();
+        
+        // And these are the formats we DON'T know how to ingest, 
+        // but nevertheless recognize as "tabular data":
+        
+        return (MIME_TYPE_TAB.equalsIgnoreCase(contentType)
+            || MIME_TYPE_FIXED_FIELD.equalsIgnoreCase(contentType) 
+            || MIME_TYPE_SAS_TRANSPORT.equalsIgnoreCase(contentType)
+            || MIME_TYPE_SAS_SYSTEM.equalsIgnoreCase(contentType));
+        
+    }
+    
+    public boolean isFileClassVideo (DataFile file) {
+        if (file == null) {
+            return false;
+        }
+        
+        String contentType = file.getContentType();
+        
+        // TODO: 
+        // check if there are video types that don't start with "audio/" - 
+        // some exotic application/... formats ?
+        
+        return (contentType != null && (contentType.toLowerCase().startsWith("video/")));    
+        
+    }
         
 }
