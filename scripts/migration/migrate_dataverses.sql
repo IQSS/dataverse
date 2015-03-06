45715f08f8086e07eefc8b5216ea771011dca009
Gustavo Durand <gdurand@iq.harvard.edu>
2015-03-06 12:07:13 -0500

    moved dataverse roles to a migrate_permissions.sql

--- scripts/migration/migrate_dataverses.sql
+++ scripts/migration/migrate_dataverses.sql
@@ -47,37 +47,7 @@ insert into dataverse (	id, affiliation, alias, dataversetype, description, name
 insert into dataversecontact (  contactemail, displayorder, dataverse_id)
         select                  trim(unnest(string_to_array(contactemail, ','))), 0, id from _dvn3_vdc;
 
--- POST
-insert into dvobjectcontainer select id from dataverse where id not in (select id from dvobjectcontainer);
 
-
------------------------
--- dataverses role assignments
------------------------
--- admin
-insert into roleassignment (	assigneeidentifier, definitionpoint_id, role_id)
-	select 			'@'|| useridentifier, vdc_id, dr.id
-	from _dvn3_vdcrole, authenticateduser, dataverserole dr
-	where _dvn3_vdcrole.vdcuser_id = authenticateduser.id
-	and role_id=3 and dr.alias='admin';
--- curator
-insert into roleassignment (	assigneeidentifier, definitionpoint_id, role_id)
-	select 			'@'|| useridentifier, vdc_id, dr.id
-	from _dvn3_vdcrole, authenticateduser, dataverserole dr
-	where _dvn3_vdcrole.vdcuser_id = authenticateduser.id
-	and role_id=2 and dr.alias='curator';
--- contributor
-insert into roleassignment (	assigneeidentifier, definitionpoint_id, role_id)
-	select 			'@'|| useridentifier, vdc_id, dr.id
-	from _dvn3_vdcrole, authenticateduser, dataverserole dr
-	where _dvn3_vdcrole.vdcuser_id = authenticateduser.id
-	and role_id=1 and dr.alias='dsContributor';
--- member
-insert into roleassignment (	assigneeidentifier, definitionpoint_id, role_id)
-	select 			'@'|| useridentifier, vdc_id, dr.id
-	from _dvn3_vdcrole, authenticateduser, dataverserole dr
-	where _dvn3_vdcrole.vdcuser_id = authenticateduser.id
-	and role_id=4 and dr.alias='member';
 
 -----------------------
 -- reset sequences
