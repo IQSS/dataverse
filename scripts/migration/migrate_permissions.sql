7e8ab9be5859325b210a747a09ecfa26a136404e
Gustavo Durand <gdurand@iq.harvard.edu>
2015-03-06 13:08:43 -0500

    added another permission table

--- scripts/migration/migrate_permissions.sql
+++ scripts/migration/migrate_permissions.sql
@@ -26,6 +26,14 @@ insert into roleassignment (	assigneeidentifier, definitionpoint_id, role_id)
 	where _dvn3_vdcrole.vdcuser_id = authenticateduser.id
 	and role_id=4 and dr.alias='member';
 
+-- groups (as members)
+-- member
+insert into roleassignment (	assigneeidentifier, definitionpoint_id, role_id)
+	select 			'&'|| groupalias, vdcs_id, dr.id
+	from _dvn3_vdc_usergroup, explicitgroup, dataverserole dr
+	where _dvn3_vdc_usergroup.allowedgroups_id = explicitgroup.id
+	and dr.alias='member';
+
 -----------------------
 -- file role assignments
 -----------------------
