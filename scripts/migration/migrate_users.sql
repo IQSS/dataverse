e2a05974fcc5657253251e00dc1bd07613477453
Gustavo Durand <gdurand@iq.harvard.edu>
2015-03-06 11:38:16 -0500

    added groups

--- scripts/migration/migrate_users.sql
+++ scripts/migration/migrate_users.sql
@@ -13,3 +13,17 @@ insert into authenticateduser(	id, affiliation, email, firstname, lastname, posi
 insert into authenticateduserlookup(	authenticationproviderid, persistentuserid, authenticateduser_id)
 	select				'builtin',  username, id
 	from _dvn3_vdcuser;
+
+-----------------------
+-- groups
+-----------------------
+
+-- only copy over groups that have users
+insert into explicitgroup(  id, description, displayname, groupalias, groupaliasinowner, owner_id)
+        select (id, friendlyname, friendlyname, '1-'||name, name, 1)
+        from _dvn3_usergroup
+        where;
+
+insert into explicitgroup_authenticateduser(  explicitgroup_id, containedauthenticatedusers_id)
+        select usergroups_id, users_id
+        from _dvn3_vdcuser_usergroup;
