17744b780a4f704d03520a90e89487e03234dcdd
ellenk <ekraffmiller@hmdc.harvard.edu>
2015-03-06 11:12:53 -0500

    add migration instructions related to dataset and datasetversion (#1068)

--- scripts/migration/migrate_datasets.sql
+++ scripts/migration/migrate_datasets.sql
@@ -1,4 +1,4 @@
-
+--copy studyversion fields to datasetversion
 update datasetversion  
  set createtime = sv.createtime,
      lastupdatetime = sv.lastupdatetime,
@@ -13,3 +13,15 @@ and d.identifier = s.studyid
 and datasetversion.dataset_id = d.id
 and datasetversion.versionnumber = sv.versionnumber
 and sv.study_id = s.id;
+
+-- set dataset.publication date to the releasetime of the earliest released studyversion
+update dvobject 
+set publicationdate = m.releasetime
+from (select dvobject.id, sv.study_id, min(sv.releasetime) as releasetime
+from _dvn3_studyversion  sv, dataset  d,  _dvn3_study s, dvobject
+where d.authority = s.authority
+and d.protocol = s.protocol
+and d.identifier = s.studyid
+and dvobject.id = d.id
+and sv.study_id = s.id
+and sv.versionstate!='DRAFT' group by sv.study_id, dvobject.id) m where m.id = dvobject.id;
