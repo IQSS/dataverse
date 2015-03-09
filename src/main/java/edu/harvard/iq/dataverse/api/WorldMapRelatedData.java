b6110755a99c4c082242103bb19faf694f8676b6
Raman Prasad <raman_prasad@harvard.edu>
2015-03-09 14:29:39 -0400

    Loop is there for #1479 -- to re-try image retrieval.  But doesn't seem to fix problem--even if set for 10 seconds, etc

--- src/main/java/edu/harvard/iq/dataverse/api/WorldMapRelatedData.java
+++ src/main/java/edu/harvard/iq/dataverse/api/WorldMapRelatedData.java
@@ -139,7 +139,7 @@ public class WorldMapRelatedData extends AbstractApiBean {
                                     , @PathParam("identifier") int identifier) {
 
         MapLayerMetadata mapLayerMetadata = this.mapLayerMetadataService.find(new Long(identifier));
-        logger.info("mapLayerMetadata retrieved. Try to retrieve image:<br />" + mapLayerMetadata.getMapImageLink());
+        logger.info("mapLayerMetadata retrieved. Try to retrieve image (after 1st time):<br />" + mapLayerMetadata.getMapImageLink());
         
         try {
             this.mapLayerMetadataService.retrieveMapImageForIcon(mapLayerMetadata);
@@ -605,12 +605,28 @@ public class WorldMapRelatedData extends AbstractApiBean {
         // notify user
         userNotificationService.sendNotification(dvUser, wmToken.getCurrentTimestamp(), UserNotification.Type.MAPLAYERUPDATED, dfile.getOwner().getLatestVersion().getId());
 
-        try {
-            // 1st attempt to retrieve icon image
-            this.mapLayerMetadataService.retrieveMapImageForIcon(savedMapLayerMetadata);
-        } catch (IOException ex) {
-            Logger.getLogger(WorldMapRelatedData.class.getName()).log(Level.SEVERE, null, ex);
-
+        
+        int attemptCnt = 0;
+        int max_attempts = 2;
+        while(attemptCnt < max_attempts) {
+            try {
+                // 1st attempt to retrieve icon image
+                attemptCnt++;
+                logger.info("retrieveMapImageForIcon - 1st time.  Attempt: " + attemptCnt);
+                if (this.mapLayerMetadataService.retrieveMapImageForIcon(savedMapLayerMetadata)==true){
+                    break;
+                }
+            } catch (IOException ex) {
+                logger.severe("Failed to retrieve image from WorldMap server");
+                Logger.getLogger(WorldMapRelatedData.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            /*
+            try {
+                Thread.sleep(2000); // Sleep 2 seconds
+            } catch (InterruptedException ex) {
+                Logger.getLogger(WorldMapRelatedData.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            */
         }
         
         return okResponse("map layer object saved!");
