862e76db33cd4afafb86f11cd2ee767c8960e92e
Philip Durbin <philip_durbin@harvard.edu>
2015-03-12 16:10:15 -0400

    enable Shibboleth login for Harvard #1587

--- scripts/api/setup-optional-harvard.sh
+++ scripts/api/setup-optional-harvard.sh
@@ -4,6 +4,8 @@ SERVER=http://localhost:8080/api
 echo "Setting up Harvard-specific settings"
 echo  "- Harvard Privacy Policy"
 curl -s -X PUT -d http://best-practices.dataverse.org/harvard-policies/harvard-privacy-policy.html $SERVER/s/settings/:ApplicationPrivacyPolicyUrl
+echo  "- Enabling Shibboleth"
+curl -X PUT -d true http://localhost:8080/api/s/settings/:ShibEnabled
 curl http://localhost:8080/api/datasetfield/load -X POST --data-binary @data/metadatablocks/customMRA.tsv -H "Content-type: text/tab-separated-values"
 curl http://localhost:8080/api/datasetfield/load -X POST --data-binary @data/metadatablocks/customGSD.tsv -H "Content-type: text/tab-separated-values"
 curl http://localhost:8080/api/datasetfield/load -X POST --data-binary @data/metadatablocks/customARCS.tsv -H "Content-type: text/tab-separated-values"
