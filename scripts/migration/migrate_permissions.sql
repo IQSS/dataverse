180cdf74b191ef79b289db40f1c97069ecd7ceac
Gustavo Durand <gdurand@iq.harvard.edu>
2015-03-23 19:04:24 -0400

    removed offsets, removed links (to separate file), add perms for creators of datasets and dataverses

--- scripts/migration/migrate_permissions.sql
+++ scripts/migration/migrate_permissions.sql
@@ -1,14 +1,14 @@
-
------------------------
--- offsets
------------------------
-update _dvn3_vdcrole set vdc_id = vdc_id + (select max(id) from _dvn3_vdcnetwork);
-update _dvn3_vdc_usergroup set vdcs_id = vdcs_id + (select max(id) from _dvn3_vdcnetwork);
---todo: offsets needed for files
-
 -----------------------
 -- dataverses role assignments
 -----------------------
+
+-- admin (from the vdc creator)
+insert into roleassignment (	assigneeidentifier, definitionpoint_id, role_id)
+	select 			'@'|| useridentifier, vdc_id, dr.id
+	from _dvn3_vdc, authenticateduser, dataverserole dr
+	where vdc.creator_id = authenticateduser.id
+	and dr.alias='admin';
+
 -- admin
 insert into roleassignment (	assigneeidentifier, definitionpoint_id, role_id)
 	select 			'@'|| useridentifier, vdc_id, dr.id
@@ -35,7 +35,6 @@ insert into roleassignment (	assigneeidentifier, definitionpoint_id, role_id)
 	and role_id=4 and dr.alias='member';
 
 -- groups (as members)
--- member
 insert into roleassignment (	assigneeidentifier, definitionpoint_id, role_id)
 	select 			'&'|| groupalias, vdcs_id, dr.id
 	from _dvn3_vdc_usergroup, explicitgroup, dataverserole dr
@@ -46,21 +45,30 @@ insert into roleassignment (	assigneeidentifier, definitionpoint_id, role_id)
 -- dataset role assignments
 -----------------------
 
+-- contributor (from the study creator)
+insert into roleassignment (	assigneeidentifier, definitionpoint_id, role_id)
+	select 			'@'|| useridentifier, ds.id, dr.id
+	from _dvn3_study s, authenticateduser, dataverserole dr, datset ds
+	where s.creator_id = authenticateduser.id
+        and ds.authority = s.authority
+        and ds.protocol = s.protocol
+        and ds.identifier = s.studyid 
+	and dr.alias='editor';
+
 -- member
 insert into roleassignment (	assigneeidentifier, definitionpoint_id, role_id)
 	select 			'@'|| useridentifier, ds.id, dr.id
-	from _dvn3_study_vdcuser, authenticateduser, dataverserole dr, datset ds
+	from _dvn3_study_vdcuser, _dvn3_study s, authenticateduser, dataverserole dr, datset ds
 	where _dvn3_study_vdcuser.allowedusers_id = authenticateduser.id
         and ds.authority = s.authority
         and ds.protocol = s.protocol
         and ds.identifier = s.studyid 
-	and role_id=4 and dr.alias='member';
+	and dr.alias='member';
 
 -- groups (as members)
--- member
 insert into roleassignment (	assigneeidentifier, definitionpoint_id, role_id)
 	select 			'&'|| groupalias, ds.id, dr.id
-	from _dvn3_study_usergroup, explicitgroup, dataverserole dr, dataset ds
+	from _dvn3_study_usergroup, _dvn3_study s, explicitgroup, dataverserole dr, dataset ds
 	where _dvn3_study_usergroup.allowedgroups_id = explicitgroup.id
         and ds.authority = s.authority
         and ds.protocol = s.protocol
@@ -82,43 +90,5 @@ insert into roleassignment (	assigneeidentifier, definitionpoint_id, role_id)
 	from _dvn3_studyfile_usergroup, explicitgroup, dataverserole dr
 	where _dvn3_studyfile_usergroup.allowedgroups_id = explicitgroup.id
 	and dr.alias='fileDownloader';
-
-
-
-
--- links (MOVE)
-
--- links to datasets
-insert into datasetlinkingdataverse (linkingdataverse_id, dataset_id, linkcreatetime)
-select c.owner_id, ds.id, now()
-from _dvn3_coll_studies link, _dvn3_vdccollection c, _dvn3_study s, dataset ds
-where link.vdc_collection_id=c.id
-and link.study_id=s.id
-and s.owner_id != c.owner_id --don't include if already part of this dataverse
-and ds.authority = s.authority
-and ds.protocol = s.protocol
-and ds.identifier = s.studyid;
-
-
--- links to root collections (now linked to dataverses)
-insert into dataverselinkingdataverse (linkingdataverse_id, dataverse_id, linkcreatetime)
-select vdc_id, owner_id, now()
-from _dvn3_vdc_linked_collections link, _dvn3_vdccollection c
-where link.linked_collection_id=c.id
-and c.parentcollection_id is null;
-
--- links to other, static collections (now linked to just the studies from them)
-insert into datasetlinkingdataverse (linkingdataverse_id, dataset_id, linkcreatetime)
-select vdc_id, ds.id, now()
-from _dvn3_vdc_linked_collections link, _dvn3_coll_studies contents, _dvn3_vdccollection c, _dvn3_study s, dataset ds
-where dv.linked_collection_id=c.id
-and c.parentcollection_id is not null
-and c.type='static'
-and c.id = contents.vdc_collection_id
-and contents.study_id=s.id
-and s.owner_id != vdc_id -- don't include if already part of this dataverse
-and ds.authority = s.authority
-and ds.protocol = s.protocol
-and ds.identifier = s.studyid;
 
 
