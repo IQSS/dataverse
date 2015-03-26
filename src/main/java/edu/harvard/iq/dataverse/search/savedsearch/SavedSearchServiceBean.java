8bb8d019c4cf32d61fa3bf88e742fb09a08e409a
Philip Durbin <philip_durbin@harvard.edu>
2015-03-26 16:52:24 -0400

    Saved Search: first cut at Entity classes #90

---
+++ src/main/java/edu/harvard/iq/dataverse/search/savedsearch/SavedSearchServiceBean.java
@@ -0,0 +1,26 @@
+package edu.harvard.iq.dataverse.search.savedsearch;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+@Stateless
+@Named
+public class SavedSearchServiceBean {
+
+    @PersistenceContext(unitName = "VDCNet-ejbPU")
+    private EntityManager em;
+
+    public List<SavedSearch> findAll() {
+        TypedQuery<SavedSearch> typedQuery = em.createQuery("SELECT OBJECT(o) FROM SavedSearch AS o", SavedSearch.class);
+        return typedQuery.getResultList();
+    }
+
+    public SavedSearch save() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
