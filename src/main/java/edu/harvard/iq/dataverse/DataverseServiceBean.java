dc2924d613f4c761acd4312a661539eabb83623d
Gustavo Durand <gdurand@iq.harvard.edu>
2015-04-02 15:32:56 -0400

    fixed typo

--- src/main/java/edu/harvard/iq/dataverse/DataverseServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/DataverseServiceBean.java
@@ -95,8 +95,8 @@ public class DataverseServiceBean implements java.io.Serializable {
             long saneNumPartitions = 1;
             numPartitions = saneNumPartitions;
         }
-        String skipClause = skipIndexed ? " AND o.indexTime is null " : "";
-        TypedQuery<Dataverse> typedQuery = em.createQuery("SELECT OBJECT(o) FROM Dataverse AS o WHERE MOD( o.id, :numPartitions) = :partitionId" +
+        String skipClause = skipIndexed ? "AND o.indexTime is null " : "";
+        TypedQuery<Dataverse> typedQuery = em.createQuery("SELECT OBJECT(o) FROM Dataverse AS o WHERE MOD( o.id, :numPartitions) = :partitionId " +
                 skipClause +
                 "ORDER BY o.id", Dataverse.class);
         typedQuery.setParameter("numPartitions", numPartitions);
