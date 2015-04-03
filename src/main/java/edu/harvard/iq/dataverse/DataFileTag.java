091f03318f0129ca3cded216a32b32aeb070e51f
Raman Prasad <raman_prasad@harvard.edu>
2015-04-03 17:54:37 -0400

    #1880: DataFileCategory.java and DataFileTag.java

--- src/main/java/edu/harvard/iq/dataverse/DataFileTag.java
+++ src/main/java/edu/harvard/iq/dataverse/DataFileTag.java
@@ -16,14 +16,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 /**
  *
  * @author Leonid Andreev
  */
 @Entity
+@Table(indexes = {@Index(columnList="datafile_id")})
 public class DataFileTag implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
