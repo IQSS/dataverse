8e536320e8eb120081ae40eb7d24e918ee385298
Raman Prasad <raman_prasad@harvard.edu>
2015-07-07 13:42:48 -0400

    minimize this page

--- src/main/java/edu/harvard/iq/dataverse/mydata/RolePermissionHelperPage.java
+++ src/main/java/edu/harvard/iq/dataverse/mydata/RolePermissionHelperPage.java
@@ -58,27 +58,10 @@ public class RolePermissionHelperPage implements java.io.Serializable {
     DataverseRoleServiceBean dataverseRoleService;
     @EJB
     RoleAssigneeServiceBean roleAssigneeService;
-    @EJB
-    DvObjectServiceBean dvObjectServiceBean;
-    @EJB
-    SearchServiceBean searchService;
     
-    private String testName = "blah";
+    
     private DataverseRolePermissionHelper rolePermissionHelper;// = new DataverseRolePermissionHelper();
-    private MyDataFinder myDataFinder;
-    private Pager pager;
-    private MyDataFilterParams filterParams;
-    private SolrQueryResponse solrQueryResponse;
     
-    private void msg(String s){
-        //System.out.println(s);
-    }
-    
-    private void msgt(String s){
-        msg("-------------------------------");
-        msg(s);
-        msg("-------------------------------");
-    }
     
     public String init() {
        // msgt("_YE_OLDE_QUERY_COUNTER_");  // for debug purposes
@@ -86,63 +69,29 @@ public class RolePermissionHelperPage implements java.io.Serializable {
         List<DataverseRole> roleList = dataverseRoleService.findAll();
         rolePermissionHelper = new DataverseRolePermissionHelper(roleList);
 
-        String userIdentifier = "dataverseAdmin";
         
         List<String> dtypes = MyDataFilterParams.defaultDvObjectTypes;
         //List<String> dtypes = Arrays.asList(DvObject.DATAFILE_DTYPE_STRING, DvObject.DATASET_DTYPE_STRING);
         //DvObject.DATAFILE_DTYPE_STRING, DvObject.DATASET_DTYPE_STRING, DvObject.DATAVERSE_DTYPE_STRING
         
         //List<String> dtypes = new ArrayList<>();
-        this.filterParams = new MyDataFilterParams(userIdentifier, dtypes, null, null, null);
         
-        this.myDataFinder = new MyDataFinder(rolePermissionHelper,
-                                        roleAssigneeService,
-                                        dvObjectServiceBean);
-        //myDataFinder.runFindDataSteps(userIdentifier);
-        this.myDataFinder.runFindDataSteps(filterParams);
-        
-        if (!this.myDataFinder.hasError()){
-
-            int paginationStart = 1;
-            boolean dataRelatedToMe = true;
-            int numResultsPerPage = 10;
-            msgt("getSolrFilterQueries: " + this.myDataFinder.getSolrFilterQueries().toString());
-            try {
-                solrQueryResponse = searchService.search(
-                        null, // no user
-                        null, // subtree, default it to Dataverse for now
-                        "*", //this.filterParams.getSearchTerm(),
-                        this.myDataFinder.getSolrFilterQueries(),//filterQueries,
-                        SearchFields.NAME_SORT, SortBy.ASCENDING,
-                        //SearchFields.RELEASE_OR_CREATE_DATE, SortBy.DESCENDING,
-                        paginationStart,
-                        dataRelatedToMe,
-                        10
-                );
-                msgt("getResultsStart: " + this.solrQueryResponse.getResultsStart());
-                msgt("getNumResultsFound: " + this.solrQueryResponse.getNumResultsFound());
-                msgt("getSolrSearchResults: " + this.solrQueryResponse.getSolrSearchResults().toString());
-                
-                //User user,
-                //Dataverse dataverse,
-                //String query, 
-                //List<String> filterQueries, String sortField, String sortOrder, int paginationStart, boolean onlyDatatRelatedToMe, int numResultsPerPage) throws SearchException {
-                
-            } catch (SearchException ex) {
-                solrQueryResponse = null;
-                Logger.getLogger(RolePermissionHelperPage.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            
-        }
-        
-        this.pager = new Pager(111, 10, 3);
-        return null;
+     return null;
     }
     
-    
-    
+        
     public DataverseRolePermissionHelper getRolePermissionHelper(){
         return this.rolePermissionHelper;
+    }
+    
+    private void msg(String s){
+        System.out.println(s);
+    }
+    
+    private void msgt(String s){
+        msg("-------------------------------");
+        msg(s);
+        msg("-------------------------------");
     }
     
 }
