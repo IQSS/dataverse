5eafee7c06571e3d5bceadef1a0813fa4c625a01
Raman Prasad <raman_prasad@harvard.edu>
2015-07-10 11:50:57 -0400

    Starting looking at account totals. restart after next week

--- src/main/java/edu/harvard/iq/dataverse/mydata/MyDataPage.java
+++ src/main/java/edu/harvard/iq/dataverse/mydata/MyDataPage.java
@@ -26,6 +26,7 @@ import static java.lang.Math.max;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Random;
 import java.util.Set;
@@ -35,6 +36,8 @@ import javax.ejb.EJB;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
 import org.apache.commons.lang.StringUtils;
 import org.primefaces.json.JSONException;
 import org.primefaces.json.JSONObject;
@@ -77,6 +80,10 @@ public class MyDataPage implements java.io.Serializable {
     private AuthenticatedUser authUser = null;
     private Boolean isSuperuserLoggedIn = null;
     
+    private long totalUserFileCount = (long) 0;
+    private long totalUserDataverseCount = (long) 0;
+    private long totalUserDatasetCount = (long) 0;
+    
     private void msg(String s){
         System.out.println(s);
     }
@@ -85,6 +92,26 @@ public class MyDataPage implements java.io.Serializable {
         msg("-------------------------------");
         msg(s);
         msg("-------------------------------");
+    }
+
+    public void setTotalUserDatasetCount(long cnt){
+        this.totalUserDatasetCount = cnt;
+    }    
+    public void setTotalUserDataverseCount(long cnt){
+        this.totalUserDataverseCount = cnt;
+    }    
+    public void setTotalUserFileCount(long cnt){
+        this.totalUserFileCount = cnt;
+    }    
+
+    public long getTotalUserDatasetCount(){
+        return this.totalUserDatasetCount;
+    }    
+    public long getTotalUserDataverseCount(){
+        return this.totalUserDataverseCount;
+    }
+    public long getTotalUserFileCount(){
+        return this.totalUserFileCount;
     }
     
     public boolean hasFilterParams(){
@@ -156,9 +183,53 @@ public class MyDataPage implements java.io.Serializable {
         //
         rolePermissionHelper = new DataverseRolePermissionHelper(getRolesUsedToCreateCheckboxes(authUser));
        
+        //this.setUserCountTotals(authUser, rolePermissionHelper);
         return null;
     }
+    /*
+    private void setUserCountTotals(AuthenticatedUser userForCounts, DataverseRolePermissionHelper rolePermissionHelper){
+        if (userForCounts == null){
+            throw new NullPointerException("userForCounts cannot be null");
+        }
+        if (rolePermissionHelper == null){
+            throw new NullPointerException("rolePermissionHelper cannot be null");
+        }
+          
+        MyDataFinder dataFinder = new MyDataFinder(rolePermissionHelper, this.roleAssigneeService, this.dvObjectServiceBean);
+        
+        DataRetrieverAPI dataRetriever = new DataRetrieverAPI();
+
+        Map<String, Long> countMap = dataRetriever.getTotalCountsFromSolrAsJavaMap(userForCounts, dataFinder);
+
+        if (countMap ==null){
+            logger.severe("MyDataPage.setUserCountTotals() jsonCountData should not be null!!!");
+            return;
+        }
     
+        // Set counts: this is alot of extra code...
+        //
+        Object cntVal = null;
+        if (countMap.containsKey(SolrQueryResponse.DATAVERSES_COUNT_KEY)){
+            cntVal = countMap.get(SolrQueryResponse.DATAVERSES_COUNT_KEY);
+            if (cntVal != null){
+                this.totalUserDataverseCount = (long)cntVal;
+            }
+        }
+        if (countMap.containsKey(SolrQueryResponse.DATASETS_COUNT_KEY)){
+            cntVal = countMap.get(SolrQueryResponse.DATAVERSES_COUNT_KEY);
+            if (cntVal != null){
+                this.totalUserDatasetCount =  (long)cntVal;
+            }
+        }
+        if (countMap.containsKey(SolrQueryResponse.FILES_COUNT_KEY)){
+            cntVal = countMap.get(SolrQueryResponse.FILES_COUNT_KEY);
+            if (cntVal != null){
+                this.totalUserFileCount = (long) cntVal;
+            }
+        }        
+  
+    }
+    */
     
     private List<DataverseRole> getRolesUsedToCreateCheckboxes(AuthenticatedUser authUser){
 
