c2546343834f5e09ff0a24faa9c31ca9d73b8341
Leonid Andreev <leonid@hmdc.harvard.edu>
2015-10-26 17:54:03 -0400

    Made the page cache the ShowFileLandingPage setting lookups.

--- src/main/java/edu/harvard/iq/dataverse/SettingsWrapper.java
+++ src/main/java/edu/harvard/iq/dataverse/SettingsWrapper.java
@@ -7,9 +7,12 @@ package edu.harvard.iq.dataverse;
 
 import edu.harvard.iq.dataverse.settings.Setting;
 import edu.harvard.iq.dataverse.settings.SettingsServiceBean;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeSet;
 import javax.ejb.EJB;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
@@ -26,20 +29,38 @@ public class SettingsWrapper implements java.io.Serializable {
     SettingsServiceBean settingService;
 
     private Map<String, String> settingsMap;
+    
+    /**
+     * Values that are considered as "true".
+     * @see #isTrue(java.lang.String, boolean) 
+     */
+    private static final Set<String> TRUE_VALUES = Collections.unmodifiableSet(
+            new TreeSet<>( Arrays.asList("1","yes", "true","allow")));
 
     public String get(String settingKey) {
         if (settingsMap == null) {
-            // initialize settings map
-            settingsMap = new HashMap<>();
-            for (Setting setting : settingService.listAll()) {
-                settingsMap.put(setting.getName(),setting.getContent());
-            }
+            initSettingsMap();
         }
-        
         
         return settingsMap.get(settingKey);
     }
+    
+    public boolean isTrueForKey(String settingKey, boolean safeDefaultIfKeyNotFound) {
+        if (settingsMap == null) {
+            initSettingsMap();
+        }
+        
+        String val = settingsMap.get(settingKey);;
+        return ( val==null ) ? safeDefaultIfKeyNotFound : TRUE_VALUES.contains(val.trim().toLowerCase() );
+    }
 
+    private void initSettingsMap() {
+        // initialize settings map
+        settingsMap = new HashMap<>();
+        for (Setting setting : settingService.listAll()) {
+            settingsMap.put(setting.getName(), setting.getContent());
+        }
+    }
 
 
 }
