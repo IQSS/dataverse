b24d390ec51b7a68dac362f4b78d5933fff7d378
Stephen Kraffmiller <skraffmiller@hmdc.harvard.edu>
2016-01-05 09:38:48 -0500

    Move insert script for default world map token type to Reference Data

--- scripts/database/reference_data.sql
+++ scripts/database/reference_data.sql
@@ -32,3 +32,16 @@ CREATE UNIQUE INDEX dataverse_alias_unique_idx on dataverse (LOWER(alias));
 --This unique index will prevent the multiple draft issue
 CREATE UNIQUE INDEX one_draft_version_per_dataset ON datasetversion
 (dataset_id) WHERE versionstate='DRAFT';
+
+
+INSERT INTO worldmapauth_tokentype
+(   name,
+    created,
+    contactemail, hostname, ipaddress, 
+    mapitlink, md5,
+    modified, timelimitminutes)
+    VALUES ( 'GEOCONNECT', current_timestamp, 
+        'support@dataverse.org',  'geoconnect.datascience.iq.harvard.edu', '140.247.115.127', 
+	'http://geoconnect.datascience.iq.harvard.edu/shapefile/map-it',
+	'38c0a931b2d582a5c43fc79405b30c22',
+            current_timestamp, 30);
--- scripts/database/upgrades/upgrade_v4.2.2_to_v4.2.3.sql
+++ scripts/database/upgrades/upgrade_v4.2.2_to_v4.2.3.sql
@@ -12,16 +12,6 @@ INSERT INTO setting(
             name, content)
     VALUES (':GeoconnectViewMaps', true);
 
-INSERT INTO worldmapauth_tokentype
-(   name,
-    created,
-    contactemail, hostname, ipaddress, 
-    mapitlink, md5,
-    modified, timelimitminutes)
-    VALUES ( 'GEOCONNECT-DEFAULT', current_timestamp, 
-        'support@dataverse.org',  'geoconnect.datascience.iq.harvard.edu', '140.247.115.127', 
-	'http://geoconnect.datascience.iq.harvard.edu/shapefile/map-it',
-	'38c0a931b2d582a5c43fc79405b30c22',
-            current_timestamp, 30);
+
 
 
