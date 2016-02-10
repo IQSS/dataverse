859f859c6f9664854d53bc2ae748d1808716a948
bionary <s.schlueter@uncc.edu>
2016-02-10 18:41:49 -0500

    More End of day commits after adding forgotten orphens.

---
+++ scripts/api/setup-getopts.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+invoked=$_
+OPTIND=1
+
+OPT_c="collection1"
+OPT_g="/home/glassfish/glassfish4"
+OPT_h="localhost"
+OPT_m="localhost"
+OPT_p="8983"
+OPT_s="localhost"
+OPT_u="http"
+
+while getopts :h:g:m FLAG; do
+  case $FLAG in
+    c)  #set option solr-collection "c"
+      OPT_c=$OPTARG
+      ;;
+    g)  #set option gfdir "g"
+      OPT_g=$OPTARG
+      ;;
+    h)  #set option hostname "h"
+      OPT_h=$OPTARG
+      ;;
+    m)  #set option mailserver "m"
+      OPT_m=$OPTARG
+      ;;
+    p)  #set option solr-port "p"
+      OPT_p=$OPTARG
+      ;;
+    s)  #set option solr-host "s"
+      OPT_s=$OPTARG
+      ;;
+    u)  #set option solr-url-schema "u"
+      OPT_u=$OPTARG
+      ;;
+    z)  #set option solrCloud-zookeeper "z"
+      OPT_z=$OPTARG
+      ;;
+    \?) #unknown option
+      echo "Unknown option: -$OPTARG" >&2
+      ;;
+    :)  #valid option requires adjacent argument
+      echo "Option -$OPTARG requires an argument." >&2
+      if[[ $invoked != $0 ]] then
+        return 1
+      else
+        exit 1
+      fi
+      ;;
+  esac
+done
