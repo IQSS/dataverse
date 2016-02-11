fc186d2fd4d8419369c271a1093837d925dbea36
bionary <s.schlueter@uncc.edu>
2016-02-10 22:51:26 -0500

    First take on adding solr initialization configurations

--- scripts/api/setup-solr.sh
+++ scripts/api/setup-solr.sh
@@ -3,7 +3,9 @@
 ## setup-getopts.sh provides a common set of shell parameter parsing OPT_* variables for dataverse/environment configuration
 SOURCE "./setup-getopts.sh"
 
-SOLR_HOST = 
-echo "Deleting all index data for Solr Collection $OPT_c at $SOLR_HOST"
-#curl http://localhost:8983/solr/update/json?commit=true -H "Content-type: application/json" -X POST -d "{\"delete\": { \"query\":\"*:*\"}}"
+
+
+SOLR_URI="${OPT_u}://${OPT_h}:${OPT_p}/solr/${OPT_c}"
+echo "Deleting all index data for Solr Collection $OPT_c at $SOLR_URI"
+curl ${SOLR_URI}/update?commit=true -H "Content-type: application/json" -X POST -d "{\"delete\": { \"query\":\"*:*\"}}"
 
