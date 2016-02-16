6fb0c6b1289048eadfb1a1bbfe9c0f86ef18fb38
bionary <s.schlueter@uncc.edu>
2016-02-16 14:49:58 -0500

    Added SolrHostColonPort dataverse server configuration

--- scripts/api/setup-solr.sh
+++ scripts/api/setup-solr.sh
@@ -5,16 +5,18 @@
 . "./bin/dataverse-getopts.sh"
 
 #### Parse output verbosity ####
-## *_CMD and _IF_TERSE, _IF_INFO, _IF_VERBOSE command variables are set in 
+## _IF_TERSE, _IF_INFO, _IF_VERBOSE command variables are set in 
 ## /dataverse/scripts/api/bin/util-set-verbosity.sh
 . "./bin/util-set-verbosity.sh"
 
 $_IF_TERSE echo "Configuring solr to index dataverse collection: ${OPT_c} ..."
 
+SOLR_URI="${OPT_u}://${OPT_s}:${OPT_p}/solr/${OPT_c}"
+DATAVERSE_SERVER="http://${OPT_h}:8080/api"
 
-SOLR_URI="${OPT_u}://${OPT_h}:${OPT_p}/solr/${OPT_c}"
+$_IF_VERBOSE echo "Deleting all index data for Solr Collection $OPT_c at $SOLR_URI"
+$_IF_VERBOSE curl ${SOLR_URI}/update?commit=true -H "Content-type: application/json" -X POST -d "{\"delete\": { \"query\":\"*:*\"}}"
 
-
-echo "Deleting all index data for Solr Collection $OPT_c at $SOLR_URI"
-#curl ${SOLR_URI}/update?commit=true -H "Content-type: application/json" -X POST -d "{\"delete\": { \"query\":\"*:*\"}}"
-
+$_IF_VERBOSE echo "Setting dataverse servers SolrHostColonPort property to ${OPT_s}:${OPT_p}"
+$_IF_VERBOSE curl -X PUT -d ${OPT_s}:${OPT_p} ${DATAVERSE_SERVER}/admin/settings/:SolrHostColonPort
+$_IF_VERBOSE echo ""
