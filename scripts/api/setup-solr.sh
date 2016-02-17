748a7e6b769dad3dc35a04801cf765768ab6369d
bionary <s.schlueter@uncc.edu>
2016-02-16 22:41:03 -0500

    renamed variable representing dataverse api entrypoint

--- scripts/api/setup-solr.sh
+++ scripts/api/setup-solr.sh
@@ -23,11 +23,11 @@ fi
 $_IF_TERSE echo "Configuring solr to index dataverse collection: ${OPT_c} ..."
 
 SOLR_URI="${OPT_u}://${OPT_s}:${OPT_p}/solr/${OPT_c}"
-DATAVERSE_SERVER="http://${OPT_h}:8080/api"
+DATAVERSE_URI="http://${OPT_h}:8080/api"
 
 $_IF_VERBOSE echo "Deleting all index data for Solr Collection $OPT_c at $SOLR_URI"
 $_IF_VERBOSE curl ${SOLR_URI}/update?commit=true -H "Content-type: application/json" -X POST -d "{\"delete\": { \"query\":\"*:*\"}}"
 
 $_IF_VERBOSE echo "Setting dataverse servers SolrHostColonPort property to ${OPT_s}:${OPT_p}"
-$_IF_VERBOSE curl -X PUT -d ${OPT_s}:${OPT_p} ${DATAVERSE_SERVER}/admin/settings/:SolrHostColonPort
+$_IF_VERBOSE curl -X PUT -d ${OPT_s}:${OPT_p} ${DATAVERSE_URI}/admin/settings/:SolrHostColonPort
 $_IF_VERBOSE echo ""
