57ed6f0157ddb0382b73d47bc48e5c2ac1a3b371
bionary <s.schlueter@uncc.edu>
2016-02-19 19:14:31 -0500

    cleanup default dataverse host

--- scripts/api/setup-solr.sh
+++ scripts/api/setup-solr.sh
@@ -11,14 +11,13 @@ if [[ -z ${OUTPUT_VERBOSITY} ]]; then OUTPUT_VERBOSITY='1'; fi
 
 $_IF_TERSE echo "Configuring solr to index dataverse collection: ${OPT_c} ..."
 
-SOLR_URI="${OPT_u}://${OPT_s}:${OPT_p}/solr/${OPT_c}"
 DATAVERSE_URI="http://${OPT_h}:8080/api"
-
-$_IF_VERBOSE echo "Deleting all index data for Solr Collection $OPT_c at $SOLR_URI"
-$_IF_VERBOSE curl ${SOLR_URI}/update?commit=true -H "Content-type: application/json" -X POST -d "{\"delete\": { \"query\":\"*:*\"}}"
 
 if [[ -z ${OPT_z} ]]; then 
   ## Undefined OPT_z mean this is a standard single-host (possibly remote) solr core setup
+  SOLR_URI="${OPT_u}://${OPT_s}:${OPT_p}/solr/${OPT_c}"
+  $_IF_VERBOSE echo "Deleting all index data for Solr Collection $OPT_c at $SOLR_URI"
+  $_IF_VERBOSE curl ${SOLR_URI}/update?commit=true -H "Content-type: application/json" -X POST -d "{\"delete\": { \"query\":\"*:*\"}}"
   $_IF_VERBOSE echo "Setting dataverse servers SolrHostColonPort property to ${OPT_s}:${OPT_p}"
   $_IF_VERBOSE curl -X PUT -d ${OPT_s}:${OPT_p} ${DATAVERSE_URI}/admin/settings/:SolrHostColonPort
 else
@@ -26,7 +25,7 @@ else
   $_IF_VERBOSE curl -X PUT -d '${OPT_z}' ${DATAVERSE_URI}/admin/settings/:SolrZookeeperEnsemble
   $_IF_VERBOSE echo "Setting dataverse servers useSolrCloudViaZookeeper property to 'yes'"
   $_IF_VERBOSE curl -X PUT -d 'yes' ${DATAVERSE_URI}/admin/settings/:useSolrCloudViaZookeeper
-end
+fi
 
 $_IF_VERBOSE echo "Setting dataverse servers SolrCollectionName property to '${OPT_c}'"
 $_IF_VERBOSE curl -X PUT -d "${OPT_c}" ${DATAVERSE_URI}/admin/settings/:SolrCollectionName
