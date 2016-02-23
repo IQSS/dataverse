601305327dacb8297a5d61af4dda47d71edaab5b
Michael Heppler <mheppler@hmdc.harvard.edu>
2016-02-23 13:39:14 -0500

    Fix some jQuery that was throwing errors in the console not working in all environments. [ref #1520]

--- doc/sphinx-guides/source/_static/navbarscroll.js
+++ doc/sphinx-guides/source/_static/navbarscroll.js
@@ -2,6 +2,7 @@
     Use to fix hidden section headers behind the navbar when using links with targets
     See: http://stackoverflow.com/questions/10732690/offsetting-an-html-anchor-to-adjust-for-fixed-header    
 */
+(function($) {
 $(function() {
   $('a[href*=#]:not([href=#])').on('click', function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
@@ -27,3 +28,4 @@ $(function() {
       }
     }
 });
+})(jQuery);
