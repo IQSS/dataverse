6550b5bea26a3e9ac48d7c11d852211afe95d04f
Stephen Kraffmiller <skraffmiller@hmdc.harvard.edu>
2016-04-12 16:14:42 -0400

    Add Guestbook name to response download

--- src/main/java/edu/harvard/iq/dataverse/GuestbookResponseServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/GuestbookResponseServiceBean.java
@@ -64,14 +64,15 @@ public class GuestbookResponseServiceBean {
     
     public List<Object[]> findArrayByDataverseId (Long dataverseId){
 
-        String queryString = "select  r.id, v.value, r.responsetime, r.downloadtype,  m.label, r.name, r.email, r.institution, r.position from guestbookresponse r,"
-                + " datasetfieldvalue v, filemetadata m, dvobject o  "
+        String queryString = "select r.id, g.name, v.value, r.responsetime, r.downloadtype,  m.label, r.name, r.email, r.institution, r.position from guestbookresponse r,"
+                + " datasetfieldvalue v, filemetadata m, dvobject o, guestbook g  "
                 + " where "  
                 + " v.datasetfield_id = (select id from datasetfield f where datasetfieldtype_id = 1 "
                 + " and datasetversion_id = (select max(id) from datasetversion where dataset_id =r.dataset_id )) "
                 + " and m.datasetversion_id = (select max(id) from datasetversion where dataset_id =r.dataset_id ) "
                 + "  and m.datafile_id = r.datafile_id "
                 + "  and r.dataset_id = o.id "
+                + "  and r.guestbook_id = g.id "
                 + " and  o.owner_id = "
                 + dataverseId.toString()
                 + ";";           
@@ -81,14 +82,15 @@ public class GuestbookResponseServiceBean {
     
     public List<Object[]> findArrayByDataverseIdAndGuestbookId (Long dataverseId, Long guestbookId){
 
-        String queryString = "select  r.id, v.value, r.responsetime, r.downloadtype,  m.label, r.name, r.email, r.institution, r.position from guestbookresponse r,"
-                + " datasetfieldvalue v, filemetadata m, dvobject o  "
+        String queryString = "select r.id, g.name, v.value, r.responsetime, r.downloadtype,  m.label, r.name, r.email, r.institution, r.position from guestbookresponse r,"
+                + " datasetfieldvalue v, filemetadata m, dvobject o, guestbook g  "
                 + " where "  
                 + " v.datasetfield_id = (select id from datasetfield f where datasetfieldtype_id = 1 "
                 + " and datasetversion_id = (select max(id) from datasetversion where dataset_id =r.dataset_id )) "
                 + " and m.datasetversion_id = (select max(id) from datasetversion where dataset_id =r.dataset_id ) "
                 + "  and m.datafile_id = r.datafile_id "
                 + "  and r.dataset_id = o.id "
+                + "  and r.guestbook_id = g.id "
                 + " and  o.owner_id = "
                 + dataverseId.toString()
                 + " and r.guestbook_id = "
@@ -109,22 +111,23 @@ public class GuestbookResponseServiceBean {
         List<Object[]> guestbookResults = em.createNativeQuery(queryString).getResultList();
         
         for (Object[] result : guestbookResults) {
-            Object[] singleResult = new Object[9];
+            Object[] singleResult = new Object[10];
             singleResult[0] = result[1];
-            if (result[2] != null){
-                singleResult[1] = new SimpleDateFormat("MM/d/yyyy").format((Date) result[2]); 
+            singleResult[1] = result[2];
+            if (result[3] != null){
+                singleResult[2] = new SimpleDateFormat("MM/d/yyyy").format((Date) result[3]); 
             } else {
-                singleResult[1] = "N/A";
+                singleResult[2] = "N/A";
             }
 
-            singleResult[2] = result[3];
             singleResult[3] = result[4];
             singleResult[4] = result[5];
             singleResult[5] = result[6];
             singleResult[6] = result[7];
             singleResult[7] = result[8];
+            singleResult[8] = result[9];
             String cqString = "select q.questionstring, r.response  from customquestionresponse r, customquestion q where q.id = r.customquestion_id and r.guestbookResponse_id = " + (Integer) result[0];
-                singleResult[8]  = em.createNativeQuery(cqString).getResultList();
+                singleResult[9]  = em.createNativeQuery(cqString).getResultList();
             
             /*
             List<CustomQuestionResponse> customResponses = em.createQuery("select o from CustomQuestionResponse as  o where o.guestbookResponse.id = " + (Integer) result[0] + " order by o.customQuestion.id ", CustomQuestionResponse.class).getResultList();
--- src/main/java/edu/harvard/iq/dataverse/GuestbookResponsesPage.java
+++ src/main/java/edu/harvard/iq/dataverse/GuestbookResponsesPage.java
@@ -90,7 +90,7 @@ public class GuestbookResponsesPage implements java.io.Serializable {
     private String convertResponsesToTabDelimited(List<Object[]> guestbookResponses) {
 
         StringBuilder sb = new StringBuilder();
-        sb.append("Dataset, Date, Type, File Name, User Name, Email, Institution, Position, Custom Questions");
+        sb.append("Guestbook, Dataset, Date, Type, File Name, User Name, Email, Institution, Position, Custom Questions");
         sb.append(END_OF_LINE);
         for (Object[] array : guestbookResponses) {
             sb.append(array[0]);
@@ -108,8 +108,10 @@ public class GuestbookResponsesPage implements java.io.Serializable {
             sb.append(array[6] == null ? "" : array[6]);
             sb.append(SEPARATOR);
             sb.append(array[7] == null ? "" : array[7]);
-            if(array[8] != null){
-                List <Object[]> responses = (List<Object[]>) array[8];               
+            sb.append(SEPARATOR);
+            sb.append(array[8] == null ? "" : array[8]);
+            if(array[9] != null){
+                List <Object[]> responses = (List<Object[]>) array[9];               
                 for (Object[] response: responses){
                     sb.append(SEPARATOR);
                     sb.append(response[0]);
