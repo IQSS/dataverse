ef20ff0e4d1dd2f1fce3d2731e99318edb26f83d
Michael Heppler <mheppler@hmdc.harvard.edu>
2016-04-27 12:42:52 -0400

    Added new JS file to external widget code that will process persistentId's and dataverse aliases in the parent window URL and update the widget view. Removed old references to widget=iframe. [ref #2499]

--- src/main/java/edu/harvard/iq/dataverse/WidgetWrapper.java
+++ src/main/java/edu/harvard/iq/dataverse/WidgetWrapper.java
@@ -23,8 +23,8 @@ public class WidgetWrapper implements java.io.Serializable {
 
     public boolean isWidgetView() {
         if (widgetView == null) {
-            String widgetParam = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("widget");
-            widgetView = widgetParam != null && widgetParam.equals("iframe");
+            String widgetParam = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("widgetScope");
+            widgetView = widgetParam != null;
         }
         return widgetView;
     }
@@ -45,7 +45,7 @@ public class WidgetWrapper implements java.io.Serializable {
 
     
     public String wrapURL(String URL) {
-        return URL + (isWidgetView() ? getParamSeparator(URL) + "widget=iframe&widgetScope=" + getWidgetScope() : "");
+        return URL + (isWidgetView() ? getParamSeparator(URL) + "widgetScope=" + getWidgetScope() : "");
     }
     
     private String getParamSeparator(String URL) {
