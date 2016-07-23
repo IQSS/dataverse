32893286261fdda8bb9ec5170451f3c8d98f6a79
Leonid Andreev <leonid@hmdc.harvard.edu>
2016-07-22 21:22:02 -0400

    added export/harvest related database updates to the release update sql file

--- scripts/database/upgrades/upgrade_v4.4_to_v4.5.sql
+++ scripts/database/upgrades/upgrade_v4.4_to_v4.5.sql
@@ -1,2 +1,6 @@
 -- A Private URL is a specialized role assignment with a token.
 ALTER TABLE roleassignment ADD COLUMN privateurltoken character varying(255);
+-- "Last Export Time" added to the dataset: 
+ALTER TABLE dataset ADD COLUMN lastExportTime TIMESTAMP;
+-- Direct link to the harvesting configuration, for harvested datasets:
+ALTER TABLE dataset ADD COLUMN harvestingClient_id bigint;
