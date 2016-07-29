03e4b6a11170bb315b3feaade57cc742b1935c03
Philip Durbin <philip_durbin@harvard.edu>
2016-07-29 13:57:21 -0400

    Confirm Email: prevent multiple tokens for a user #2170

--- src/main/java/edu/harvard/iq/dataverse/confirmemail/ConfirmEmailData.java
+++ src/main/java/edu/harvard/iq/dataverse/confirmemail/ConfirmEmailData.java
@@ -43,7 +43,7 @@ public class ConfirmEmailData implements Serializable {
     private String token;
 
     @OneToOne
-    @JoinColumn(nullable = false)
+    @JoinColumn(nullable = false, unique = true)
     private AuthenticatedUser authenticatedUser;
 
     @Column(nullable = false)
