5d4920d8515f6fee98f53e8297cf9e17bd28c1c9
Leonid Andreev <leonid@hmdc.harvard.edu>
2016-08-17 18:06:34 -0400

    A fixed for the cached dataverse name (I think)

--- src/main/java/edu/harvard/iq/dataverse/search/IndexServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/search/IndexServiceBean.java
@@ -171,7 +171,7 @@ public class IndexServiceBean {
             solrInputDocument.addField(SearchFields.SOURCE, HARVESTED);
         } else { (this means that all dataverses are "local" - should this be removed? */
             solrInputDocument.addField(SearchFields.IS_HARVESTED, false);
-            solrInputDocument.addField(SearchFields.METADATA_SOURCE, rootDataverseName);
+            solrInputDocument.addField(SearchFields.METADATA_SOURCE, findRootDataverseCached().getName()); //rootDataverseName);
         /*}*/
 
         addDataverseReleaseDateToSolrDoc(solrInputDocument, dataverse);
@@ -679,7 +679,7 @@ public class IndexServiceBean {
             solrInputDocument.addField(SearchFields.METADATA_SOURCE, HARVESTED);
         } else {
             solrInputDocument.addField(SearchFields.IS_HARVESTED, false);
-            solrInputDocument.addField(SearchFields.METADATA_SOURCE, rootDataverseName);
+            solrInputDocument.addField(SearchFields.METADATA_SOURCE, findRootDataverseCached().getName()); //rootDataverseName);
         }
 
         DatasetVersion datasetVersion = indexableDataset.getDatasetVersion();
