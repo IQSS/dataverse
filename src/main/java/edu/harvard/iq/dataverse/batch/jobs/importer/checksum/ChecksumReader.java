5fcc9c060218c59015d9ed533e81d82dc1599125
bmckinney <mckinney.sw@gmail.com>
2016-11-16 11:26:17 -0500

    minor refactor

--- src/main/java/edu/harvard/iq/dataverse/batch/jobs/importer/checksum/ChecksumReader.java
+++ src/main/java/edu/harvard/iq/dataverse/batch/jobs/importer/checksum/ChecksumReader.java
@@ -59,6 +59,8 @@ public class ChecksumReader extends AbstractItemReader {
     private String persistentUserData = "";
 
     File directory;
+    File manifest;
+    
 
     ArrayList<ChecksumRecord> records = new ArrayList<>();
 
@@ -92,8 +94,7 @@ public class ChecksumReader extends AbstractItemReader {
         if (preflight()) {
             StreamFactory factory = StreamFactory.newInstance();
             factory.loadResource("mapping.xml");
-            BeanReader in = factory.createReader("checksumRecord",
-                    new File(this.directory.getAbsolutePath() + FILE_SEPARATOR + checksumManifest));
+            BeanReader in = factory.createReader("checksumRecord", manifest);
             ChecksumRecord record;
             while ((record = (ChecksumRecord) in.read()) != null) {
                 record.setType(checksumType); // set algorithm, in anticipation of multi-algorithm support in dataverse
@@ -143,7 +144,7 @@ public class ChecksumReader extends AbstractItemReader {
     public boolean preflight() {
         String preflightMessage;
         // make sure the checksum manifest exists
-        File manifest = new File(this.directory.getAbsolutePath() + FILE_SEPARATOR + checksumManifest);
+        manifest = new File(this.directory.getAbsolutePath() + FILE_SEPARATOR + checksumManifest);
         if (!manifest.exists()) {
             this.preflight = false;
             preflightMessage = "The checksum manifest cannot be found: " + manifest.getAbsolutePath();
