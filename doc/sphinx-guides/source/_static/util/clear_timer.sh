bbb67bea29e985dadf902b74724a632923c437e3
Pete Meyer <pameyer@crystal.harvard.edu>
2017-03-06 16:58:04 -0500

    don't assume glassfish is stopped

--- doc/sphinx-guides/source/_static/util/clear_timer.sh
+++ doc/sphinx-guides/source/_static/util/clear_timer.sh
@@ -2,9 +2,11 @@
 
 # EBJ timers sometimes cause problems; utility to clear generated directories and database rows
 
-# assumes that glassfish is not running, and 
-# also assumes this script is being run as root, and that the postgres user had passwordless 
+# assumes this script is being run as root, and that the postgres user had passwordless 
 # access to the database (local sockets, or appropriate environmental variables).
+
+# will restart glassfish if it's stopped; comment out the `start-domain` command at the end
+# if you'd like to avoid that.
 
 # directory where glassfish is installed
 GLASSFISH_DIR=/usr/local/glassfish4
@@ -18,9 +20,14 @@ DV_DB=dvndb
 # OS user for the database
 DB_USER=postgres
 
+# stop the glassfish domain (generates a warning if glassfish is stopped)
+${GLASSFISH_DIR}/bin/asadmin stop-domain
+
 rm -rf ${GLASSFISH_DIR}/${DV_DIR}/generated/
 rm -rf ${GLASSFISH_DIR}/${DV_DIR}/osgi-cache/felix
 
 sudo -u ${DB_USER} psql ${DV_DB} -c 'delete from "EJB__TIMER__TBL"';
 
+# restart the domain (also generates a warning if glassfish is stopped)
+${GLASSFISH_DIR}/bin/asadmin start-domain
 
