61795c357127a6e94c4e9ebaedb104a803c9335c
Philip Durbin <philip_durbin@harvard.edu>
2017-03-21 11:54:23 -0400

    improvements to Vagrant TwoRavens script #2212

--- scripts/vagrant/install-tworavens.sh
+++ scripts/vagrant/install-tworavens.sh
@@ -4,23 +4,32 @@ exit 1
 cd /root
 yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
 yum install -y R R-devel
+# FIXME: /dataverse is mounted in Vagrant but not other places
 yum install -y /dataverse/doc/sphinx-guides/source/_static/installation/files/home/rpmbuild/rpmbuild/RPMS/x86_64/rapache-1.2.6-rpm0.x86_64.rpm
 yum install -y gcc-gfortran # to build R packages
-COMMIT=9b4430af6fb4dee4f7061c72b37c8e48ebff0ad2
+COMMIT=a6869eb28693d6df529e7cb3888c40de5f302b66
+UNZIPPED=TwoRavens-$COMMIT
 if [ ! -f $COMMIT ]; then
-  wget https://github.com/IQSS/TwoRavens/archive/9b4430af6fb4dee4f7061c72b37c8e48ebff0ad2.zip
-  unzip 9b4430af6fb4dee4f7061c72b37c8e48ebff0ad2
-  cd TwoRavens-9b4430af6fb4dee4f7061c72b37c8e48ebff0ad2/r-setup
-  # FIXME: This commit is no good because there's an "exit 0" in r-setup.sh. For now, edit the script by hand and remove it.
-  #./r-setup.sh # This is expected to take a while. Look for lines like "Package Zelig successfully installed" and "Successfully installed Dataverse R framework".
+  wget https://github.com/IQSS/TwoRavens/archive/$COMMIT.zip
+  unzip $COMMIT
+  cd $UNZIPPED/r-setup
+  ./r-setup.sh # This is expected to take a while. Look for lines like "Package Zelig successfully installed" and "Successfully installed Dataverse R framework".
 fi
+# FIXME: copy preprocess.R into Glassfish while running and overwrite it
 curl -X PUT -d true http://localhost:8080/api/admin/settings/:TwoRavensTabularView
-# Port 8888 because we're running in Vagrant. This gets a little weird. Had to mess with the app_ddi.js file...
+# Port 8888 because we're running in Vagrant. On the dev1 server we use https://dev1.dataverse.org/dataexplore/gui.html
 curl -X PUT -d http://localhost:8888/dataexplore/gui.html http://localhost:8080/api/admin/settings/:TwoRavensUrl
 cd /root
 DIR=/var/www/html/dataexplore
 if [ ! -d $DIR ]; then
-  cp -r  TwoRavens-9b4430af6fb4dee4f7061c72b37c8e48ebff0ad2 $DIR
+  cp -r $UNZIPPED $DIR
 fi
 cd $DIR
+# The plan is to remove this hack of dropping preprocess.R into a deployed war file directory. See https://github.com/IQSS/dataverse/issues/3372
+# FIXME: don't assume version 4.6.1
+#diff /var/www/html/dataexplore/rook/preprocess/preprocess.R /usr/local/glassfish4/glassfish/domains/domain1/applications/dataverse-4.6.1/WEB-INF/classes/edu/harvard/iq/dataverse/rserve/scripts/preprocess.R
+# FIXME: If `diff` shows a difference, which is likely, copy the version from TwoRavens to the Glassfish directory.
+#cp /var/www/html/dataexplore/rook/preprocess/preprocess.R /usr/local/glassfish4/glassfish/domains/domain1/applications/dataverse-4.6.1/WEB-INF/classes/edu/harvard/iq/dataverse/rserve/scripts/preprocess.R
+# FIXME: restart Glassfish if you had to update preprocess.R above.
+# FIXME: Vagrant with it's weird 8888 port forwarding isn't working. On the dev1 server, TwoRavens works fine if you supply "https://dev1.dataverse.org" for both URLs.
 echo "Next, run ./install.pl after you cd to $DIR"
