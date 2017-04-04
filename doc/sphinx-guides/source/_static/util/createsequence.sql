77395ea948e2f38177cdd24e2881d3fa870d39c2
Pete Meyer <pameyer@crystal.harvard.edu>
2017-04-04 13:16:53 -0400

    split postgresql procedural language into separate example; update config doc

--- doc/sphinx-guides/source/_static/util/createsequence.sql
+++ doc/sphinx-guides/source/_static/util/createsequence.sql
@@ -19,27 +19,6 @@ CACHE 1;
 
 ALTER TABLE datasetidentifier_seq OWNER TO "dvnapp";
 
--- handle absence of CREATE OR REPLACE LANGUAGE for postgresql 8.4 or older
--- courtesy of the postgres wiki: https://wiki.postgresql.org/wiki/CREATE_OR_REPLACE_LANGUAGE
-CREATE OR REPLACE FUNCTION make_plpgsql()
-RETURNS VOID
-LANGUAGE SQL
-AS $$
-CREATE LANGUAGE plpgsql;
-$$;
- 
-SELECT
-    CASE
-    WHEN EXISTS(
-        SELECT 1
-        FROM pg_catalog.pg_language
-        WHERE lanname='plpgsql'
-    )
-    THEN NULL
-    ELSE make_plpgsql() END;
- 
-DROP FUNCTION make_plpgsql();
-
 -- And now create a PostgresQL FUNCTION, for JPA to 
 -- access as a NamedStoredProcedure:
 
