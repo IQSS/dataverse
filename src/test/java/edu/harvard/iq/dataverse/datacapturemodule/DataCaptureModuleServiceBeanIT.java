05c09a38c6aa86d36b9a2c44656f94b6330daf8e
Philip Durbin <philip_durbin@harvard.edu>
2017-05-19 14:52:11 -0400

    got java.net.HttpURLConnection version working

--- src/test/java/edu/harvard/iq/dataverse/datacapturemodule/DataCaptureModuleServiceBeanIT.java
+++ src/test/java/edu/harvard/iq/dataverse/datacapturemodule/DataCaptureModuleServiceBeanIT.java
@@ -6,11 +6,24 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import org.junit.Test;
 
 public class DataCaptureModuleServiceBeanIT {
+
+    private static final Logger logger = Logger.getLogger(DataCaptureModuleServiceBeanIT.class.getCanonicalName());
 
     @Test
     public void testUrDotPy() {
@@ -67,12 +80,39 @@ public class DataCaptureModuleServiceBeanIT {
     }
 
     @Test
-    public void testSettingRestAssured() {
+    public void testRestAssured() {
         String datasetId = "3813";
         String bodyString = "datasetIdentifier=" + datasetId;
         Response response = given().body(bodyString).when().post("http://localhost:8888/sr.py");
         response.prettyPrint();
         System.out.println("status: " + response.getStatusCode());
+    }
+
+    @Test
+    public void testHttpURL2() throws MalformedURLException, ProtocolException, IOException {
+        System.out.println("BEGIN");
+        String datasetId = "3813";
+        String bodyString = "datasetIdentifier=" + datasetId;
+        byte[] postData = bodyString.getBytes(StandardCharsets.UTF_8);
+        int postDataLength = postData.length;
+        String request = "http://localhost:8888/sr.py";
+        URL url = new URL(request);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setDoOutput(true);
+        conn.setInstanceFollowRedirects(false);
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        conn.setRequestProperty("charset", "utf-8");
+        conn.setRequestProperty("Content-Length", Integer.toString(postDataLength));
+        conn.setUseCaches(false);
+        try (DataOutputStream wr = new DataOutputStream(conn.getOutputStream())) {
+            wr.write(postData);
+        }
+        InputStreamReader inputStreamReader = new InputStreamReader((InputStream) conn.getContent());
+        String result = new BufferedReader(inputStreamReader)
+                .lines().collect(Collectors.joining("\n"));
+        System.out.println("result: " + result);
+        System.out.println("END");
     }
 
 }
