e753d73b8f917ce01f5d44d27446cc06842f3c34
Philip Durbin <philip_durbin@harvard.edu>
2017-05-19 14:16:38 -0400

    more low level tests

--- src/test/java/edu/harvard/iq/dataverse/datacapturemodule/DataCaptureModuleServiceBeanIT.java
+++ src/test/java/edu/harvard/iq/dataverse/datacapturemodule/DataCaptureModuleServiceBeanIT.java
@@ -1,5 +1,7 @@
 package edu.harvard.iq.dataverse.datacapturemodule;
 
+import static com.jayway.restassured.RestAssured.given;
+import com.jayway.restassured.response.Response;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
@@ -29,7 +31,7 @@ public class DataCaptureModuleServiceBeanIT {
 
     @Test
     public void testSrDotPy() {
-        String datasetId = "3856";
+        String datasetId = "3813";
         String bodyString = "datasetIdentifier=" + datasetId;
 //        JsonNode jsonNode = new JsonNode(jsonString);
         try {
@@ -38,10 +40,39 @@ public class DataCaptureModuleServiceBeanIT {
                     .body(bodyString)
                     //                    .asJson();
                     .asString();
-            System.out.println("out: " + uploadRequest.getBody());
+            System.out.println("status: " + uploadRequest.getStatus());
+            System.out.println("out:\n" + uploadRequest.getBody());
         } catch (UnirestException ex) {
             Logger.getLogger(DataCaptureModuleServiceBeanIT.class.getName()).log(Level.SEVERE, null, ex);
         }
 
     }
+
+    @Test
+    public void testSettingUnirest() {
+        String datasetId = "3813";
+        String bodyString = "datasetIdentifier=" + datasetId;
+//        JsonNode jsonNode = new JsonNode(jsonString);
+        try {
+            HttpResponse<String> uploadRequest = Unirest.put("http://localhost:8080/api/admin/settings")
+                    //                    .body(jsonString)
+                    .body(bodyString)
+                    //                    .asJson();
+                    .asString();
+            System.out.println("status: " + uploadRequest.getStatus());
+            System.out.println("out:\n" + uploadRequest.getBody());
+        } catch (UnirestException ex) {
+            Logger.getLogger(DataCaptureModuleServiceBeanIT.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Test
+    public void testSettingRestAssured() {
+        String datasetId = "3813";
+        String bodyString = "datasetIdentifier=" + datasetId;
+        Response response = given().body(bodyString).when().post("http://localhost:8888/sr.py");
+        response.prettyPrint();
+        System.out.println("status: " + response.getStatusCode());
+    }
+
 }
