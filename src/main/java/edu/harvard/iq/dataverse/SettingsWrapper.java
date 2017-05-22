19246bc3d08d189e9c0c453d97b56b1d0105ae82
Raman Prasad <raman_prasad@harvard.edu>
2017-05-22 15:44:21 -0400

    example of using file for handlebars template. #3637

--- src/main/java/edu/harvard/iq/dataverse/SettingsWrapper.java
+++ src/main/java/edu/harvard/iq/dataverse/SettingsWrapper.java
@@ -7,20 +7,39 @@ package edu.harvard.iq.dataverse;
 
 import com.github.jknack.handlebars.Context;
 import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
+import com.github.jknack.handlebars.io.FileTemplateLoader;
+import com.github.jknack.handlebars.io.TemplateLoader;
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import edu.harvard.iq.dataverse.settings.Setting;
 import edu.harvard.iq.dataverse.settings.SettingsServiceBean;
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import static java.security.AccessController.getContext;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import javax.servlet.ServletContext;
 
 /**
  *
@@ -34,6 +53,10 @@ public class SettingsWrapper implements java.io.Serializable {
     SettingsServiceBean settingService;
 
     private Map<String, String> settingsMap;
+    
+    private static final Logger logger = Logger.getLogger(SettingsWrapper.class.getCanonicalName());
+
+  
     
     /**
      * Values that are considered as "true".
@@ -87,35 +110,86 @@ public class SettingsWrapper implements java.io.Serializable {
         }
         return guidesBaseUrl;
     }
+    
+ 
+    
+     private static String readLocalResource(String path) {
+        
+        logger.fine(String.format("Data Frame Service: readLocalResource: reading local path \"%s\"", path));
 
+        // Get stream
+        InputStream resourceStream = SettingsWrapper.class.getResourceAsStream(path);
+        String resourceAsString = "";
+
+        // Try opening a buffered reader stream
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(resourceStream, "UTF-8"));
+
+            String line = null;
+            while ((line = rd.readLine()) != null) {
+                resourceAsString = resourceAsString.concat(line + "\n");
+            }
+            resourceStream.close();
+        } catch (IOException ex) {
+            logger.warning(String.format("SettingsWrapper: (readLocalResource) resource stream from path \"%s\" was invalid", path));
+        }
+
+        // Return string
+        return resourceAsString;
+    }
     
     public String getMustacheMessage() throws IOException{
-        
-        //String data = "{\"subject\": \"World\", \"link\": \"http://www.dataverse.org\"}";
+
+        // -----------------------------
+        // Load the Data  from the database
+        // -----------------------------
         String data = this.get(":GuidesData");
+
+        logger.info("getMustacheMessage data loaded: " + data);
+        // -----------------------------
+        // Load the Template from the database       
+        // -----------------------------
         String templateText = this.get(":GuidesTemplate");//"Go to <a href=\"{{guides}}\">{{guides}}</a>!";
         //String templateText = "Go to <a href=\"{{link}}\">{{subject}}</a>!";
+        logger.info("getMustacheMessage db template: " + templateText);
         
-        Handlebars handlebars = new Handlebars();
+        // Load from a file
+        templateText = readLocalResource("navbar/guides.html");
+                
+        
+        // -----------------------------
+        // Convert the JSON Data to a Map
+        // -----------------------------    
         Gson gson = new Gson();
-
         java.lang.reflect.Type type = new TypeToken<Map<String, Object>>(){}.getType();
         Map<String, Object> map = gson.fromJson(data, type);   
-        //logger.info("guide links: " + map.size());
 
+        // -----------------------------
+        // Render the template
+        // -----------------------------                
+        //templateText = readResourceFile();
+        logger.info("getMustacheMessage try file template: ??" + templateText );
+
+        // Inline compiliation
+        Handlebars handlebars = new Handlebars();
         com.github.jknack.handlebars.Template template = handlebars.compileInline(templateText);
-        Context context = Context.newBuilder(map).build();
-
-        //com.github.jknack.handlebars.Template template = handlebars.compileInline("<a href='javascript:alert(\"{{this}}\");'>{{this}}</a>");
-        //String resultString = template.apply("guides.org");
-
-        return template.apply(context); // + " (ok)";
+               
         
-        /*
-        {
+        Context context = Context.newBuilder(map).build();
+        return template.apply(context); 
+        
+    }
+    
+}
+
+/*
+--------------
+Example data
+--------------
+{
     "guide_links": [
         {
-            "name": "User Guide",
+            "name": "Ye User Guide",
             "link": "http://guides.dataverse.org/en/latest/user/"
         },
         {
@@ -127,26 +201,24 @@ public class SettingsWrapper implements java.io.Serializable {
             "link": "http://guides.dataverse.org/en/latest/installation/"
         },
         {
-            "name": "API Guide",
+            "name": "The Old API Guide",
             "link": "http://guides.dataverse.org/en/latest/api/"
         }
 
     ]
 }
-        
-        
-        
-        <ul class="people_list">
-  {{#if guide_links}}
+
+--------------
+Example template
+--------------
+<ul class="dropdown-menu">                            
+  {{#if guide_links}}  
       {{#each guide_links}}
         <li><a href="{{this.link}}" target="_blank">{{this.name}}</a></li>
       {{/each}}
   {{else}}
-        (no guides)
+        <li>(no guides)</li>
   {{/if}}
 </ul>
-        */
-    }
-    
-}
 
+*/
