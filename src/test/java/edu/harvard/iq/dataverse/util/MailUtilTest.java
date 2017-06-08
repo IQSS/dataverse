eec5a2e5a05ecb4210e80a12a33d84418a6642d8
Philip Durbin <philip_durbin@harvard.edu>
2017-06-08 18:10:35 -0400

    remove cases of "Dataverse Support" #3611 #2710

--- src/test/java/edu/harvard/iq/dataverse/util/MailUtilTest.java
+++ src/test/java/edu/harvard/iq/dataverse/util/MailUtilTest.java
@@ -18,16 +18,16 @@ public class MailUtilTest {
 
     @Test
     public void testParseSystemAddress() {
-        assertEquals("support@dataverse.org", MailUtil.parseSystemAddress("support@dataverse.org").getAddress());
-        assertEquals("support@dataverse.org", MailUtil.parseSystemAddress("Dataverse Support <support@dataverse.org>").getAddress());
-        assertEquals("Dataverse Support", MailUtil.parseSystemAddress("Dataverse Support <support@dataverse.org>").getPersonal());
-        assertEquals("support@dataverse.org", MailUtil.parseSystemAddress("\"Dataverse Support\" <support@dataverse.org>").getAddress());
-        assertEquals("Dataverse Support", MailUtil.parseSystemAddress("\"Dataverse Support\" <support@dataverse.org>").getPersonal());
+        assertEquals("support@librascholar.edu", MailUtil.parseSystemAddress("support@librascholar.edu").getAddress());
+        assertEquals("support@librascholar.edu", MailUtil.parseSystemAddress("LibraScholar Support Team <support@librascholar.edu>").getAddress());
+        assertEquals("LibraScholar Support Team", MailUtil.parseSystemAddress("LibraScholar Support Team <support@librascholar.edu>").getPersonal());
+        assertEquals("support@librascholar.edu", MailUtil.parseSystemAddress("\"LibraScholar Support Team\" <support@librascholar.edu>").getAddress());
+        assertEquals("LibraScholar Support Team", MailUtil.parseSystemAddress("\"LibraScholar Support Team\" <support@librascholar.edu>").getPersonal());
         assertEquals(null, MailUtil.parseSystemAddress(null));
         assertEquals(null, MailUtil.parseSystemAddress(""));
-        assertEquals(null, MailUtil.parseSystemAddress("Dataverse Support support@dataverse.org"));
-        assertEquals(null, MailUtil.parseSystemAddress("\"Dataverse Support <support@dataverse.org>"));
-        assertEquals(null, MailUtil.parseSystemAddress("support1@dataverse.org, support2@dataverse.org"));
+        assertEquals(null, MailUtil.parseSystemAddress("LibraScholar Support Team support@librascholar.edu"));
+        assertEquals(null, MailUtil.parseSystemAddress("\"LibraScholar Support Team <support@librascholar.edu>"));
+        assertEquals(null, MailUtil.parseSystemAddress("support1@dataverse.org, support@librascholar.edu"));
     }
 
     @Test
