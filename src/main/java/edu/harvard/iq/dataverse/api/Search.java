d1aed43116429223dc3bb56a0af49c3f153aa13f
Stephen Kraffmiller <skraffmiller@hmdc.harvard.edu>
2017-06-21 13:53:52 -0400

    #3900 remove unused import

--- src/main/java/edu/harvard/iq/dataverse/api/Search.java
+++ src/main/java/edu/harvard/iq/dataverse/api/Search.java
@@ -22,7 +22,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.json.Json;
