19e5454992a9be703c1e6612b457286bee70266d
oscardssmith <oscardssmith@gmail.com>
2017-06-26 11:07:56 -0400

    added date-time parsing to tests

--- src/test/java/edu/harvard/iq/dataverse/ingest/tabulardata/impl/plugins/csv/CSVFileReaderTest.java
+++ src/test/java/edu/harvard/iq/dataverse/ingest/tabulardata/impl/plugins/csv/CSVFileReaderTest.java
@@ -8,10 +8,8 @@ package edu.harvard.iq.dataverse.ingest.tabulardata.impl.plugins.csv;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -32,15 +30,15 @@ public class CSVFileReaderTest {
     @Test
     public void testRead() {
         String testFile = "src/test/java/edu/harvard/iq/dataverse/ingest/tabulardata/impl/plugins/csv/InjestCSV.csv";
-        String[] expResult = {"ints	Strings	Dates	Not quite Dates	Numbers	Not quite Ints	Not quite Numbers	Column that hates you and is so long that things might start breaking because we previously had a 255 character limit on length for things even when that might not be completely justified. Wow this is an increadibly long header name. Who made this? Oh, that's right, I did.",
-            "-199	\"hello\"	2017-06-20	\"2017/06/20\"	0.0	1	\"2\"	\"823478788778713\"",
-            "2	\"Sdfwer\"	2017-06-20	\"1100/06/20\"	Inf	2	\"NaN\"	\",1,2,3\"",
-            "0	\"cjlajfo.\"	2017-06-20	\"3000/06/20\"	-Inf	3	\"inf\"	\"\\casdf\"",
-            "-1	\"Mywer\"	2017-06-20	\"06-20-2011\"	3.141592653	4	\"4.8\"	\" \\\"  \"",
-            "266128	\"Sf\"	2017-06-20	\"06-20-1917\"	0	5	\"Inf+11\"	\"\"",
-            "0	\"werxc\"	2017-06-20	\"03/03/1817\"	123	6.000001	\"11-2\"	\"adf\\0\\na\\td",
+        String[] expResult = {"ints	Strings	Times	Not quite Times	Dates	Not quite Dates	Numbers	Not quite Ints	Not quite Numbers	Column that hates you and is so long that things might start breaking because we previously had a 255 character limit on length for things even when that might not be completely justified. Wow this is an increadibly long header name. Who made this? Oh, that's right, I did.",
+            "-199	\"hello\"	2013-04-08 13:14:23	2013-04-08 13:14:23	2017-06-20	\"2017/06/20\"	0.0	1	\"2\"	\"823478788778713\"",
+            "2	\"Sdfwer\"	2013-04-08 13:14:23	2013-04-08 13:14:23	2017-06-20	\"1100/06/20\"	Inf	2	\"NaN\"	\",1,2,3\"",
+            "0	\"cjlajfo.\"	2013-04-08 13:14:23	2013-04-08 13:14:23	2017-06-20	\"3000/06/20\"	-Inf	3	\"inf\"	\"\\casdf\"",
+            "-1	\"Mywer\"	2013-04-08 13:14:23	2013-04-08 13:14:23	2017-06-20	\"06-20-2011\"	3.141592653	4	\"4.8\"	\" \\\"  \"",
+            "266128	\"Sf\"	2013-04-08 13:14:23	2013-04-08 13:14:23	2017-06-20	\"06-20-1917\"	0	5	\"Inf+11\"	\"\"",
+            "0	\"werxc\"	2013-04-08 13:14:23	2013-04-08 13:14:23	2017-06-20	\"03/03/1817\"	123	6.000001	\"11-2\"	\"adf\\0\\na\\td",
             "sf\"",
-            "-2389	\"Dfjl\"	2017-06-20	\"2017-03-12\"	NaN	2	\"nap\"	\"💩⌛👩🏻■\""};
+            "-2389	\"Dfjl\"	2013-04-08 13:14:23	2013-04-08 13:14:72	2017-06-20	\"2017-03-12\"	NaN	2	\"nap\"	\"💩⌛👩🏻■\""};
         BufferedReader result = null;
         try (BufferedInputStream stream = new BufferedInputStream(
                 new FileInputStream(testFile))) {
@@ -81,7 +79,6 @@ public class CSVFileReaderTest {
             String expMessage = "Stream can't be null.";
             assertEquals(expMessage, ex.getMessage());
         }
-        BufferedReader result = null;
         try (BufferedInputStream stream = new BufferedInputStream(
                 new FileInputStream(brokenFile))) {
             new CSVFileReader(new CSVFileReaderSpi()).read(stream, null);
