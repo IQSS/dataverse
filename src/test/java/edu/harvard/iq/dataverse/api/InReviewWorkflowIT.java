16ea2a3b32ca7c995680399ff8b76f88074393bc
Brian Silverstein <bsilverstein95@gmail.com>
2017-07-10 10:43:23 -0400

    Added new REST assured tests for dataset return #3943

--- src/test/java/edu/harvard/iq/dataverse/api/InReviewWorkflowIT.java
+++ src/test/java/edu/harvard/iq/dataverse/api/InReviewWorkflowIT.java
@@ -7,6 +7,7 @@ import edu.harvard.iq.dataverse.authorization.DataverseRole;
 import java.util.logging.Logger;
 import javax.json.Json;
 import javax.json.JsonObjectBuilder;
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
 import static javax.ws.rs.core.Response.Status.CREATED;
 import static javax.ws.rs.core.Response.Status.OK;
 import static javax.ws.rs.core.Response.Status.UNAUTHORIZED;
@@ -85,6 +86,19 @@ public class InReviewWorkflowIT {
                 .body("message", equalTo("User @" + authorUsername + " is not permitted to perform requested action."))
                 .statusCode(UNAUTHORIZED.getStatusCode());
 
+        Response createJoeRandom = UtilIT.createRandomUser();
+        createJoeRandom.prettyPrint();
+        createJoeRandom.then().assertThat()
+                .statusCode(OK.getStatusCode());
+        String joeRandomUsername = UtilIT.getUsernameFromResponse(createJoeRandom);
+        String joeRandomApiToken = UtilIT.getApiTokenFromResponse(createJoeRandom);
+
+        Response submitForReviewFail = UtilIT.submitDatasetForReview(datasetPersistentId, joeRandomApiToken);
+        submitForReviewFail.prettyPrint();
+        submitForReviewFail.then().assertThat()
+                .body("message", equalTo("User @" + joeRandomUsername + " is not permitted to perform requested action."))
+                .statusCode(UNAUTHORIZED.getStatusCode());
+
         Response submitForReview = UtilIT.submitDatasetForReview(datasetPersistentId, authorApiToken);
         submitForReview.prettyPrint();
         submitForReview.then().assertThat()
@@ -99,8 +113,47 @@ public class InReviewWorkflowIT {
                 .body("data.notifications[1]", equalTo(null))
                 .statusCode(OK.getStatusCode());
 
-        String comments = "You forgot to upload any files.";
+        String joeRandomComments = "Joe Random says you'll never graduate.";
+        JsonObjectBuilder joeRandObj = Json.createObjectBuilder();
+        joeRandObj.add("comments", joeRandomComments);
+
+        // Joe Random - a real jerk - tries returning a dataset unrightfully with some mean comments.
+        // Despite being somewhere he already shouldn't be, he is not authorized to return it, thankfully.
+        Response returnToAuthorFail = UtilIT.returnDatasetToAuthor(datasetPersistentId, joeRandObj.build(), joeRandomApiToken);
+        returnToAuthorFail.prettyPrint();
+        returnToAuthorFail.then().assertThat()
+                .body("message", equalTo("User @" + joeRandomUsername + " is not permitted to perform requested action."))
+                .statusCode(UNAUTHORIZED.getStatusCode());
+
+        // TODO: test where curator neglecting to leave a comment. Should fail with "reason for return" required.
+        String noComments = "";
         JsonObjectBuilder jsonObjectBuilder = Json.createObjectBuilder();
+        jsonObjectBuilder.add("comments", noComments);
+        Response returnToAuthorNoComment = UtilIT.returnDatasetToAuthor(datasetPersistentId, jsonObjectBuilder.build(), curatorApiToken);
+        returnToAuthorNoComment.prettyPrint();
+        returnToAuthorNoComment.then().assertThat()
+                .body("comments", equalTo(null))
+                .statusCode(BAD_REQUEST.getStatusCode());
+
+        // TODO: For now, show that a "reason for return" of over 200 characters is saved. Once the limit is in place, change the test to fail.
+        String longComment = "Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed foo.";
+        jsonObjectBuilder.add("comments", longComment);
+        Response returnToAuthorWithLongComment = UtilIT.returnDatasetToAuthor(datasetPersistentId, jsonObjectBuilder.build(), curatorApiToken);
+        returnToAuthorWithLongComment.prettyPrint();
+        returnToAuthorWithLongComment.then().assertThat()
+                // With no char limit in place, the long comment can be used when returning a dataset. Change this assertion once limit exists.
+                .body("data.inReview", equalTo(false))
+                .statusCode(OK.getStatusCode());
+
+        // The author submits once again (no real changes made)
+        Response submitForReviewAgain = UtilIT.submitDatasetForReview(datasetPersistentId, authorApiToken);
+        submitForReviewAgain.prettyPrint();
+        submitForReviewAgain.then().assertThat()
+                .body("data.inReview", equalTo(true))
+                .statusCode(OK.getStatusCode());
+        
+        // Successfully return dataset to author for reason: "You forgot to upload any files."
+        String comments = "You forgot to upload any files.";
         jsonObjectBuilder.add("comments", comments);
         Response returnToAuthor = UtilIT.returnDatasetToAuthor(datasetPersistentId, jsonObjectBuilder.build(), curatorApiToken);
         returnToAuthor.prettyPrint();
@@ -108,14 +161,15 @@ public class InReviewWorkflowIT {
                 .body("data.inReview", equalTo(false))
                 .statusCode(OK.getStatusCode());
 
-        Response authorsChecksForCommentsAgain = UtilIT.getNotifications(authorApiToken);
-        authorsChecksForCommentsAgain.prettyPrint();
-        authorsChecksForCommentsAgain.then().assertThat()
+        Response authorChecksForCommentsAgain = UtilIT.getNotifications(authorApiToken);
+        authorChecksForCommentsAgain.prettyPrint();
+        authorChecksForCommentsAgain.then().assertThat()
                 .body("data.notifications[0].type", equalTo("RETURNEDDS"))
                 // The author thinks, "This why we have curators!"
+                // FIXME: change to expect curator to also see the reason for return
                 .body("data.notifications[0].comments", equalTo("You forgot to upload any files."))
-                .body("data.notifications[1].type", equalTo("CREATEACC"))
-                .body("data.notifications[1].comments", equalTo(null))
+                .body("data.notifications[1].type", equalTo("RETURNEDDS"))
+                .body("data.notifications[1].comments", equalTo("You forgot to upload any files."))
                 .statusCode(OK.getStatusCode());
 
         // The author upload the file she forgot.
@@ -134,17 +188,19 @@ public class InReviewWorkflowIT {
                 .body("data.inReview", equalTo(true))
                 .statusCode(OK.getStatusCode());
 
-        // The author checks to see if the author has resubmitted yet.
+        // The curator checks to see if the author has resubmitted yet.
         Response curatorChecksNotifications = UtilIT.getNotifications(curatorApiToken);
         curatorChecksNotifications.prettyPrint();
         curatorChecksNotifications.then().assertThat()
                 // TODO: Test this issue from the UI as well: https://github.com/IQSS/dataverse/issues/2526
                 .body("data.notifications[0].type", equalTo("SUBMITTEDDS"))
-                .body("data.notifications[0].comments", equalTo(null))
+                .body("data.notifications[0].comments", equalTo("You forgot to upload any files."))
                 .body("data.notifications[1].type", equalTo("SUBMITTEDDS"))
-                .body("data.notifications[1].comments", equalTo(null))
-                .body("data.notifications[2].type", equalTo("CREATEACC"))
-                .body("data.notifications[2].comments", equalTo(null))
+                .body("data.notifications[1].comments", equalTo("You forgot to upload any files."))
+                .body("data.notifications[2].type", equalTo("SUBMITTEDDS"))
+                .body("data.notifications[2].comments", equalTo("You forgot to upload any files."))
+                .body("data.notifications[3].type", equalTo("CREATEACC"))
+                .body("data.notifications[3].comments", equalTo(null))
                 .statusCode(OK.getStatusCode());
 
         // The curator publishes the dataverse.
@@ -166,8 +222,10 @@ public class InReviewWorkflowIT {
                 .body("data.notifications[1].type", equalTo("RETURNEDDS"))
                 // The reason for return is deleted on publish. It's water under the bridge.
                 .body("data.notifications[1].comments", equalTo(null))
-                .body("data.notifications[2].type", equalTo("CREATEACC"))
+                .body("data.notifications[2].type", equalTo("RETURNEDDS"))
                 .body("data.notifications[2].comments", equalTo(null))
+                .body("data.notifications[3].type", equalTo("CREATEACC"))
+                .body("data.notifications[3].comments", equalTo(null))
                 .statusCode(OK.getStatusCode());
 
         // These println's are here in case you want to log into the GUI to see what notifications look like.
