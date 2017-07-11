4f237a84862af7a31e1103fe996a04fb26fcfc1a
ferrys <ferrys@bu.edu>
2017-07-11 13:55:12 -0400

    #3752 sql update script for removing faulty authenticated users

--- scripts/database/upgrades/upgrade_v4.7_to_v4.7.1.sql
+++ scripts/database/upgrades/upgrade_v4.7_to_v4.7.1.sql
@@ -5,3 +5,14 @@ ALTER TABLE authenticateduser ADD COLUMN createdtime TIMESTAMP NOT NULL DEFAULT 
 ALTER TABLE authenticateduser ADD COLUMN lastlogintime TIMESTAMP DEFAULT NULL;
 ALTER TABLE authenticateduser ADD COLUMN lastapiusetime TIMESTAMP DEFAULT NULL;
 ALTER TABLE authenticateduser DROP COLUMN modificationtime;
+
+-- Optional commands for listing usernames and ids for faulty users who will be deleted below
+-- SELECT DISTINCT A.useridentifier FROM authenticateduserlookup AL, authenticateduser A, builtinuser B WHERE AL.authenticationproviderid = 'builtin' AND A.useridentifier NOT IN (SELECT username FROM builtinuser);
+-- SELECT DISTINCT A.id FROM authenticateduserlookup AL, authenticateduser A, builtinuser B WHERE AL.authenticationproviderid = 'builtin' AND A.useridentifier NOT IN (SELECT username FROM builtinuser);
+
+-- Removing authenticated builtin users who do not exist in the builtin table because they were created through faulty validation
+DELETE FROM authenticateduserlookup WHERE persistentuserid IN (SELECT DISTINCT A.useridentifier FROM authenticateduserlookup AL, authenticateduser A, builtinuser B WHERE AL.authenticationproviderid = 'builtin' AND A.useridentifier NOT IN (SELECT username FROM builtinuser));
+DELETE FROM confirmemaildata WHERE authenticateduser_id IN (SELECT DISTINCT A.id FROM authenticateduserlookup AL, authenticateduser A, builtinuser B WHERE AL.authenticationproviderid = 'builtin' AND A.useridentifier NOT IN (SELECT username FROM builtinuser));
+DELETE FROM usernotification WHERE user_id IN (SELECT DISTINCT A.id FROM authenticateduserlookup AL, authenticateduser A, builtinuser B WHERE AL.authenticationproviderid = 'builtin' AND A.useridentifier NOT IN (SELECT username FROM builtinuser));
+DELETE FROM guestbookresponse WHERE authenticateduser_id IN (SELECT DISTINCT A.id FROM authenticateduserlookup AL, authenticateduser A, builtinuser B WHERE AL.authenticationproviderid = 'builtin' AND A.useridentifier NOT IN (SELECT username FROM builtinuser));
+DELETE FROM authenticateduser WHERE useridentifier IN (SELECT DISTINCT A.useridentifier FROM authenticateduserlookup AL, authenticateduser A, builtinuser B WHERE AL.authenticationproviderid = 'builtin' AND A.useridentifier NOT IN (SELECT username FROM builtinuser));
