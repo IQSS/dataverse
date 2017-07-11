2bce699f64b833b3f778a1ddaad0f0f47ee6b822
ferrys <ferrys@bu.edu>
2017-07-11 16:18:49 -0400

    #3752 optimizing sql script for removing bad users

--- scripts/database/upgrades/upgrade_v4.7_to_v4.7.1.sql
+++ scripts/database/upgrades/upgrade_v4.7_to_v4.7.1.sql
@@ -6,13 +6,13 @@ ALTER TABLE authenticateduser ADD COLUMN lastlogintime TIMESTAMP DEFAULT NULL;
 ALTER TABLE authenticateduser ADD COLUMN lastapiusetime TIMESTAMP DEFAULT NULL;
 ALTER TABLE authenticateduser DROP COLUMN modificationtime;
 
--- Optional commands for listing usernames and ids for faulty users who will be deleted below
--- SELECT DISTINCT A.useridentifier FROM authenticateduserlookup AL, authenticateduser A, builtinuser B WHERE AL.authenticationproviderid = 'builtin' AND A.useridentifier NOT IN (SELECT username FROM builtinuser);
--- SELECT DISTINCT A.id FROM authenticateduserlookup AL, authenticateduser A, builtinuser B WHERE AL.authenticationproviderid = 'builtin' AND A.useridentifier NOT IN (SELECT username FROM builtinuser);
-
 -- Removing authenticated builtin users who do not exist in the builtin table because they were created through faulty validation
-DELETE FROM authenticateduserlookup WHERE persistentuserid IN (SELECT DISTINCT A.useridentifier FROM authenticateduserlookup AL, authenticateduser A, builtinuser B WHERE AL.authenticationproviderid = 'builtin' AND A.useridentifier NOT IN (SELECT username FROM builtinuser));
-DELETE FROM confirmemaildata WHERE authenticateduser_id IN (SELECT DISTINCT A.id FROM authenticateduserlookup AL, authenticateduser A, builtinuser B WHERE AL.authenticationproviderid = 'builtin' AND A.useridentifier NOT IN (SELECT username FROM builtinuser));
-DELETE FROM usernotification WHERE user_id IN (SELECT DISTINCT A.id FROM authenticateduserlookup AL, authenticateduser A, builtinuser B WHERE AL.authenticationproviderid = 'builtin' AND A.useridentifier NOT IN (SELECT username FROM builtinuser));
-DELETE FROM guestbookresponse WHERE authenticateduser_id IN (SELECT DISTINCT A.id FROM authenticateduserlookup AL, authenticateduser A, builtinuser B WHERE AL.authenticationproviderid = 'builtin' AND A.useridentifier NOT IN (SELECT username FROM builtinuser));
-DELETE FROM authenticateduser WHERE useridentifier IN (SELECT DISTINCT A.useridentifier FROM authenticateduserlookup AL, authenticateduser A, builtinuser B WHERE AL.authenticationproviderid = 'builtin' AND A.useridentifier NOT IN (SELECT username FROM builtinuser));
+-- creates view containing authentication ids that you will be deleting
+CREATE TEMP VIEW useridstodelete AS (SELECT DISTINCT a.id FROM authenticateduserlookup al, authenticateduser a WHERE al.authenticateduser_id = a.id AND al.authenticationproviderid = 'builtin'  AND a.useridentifier NOT IN (SELECT username FROM builtinuser));
+-- commands to remove the users from the appropriate tables
+-- TOOD: does this cascade?
+DELETE FROM authenticateduserlookup WHERE authenticateduser_id IN (SELECT * FROM useridstodelete);
+DELETE FROM confirmemaildata WHERE authenticateduser_id IN (SELECT * FROM useridstodelete);
+DELETE FROM usernotification WHERE user_id IN (SELECT * FROM useridstodelete);
+DELETE FROM guestbookresponse WHERE authenticateduser_id IN (SELECT * FROM useridstodelete);
+DELETE FROM authenticateduser WHERE id IN (SELECT * FROM useridstodelete);
