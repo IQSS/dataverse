02aa3f7e88ca86ebb8b743670a599ebb39589944
Philip Durbin <philip_durbin@harvard.edu>
2017-08-01 11:14:02 -0400

    refactor into UtilIT.dataCaptureModuleChecksumValidation #3942

--- src/test/java/edu/harvard/iq/dataverse/api/DatasetsIT.java
+++ src/test/java/edu/harvard/iq/dataverse/api/DatasetsIT.java
@@ -993,97 +993,6 @@ public class DatasetsIT {
                 .body("message", equalTo(":UploadMethods does not contain dcm/rsync+ssh."))
                 .statusCode(METHOD_NOT_ALLOWED.getStatusCode());
 
-        boolean readyToStartWorkingOnChecksumValidationReporting = false;
-        if (!readyToStartWorkingOnChecksumValidationReporting) {
-            return;
-        }
-
-        /**
-         * Here we are pretending to be the Data Capture Module reporting on if
-         * checksum validation success or failure. Don't notify the user on
-         * success (too chatty) but do notify on failure.
-         *
-         * @todo On success a process should be kicked off to crawl the files so
-         * they are imported into Dataverse. Once the crawling and importing is
-         * complete, notify the user.
-         *
-         * @todo What authentication should be used here? The API token of the
-         * user? (If so, pass the token in the initial upload request payload.)
-         * This is suboptimal because of the security risk of having the Data
-         * Capture Module store the API token. Or should Dataverse be able to be
-         * configured so that it only will receive these messages from trusted
-         * IP addresses? Should there be a shared secret that's used for *all*
-         * requests from the Data Capture Module to Dataverse?
-         */
-        JsonObjectBuilder wrongDataset = Json.createObjectBuilder();
-        wrongDataset.add("userId", userId);
-        wrongDataset.add("datasetId", datasetId2);
-        wrongDataset.add("status", "validation passed");
-        Response datasetWithoutRsyncScript = given()
-                .body(wrongDataset.build().toString())
-                .contentType(ContentType.JSON)
-                .post("/api/datasets/" + datasetId2 + "/dataCaptureModule/checksumValidation");
-        datasetWithoutRsyncScript.prettyPrint();
-
-        datasetWithoutRsyncScript.then().assertThat()
-                .statusCode(400)
-                .body("message", equalTo("Dataset id " + datasetId2 + " does not have an rsync script."));
-
-        JsonObjectBuilder badNews = Json.createObjectBuilder();
-        badNews.add("userId", userId);
-        badNews.add("datasetId", datasetId);
-        // Status options are documented at https://github.com/sbgrid/data-capture-module/blob/master/doc/api.md#post-upload
-        badNews.add("status", "validation failed");
-        Response uploadFailed = given()
-                .body(badNews.build().toString())
-                .contentType(ContentType.JSON)
-                .post("/api/datasets/" + datasetId + "/dataCaptureModule/checksumValidation");
-        uploadFailed.prettyPrint();
-
-        uploadFailed.then().assertThat()
-                /**
-                 * @todo Double check that we're ok with 200 here. We're saying
-                 * "Ok, the bad news was delivered."
-                 */
-                .statusCode(200)
-                .body("data.message", equalTo("User notified about checksum validation failure."));
-
-        /**
-         * @todo How can we test what the checksum validation notification looks
-         * like in the GUI? There is no API for retrieving notifications.
-         *
-         * @todo How can we test that the email notification looks ok?
-         */
-//        System.out.println("try logging in with " + username);
-        // Meanwhile, the user trys uploading again...
-        JsonObjectBuilder goodNews = Json.createObjectBuilder();
-        goodNews.add("userId", userId);
-        goodNews.add("datasetId", datasetId);
-        goodNews.add("status", "validation passed");
-        Response uploadSuccessful = given()
-                .body(goodNews.build().toString())
-                .contentType(ContentType.JSON)
-                .post("/api/datasets/" + datasetId + "/dataCaptureModule/checksumValidation");
-        uploadSuccessful.prettyPrint();
-
-        uploadSuccessful.then().assertThat()
-                .statusCode(200)
-                .body("data.message", startsWith("Next we will write code to kick off crawling and importing of files"));
-
-        Response deleteDatasetResponse = UtilIT.deleteDatasetViaNativeApi(datasetId, apiToken);
-        deleteDatasetResponse.prettyPrint();
-        assertEquals(200, deleteDatasetResponse.getStatusCode());
-
-        Response deleteDataverseResponse = UtilIT.deleteDataverse(dataverseAlias, apiToken);
-        deleteDataverseResponse.prettyPrint();
-        assertEquals(200, deleteDataverseResponse.getStatusCode());
-
-        Response deleteUserResponse = UtilIT.deleteUser(username);
-        deleteUserResponse.prettyPrint();
-        assertEquals(200, deleteUserResponse.getStatusCode());
-
-        UtilIT.deleteUser(noPermsUsername);
-
     }
 
     /**
@@ -1091,7 +1000,7 @@ public class DatasetsIT {
      * not necessary
      */
     @Test
-    public void testDcmNotifications() throws IOException, InterruptedException {
+    public void testDcmChecksumValidationMessages() throws IOException, InterruptedException {
 
         Response setDcmUrl = UtilIT.setSetting(SettingsServiceBean.Key.DataCaptureModuleUrl, "http://localhost:8888");
         setDcmUrl.then().assertThat()
@@ -1170,31 +1079,24 @@ public class DatasetsIT {
         Can't find dataset - give bad dataset ID
          */
         JsonObjectBuilder wrongDataset = Json.createObjectBuilder();
+        String fakeDatasetId = "78921457982457921";
         wrongDataset.add("userId", userId);
-        wrongDataset.add("datasetId", "78921457982457921");
+        wrongDataset.add("datasetId", fakeDatasetId);
         wrongDataset.add("status", "validation passed");
         UtilIT.makeSuperUser(username);
-        Response datasetNotFound = given()
-                .header(UtilIT.API_TOKEN_HTTP_HEADER, apiToken)
-                .body(wrongDataset.build().toString())
-                .contentType(ContentType.JSON)
-                .post("/api/datasets/" + "78921457982457921" + "/dataCaptureModule/checksumValidation");
+        Response datasetNotFound = UtilIT.dataCaptureModuleChecksumValidation(fakeDatasetId, wrongDataset.build(), apiToken);
         datasetNotFound.prettyPrint();
 
         datasetNotFound.then().assertThat()
                 .statusCode(404)
-                .body("message", equalTo("Dataset with ID 78921457982457921 not found."));
+                .body("message", equalTo("Dataset with ID " + fakeDatasetId + " not found."));
 
         JsonObjectBuilder badNews = Json.createObjectBuilder();
         badNews.add("userId", userId);
         badNews.add("datasetId", datasetId2);
         // Status options are documented at https://github.com/sbgrid/data-capture-module/blob/master/doc/api.md#post-upload
         badNews.add("status", "validation failed");
-        Response uploadFailed = given()
-                .header(UtilIT.API_TOKEN_HTTP_HEADER, apiToken)
-                .body(badNews.build().toString())
-                .contentType(ContentType.JSON)
-                .post("/api/datasets/" + datasetId + "/dataCaptureModule/checksumValidation");
+        Response uploadFailed = UtilIT.dataCaptureModuleChecksumValidation(datasetId.toString(), badNews.build(), apiToken);
         uploadFailed.prettyPrint();
 
         uploadFailed.then().assertThat()
@@ -1251,11 +1153,7 @@ public class DatasetsIT {
         goodNews.add("uploadFolder", uploadFolder);
         goodNews.add("datasetIdentifier", identifier);
         goodNews.add("totalSize", totalSize);
-        Response uploadSuccessful = given()
-                .body(goodNews.build().toString())
-                .contentType(ContentType.JSON)
-                .header(API_TOKEN_HTTP_HEADER, apiToken)
-                .post("/api/datasets/" + datasetId + "/dataCaptureModule/checksumValidation");
+        Response uploadSuccessful = UtilIT.dataCaptureModuleChecksumValidation(datasetId.toString(), goodNews.build(), apiToken);
         /**
          * Errors here are expected unless you've set doExtraTesting to true to
          * do the jobs of the rsync script and the DCM to create the files.sha
