ce949c93a556b12fd46829ae37ff4808adc3a95f
Philip Durbin <philip_durbin@harvard.edu>
2017-09-25 15:39:18 -0400

    Docker tag with no issue number (kick-the-tires) #4040
    The idea is that we won't mess with the kick-the-tires for a bit so that
    people can play with Minishift without that tag constantly changing
    under their feet while we work on other efforts in other tags such as
    getting the containers to run as non-root and getting them to run in
    1 GB of memory.

--- conf/docker/build.sh
+++ conf/docker/build.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 # Creates images and pushes them to Docker Hub.
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+# FIXME: Make this script dynamic so you can switch the tag to the branch you're on or a tagged release.
+TAG=kick-the-tires
 # Use "conf" directory as context so we can copy schema.xml into Solr image.
-docker build -t iqss/dataverse-solr:$GIT_BRANCH -f solr/Dockerfile ../../conf
-docker push iqss/dataverse-solr:$GIT_BRANCH
+docker build -t iqss/dataverse-solr:$TAG -f solr/Dockerfile ../../conf
+docker push iqss/dataverse-solr:$TAG
 # TODO: Think about if we really need dataverse.war because it's in dvinstall.zip.
 cp ../../target/dataverse*.war dataverse-glassfish/dataverse.war
 cp ../../scripts/installer/dvinstall.zip dataverse-glassfish
 cp ../../doc/sphinx-guides/source/_static/util/default.config dataverse-glassfish
 cp ../../downloads/glassfish-4.1.zip dataverse-glassfish
 cp ../../downloads/weld-osgi-bundle-2.2.10.Final-glassfish4.jar dataverse-glassfish
-docker build -t iqss/dataverse-glassfish:$GIT_BRANCH dataverse-glassfish
-docker push iqss/dataverse-glassfish:$GIT_BRANCH
+docker build -t iqss/dataverse-glassfish:$TAG dataverse-glassfish
+docker push iqss/dataverse-glassfish:$TAG
