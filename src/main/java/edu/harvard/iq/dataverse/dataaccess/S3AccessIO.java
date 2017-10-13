c758569863faadde32ec2515b909113a90ceec51
matthew-a-dunlap <matthew.a.dunlap@gmail.com>
2017-10-13 17:45:23 -0400

    S3Access aux save bugfix (size == 0) #4185

--- src/main/java/edu/harvard/iq/dataverse/dataaccess/S3AccessIO.java
+++ src/main/java/edu/harvard/iq/dataverse/dataaccess/S3AccessIO.java
@@ -345,7 +345,7 @@ public class S3AccessIO<T extends DvObject> extends StorageIO<T> {
 
     @Override
     public void saveInputStreamAsAux(InputStream inputStream, String auxItemTag, Long filesize) throws IOException {
-        if (filesize == null || filesize < 0) {
+        if (filesize == null || filesize <= 0) {
             saveInputStreamAsAux(inputStream, auxItemTag);
         } else {
             if (!this.canWrite()) {
