7a1bc4373cccb8004d2160045974662378fd834f
Pete Meyer <pameyer@crystal.harvard.edu>
2017-10-19 18:17:12 -0400

    put existing response text into files to allow validation

--- src/test/java/edu/harvard/iq/dataverse/authorization/providers/oauth2/impl/OrcidOAuth2APTest.java
+++ src/test/java/edu/harvard/iq/dataverse/authorization/providers/oauth2/impl/OrcidOAuth2APTest.java
@@ -5,14 +5,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
+import org.junit.Before;
 
 /**
  *
  * @author michael
  */
 public class OrcidOAuth2APTest extends OrcidOAuth2AP {
-
+	private final String response_file="src/test/resources/xml/oauth2/orcid/v12_response.xml";
+	private static String RESPONSE = null;
+	/*
     private static final String RESPONSE
             = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
             + "<orcid-message xmlns=\"http://www.orcid.org/ns/orcid\">\n"
@@ -46,7 +50,10 @@ public class OrcidOAuth2APTest extends OrcidOAuth2AP {
             + "        </orcid-bio>\n"
             + "    </orcid-profile>\n"
             + "</orcid-message>";
-
+	    */
+	private final String no_email_has_affiliation_file="src/test/resources/xml/oauth2/orcid/v12_no_email_has_aff.xml";
+	private static String NO_EMAIL_HAS_AFFILIATION = null;
+	/*
     private final String NO_EMAIL_HAS_AFFILIATION = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
             + "<orcid-message xmlns=\"http://www.orcid.org/ns/orcid\">\n"
             + "    <message-version>1.2</message-version>\n"
@@ -106,14 +113,47 @@ public class OrcidOAuth2APTest extends OrcidOAuth2AP {
             + "        </orcid-activities>\n"
             + "    </orcid-profile>\n"
             + "</orcid-message>";
+	    */
 
+	private final String no_email_file="src/test/resources/xml/oauth2/orcid/v12_no_email.xml";
+	private static String NO_EMAIL = null;
     public OrcidOAuth2APTest() {
         super("", "", "");
+    }
+
+	
+
+    @Before
+    public void setUp()
+    {
+	    RESPONSE = loadResponseXML( response_file );
+	    NO_EMAIL_HAS_AFFILIATION = loadResponseXML( no_email_has_affiliation_file );
+	    NO_EMAIL = loadResponseXML( no_email_file );
+    }
+    /**
+     * load XML responses from filesystem (resources).
+     * Why? To allow validating against the XSD prior to 1.2 -> 2.0 upgrade
+     */
+    private static String loadResponseXML(String fname)
+    {
+	    String txt = null;
+	    try
+	    {
+		    java.io.File inp = new java.io.File( fname );
+		    //java.io.InputStream inp = new java.io.FileInputStream( new java.io.File( fname ) );
+		    txt = org.apache.commons.io.FileUtils.readFileToString( inp );
+	    }
+	    catch( java.io.IOException ie )
+	    {
+		    // no-op; assert that the needed strings are not null in tests
+	    }
+	    return txt;
     }
 
     @Test
     public void testParseUserResponse() {
         OrcidOAuth2AP sut = new OrcidOAuth2AP("clientId", "clientSecret", "userEndpoint");
+	assertNotNull( RESPONSE );
         System.out.println("withEmailResponse = " + RESPONSE);
         final AbstractOAuth2AuthenticationProvider.ParsedUserResponse actual = sut.parseUserResponse(RESPONSE);
 
@@ -131,6 +171,7 @@ public class OrcidOAuth2APTest extends OrcidOAuth2AP {
     @Test
     public void testParseUserResponseNoEmailHasAffiliation() {
         OrcidOAuth2AP sut = new OrcidOAuth2AP("clientId", "clientSecret", "userEndpoint");
+	assertNotNull( NO_EMAIL_HAS_AFFILIATION );
         final AbstractOAuth2AuthenticationProvider.ParsedUserResponse actual = sut.parseUserResponse(NO_EMAIL_HAS_AFFILIATION);
 
         assertEquals("0000-0002-3283-0661", actual.userIdInProvider);
@@ -148,9 +189,9 @@ public class OrcidOAuth2APTest extends OrcidOAuth2AP {
     @Test
     public void testParseUserResponse_noEmails() {
         OrcidOAuth2AP sut = new OrcidOAuth2AP("clientId", "clientSecret", "userEndpoint");
-        String noEmailResponse = RESPONSE.replaceAll("<contact-details>.*</contact-details>", "");
-        System.out.println("noEmailResponse = " + noEmailResponse);
-        final AbstractOAuth2AuthenticationProvider.ParsedUserResponse actual = sut.parseUserResponse(noEmailResponse);
+	assertNotNull( NO_EMAIL );
+        System.out.println("noEmailResponse = " + NO_EMAIL );
+        final AbstractOAuth2AuthenticationProvider.ParsedUserResponse actual = sut.parseUserResponse(NO_EMAIL);
 
         assertEquals("0000-0002-3283-0661", actual.userIdInProvider);
         assertEquals("Pete.Dataversky", actual.username);
