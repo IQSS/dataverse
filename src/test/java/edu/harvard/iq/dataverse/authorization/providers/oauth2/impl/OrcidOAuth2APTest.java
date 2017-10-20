29507f600d51838243ff201da648b87d6ffe1f02
Pete Meyer <pameyer@crystal.harvard.edu>
2017-10-20 14:52:26 -0400

    initial switch of test to 2.0

--- src/test/java/edu/harvard/iq/dataverse/authorization/providers/oauth2/impl/OrcidOAuth2APTest.java
+++ src/test/java/edu/harvard/iq/dataverse/authorization/providers/oauth2/impl/OrcidOAuth2APTest.java
@@ -14,11 +14,14 @@ import org.junit.Before;
  * @author michael
  */
 public class OrcidOAuth2APTest extends OrcidOAuth2AP {
-	private final String response_file="src/test/resources/xml/oauth2/orcid/v12_response.xml";
+	//private final String response_file="src/test/resources/xml/oauth2/orcid/v12_response.xml";
+	private final String response_file="src/test/resources/xml/oauth2/orcid/v20_response.xml";
 	private static String RESPONSE = null;
-	private final String no_email_has_affiliation_file="src/test/resources/xml/oauth2/orcid/v12_no_email_has_aff.xml";
+	//private final String no_email_has_affiliation_file="src/test/resources/xml/oauth2/orcid/v12_no_email_has_aff.xml";
+	private final String no_email_has_affiliation_file="src/test/resources/xml/oauth2/orcid/v20_no_email_has_aff.xml";
 	private static String NO_EMAIL_HAS_AFFILIATION = null;
-	private final String no_email_file="src/test/resources/xml/oauth2/orcid/v12_no_email.xml";
+	//private final String no_email_file="src/test/resources/xml/oauth2/orcid/v12_no_email.xml";
+	private final String no_email_file="src/test/resources/xml/oauth2/orcid/v20_no_email.xml";
 	private static String NO_EMAIL = null;
     public OrcidOAuth2APTest() {
         super("", "", "");
@@ -62,13 +65,14 @@ public class OrcidOAuth2APTest extends OrcidOAuth2AP {
 
         assertEquals("0000-0003-2591-1698", actual.userIdInProvider);
         //assertEquals("bob", actual.username);
-        assertEquals("bdoc", actual.username);
+        //assertEquals("bdoc", actual.username);
+        assertEquals("Bob.Doc", actual.username);
         assertEquals("Bob T.", actual.displayInfo.getFirstName());
         assertEquals("Doc", actual.displayInfo.getLastName());
-        assertEquals("bdoc@mailinator.com", actual.displayInfo.getEmailAddress());
+        assertEquals("bdoc@mailinator.com", actual.displayInfo.getEmailAddress());//FIXME - failing
         assertEquals("", actual.displayInfo.getAffiliation());
         assertEquals("", actual.displayInfo.getPosition());
-        assertEquals(Arrays.asList("bdoc@mailinator.com", "bdoc2@mailinator.com"), actual.emails);
+        assertEquals(Arrays.asList("bdoc@mailinator.com", "bdoc2@mailinator.com"), actual.emails); //FIXME - failing
 
     }
 
