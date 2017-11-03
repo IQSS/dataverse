28490ddc573f8579578f7744e1c1bdde3069670f
Alex Ivanov <alex@calmforce.com>
2017-11-03 04:53:31 -0400

    QDR-874: Adjust suggested citation in QDR Dataverse

--- src/main/java/edu/harvard/iq/dataverse/DataCitation.java
+++ src/main/java/edu/harvard/iq/dataverse/DataCitation.java
@@ -157,22 +157,28 @@ public class DataCitation {
         // first add comma separated parts        
         List<String> citationList = new ArrayList<>();
         citationList.add(formatString(authors, html));
-        citationList.add(year);
-        citationList.add(formatString(title, html, "\""));
+        citationList.add(year);        
+        // QDRCustom: Add period to title string
+        String titleStr = title + ".";
+        citationList.add(formatString(titleStr, html, "\""));         
+        // QDRCustom: Use "Qualitative Data Repository" as distributor name
+        citationList.add(formatString("Qualitative Data Repository", html));
+        // QDRCustom: Show persistentID after distributor name
         if (persistentId != null) {
             citationList.add(formatURL(persistentId.toString(), persistentId.toURL().toString(), html));
         }
-        citationList.add(formatString(distributors, html));
-        citationList.add(version);
+        citationList.add(version);       
 
         StringBuilder citation = new StringBuilder(
                 citationList.stream()
                 .filter(value -> !StringUtils.isEmpty(value))
-                .collect(Collectors.joining(", ")));
+                // QDRCustom: Use period to join values, not comma
+                .collect(Collectors.joining(". ")));
 
         // append UNF
         if (!StringUtils.isEmpty(UNF)) {
-            citation.append(", ").append(UNF);
+            // QDRCustom: Use period to join values, not comma
+            citation.append(". ").append(UNF);
         }
 
         for (DatasetField dsf : optionalValues) {
