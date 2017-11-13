0e8c8a97493aef433a7c826d6dd2f2b332598cdd
Leonid Andreev <leonid@hmdc.harvard.edu>
2017-11-13 15:36:40 -0500

    Added code for recording backup failures in the status table. (#4157)

--- scripts/backup/run_backup/database.py
+++ scripts/backup/run_backup/database.py
@@ -1,129 +1,63 @@
+#!/usr/bin/env python
+
+import ConfigParser
 import psycopg2
 import sys
-import pprint
-from config import (ConfigSectionMap)
+from database import (query_database, get_last_timestamp, record_datafile_status)
+from storage import (open_dataverse_file)
+from backup import (backup_file)
+import io
 
-dataverse_db_connection=None
-backup_db_connection=None
+def main():
 
-def create_database_connection(database='database'):
-    Host = ConfigSectionMap("Database")['host']
-    Port = ConfigSectionMap("Database")['port']
-    Database = ConfigSectionMap("Database")[database]
-    Username = ConfigSectionMap("Database")['username']
-    Password = ConfigSectionMap("Database")['password']
-
-    print "Database Host: %s" % (Host)
-    print "Database Port: %s" % (Port)
-    print "Database Name: %s" % (Database)
-    print "Username: %s" % (Username)
-    print "Password: %s" % (Password)
-
-    #Define our connection string                                                                                                                                   
-    conn_string = "host='"+Host+"' dbname='"+Database+"' user='"+Username+"' password='"+Password+"'"
-
-    print "Connecting to database\n->%s" % (conn_string)
-
-    # get a connection, if a connect cannot be made an exception will be raised here
-    conn = psycopg2.connect(conn_string)
-
-    return conn
-
-def get_backupdb_connection():
-    global backup_db_connection
-
-    if backup_db_connection is None:
-        backup_db_connection = create_database_connection('backupdatabase')
-
-    return backup_db_connection
-
-def query_database(sinceTimestamp=None):
-    global dataverse_db_connection
-
-    dataverse_db_connection = create_database_connection()
-
-    cursor = dataverse_db_connection.cursor()
-    print "Connected!\n"
-
-    # select data files from the database 
-    dataverse_query="SELECT s.authority,s.identifier,o.storageidentifier,f.checksumtype,f.checksumvalue,f.filesize,o.createdate FROM dataset s, datafile f, dvobject o WHERE o.id = f.id AND o.owner_id = s.id AND s.harvestingclient_id IS null"
-    if sinceTimestamp is None:
-        cursor.execute(dataverse_query)
+    time_stamp = get_last_timestamp()
+    if time_stamp is None:
+        print "No time stamp! first run."
+        records = query_database()
     else:
-        dataverse_query = dataverse_query+" AND o.createdate > %s"
-        cursor.execute(dataverse_query, (sinceTimestamp,))
+        print "last backup: "+time_stamp
+        records = query_database(time_stamp)
+
+    for result in records:
+        dataset_authority = result[0]
+        dataset_identifier = result[1]
+        storage_identifier = result[2]
+        checksum_type = result[3]
+        checksum_value = result[4]
+        file_size = result[5]
+        create_time = result[6]
+
+        if (checksum_value is None):
+            checksum_value = "MISSING"
 
 
-    records = cursor.fetchall()
+        if (storage_identifier is not None and dataset_identifier is not None and dataset_authority is not None):
+            print dataset_authority + "/" + dataset_identifier + "/" + storage_identifier + ", " + checksum_type + ": " + checksum_value
 
-    return records
+            file_input=None
 
-def get_last_timestamp():
-    backup_db_connection = get_backupdb_connection()
+            try: 
+                file_input = open_dataverse_file(dataset_authority, dataset_identifier, storage_identifier)
+            except:
+                print "failed to open file "+storage_identifier
+                file_input=None
 
-    cursor = backup_db_connection.cursor()
-    print "Connected!\n"
+            
+            if (file_input is not None):
+                try:
+                    backup_file(file_input, dataset_authority, dataset_identifier, storage_identifier, checksum_type, checksum_value, file_size)
+                    print "backed up file "+storage_identifier
+                    record_datafile_status(dataset_authority, dataset_identifier, storage_identifier, 'OK', create_time)
+                except ValueError:
+                    print "failed to back up file "+storage_identifier
+                    record_datafile_status(dataset_authority, dataset_identifier, storage_identifier, 'FAIL_WRITE', create_time)
+                    #TODO: add a separate failure status 'FAIL_VERIFY' - for when it looked like we were able to copy the file 
+                    # onto the remote storage system, but the checksum verification failed (?)
+            else:
+                record_datafile_status(dataset_authority, dataset_identifier, storage_identifier, 'FAIL_READ', create_time)
 
-    # select the last timestamp from the datafilestatus table: 
-    dataverse_query="SELECT lastbackuptime FROM datafilestatus ORDER BY lastbackuptime DESC LIMIT 1"
-
-    cursor.execute(dataverse_query)
-
-    record = cursor.fetchone()
-
-    if record is None:
-        print "table is empty"
-        return None
-
-    timestamp = record[0]
-    timestamp_str = timestamp.strftime('%Y-%m-%d %H:%M:%S')
-
-    return timestamp_str
-
-def get_datafile_status(dataset_authority, dataset_identifier, storage_identifier):
-    backup_db_connection = get_backupdb_connection()
-    cursor = backup_db_connection.cursor()
-
-    # select the last timestamp from the datafilestatus table: 
-    
-    dataverse_query="SELECT status FROM datafilestatus WHERE datasetidentifier=%s AND storageidentifier=%s;"
-
-    dataset_id=dataset_authority+"/"+dataset_identifier
-
-    cursor.execute(dataverse_query, (dataset_id, storage_identifier))
-
-    record = cursor.fetchone()
-
-    if record is None:
-        print "no backup status for this file"
-        return None
-
-    backupstatus = record[0]
-    print "last backup status: "+backupstatus
-    return backupstatus
-
-def record_datafile_status(dataset_authority, dataset_identifier, storage_identifier, status, timestamp):
-    current_status = get_datafile_status(dataset_authority, dataset_identifier, storage_identifier)
-
-    backup_db_connection = get_backupdb_connection()
-    cursor = backup_db_connection.cursor()
-
-    timestamp_str = timestamp.strftime('%Y-%m-%d %H:%M:%S')
-
-    if current_status is None:
-        query = "INSERT INTO datafilestatus (status, lastbackuptime, lastbackupmethod, datasetidentifier, storageidentifier) VALUES (%s, %s, %s, %s, %s);"
-    else:
-        query = "UPDATE datafilestatus SET status=%s, lastbackuptime=%s, lastbackupmethod=%s WHERE datasetidentifier=%s AND storageidentifier=%s;"
-
-    dataset_id=dataset_authority+"/"+dataset_identifier
-    backup_method = ConfigSectionMap("Backup")['storagetype']
-
-    cursor.execute(query, (status, timestamp_str, backup_method, dataset_id, storage_identifier))
-
-    # finalize transaction:
-    backup_db_connection.commit()
-    cursor.close()
-        
+if __name__ == "__main__":
+    main()
 
 
 
