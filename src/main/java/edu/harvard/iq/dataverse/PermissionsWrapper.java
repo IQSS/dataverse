2ba605521bbbde42db3325694e1ea008516ccbb3
matthew-a-dunlap <matthew.a.dunlap@gmail.com>
2017-12-08 13:20:06 -0500

    Added db-friendly permissions checks to wrapper #4319

--- src/main/java/edu/harvard/iq/dataverse/PermissionsWrapper.java
+++ src/main/java/edu/harvard/iq/dataverse/PermissionsWrapper.java
@@ -9,9 +9,13 @@ import edu.harvard.iq.dataverse.authorization.Permission;
 import edu.harvard.iq.dataverse.authorization.users.User;
 import edu.harvard.iq.dataverse.engine.command.Command;
 import edu.harvard.iq.dataverse.engine.command.DataverseRequest;
+import edu.harvard.iq.dataverse.engine.command.exception.IllegalCommandException;
 import edu.harvard.iq.dataverse.engine.command.impl.*;
+import edu.harvard.iq.dataverse.util.BundleUtil;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 import javax.ejb.EJB;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
@@ -142,6 +146,58 @@ public class PermissionsWrapper implements java.io.Serializable {
         return doesSessionUserHaveDataSetPermission(dr, dataset, Permission.EditDataset);
     }
     
+    public void checkEditDatasetLock(Dataset dataset, DataverseRequest dataverseRequest, Command command) throws IllegalCommandException {
+        if (dataset.isLocked()) {
+            if (dataset.isLockedFor(DatasetLock.Reason.InReview)) {
+                // The "InReview" lock is not really a lock for curators. They can still make edits.
+                
+        //MAD: here I need to use the wrapper instead
+        //MAD: getRequiredPermissions I think throws a IllegalArgumentException. not sure what to do if so... probably just let it trickle up if this is all based around exceptions???
+            //command.getRequiredPermissions()
+
+                //if (!isUserAllowedOn(dataverseRequest.getUser(), new PublishDatasetCommand(dataset, dataverseRequest, true), dataset)) {
+                if (!doesSessionUserHaveDataSetPermissions(dvRequestService.getDataverseRequest(), dataset, new PublishDatasetCommand(dataset, dataverseRequest, true))) {
+                    throw new IllegalCommandException(BundleUtil.getStringFromBundle("dataset.message.locked.editNotAllowedInReview"), command);
+                }
+            }
+            if (dataset.isLockedFor(DatasetLock.Reason.Ingest)) {
+                throw new IllegalCommandException(BundleUtil.getStringFromBundle("dataset.message.locked.editNotAllowed"), command);
+            }
+            // TODO: Do we need to check for "Workflow"? Should the message be more specific?
+            if (dataset.isLockedFor(DatasetLock.Reason.Workflow)) {
+                throw new IllegalCommandException(BundleUtil.getStringFromBundle("dataset.message.locked.editNotAllowed"), command);
+            }
+            // TODO: Do we need to check for "DcmUpload"? Should the message be more specific?
+            if (dataset.isLockedFor(DatasetLock.Reason.DcmUpload)) {
+                throw new IllegalCommandException(BundleUtil.getStringFromBundle("dataset.message.locked.editNotAllowed"), command);
+            }
+        }
+    }
+    
+    //MAD: same idea as above
+    //MAD: write tests for both?
+    public void checkDownloadFileLock(Dataset dataset, DataverseRequest dataverseRequest, Command command) throws IllegalCommandException {
+        if (dataset.isLocked()) {
+            if (dataset.isLockedFor(DatasetLock.Reason.InReview)) {
+                // The "InReview" lock is not really a lock for curators or contributors. They can still download.                
+                //if (!isUserAllowedOn(dataverseRequest.getUser(), new UpdateDatasetCommand(dataset, dataverseRequest), dataset)) {
+                if (!doesSessionUserHaveDataSetPermissions(dvRequestService.getDataverseRequest(), dataset, new UpdateDatasetCommand(dataset, dataverseRequest))) {
+                    throw new IllegalCommandException(BundleUtil.getStringFromBundle("dataset.message.locked.downloadNotAllowedInReview"), command);
+                }
+            }
+            if (dataset.isLockedFor(DatasetLock.Reason.Ingest)) {
+                throw new IllegalCommandException(BundleUtil.getStringFromBundle("dataset.message.locked.downloadNotAllowed"), command);
+            }
+            // TODO: Do we need to check for "Workflow"? Should the message be more specific?
+            if (dataset.isLockedFor(DatasetLock.Reason.Workflow)) {
+                throw new IllegalCommandException(BundleUtil.getStringFromBundle("dataset.message.locked.downloadNotAllowed"), command);
+            }
+            // TODO: Do we need to check for "DcmUpload"? Should the message be more specific?
+            if (dataset.isLockedFor(DatasetLock.Reason.DcmUpload)) {
+                throw new IllegalCommandException(BundleUtil.getStringFromBundle("dataset.message.locked.downloadNotAllowed"), command);
+            }
+        }
+    }
             
     
     /**
@@ -178,6 +234,30 @@ public class PermissionsWrapper implements java.io.Serializable {
         // return true/false
         return hasPermission;
     }
+    
+    //Takes in a command, gets the permissions to check and calls the individual permissions check
+    public boolean doesSessionUserHaveDataSetPermissions(DataverseRequest req, Dataset dataset, Command command){
+        //MAD: In here I need to call doesSessionUserHaveDataSetPermission for each permission. I have no idea what that map is tho...
+        //it seems the map is "dataverse name" "required permissions" but may be a singleton in most cases???
+        
+        //public static void printMap(Map mp)
+        Map<String, Set<Permission>> permissionsToCheck = command.getRequiredPermissions();
+        Iterator it = permissionsToCheck.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, Set<Permission>> pair = (Map.Entry)it.next();
+
+            for (Permission p : pair.getValue()) {
+                if(!doesSessionUserHaveDataSetPermission(req, dataset, p)) {
+                    return false;
+                }
+            }
+           
+            it.remove(); // avoids a ConcurrentModificationException
+        }
+
+        return true;
+    }
+    
     /**
      *  Does this dvoObject have "Permission.DownloadFile"?
      * @param dvo
