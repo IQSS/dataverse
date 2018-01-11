87ef73eb7aeff697e8aa86d16c0fe877fb9280f6
matthew-a-dunlap <matthew.a.dunlap@gmail.com>
2018-01-11 17:45:09 -0500

    Saving and some display of provenance content #4343
    Currently only user input that is sent to be saved is displayed. Still need to pull content already saved to the popup when first opened.

--- src/main/java/edu/harvard/iq/dataverse/ProvenanceUploadFragmentBean.java
+++ src/main/java/edu/harvard/iq/dataverse/ProvenanceUploadFragmentBean.java
@@ -16,6 +16,7 @@ import org.primefaces.context.RequestContext;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.UploadedFile;
 import edu.harvard.iq.dataverse.engine.command.impl.PersistProvJsonProvCommand;
+import edu.harvard.iq.dataverse.engine.command.impl.PersistProvFreeFormCommand;
 import java.util.logging.Level;
 import javax.ejb.EJB;
 import javax.faces.view.ViewScoped;
@@ -43,34 +44,46 @@ public class ProvenanceUploadFragmentBean extends AbstractApiBean implements jav
      */
     
     private Long fileId = null;
-    UploadedFile uploadedJsonFile;
+    private UploadedFile jsonTempFile;
+    private String provFreeformText;
     
     @EJB
     DataFileServiceBean datafileService;
     @Inject
     DataverseRequestServiceBean dvRequestService;
     
-    public void handleFileUpload(FileUploadEvent event) throws IOException {
-    
-        //this really needs to store the content to a temp location.... does it actually need to be a temp file?
-        
-        
-        //"text/plain" as well?
-        uploadedJsonFile = event.getFile(); //TOOD: try/catch this too?
-        if(( uploadedJsonFile != null) && "application/json".equalsIgnoreCase(uploadedJsonFile.getContentType())) {
-            String jsonString = IOUtils.toString(uploadedJsonFile.getInputstream()); //may need to specify encoding
-            try {
-                execCommand(new PersistProvJsonProvCommand(dvRequestService.getDataverseRequest(), datafileService.find(fileId), jsonString)); //DataverseRequest aRequest, DataFile dataFile, String userInput)       
-            } catch (WrappedResponse ex) {
-                Logger.getLogger(ProvenanceUploadFragmentBean.class.getName()).log(Level.SEVERE, null, ex);
-                //MAD: Catch error better
-            }
-        }
-        //DataFile dataFile = fileSvc.find(idSuppliedAsLong);
-       
+    public void handleFileUpload(FileUploadEvent event) {
+        jsonTempFile = event.getFile();
     }
     
-    public void storeUploadedProvJson() {
+    //MAD: Rename to just saveContent to reflect the free text as well
+    public void saveUploadedContent() {
+        try {
+            //"text/plain" as well?
+            if(null != jsonTempFile && "application/json".equalsIgnoreCase(jsonTempFile.getContentType())) { //MAD: This needs a check for whether its been saved already?
+                String jsonString = IOUtils.toString(jsonTempFile.getInputstream()); //may need to specify encoding
+                try {
+                    execCommand(new PersistProvJsonProvCommand(dvRequestService.getDataverseRequest(), datafileService.find(fileId), jsonString)); //DataverseRequest aRequest, DataFile dataFile, String userInput)       
+                } catch (WrappedResponse ex) {
+                    Logger.getLogger(ProvenanceUploadFragmentBean.class.getName()).log(Level.SEVERE, null, ex);
+                    //MAD: Catch error better
+                }
+            }
+            
+            
+            
+        } catch (IOException e) {
+            Logger.getLogger(ProvenanceUploadFragmentBean.class.getName()).log(Level.SEVERE, null, e);
+            //what to do if file is not there when trying to save? Anything at all? if nothing probably should just say it throws
+        } 
+        try { //freeform. maybe collapse with above
+            if(!(null == provFreeformText || "".equals(provFreeformText))) {
+                execCommand(new PersistProvFreeFormCommand(dvRequestService.getDataverseRequest(), datafileService.find(fileId), provFreeformText));
+            }
+        }        
+        catch (WrappedResponse ex) {
+            Logger.getLogger(ProvenanceUploadFragmentBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
     }
     
@@ -82,11 +95,23 @@ public class ProvenanceUploadFragmentBean extends AbstractApiBean implements jav
         return false;
     }
     
-    public String getTempProvJson() {
-        return "";
+    //What do I actually need to return for these??? Just the name of the file?
+    public String getTempJsonFileName() {
+        if(null == jsonTempFile) {
+            return "";
+        }
+        return jsonTempFile.getFileName();
     }
     
-    public String getStoredProvJson() {
-        return "";
+//    public String getStoredJsonFile() {
+//        return "";
+//    }
+        
+    public String getFreeformText() {
+        return provFreeformText;
+    }
+    
+    public void setFreeformText(String freeformText) {
+        provFreeformText = freeformText;
     }
 }
