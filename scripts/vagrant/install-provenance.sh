de514146eafe7f51140e218e55cdbea47cf1f67f
Philip Durbin <philip_durbin@harvard.edu>
2018-02-06 16:59:23 -0500

    build Python bindings for CPL using newer Swig #4364

--- scripts/vagrant/install-provenance.sh
+++ scripts/vagrant/install-provenance.sh
@@ -10,7 +10,19 @@ mv json.hpp include
 source /opt/rh/devtoolset-7/enable
 sudo make install
 echo "Building Python bindings for CPL"
-sudo yum install -y swig
+echo "Install Swig 3.x (2.x from yum is too old), requires prce-devel"
+sudo yum install -y pcre-devel
+cd
+mkdir swig
+cd swig
+wget http://prdownloads.sourceforge.net/swig/swig-3.0.12.tar.gz
+tar xvfz swig-3.0.12.tar.gz
+cd swig-3.0.12
+./configure
+make
+sudo make install
+echo "Done building and installing Swig"
 cd /home/vagrant/prov-cpl/bindings/python
-# FIXME: this `make release` is failing with `include/cplxx.h:356: Error: Syntax error in input(1)`. See https://github.com/ProvTools/prov-cpl/issues/7
+sudo yum install -y python-devel
 make release
+sudo make install
