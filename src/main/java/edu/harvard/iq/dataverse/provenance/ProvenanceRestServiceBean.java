ebef653fc7cce56f4f19c1414c31b405222f1789
Philip Durbin <philip_durbin@harvard.edu>
2018-02-12 14:48:51 -0500

    get uploadProvJsonForBundle to return something #4349

--- src/main/java/edu/harvard/iq/dataverse/provenance/ProvenanceRestServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/provenance/ProvenanceRestServiceBean.java
@@ -34,6 +34,7 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import edu.harvard.iq.dataverse.util.json.JsonUtil;
 import java.io.StringReader;
 import javax.inject.Inject;
 import javax.json.JsonObject;
@@ -173,21 +174,29 @@ public class ProvenanceRestServiceBean {
     //	}]
     //}
     //
-    //MAD: Untested
-    // FIXME: Support sending standard javax.json.JsonObject (JSON-P, JRS 353) rather than org.json.JSONObject.
-    public boolean uploadProvJsonForBundle(JSONObject provJson, String bundleName) throws UnirestException {
-         HttpResponse<JsonNode> response = Unirest.post(provBaseUrl + "/provapi/bundle")
-                .body("{'bundle_name':'" + bundleName + "',"
-                        +"'JSON': {"+ provJson +"}}")
+    public JsonObject uploadProvJsonForBundle(JsonObject provJson, String bundleName) throws UnirestException {
+        JsonObjectBuilder job = Json.createObjectBuilder();
+        job.add("bundle_name", bundleName);
+        job.add("JSON", provJson);
+        JsonObject jsonToSend = job.build();
+        logger.info("to send: " + JsonUtil.prettyPrint(jsonToSend.toString()));
+        HttpResponse<JsonNode> response = Unirest.post(provBaseUrl + "/provapi/json")
+                .header("Content-Type", "application/json")
+                .body(jsonToSend.toString())
                 .asJson();
         if (response.getStatus() != 200) {
             throw new RuntimeException("Response code for provenance postBundle: " + response.getStatus() + ", " + response.getStatusText());
         }
-        return true; //MAD: this should change to reflect a real response or not be used. 
+        JSONObject orgJsonObject = response.getBody().getObject();
+        /**
+         * Yes, there is some inefficiency in converting this to the standard
+         * (JRS 353) JSON-P format but it's worth using the standard to avoid
+         * having to deal with a proliferation of Java implementations for JSON.
+         */
+        JsonReader jsonReader = Json.createReader(new StringReader(orgJsonObject.toString()));
+        return jsonReader.readObject();
     }
-    
-    
-            
+
 //POST /provapi/lookup/bundles
 //looks up multiple bundles
 //params:
--- src/test/java/edu/harvard/iq/dataverse/provenance/ProvenanceRestServiceBeanIT.java
+++ src/test/java/edu/harvard/iq/dataverse/provenance/ProvenanceRestServiceBeanIT.java
@@ -1,8 +1,12 @@
 package edu.harvard.iq.dataverse.provenance;
 
+import com.mashape.unirest.http.exceptions.UnirestException;
+import edu.harvard.iq.dataverse.util.json.JsonUtil;
 import java.util.Map;
+import java.util.UUID;
+import javax.json.Json;
 import javax.json.JsonObject;
-import org.json.JSONObject;
+import javax.json.JsonObjectBuilder;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -13,7 +17,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import org.junit.Ignore;
 
 /**
  * These tests are not expected to pass unless you have the prov service
@@ -93,14 +96,8 @@ public class ProvenanceRestServiceBeanIT {
         Map<String, String> result = provenanceRestServiceBean.getBundleId(bundleId);
         System.out.println("result: " + result);
         assertEquals("someName", result.get("name"));
-        // FIXME: Trying to get the id of a non-existent should simply return null rather than throwing a JSONException.
-        Exception expectedException = null;
-        try {
-            Map<String, String> maxResult = provenanceRestServiceBean.getBundleId(Long.MAX_VALUE);
-        } catch (org.json.JSONException ex) {
-            expectedException = ex;
-        }
-        assertNotNull(expectedException);
+        Map<String, String> maxResult = provenanceRestServiceBean.getBundleId(Long.MAX_VALUE);
+        assertNull(maxResult);
     }
 
     /**
@@ -142,9 +139,8 @@ public class ProvenanceRestServiceBeanIT {
         long bundleId = provenanceRestServiceBean.createEmptyBundleFromName("justAnIdWithoutJsonUploadedYet");
         JsonObject result = provenanceRestServiceBean.getBundleJson(bundleId);
         System.out.println("result: " + result);
-        assertEquals("", result.getString("JSON"));
+        assertEquals("null", result.getString("JSON"));
         JsonObject maxResult = provenanceRestServiceBean.getBundleJson(Long.MAX_VALUE);
-        // TODO: Report that it's weird to get a 200 response and "JSON": "" back rather than a 404 for a bundle that doesn't exit.
         System.out.println("maxResult: " + maxResult);
     }
 
@@ -152,17 +148,36 @@ public class ProvenanceRestServiceBeanIT {
      * Test of uploadProvJsonForBundle method, of class
      * ProvenanceRestServiceBean.
      */
-    // FIXME: Remove this Ignore and test this.
-    @Ignore
     @Test
-    public void testUploadProvJsonForBundle() throws Exception {
+    public void testUploadProvJsonForBundle() throws UnirestException {
         System.out.println("uploadProvJsonForBundle");
-        // FIXME: Support sending standard javax.json.JsonObject (JSON-P, JRS 353) rather than org.json.JSONObject.
-        JSONObject provJson = null;
-        boolean expResult = false;
-        long bundleId = provenanceRestServiceBean.createEmptyBundleFromName("weAreGoingToUploadSomeJson");
-        String bundleName = "huh? Shouldn't we be sending an id?";
-        boolean result = provenanceRestServiceBean.uploadProvJsonForBundle(provJson, bundleName);
+        JsonObjectBuilder innerJson = Json.createObjectBuilder();
+        innerJson.add("entity", Json.createObjectBuilder()
+                .add("bbc:news/science-environment-17526723", Json.createObjectBuilder()
+                        .add("prov:type", "a news item for desktop"))
+                .add("bbc:news/mobile/science-environment-17526723", Json.createObjectBuilder()
+                        .add("prov:type", "a news item for mobile devices"))
+        );
+        innerJson.add("alternateOf", Json.createObjectBuilder()
+                .add("_:aO1", Json.createObjectBuilder()
+                        .add("prov:alternate2", "bbc:news/science-environment-17526723")
+                        .add("prov:alternate1", "bbc:news/mobile/science-environment-17526723")
+                )
+        );
+        JsonObject provJson = innerJson.build();
+        String uuid = UUID.randomUUID().toString();
+        long bundleId = provenanceRestServiceBean.createEmptyBundleFromName(uuid);
+        System.out.println("uploading JSON for bundle id " + bundleId);
+        // TODO: What would a better bundle name be?
+        String bundleName = bundleId + "-uploadJson";
+        JsonObject result = provenanceRestServiceBean.uploadProvJsonForBundle(provJson, bundleName);
+        System.out.println("result: " + result);
+        System.out.println("result: " + JsonUtil.prettyPrint(result.toString()));
+        long idReturned = result.getJsonNumber("id").longValue();
+        System.out.println("id returned: " + idReturned);
+        assertTrue(idReturned > 0 && idReturned <= Long.MAX_VALUE);
+        // Is the id returned (i.e. 51) always one greater than the bundle id (i.e. 50)?
+        assertEquals(idReturned, bundleId + 1);
     }
 
 }
