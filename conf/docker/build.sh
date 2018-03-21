97740ce1d13991fd0785100db060ce244d439556
Philip Durbin <philip_durbin@harvard.edu>
2018-03-21 17:07:09 -0400

    try to get solr 7 working in minishift/openshift #4158

--- conf/docker/build.sh
+++ conf/docker/build.sh
@@ -34,6 +34,9 @@ docker push iqss/dataverse-solr:$TAG
 # FIXME: Automate the building of dataverse.war and dvinstall.zip. Think about https://github.com/IQSS/dataverse/issues/3974 and https://github.com/IQSS/dataverse/pull/3975
 cp ../../target/dataverse*.war dataverse-glassfish/dataverse.war
 cp ../../scripts/installer/dvinstall.zip dataverse-glassfish
+# Note that default.config needs to have the following values:
+# POSTGRES_SERVER dataverse-postgresql-service
+# SOLR_LOCATION   dataverse-solr-service:8983
 cp ../../doc/sphinx-guides/source/_static/util/default.config dataverse-glassfish
 cp ../../downloads/glassfish-4.1.zip dataverse-glassfish
 cp ../../downloads/weld-osgi-bundle-2.2.10.Final-glassfish4.jar dataverse-glassfish
--- conf/docker/solr/entrypoint.sh
+++ conf/docker/solr/entrypoint.sh
@@ -3,6 +3,8 @@
 if [ "$1" = 'solr' ]; then
     cd /usr/local/solr-7.2.1/
     bin/solr start
+    bin/solr create_core -c collection1 -d server/solr/collection1/conf
+    sleep infinity
 elif [ "$1" = 'usage' ]; then
     echo  'docker run -d iqss/dataverse-solr solr'
 else
