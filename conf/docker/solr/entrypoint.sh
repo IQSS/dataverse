94786ff22567e20eca39868b2b1b23bf2a6a563b
Philip Durbin <philip_durbin@harvard.edu>
2018-03-22 13:00:56 -0400

    for now, run Solr out of /tmp in openshift #4158
    This is an awful hack to just get Solr working at all. We were getting
    AccessDeniedException when starting it. FIXME added.
    
    The Dockerfile was also simplified to remove lines that don't seem to
    serve any apparent purpose:
    
    RUN ln -s /usr/local/solr-7.2.1/server/logs /var/log/solr
    VOLUME  /usr/local/solr-7.2.1/server/solr/collection1/data

--- conf/docker/solr/entrypoint.sh
+++ conf/docker/solr/entrypoint.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# FIXME: Don't run Solr out of /tmp!
+# Solr is /tmp to avoid AccessDeniedException under Minishift/OpenShift.
+SOLR_DIR=/tmp/solr-7.2.1
+
 if [ "$1" = 'solr' ]; then
-    cd /usr/local/solr-7.2.1/
+    cd /tmp
+    tar xvfz solr-7.2.1.tgz
+    cp -r $SOLR_DIR/server/solr/configsets/_default $SOLR_DIR/server/solr/collection1
+    cp /tmp/schema.xml $SOLR_DIR/server/solr/collection1/conf
+    cp /tmp/solrconfig.xml $SOLR_DIR/server/solr/collection1/conf
+    cd $SOLR_DIR
     bin/solr start
     bin/solr create_core -c collection1 -d server/solr/collection1/conf
     sleep infinity
