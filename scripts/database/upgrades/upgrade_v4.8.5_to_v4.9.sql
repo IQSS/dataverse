39c527d604ee6c272deb93f8ac259270e5cb408c
matthew-a-dunlap <matthew.a.dunlap@gmail.com>
2018-03-23 16:07:24 -0400

    provenance cplid as integer in psql #4343

--- scripts/database/upgrades/upgrade_v4.8.5_to_v4.9.sql
+++ scripts/database/upgrades/upgrade_v4.8.5_to_v4.9.sql
@@ -3,5 +3,5 @@ ALTER TABLE externaltool ALTER COLUMN type SET NOT NULL;
 -- Previously, the only explore tool was TwoRavens. We now persist the name of the tool.
 UPDATE guestbookresponse SET downloadtype = 'TwoRavens' WHERE downloadtype = 'Explore';
 ALTER TABLE filemetadata ADD COLUMN provfreeform text;
-ALTER TABLE filemetadata ADD COLUMN cplid character varying(255);
+ALTER TABLE filemetadata ADD COLUMN cplid int;
 ALTER TABLE filemetadata ADD COLUMN provbundleobjname text;
