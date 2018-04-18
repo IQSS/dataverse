47774d1f2e04f6ef338b1e648a0c22eaa3b13b92
Jim Myers <myersjd@umich.edu>
2018-04-18 11:18:14 -0400

    Fix compile errors and Minor updates
    Changing from address personal name to reflect reply address

--- src/main/java/edu/harvard/iq/dataverse/MailServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/MailServiceBean.java
@@ -31,6 +31,7 @@ import java.util.logging.Logger;
 import javax.annotation.Resource;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.mail.Address;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
@@ -79,7 +80,7 @@ public class MailServiceBean implements java.io.Serializable {
     public MailServiceBean() {
     }
 
-    public void sendMail(String host, String from, String to, String subject, String messageText) {
+    public void sendMail(String host, String reply, String to, String subject, String messageText) {
         Properties props = System.getProperties();
         props.put("mail.smtp.host", host);
         Session session = Session.getDefaultInstance(props, null);
@@ -89,8 +90,10 @@ public class MailServiceBean implements java.io.Serializable {
             String[] recipientStrings = to.split(",");
             InternetAddress[] recipients = new InternetAddress[recipientStrings.length];
             try {
-            	msg.setFrom(getSystemAddress());
-                msg.setReplyTo(new InternetAddress(from, charset));
+            	InternetAddress fromAddress=getSystemAddress();
+            	fromAddress.setPersonal(fromAddress.getPersonal() + "on behalf of " + reply, charset);
+            	msg.setFrom(fromAddress);
+                msg.setReplyTo(new Address[] {new InternetAddress(reply, charset)});
                 for (int i = 0; i < recipients.length; i++) {
                     recipients[i] = new InternetAddress(recipientStrings[i], "", charset);
                 }
@@ -167,18 +170,24 @@ public class MailServiceBean implements java.io.Serializable {
         sendMail(from, to, subject, messageText, new HashMap<>());
     }
 
-    public void sendMail(String from, String to, String subject, String messageText, Map<Object, Object> extraHeaders) {
+    public void sendMail(String reply, String to, String subject, String messageText, Map<Object, Object> extraHeaders) {
         try {
             MimeMessage msg = new MimeMessage(session);
             //Always send from system address to avoid email being blocked
-            msg.setFrom(getSystemAddress());
+            InternetAddress fromAddress=getSystemAddress();
+            try {
+              fromAddress.setPersonal(fromAddress.getPersonal() + "on behalf of " + reply, charset);
+            } catch (UnsupportedEncodingException ex) {
+                logger.severe(ex.getMessage());
+            }
+            msg.setFrom(fromAddress);
              
-            if (from.matches(EMAIL_PATTERN)) {
+            if (reply.matches(EMAIL_PATTERN)) {
             	//But set the reply-to address to direct replies to the requested 'from' party if it is a valid email address	
-                msg.setReplyTo(new InternetAddress(from));
+                msg.setReplyTo(new Address[] {new InternetAddress(reply)});
             } else {
                 //Otherwise include the invalid 'from' address in the message
-                messageText = "From: " + from + "\n\n" + messageText;
+                messageText = "From: " + reply + "\n\n" + messageText;
             }
             msg.setSentDate(new Date());
             msg.setRecipients(Message.RecipientType.TO,
