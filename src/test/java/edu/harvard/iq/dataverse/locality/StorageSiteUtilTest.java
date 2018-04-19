b8d07d55784869e0f44ac0cdb88ed8e2f078f907
Philip Durbin <philip_durbin@harvard.edu>
2018-04-19 12:13:25 -0400

    increase code coverage to 100% for StorageSiteUtil #4396

--- src/test/java/edu/harvard/iq/dataverse/locality/StorageSiteUtilTest.java
+++ src/test/java/edu/harvard/iq/dataverse/locality/StorageSiteUtilTest.java
@@ -1,6 +1,8 @@
 package edu.harvard.iq.dataverse.locality;
 
 import edu.harvard.iq.dataverse.util.json.JsonUtil;
+import java.util.ArrayList;
+import java.util.List;
 import javax.json.Json;
 import javax.json.JsonObjectBuilder;
 import org.junit.Test;
@@ -37,6 +39,52 @@ public class StorageSiteUtilTest {
         job.add(StorageSite.PRIMARY_STORAGE, true);
         job.add(StorageSite.TRANSFER_PROTOCOLS, "junk");
         StorageSiteUtil.parse(job.build());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNonBoolean() throws Exception {
+        JsonObjectBuilder job = Json.createObjectBuilder();
+        job.add(StorageSite.HOSTNAME, "myHostname");
+        job.add(StorageSite.NAME, "myName");
+        job.add(StorageSite.PRIMARY_STORAGE, "not a boolean");
+        job.add(StorageSite.TRANSFER_PROTOCOLS, "rsync");
+        StorageSiteUtil.parse(job.build());
+    }
+
+    @Test(expected = Exception.class)
+    public void testSecondPrimaryNotAllowed() throws Exception {
+        StorageSite newStorageSite = new StorageSite();
+        newStorageSite.setPrimaryStorage(true);
+        List<StorageSite> exitingSites = new ArrayList<>();
+        StorageSite existingSite1 = new StorageSite();
+        existingSite1.setPrimaryStorage(true);
+        exitingSites.add(existingSite1);
+        StorageSiteUtil.ensureOnlyOnePrimary(newStorageSite, exitingSites);
+    }
+
+    @Test
+    public void testSecondNonPrimaryIsAllowed() throws Exception {
+        StorageSite newStorageSite = new StorageSite();
+        List<StorageSite> exitingSites = new ArrayList<>();
+        StorageSite existingSite1 = new StorageSite();
+        existingSite1.setPrimaryStorage(true);
+        exitingSites.add(existingSite1);
+        StorageSiteUtil.ensureOnlyOnePrimary(newStorageSite, exitingSites);
+    }
+
+    @Test
+    public void testCanAddPrimaryWhenNoExistingPrimary() throws Exception {
+        StorageSite newStorageSite = new StorageSite();
+        newStorageSite.setPrimaryStorage(true);
+        List<StorageSite> exitingSites = new ArrayList<>();
+        StorageSite existingSite1 = new StorageSite();
+        exitingSites.add(existingSite1);
+        StorageSiteUtil.ensureOnlyOnePrimary(newStorageSite, exitingSites);
+    }
+
+    @Test
+    public void testJustForCodeCoverage() {
+        new StorageSiteUtil();
     }
 
 }
