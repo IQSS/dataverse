795f4ae4654564d6f77ef97ba1aa06e6f7fb5329
Pete Meyer <pameyer@crystal.harvard.edu>
2018-04-23 17:51:03 -0400

    starting dv/dcm test env

---
+++ conf/docker-dcm/configure_dcm.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "dcm configs on dv side to be done"
+
+# in homage to dataverse traditions, reset to insecure "burrito" admin API key
+sudo -u postgres psql -c "update apitoken set tokenstring='burrito' where id=1;"
+
+# dataverse configs for DCM
+curl -X PUT -d "SHA-1" "http://localhost:8080/api/admin/settings/:FileFixityChecksumAlgorithm"
+curl -X PUT "http://localhost:8080/api/admin/settings/:UploadMethods" -d "dcm/rsync+ssh"
+curl -X PUT "http://localhost:8080/api/admin/settings/:DataCaptureModuleUrl" -d "http://dcmsrv"
+
+# configure for RSAL downloads; but no workflows or RSAL yet
+curl -X PUT "http://localhost:8080/api/admin/settings/:DownloadMethods" -d "rsal/rsync"
+
+# symlink `hold` volume 
+ln -s /hold /usr/local/glassfish4/domains/domain1/files
+
---
+++ conf/docker-dcm/dcmsrv.dockerfile
@@ -0,0 +1,15 @@
+# build from repo root
+FROM centos:6
+RUN yum install -y epel-release
+COPY dcm-0.1-0.noarch.rpm /tmp/
+COPY bashrc /root/.bashrc
+COPY test_install.sh /root/
+RUN yum localinstall -y /tmp/dcm-0.1-0.noarch.rpm
+RUN pip install -r /opt/dcm/requirements.txt
+RUN /root/test_install.sh
+RUN useradd glassfish
+COPY entrypoint-dcm.sh /
+EXPOSE 80
+EXPOSE 22
+VOLUME /hold
+CMD ["/entrypoint-dcm.sh"]
