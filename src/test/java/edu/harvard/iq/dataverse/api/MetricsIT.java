b819c70d7443585a0f211426423f3c433df80c64
Philip Durbin <philip_durbin@harvard.edu>
2018-04-24 12:28:06 -0400

    assert that metrics API endpoints return 200 OK #4527

--- src/test/java/edu/harvard/iq/dataverse/api/MetricsIT.java
+++ src/test/java/edu/harvard/iq/dataverse/api/MetricsIT.java
@@ -2,6 +2,7 @@ package edu.harvard.iq.dataverse.api;
 
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.response.Response;
+import static javax.ws.rs.core.Response.Status.OK;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -16,30 +17,40 @@ public class MetricsIT {
     public void testGetDataverseByCategory() {
         Response response = UtilIT.metricsDataverseByCategory();
         response.prettyPrint();
+        response.then().assertThat()
+                .statusCode(OK.getStatusCode());
     }
 
     @Test
     public void testGetDataversesByMonth() {
         Response response = UtilIT.metricsDataversesByMonth();
         response.prettyPrint();
+        response.then().assertThat()
+                .statusCode(OK.getStatusCode());
     }
 
     @Test
     public void testGetDownloadsByMonth() {
         Response response = UtilIT.metricsDownloadsByMonth();
         response.prettyPrint();
+        response.then().assertThat()
+                .statusCode(OK.getStatusCode());
     }
 
     @Test
     public void testGetDatasetsByMonth() {
         Response response = UtilIT.metricsDatasetsByMonth();
         response.prettyPrint();
+        response.then().assertThat()
+                .statusCode(OK.getStatusCode());
     }
 
     @Test
     public void testGetDatasetsBySubject() {
         Response response = UtilIT.metricsDatasetsBySubject();
         response.prettyPrint();
+        response.then().assertThat()
+                .statusCode(OK.getStatusCode());
     }
 
 }
