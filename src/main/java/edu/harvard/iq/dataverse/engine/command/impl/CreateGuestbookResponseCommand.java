1537fa968bff415ade3fbf825abec774776531ce
Jim Myers <myersjd@umich.edu>
2018-05-02 13:37:26 -0400

    Revert "yet another"
    This reverts commit 61c138e9cb1e227d07bc2540a0da67b30b68ed1d.

--- src/main/java/edu/harvard/iq/dataverse/engine/command/impl/CreateGuestbookResponseCommand.java
+++ src/main/java/edu/harvard/iq/dataverse/engine/command/impl/CreateGuestbookResponseCommand.java
@@ -12,11 +12,8 @@ import edu.harvard.iq.dataverse.engine.command.CommandContext;
 import edu.harvard.iq.dataverse.engine.command.DataverseRequest;
 import edu.harvard.iq.dataverse.engine.command.RequiredPermissions;
 import edu.harvard.iq.dataverse.engine.command.exception.CommandException;
-import main.java.edu.harvard.iq.dataverse.FileDownloadHelper;
-
 import java.sql.Timestamp;
 import java.util.Date;
-import java.util.logging.Logger;
 
 /**
  *
@@ -25,7 +22,6 @@ import java.util.logging.Logger;
 @RequiredPermissions({})
 public class CreateGuestbookResponseCommand extends AbstractVoidCommand  {
     private final GuestbookResponse response;
-    private static final Logger logger = Logger.getLogger(CreateGuestbookResponseCommand.class.getCanonicalName());
     public CreateGuestbookResponseCommand(DataverseRequest aRequest, GuestbookResponse responseIn, Dataset affectedDataset) {
         super(aRequest, affectedDataset);
         response = responseIn;
@@ -35,7 +31,6 @@ public class CreateGuestbookResponseCommand extends AbstractVoidCommand  {
     protected void executeImpl(CommandContext ctxt) throws CommandException {
        Timestamp createDate = new Timestamp(new Date().getTime());
        response.setResponseTime(createDate);
-       logger.info("CGBRC: writeResponse: " + response.isWriteResponse());
        ctxt.responses().save(response);
     }
     
