cb0b33a7946ad7f21d85a7d666d619be0b4dd71a
Jayanthy Chengan <jayanthy.chengan@utoronto.ca>
2018-06-27 11:46:09 -0400

    removed comments

--- src/main/java/edu/harvard/iq/dataverse/DataverseLocaleBean.java
+++ src/main/java/edu/harvard/iq/dataverse/DataverseLocaleBean.java
@@ -24,7 +24,7 @@ import org.json.*;
 public class DataverseLocaleBean implements Serializable {
 
 
-    private static final Logger logger = Logger.getLogger(DataverseLocale.class.getCanonicalName());
+    private static final Logger logger = Logger.getLogger(DataverseLocaleBean.class.getCanonicalName());
 
     private List<DataverseLocale> dataverseLocales;
     private static Map<String,Object> languages;
@@ -42,13 +42,10 @@ public class DataverseLocaleBean implements Serializable {
     {
         if (context== null){
             locale= new Locale ("");
-            logger.info("  step1 ***");
         } else if(context.getViewRoot().getLocale().getLanguage()== "en_US" || context.getViewRoot().getLocale().getLanguage()== "en"){
             locale= new Locale ("");
-            logger.info("  step2 ***");
         }else{
             locale= context.getViewRoot().getLocale();
-            logger.info("  step3 ***");
         }
     }
 
@@ -68,7 +65,6 @@ public class DataverseLocaleBean implements Serializable {
             while ((read = reader.read(chars)) != -1)
                 buffer.append(chars, 0, read);
 
-
             return buffer.toString();
         } finally {
             if (reader != null)
@@ -76,35 +72,6 @@ public class DataverseLocaleBean implements Serializable {
         }
     }
 
-    public Map<String, Object> getCountriesInMap_Bkup() {
-
-        if(languages == null) {
-            try {
-
-                languages = new LinkedHashMap<String, Object>();
-
-
-                json_obj = new JSONObject(readUrl(json_url));
-                //note the saved affiliation is the "title" of the affiliates.json file
-
-                JSONArray json_array = json_obj.getJSONArray("languages");
-                // logger.info("  loading file ********************* " + json_url + "******* " + json_array.length());
-                for (int i = 0; i < json_array.length(); i++) {
-                    String title = json_array.getJSONObject(i).getString("title");
-                    String localeStr = json_array.getJSONObject(i).getString("locale");
-
-                    languages.put(title, new Locale(localeStr));
-                    //logger.info("  put languages ********************* " + title + " ** " + localeStr + " ** " + i);
-                }
-
-            } catch (Exception e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-
-        }
-        return languages;
-    }
 
     public List<DataverseLocale> getCountriesInMap() {
         dataverseLocales = new ArrayList<DataverseLocale>();
@@ -117,7 +84,6 @@ public class DataverseLocaleBean implements Serializable {
                 String localeStr = json_array.getJSONObject(i).getString("locale");
 
                 dataverseLocales.add(new DataverseLocale(i , title, new Locale(localeStr)));
-                //logger.info("  put countries ********************* " + title + " ** " + localeStr + " ** " + i);
             }
         } catch (Exception e) {
             // TODO Auto-generated catch block
@@ -128,98 +94,34 @@ public class DataverseLocaleBean implements Serializable {
     }
 
     public Locale getLocale(){
-        logger.info("  getting the locale  ********************* " + this.locale );
         return this.locale;
     }
 
     public void setLocale(Locale _locale){
-        logger.info("  Setting the locale  ********************* " + _locale   );
         this.locale = _locale;
     }
 
     public String getLocaleCode() {
-
-        logger.info("  getting the localeCode ********************* " + localeCode );
         return localeCode;
     }
 
-
     public void setLocaleCode(String localeCode) {
-
-        logger.info("  Setting the localeCode ********************* " + localeCode   );
         this.localeCode = localeCode;
-
     }
-
-
-    public void countryLocaleCodeChanged_BKup(ValueChangeEvent e){
-
-
-        String newLocaleValue = e.getNewValue().toString();
-
-        //System.out.println( "Changing locale value " + newLocaleValue );
-        logger.info("  Changing locale value ********************* " + newLocaleValue );
-
-
-        //loop a map to compare the locale code
-        for (Map.Entry<String, Object> entry : languages.entrySet()) {
-
-            if(entry.getValue().toString().equals(newLocaleValue)){
-
-                FacesContext.getCurrentInstance()
-                        .getViewRoot().setLocale((Locale) entry.getValue());
-
-                setLocaleCode(newLocaleValue);
-                setLocale((Locale) entry.getValue());
-
-                logger.info("  Changed to locale value ********************* " +  entry.getValue() );
-            }
-        }
-
-        try{
-
-            String url = ((HttpServletRequest)FacesContext.getCurrentInstance()
-                    .getExternalContext().getRequest()).getHeader("referer");
-            FacesContext.getCurrentInstance().getExternalContext().redirect(url);
-
-            //logger.info("  Changed to locale value ********************* " +  locale );
-
-        }catch(IOException ioe){
-            logger.log(Level.SEVERE, "rediect error", ioe);
-        }
-
-    }
-
 
     public void countryLocaleCodeChanged(ValueChangeEvent e){
-        logger.info("  countryLocaleCodeChanged ********************* "    );
-
-        //DataverseLocale newValue = (DataverseLocale)e.getNewValue();
-        //String lang = newValue.getDisplayName();
-
         String id = e.getNewValue().toString();
-
-        logger.info("  Changing locale value ********************* " + id );
 
         for (Iterator<DataverseLocale> it = dataverseLocales.iterator(); it.hasNext(); ) {
             DataverseLocale dL = it.next();
 
-            logger.info("   ********************* " );
-            logger.info("  get dL.getDisplayName()********************* " + dL.getDisplayName() );
-            logger.info("  get dL.getName()********************* " + dL.getName() );
-            logger.info("  get dL.getId()********************* " + dL.getId() );
-            logger.info("   ********************* " );
-
             if (dL.getId().toString().equals(id)) {
-
                 FacesContext.getCurrentInstance()
                         .getViewRoot().setLocale((Locale) dL.getName());
 
                 setLocaleCode(dL.getId().toString());
                 setLocale((Locale) dL.getName());
-
-                logger.info("  Changed to locale value ********************* " +  dL.getDisplayName()  );
-            }
+             }
         }
 
         try {
@@ -227,8 +129,6 @@ public class DataverseLocaleBean implements Serializable {
             String url = ((HttpServletRequest) FacesContext.getCurrentInstance()
                     .getExternalContext().getRequest()).getHeader("referer");
             FacesContext.getCurrentInstance().getExternalContext().redirect(url);
-
-            logger.info("  forwarded to locale value ********************* " +  locale );
 
         } catch (IOException ioe) {
             ioe.printStackTrace();
