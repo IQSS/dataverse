7d125b5912ee087c901126769cd1644bc2b5b6f2
Jim Myers <myersjd@umich.edu>
2018-07-23 15:54:02 -0400

    avoid second exception
    when testing for #3328, the call to
    idServiceBean.getProviderInformation() in the catch clause causes an
    javax.ejb.EJBException due to a null pointer exception that appears to
    be caused by an earlier javax.ejb.TransactionRolledbackLocalException
    when the 400 response is received from datacite.
    
    One way to avoid this is to retrieve the provider information prior to
    any exceptions. I've verified that this results in the
    dataset.publish.error being shown.

--- src/main/java/edu/harvard/iq/dataverse/engine/command/impl/FinalizeDatasetPublicationCommand.java
+++ src/main/java/edu/harvard/iq/dataverse/engine/command/impl/FinalizeDatasetPublicationCommand.java
@@ -25,6 +25,7 @@ import edu.harvard.iq.dataverse.workflow.WorkflowContext.TriggerType;
 import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.Date;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import edu.harvard.iq.dataverse.GlobalIdServiceBean;
@@ -181,6 +182,7 @@ public class FinalizeDatasetPublicationCommand extends AbstractPublishDatasetCom
         String protocol = getDataset().getProtocol();
         GlobalIdServiceBean idServiceBean = GlobalIdServiceBean.getBean(protocol, ctxt);
         if ( idServiceBean != null ){
+        	List<String> args = idServiceBean.getProviderInformation();
             try {
             	//A false return value indicates a failure in calling the service
             	if(!idServiceBean.publicizeIdentifier(dataset)) throw new Exception();
@@ -195,7 +197,7 @@ public class FinalizeDatasetPublicationCommand extends AbstractPublishDatasetCom
                 }
             } catch (Throwable e) {
                 ctxt.datasets().removeDatasetLocks(dataset, DatasetLock.Reason.pidRegister);
-                throw new CommandException(BundleUtil.getStringFromBundle("dataset.publish.error", idServiceBean.getProviderInformation()),this); 
+                throw new CommandException(BundleUtil.getStringFromBundle("dataset.publish.error", args),this); 
             }
         }
     }
