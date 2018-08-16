6664c88f414c91eb322c347e2229bf85887e1b33
Jim Myers <myersjd@umich.edu>
2018-08-16 16:13:38 -0400

    Handle versions

--- src/main/java/edu/harvard/iq/dataverse/workflow/internalspi/DPNSubmissionWorkflowStep.java
+++ src/main/java/edu/harvard/iq/dataverse/workflow/internalspi/DPNSubmissionWorkflowStep.java
@@ -3,27 +3,25 @@ package edu.harvard.iq.dataverse.workflow.internalspi;
 import edu.harvard.iq.dataverse.export.BagIt_Exporter;
 import edu.harvard.iq.dataverse.export.ExportException;
 import edu.harvard.iq.dataverse.export.ExportService;
-import edu.harvard.iq.dataverse.export.OAI_OREExporter;
 import edu.harvard.iq.dataverse.settings.SettingsServiceBean;
 import edu.harvard.iq.dataverse.workflow.WorkflowContext;
 import edu.harvard.iq.dataverse.workflow.step.Failure;
 import edu.harvard.iq.dataverse.workflow.step.WorkflowStep;
 import edu.harvard.iq.dataverse.workflow.step.WorkflowStepResult;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.ejb.EJB;
 
-import org.apache.commons.cli.Options;
+import org.apache.commons.codec.binary.Base64;
 import org.duracloud.client.ContentStore;
 import org.duracloud.client.ContentStoreManager;
 import org.duracloud.client.ContentStoreManagerImpl;
@@ -38,97 +36,93 @@ import org.duracloud.error.ContentStoreException;
  */
 public class DPNSubmissionWorkflowStep implements WorkflowStep {
 
-	@EJB
-	SettingsServiceBean settingsService;
+    @EJB
+    SettingsServiceBean settingsService;
 
-	private static final Logger logger = Logger.getLogger(DPNSubmissionWorkflowStep.class.getName());
-	private static final String DEFAULT_PORT = "443";
-	private static final String DEFAULT_CONTEXT = "durastore";
+    private static final Logger logger = Logger.getLogger(DPNSubmissionWorkflowStep.class.getName());
+    private static final String DEFAULT_PORT = "443";
+    private static final String DEFAULT_CONTEXT = "durastore";
 
-	private String host;
-	private String username;
-	private String password;
-	
+    private final Map<String, String> params;
 
-	private final Map<String, String> params;
+    public DPNSubmissionWorkflowStep(Map<String, String> paramSet) {
+        params = new HashMap<>(paramSet);
+    }
 
-	public DPNSubmissionWorkflowStep(Map<String, String> paramSet) {
-		params = new HashMap<>(paramSet);
-	}
+    @Override
+    public WorkflowStepResult run(WorkflowContext context) {
 
-	@Override
-	public WorkflowStepResult run(WorkflowContext context) {
-		for (String key : params.keySet()) {
-			logger.info("DPN Workflow Param: " + key + " : " + params.get(key));
-		}
-		String port = params.containsKey("port") ? params.get("port") : DEFAULT_PORT;
-		String dpnContext = params.containsKey("context") ? params.get("context") : DEFAULT_CONTEXT;
-		ContentStoreManager storeManager = new ContentStoreManagerImpl(params.get("host"), port, dpnContext);
-		Credential credential = new Credential(params.get("username"), params.get("password"));
-		storeManager.login(credential);
+        String port = params.containsKey("port") ? params.get("port") : DEFAULT_PORT;
+        String dpnContext = params.containsKey("context") ? params.get("context") : DEFAULT_CONTEXT;
+        ContentStoreManager storeManager = new ContentStoreManagerImpl(params.get("host"), port, dpnContext);
+        Credential credential = new Credential(params.get("username"), params.get("password"));
+        storeManager.login(credential);
 
-		ContentStore store;
-		try {
-			store = storeManager.getPrimaryContentStore();
+        String name = context.getDataset().getGlobalId().asString().replace(':', '-').replace('/', '-')
+                .replace('.', '-').toLowerCase();
 
-			String name = context.getDataset().getGlobalId().asString().replace(':', '-').replace('/', '-').replace('.','-')
-					.toLowerCase();
-			store.createSpace(name);
-			// Store file
+        ContentStore store;
+        try {
+            store = storeManager.getPrimaryContentStore();
 
-			String contentId = name + ".zip";
-			String checksum = store.addContent(name, contentId,
-					ExportService.getInstance(settingsService).getExport(context.getDataset(), BagIt_Exporter.NAME),
-					-1l, null, null, null);
-			System.out.println("Content added with checksum: " + checksum);
+            store.createSpace(name);
+        } catch (ContentStoreException e) {
+            // TODO Auto-generated catch block
+            logger.warning(e.getMessage());
+            e.printStackTrace();
+            String mesg = "DPN Submission Failure";
+            if (!(context.getNextVersionNumber() == 1) || !(context.getNextMinorVersionNumber() == 0)) {
+                mesg = mesg + ": Prior Version archiving not yet complete?";
+            }
+            return new Failure("Unable to create DPN space with name: " + name, mesg);
+        }
+        // Store file
 
-			// Print contents of space after content addition
-			System.out.println("\n\n*** Content Listing of " + name + " - after file is added ***\n\n");
-			printContentListing(store, name);
-			
-			logger.info("DPN step:");
-			logger.log(Level.INFO, "Submitted {0} to DPN", name);
-			logger.log(Level.INFO, "Dataset id:{0}", context.getDataset().getId());
-			logger.log(Level.INFO, "Trigger Type {0}", context.getType());
-			logger.log(Level.INFO, "Next version:{0}.{1} isMinor:{2}", new Object[] { context.getNextVersionNumber(),
-					context.getNextMinorVersionNumber(), context.isMinorRelease() });
+        String contentId = name + ".v" + context.getNextVersionNumber() + "." + context.getNextMinorVersionNumber()
+                + ".zip";
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
 
-		} catch (ContentStoreException | ExportException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		// Delete file
-		// store.deleteContent(spaceId, contentId);
+            DigestInputStream digestInputStream = new DigestInputStream(
+                    ExportService.getInstance(settingsService).getExport(context.getDataset(), BagIt_Exporter.NAME),
+                    messageDigest);
 
-		// Print contents of space after content is removed
-		// System.out.println("\n\n*** Content Listing of " + spaceId +
-		// " - after file is deleted ***\n\n");
-		// printContentListing(store, spaceId);
+            String checksum = store.addContent(name, contentId, digestInputStream, -1l, null, null, null);
+            logger.info("Content: " + name + " added with checksum: " + checksum);
+            if (!checksum.equals(Base64.encodeBase64String(digestInputStream.getMessageDigest().digest()))) {
+                return new Failure("Error in transferring file to DPN",
+                        "DPN Submission Failure: incomplete archive transfer");
+            }
 
-		// System.out.println("\n\nSimple API Example process complete.");
-		// }
+            logger.info("DPN step:");
+            logger.log(Level.FINE, "Submitted {0} to DPN", name);
+            logger.log(Level.FINE, "Dataset id:{0}", context.getDataset().getId());
+            logger.log(Level.FINE, "Trigger Type {0}", context.getType());
+            logger.log(Level.FINE, "Next version:{0}.{1} isMinor:{2}", new Object[] { context.getNextVersionNumber(),
+                    context.getNextMinorVersionNumber(), context.isMinorRelease() });
 
+        } catch (ContentStoreException | ExportException | IOException e) {
+            // TODO Auto-generated catch block
+            logger.warning(e.getMessage());
+            e.printStackTrace();
+            return new Failure("Error in transferring file to DPN",
+                    "DPN Submission Failure: archive file not transferred");
+        } catch (NoSuchAlgorithmException e) {
+            logger.severe("MD5 MessageDigest not available!");
+        }
 
-		return WorkflowStepResult.OK;
-	}
+        return WorkflowStepResult.OK;
+    }
 
-	/**
-	 * Prints contents of space
-	 */
-	private void printContentListing(ContentStore store, String spaceId) throws ContentStoreException {
-		Iterator<String> spaceContents = store.getSpaceContents(spaceId, null);
-		while (spaceContents.hasNext()) {
-			System.out.println(spaceContents.next());
-		}
-	}
+    @Override
+    public WorkflowStepResult resume(WorkflowContext context, Map<String, String> internalData, String externalData) {
+        throw new UnsupportedOperationException("Not supported yet."); // This class does not need to resume.
+    }
 
-	@Override
-	public WorkflowStepResult resume(WorkflowContext context, Map<String, String> internalData, String externalData) {
-		throw new UnsupportedOperationException("Not supported yet."); // This class does not need to resume.
-	}
-
-	@Override
-	public void rollback(WorkflowContext context, Failure reason) {
-		logger.log(Level.INFO, "rolling back workflow invocation {0}", context.getInvocationId());
-	}
+    @Override
+    public void rollback(WorkflowContext context, Failure reason) {
+        logger.log(Level.INFO, "rolling back workflow invocation {0}", context.getInvocationId());
+        logger.warning("Manual cleanup of DPN Space: " + context.getDataset().getGlobalId().asString().replace(':', '-')
+                .replace('/', '-').replace('.', '-').toLowerCase() + " may be required");
+    }
 }
