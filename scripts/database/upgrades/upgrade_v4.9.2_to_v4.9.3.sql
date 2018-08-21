4062addef96fecdd24a781c25c835419389fa68f
Benjamin Martinez <benjamin.martiinez@gmail.com>
2018-08-21 11:54:12 -0400

    New SQL script to drop 5 columns from builtin user table #4565

---
+++ scripts/database/upgrades/upgrade_v4.9.2_to_v4.9.3.sql
@@ -0,0 +1,5 @@
+ALTER TABLE builtinuser REMOVE COLUMN affiliation character varying(255);
+ALTER TABLE builtinuser REMOVE COLUMN email character varying(255);
+ALTER TABLE builtinuser REMOVE COLUMN firstname character varying(255);
+ALTER TABLE builtinuser REMOVE COLUMN lastname character varying(255);
+ALTER TABLE builtinuser REMOVE COLUMN "position" character varying(255);
