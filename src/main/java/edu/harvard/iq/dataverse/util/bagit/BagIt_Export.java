90eaa103a2f5500ad18e33ff710ac5ce2f3f9fb1
Jim Myers <myersjd@umich.edu>
2018-08-24 11:35:57 -0400

    need ORE map for specific version

--- src/main/java/edu/harvard/iq/dataverse/util/bagit/BagIt_Export.java
+++ src/main/java/edu/harvard/iq/dataverse/util/bagit/BagIt_Export.java
@@ -9,6 +9,7 @@ import edu.harvard.iq.dataverse.export.OAI_OREExporter;
 import edu.harvard.iq.dataverse.settings.SettingsServiceBean;
 import edu.harvard.iq.dataverse.Dataset;
 
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -25,11 +26,10 @@ public class BagIt_Export {
     public static void exportDatasetVersionAsBag(DatasetVersion version, ApiToken apiToken,
             SettingsServiceBean settingsService, OutputStream outputStream) throws Exception {
 
-        Dataset dataset = version.getDataset();
-        InputStream mapInputStream = ExportService.getInstance(settingsService).getExport(dataset,
-                OAI_OREExporter.NAME);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ExportService.getInstance(settingsService).exportFormatToStream(version, OAI_OREExporter.NAME, out);
         JsonParser jsonParser = new JsonParser();
-        JsonObject oremap = (JsonObject) jsonParser.parse(new InputStreamReader(mapInputStream, "UTF-8"));
+        JsonObject oremap = (JsonObject) jsonParser.parse(out.toString("UTF-8"));
 
         BagGenerator bagger = new BagGenerator(oremap);
         bagger.setAuthenticationKey(apiToken.getTokenString());
