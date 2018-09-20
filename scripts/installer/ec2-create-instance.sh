42c73916e44169c3c1da1f51646e616dedda632d
Philip Durbin <philip_durbin@harvard.edu>
2018-09-19 21:52:11 -0400

    create key pair per instance #4990

--- scripts/installer/ec2-create-instance.sh
+++ scripts/installer/ec2-create-instance.sh
@@ -34,18 +34,18 @@ else
   echo "*Security group already exists."
 fi
 
-echo "*Checking for existing key pair"
-if ! [ -f devenv-key.pem ]; then
-  echo "*Creating key pair"
-  PRIVATE_KEY=$(aws ec2 create-key-pair --key-name devenv-key --query 'KeyMaterial' --output text)
-  if [[ $PRIVATE_KEY == '-----BEGIN RSA PRIVATE KEY-----'* ]]; then
-    printf -- "$PRIVATE_KEY" >devenv-key.pem
-    chmod 400 devenv-key.pem
-    echo "*New key pair created"
-  fi
-  echo "*End creating key pair"
+RANDOM_STRING="$(uuidgen | cut -c-8)"
+KEY_NAME="key-$USER-$RANDOM_STRING"
+
+PRIVATE_KEY=$(aws ec2 create-key-pair --key-name $KEY_NAME --query 'KeyMaterial' --output text)
+if [[ $PRIVATE_KEY == '-----BEGIN RSA PRIVATE KEY-----'* ]]; then
+  PEM_FILE="$KEY_NAME.pem"
+  printf -- "$PRIVATE_KEY" >$PEM_FILE
+  chmod 400 $PEM_FILE
+  echo "Your newly created private key file is \"$PEM_FILE\". Keep it secret. Keep it safe."
 else
-  echo "*Key pair alraedy exists."
+  echo "Could not create key pair. Exiting."
+  exit 1
 fi
 
 #AMI ID for centos7 acquired by this (very slow) query Sept 10th 2018
@@ -56,22 +56,24 @@ fi
 #Using this image ID a 1-time requires subscription per root account, which was done through the UI
 #Also, change the instance size as your own peril. Previous attempts of setting it smaller than medium have caused solr and maven to crash weirdly during install
 echo "*Creating ec2 instance"
-INSTANCE_ID=$(aws ec2 run-instances --image-id ami-9887c6e7 --security-groups devenv-sg --count 1 --instance-type t2.medium --key-name devenv-key --query 'Instances[0].InstanceId' --block-device-mappings '[ { "DeviceName": "/dev/sda1", "Ebs": { "DeleteOnTermination": true } } ]' | tr -d \")
+INSTANCE_ID=$(aws ec2 run-instances --image-id ami-9887c6e7 --security-groups devenv-sg --count 1 --instance-type t2.medium --key-name $KEY_NAME --query 'Instances[0].InstanceId' --block-device-mappings '[ { "DeviceName": "/dev/sda1", "Ebs": { "DeleteOnTermination": true } } ]' | tr -d \")
 echo "Instance ID: "$INSTANCE_ID
 echo "*End creating EC2 instance"
 
 PUBLIC_DNS=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[*].Instances[*].[PublicDnsName]" --output text)
 PUBLIC_IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[*].Instances[*].[PublicIpAddress]" --output text)
 
-echo "Connecting to the instance. This may take a minute as it is being spun up"
+USER_AT_HOST="centos@${PUBLIC_DNS}"
+echo "New instance created with ID \"$INSTANCE_ID\". To ssh into it:"
+echo "ssh -i $PEM_FILE $USER_AT_HOST"
 
-echo "New EC2 instance created at $PUBLIC_DNS"
+echo "Please wait at least 15 minutes while the branch \"$BRANCH_NAME\" is being deployed."
 
 #ssh into instance now and run ansible stuff
 #Note: an attempt was made to pass the branch name in the ansible-playbook call
 # via -e "dataverse.branch=$BRANCH_NAME", but it gets overwritten due to the order
 # of operations for where ansible looks for variables.
-ssh -i devenv-key.pem -o 'StrictHostKeyChecking no' -o 'UserKnownHostsFile=/dev/null' -o 'ConnectTimeout=300' centos@${PUBLIC_DNS} <<EOF
+ssh -T -i $PEM_FILE -o 'StrictHostKeyChecking no' -o 'UserKnownHostsFile=/dev/null' -o 'ConnectTimeout=300' $USER_AT_HOST <<EOF
 sudo yum -y install git nano ansible
 git clone https://github.com/IQSS/dataverse-ansible.git dataverse
 export ANSIBLE_ROLES_PATH=.
@@ -79,4 +81,7 @@ sed -i "s/branch:/branch: $BRANCH_NAME/" dataverse/defaults/main.yml
 ansible-playbook -i dataverse/inventory dataverse/dataverse.pb --connection=local
 EOF
 
-echo "New EC2 instance created at $PUBLIC_DNS (Public IP $PUBLIC_IP ). When you are done, please terminate your instance with: aws ec2 terminate-instances --instance-ids $INSTANCE_ID"
+echo "To ssh into the new instance:"
+echo "ssh -i $PEM_FILE $USER_AT_HOST"
+echo "When you are done, please terminate your instance with:"
+echo "aws ec2 terminate-instances --instance-ids $INSTANCE_ID"
