73179984c2f972faed875d944acd917337ed7640
Philip Durbin <philip_durbin@harvard.edu>
2018-09-21 16:45:45 -0400

    cleanup #4990

--- scripts/installer/ec2-create-instance.sh
+++ scripts/installer/ec2-create-instance.sh
@@ -1,5 +1,6 @@
-#!/bin/bash -x
-#Initially Referred to this doc: https://docs.aws.amazon.com/cli/latest/userguide/tutorial-ec2-ubuntu.html
+#!/bin/bash
+
+# For docs, see the "Deployment" page in the Dev Guide.
 
 SUGGESTED_REPO_URL='https://github.com/IQSS/dataverse.git'
 SUGGESTED_BRANCH='develop'
@@ -25,7 +26,6 @@ while getopts ":r:b:" o; do
   esac
 done
 
-#Make sure "aws" binary is available
 AWS_CLI_VERSION=$(aws --version)
 if [[ "$?" -ne 0 ]]; then
   echo 'The "aws" program could not be executed. Is it in your $PATH?'
@@ -44,7 +44,6 @@ if [[ $(git ls-remote --heads $REPO_URL $BRANCH_NAME | wc -l) -eq 0 ]]; then
   exit 1
 fi
 
-#Create security group if it doesn't already exist
 SECURITY_GROUP='dataverse-sg'
 GROUP_CHECK=$(aws ec2 describe-security-groups --group-name $SECURITY_GROUP)
 if [[ "$?" -ne 0 ]]; then
@@ -70,17 +69,21 @@ else
   exit 1
 fi
 
-#AMI ID for centos7 acquired by this (very slow) query Sept 10th 2018
-#This does not need to be run every time, leaving it in here so it is remembered
-#aws ec2 describe-images  --owners 'aws-marketplace' --filters 'Name=product-code,Values=aw0evgkw8e5c1q413zgy5pjce' --query 'sort_by(Images, &CreationDate)[-1].[ImageId]' --output 'text'
-
-#The AMI ID only works for region us-east-1, for now just forcing that
-#Using this image ID a 1-time requires subscription per root account, which was done through the UI
-#Also, change the instance size as your own peril. Previous attempts of setting it smaller than medium have caused solr and maven to crash weirdly during install
-echo "*Creating ec2 instance"
-INSTANCE_ID=$(aws ec2 run-instances --image-id ami-9887c6e7 --security-groups $SECURITY_GROUP --count 1 --instance-type t2.medium --key-name $KEY_NAME --query 'Instances[0].InstanceId' --block-device-mappings '[ { "DeviceName": "/dev/sda1", "Ebs": { "DeleteOnTermination": true } } ]' | tr -d \")
+# The AMI ID may change in the future and the way to look it up is with the
+# following command, which takes a long time to run:
+#
+# aws ec2 describe-images  --owners 'aws-marketplace' --filters 'Name=product-code,Values=aw0evgkw8e5c1q413zgy5pjce' --query 'sort_by(Images, &CreationDate)[-1].[ImageId]' --output 'text'
+#
+# To use this AMI, we subscribed to it from the AWS GUI.
+# AMI IDs are specific to the region.
+AMI_ID='ami-9887c6e7'
+# Smaller than medium lead to Maven and Solr problems.
+SIZE='t2.medium'
+echo "Creating EC2 instance"
+# TODO: Add some error checking for "ec2 run-instances".
+INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --security-groups $SECURITY_GROUP --count 1 --instance-type $SIZE --key-name $KEY_NAME --query 'Instances[0].InstanceId' --block-device-mappings '[ { "DeviceName": "/dev/sda1", "Ebs": { "DeleteOnTermination": true } } ]' | tr -d \")
 echo "Instance ID: "$INSTANCE_ID
-echo "*End creating EC2 instance"
+echo "End creating EC2 instance"
 
 PUBLIC_DNS=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[*].Instances[*].[PublicDnsName]" --output text)
 PUBLIC_IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[*].Instances[*].[PublicIpAddress]" --output text)
@@ -91,6 +94,7 @@ echo "ssh -i $PEM_FILE $USER_AT_HOST"
 
 echo "Please wait at least 15 minutes while the branch \"$BRANCH_NAME\" from $REPO_URL is being deployed."
 
+# TODO: Add some error checking for this ssh command.
 ssh -T -i $PEM_FILE -o 'StrictHostKeyChecking no' -o 'UserKnownHostsFile=/dev/null' -o 'ConnectTimeout=300' $USER_AT_HOST <<EOF
 sudo yum -y install epel-release
 sudo yum -y install git nano ansible
@@ -99,9 +103,9 @@ export ANSIBLE_ROLES_PATH=.
 ansible-playbook -i dataverse/inventory dataverse/dataverse.pb --connection=local --extra-vars "dataverse_branch=$BRANCH_NAME dataverse_repo=$REPO_URL"
 EOF
 
-#Port 8080 has been added because Ansible puts a redirect in place
-#from HTTP to HTTPS and the cert is invalid (self-signed), forcing
-#the user to click through browser warnings.
+# Port 8080 has been added because Ansible puts a redirect in place
+# from HTTP to HTTPS and the cert is invalid (self-signed), forcing
+# the user to click through browser warnings.
 CLICKABLE_LINK="http://${PUBLIC_DNS}:8080"
 echo "To ssh into the new instance:"
 echo "ssh -i $PEM_FILE $USER_AT_HOST"
