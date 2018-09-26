e44e1b57d6c71a38968cb2e5f8d37d41fd1066ed
Jim Myers <myersjd@umich.edu>
2018-09-26 17:42:10 -0400

    static URI for dvcore

--- src/main/java/edu/harvard/iq/dataverse/util/json/JsonLDNamespace.java
+++ src/main/java/edu/harvard/iq/dataverse/util/json/JsonLDNamespace.java
@@ -1,7 +1,5 @@
 package edu.harvard.iq.dataverse.util.json;
 
-import edu.harvard.iq.dataverse.util.SystemConfig;
-
 public class JsonLDNamespace {
 
 	String prefix;
@@ -9,8 +7,7 @@ public class JsonLDNamespace {
 	
 	String url;
 
-	//FixMe - use a universal Dataverse URL rather than an instance one for Core terms
-	public static JsonLDNamespace dvcore = new JsonLDNamespace("dvcore", SystemConfig.getDataverseSiteUrlStatic() + "/schema/core#");
+	public static JsonLDNamespace dvcore = new JsonLDNamespace("dvcore", "https://dataverse.org/schema/core#");
 	public static JsonLDNamespace dcterms = new JsonLDNamespace("dcterms","http://purl.org/dc/terms/");
 	public static JsonLDNamespace ore = new JsonLDNamespace("ore","http://www.openarchives.org/ore/terms/");
 	public static JsonLDNamespace schema = new JsonLDNamespace("schema","http://schema.org/");
