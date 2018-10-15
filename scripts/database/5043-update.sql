657b436b41bc101fe6167344641244ba12eb3b81
Jim Myers <myersjd@umich.edu>
2018-10-15 10:55:34 -0400

    moving required db changes to main upgrade script

--- scripts/database/5043-update.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE datasetfieldtype ADD COLUMN uri text;
-ALTER TABLE metadatablock ADD COLUMN namespaceuri text;
--- scripts/database/upgrades/upgrade_v4.9.4_to_v4.10.sql
+++ scripts/database/upgrades/upgrade_v4.9.4_to_v4.10.sql
@@ -1,4 +1,6 @@
 ALTER TABLE usernotification
 ADD requestor_id BIGINT;
+ALTER TABLE datasetfieldtype ADD COLUMN uri text;
+ALTER TABLE metadatablock ADD COLUMN namespaceuri text;
 
 
