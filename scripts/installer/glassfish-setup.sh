49979a5e9cec54ad1b624ad01805866f5e0fafca
Don Sizemore <don.sizemore@gmail.com>
2018-10-16 07:43:23 -0400

    fix literal escape

--- scripts/installer/glassfish-setup.sh
+++ scripts/installer/glassfish-setup.sh
@@ -74,10 +74,12 @@ function preliminary_setup()
   # DataCite DOI Settings
   # (we can no longer offer EZID with their shared test account)
   # jvm-options use colons as separators, escape as literal
-  DOI_BASEURL_ESC=`echo $DOI_BASEURL | sed -e 's/:/\\:/'`
+  DOI_BASEURL_ESC=`echo $DOI_BASEURL | sed -e 's/:/\\\:/'`
+  echo "${DOI_BASEURL}"
+  echo "$DOI_BASEURL_ESC"
   ./asadmin $ASADMIN_OPTS create-jvm-options "\-Ddoi.username=${DOI_USERNAME}"
   ./asadmin $ASADMIN_OPTS create-jvm-options "\-Ddoi.password=${DOI_PASSWORD}"
-  ./asadmin $ASADMIN_OPTS create-jvm-options "\-Ddoi.baseurlstring=${DOI_BASEURL_ESC}"
+  ./asadmin $ASADMIN_OPTS create-jvm-options "\-Ddoi.baseurlstring=$DOI_BASEURL_ESC"
 
   ./asadmin $ASADMIN_OPTS create-jvm-options "-Ddataverse.timerServer=true"
   # enable comet support
