8e168d84fa0302599b14f4f15f96f23bb09661e2
chenganj <jayanthy.chengan@utoronto.ca>
2018-11-15 13:14:53 -0500

    using DateFormat.DEFAULT instead of SimpleDateFormat

--- src/main/java/edu/harvard/iq/dataverse/util/DateUtil.java
+++ src/main/java/edu/harvard/iq/dataverse/util/DateUtil.java
@@ -3,12 +3,10 @@ package edu.harvard.iq.dataverse.util;
 import edu.harvard.iq.dataverse.DataverseLocaleBean;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 
 /**
  *
@@ -16,62 +14,34 @@ import java.util.logging.Logger;
  */
 public class DateUtil {
 
-    private static final Logger logger = Logger.getLogger(DateUtil.class.getCanonicalName());
-
-
     public static String formatDate(Date dateToformat) {
-        try
-        {
+        String formattedDate;
+        DateFormat dateFormatter;
+        try{
             DataverseLocaleBean d = new DataverseLocaleBean();
-            Locale bundle_locale = new Locale(d.getLocaleCode());
+            Locale currentLocale = new Locale(d.getLocaleCode());
 
-            String formattedDate = "";
-
-            if(bundle_locale.getLanguage().contains("fr"))
-            {
-                DateFormat df = new SimpleDateFormat("dd MMM yyyy",bundle_locale);
-                formattedDate = df.format(dateToformat);
-            }
-            else {
-                DateFormat df = new SimpleDateFormat("MMM dd, yyyy", bundle_locale);
-                formattedDate = df.format(dateToformat);
-            }
+            dateFormatter = DateFormat.getDateInstance(DateFormat.DEFAULT, currentLocale);
+            formattedDate = dateFormatter.format(dateToformat);
 
             return formattedDate;
         }
-        catch(Exception e)
-        {
+        catch(Exception e) {
             return null;
         }
     }
 
-    public static String formatDate(String dateToformat) {
-
-        try
-        {
-            DataverseLocaleBean d = new DataverseLocaleBean();
-            Locale bundle_locale = new Locale(d.getLocaleCode());
-
-            String formattedDate = "";
-
-            DateFormat inputFormat = new SimpleDateFormat("MMM dd, yyyy");
-            Date _date = inputFormat.parse(dateToformat);
-
-            if(bundle_locale.getLanguage().contains("fr"))
-            {
-                DateFormat df = new SimpleDateFormat("dd MMM yyyy",bundle_locale);
-                formattedDate = df.format(_date);
-            }
-            else {
-                DateFormat df = new SimpleDateFormat("MMM dd, yyyy", bundle_locale);
-                formattedDate = df.format(_date);
-            }
-
+    public static String formatDate(String dateToformat){
+        String formattedDate = "";
+        DateFormat inputFormat = DateFormat.getDateInstance(DateFormat.DEFAULT);
+        Date _date = null;
+        try {
+            _date = inputFormat.parse(dateToformat);
+            formattedDate = formatDate(_date);
             return formattedDate;
-        }
-        catch(Exception e)
-        {
+        } catch (ParseException e) {
+            e.printStackTrace();
             return null;
         }
-    }
+     }
 }
