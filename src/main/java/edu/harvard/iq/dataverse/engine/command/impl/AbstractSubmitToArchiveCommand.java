a474393ce5b445411cafc9f9c6f76312cc66a01c
Jim Myers <myersjd@umich.edu>
2018-12-06 14:24:28 -0500

    extend AbstractCommand

--- src/main/java/edu/harvard/iq/dataverse/engine/command/impl/AbstractSubmitToArchiveCommand.java
+++ src/main/java/edu/harvard/iq/dataverse/engine/command/impl/AbstractSubmitToArchiveCommand.java
@@ -5,7 +5,7 @@ import edu.harvard.iq.dataverse.DvObject;
 import edu.harvard.iq.dataverse.authorization.Permission;
 import edu.harvard.iq.dataverse.authorization.users.ApiToken;
 import edu.harvard.iq.dataverse.authorization.users.AuthenticatedUser;
-import edu.harvard.iq.dataverse.engine.command.Command;
+import edu.harvard.iq.dataverse.engine.command.AbstractCommand;
 import edu.harvard.iq.dataverse.engine.command.CommandContext;
 import edu.harvard.iq.dataverse.engine.command.DataverseRequest;
 import edu.harvard.iq.dataverse.engine.command.RequiredPermissions;
@@ -13,28 +13,21 @@ import edu.harvard.iq.dataverse.engine.command.exception.CommandException;
 import edu.harvard.iq.dataverse.settings.SettingsServiceBean;
 import edu.harvard.iq.dataverse.workflow.step.WorkflowStepResult;
 
-import static edu.harvard.iq.dataverse.engine.command.CommandHelper.CH;
-
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 import java.util.logging.Logger;
 
 @RequiredPermissions(Permission.PublishDataset)
-public abstract class AbstractSubmitToArchiveCommand implements Command<DatasetVersion> {
+public abstract class AbstractSubmitToArchiveCommand extends AbstractCommand<DatasetVersion> {
 
     private final DatasetVersion version;
-    private final DataverseRequest request;
-    private final Map<String, DvObject> affectedDvObjects;
     private final Map<String, String> requestedSettings = new HashMap<String, String>();
     private static final Logger logger = Logger.getLogger(AbstractSubmitToArchiveCommand.class.getName());
     
     public AbstractSubmitToArchiveCommand(DataverseRequest aRequest, DatasetVersion version) {
-        this.request = aRequest;
+        super(aRequest, version.getDataset());
         this.version = version;
-        affectedDvObjects = new HashMap<>();
-        affectedDvObjects.put("", version.getDataset());
     }
 
     @Override
@@ -51,35 +44,13 @@ public abstract class AbstractSubmitToArchiveCommand implements Command<DatasetV
             }
         }
         
-        AuthenticatedUser user = request.getAuthenticatedUser();
+        AuthenticatedUser user = getRequest().getAuthenticatedUser();
         ApiToken token = ctxt.authentication().findApiTokenByUser(user);
         if ((token == null) || (token.getExpireTime().before(new Date()))) {
             token = ctxt.authentication().generateApiTokenForUser(user);
         }
         performArchiveSubmission(version, token, requestedSettings);
         return ctxt.em().merge(version);
-    }
-
-    @Override
-    public Map<String, DvObject> getAffectedDvObjects() {
-
-        return affectedDvObjects;
-    }
-
-    @Override
-    public DataverseRequest getRequest() {
-        return request;
-    }
-
-    @Override
-    public Map<String, Set<Permission>> getRequiredPermissions() {
-        return CH.permissionsRequired(getClass());
-    }
-
-    @Override
-    public String describe() {
-        return "DatasetVersion: [" + version.getId() + " " + version.getDataset().getDisplayName() + ".v"
-                + version.getFriendlyVersionNumber()+"]";
     }
 
     /**
@@ -94,5 +65,11 @@ public abstract class AbstractSubmitToArchiveCommand implements Command<DatasetV
      * @param requestedSettings - a map of the names/values for settings required by this archiver (sent because this class is not part of the EJB context (by design) and has no direct access to service beans).
      */
     abstract public WorkflowStepResult performArchiveSubmission(DatasetVersion version, ApiToken token, Map<String, String> requestedSetttings);
+
+    @Override
+    public String describe() {
+        return super.describe() + "DatasetVersion: [" + version.getId() + " (v"
+                + version.getFriendlyVersionNumber()+")]";
+    }
 
 }
