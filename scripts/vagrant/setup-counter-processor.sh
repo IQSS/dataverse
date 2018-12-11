e835e064239e452763d6264095c20998358a1b30
Philip Durbin <philip_durbin@harvard.edu>
2018-12-11 15:10:45 -0500

    stand up counter-processor, continued #4821

--- scripts/vagrant/setup-counter-processor.sh
+++ scripts/vagrant/setup-counter-processor.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 echo "Setting up counter-processor"
 echo "Installing dependencies"
-yum -y install unzip
+yum -y install unzip vim-enhanced
 yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 # EPEL provides Python 3.6.6, new enough (3.6.4 in .python-version)
 yum -y install python36 jq
 # "ensurepip" tip from https://stackoverflow.com/questions/50408941/recommended-way-to-install-pip3-on-centos7/52518512#52518512
 python3.6 -m ensurepip
+# FIXME: actually use this dedicated "counter" user.
 COUNTER_USER=counter
 echo "Ensuring Unix user '$COUNTER_USER' exists"
 useradd $COUNTER_USER || :
@@ -17,20 +18,28 @@ if [ ! -e $UNZIPPED_DIR ]; then
   echo "Downloading and unzipping $ZIP_FILE"
   wget https://github.com/CDLUC3/counter-processor/archive/$ZIP_FILE
   unzip $ZIP_FILE
-  # wget https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz
-  # tar xfz GeoLite2-Country.tar.gz
-  # TODO: put GeoLite2-Country.mmdb in maxmind_geoip directory.
 fi
 cd $UNZIPPED_DIR
+GEOIP_DIR='maxmind_geoip'
+GEOIP_FILE='GeoLite2-Country.mmdb'
+GEOIP_PATH_TO_FILE="$GEOIP_DIR/$GEOIP_FILE"
+if [ ! -e $GEOIP_PATH_TO_FILE ]; then
+  echo "let's do this thing"
+  TARBALL='GeoLite2-Country.tar.gz'
+  wget https://geolite.maxmind.com/download/geoip/database/$TARBALL
+  tar xfz GeoLite2-Country.tar.gz
+  # Glob (*) below because of directories like "GeoLite2-Country_20181204".
+  GEOIP_UNTARRED_DIR='GeoLite2-Country_*'
+  mv $GEOIP_UNTARRED_DIR/$GEOIP_FILE $GEOIP_PATH_TO_FILE
+  rm -rf $TARBALL $GEOIP_UNTARRED_DIR
+fi
 pip3 install -r requirements.txt
-# trying to parse sample_logs/counter_2018-05-08.log
-cp -r sample_logs log
-# `touch` to avoid this error: No such file or directory: 'log/counter_2018-05-01.log'
+# For now, parsing sample_logs/counter_2018-05-08.log
 for i in `echo {00..31}`; do
-  touch log/counter_2018-05-$i.log
+  # avoid errors like: No such file or directory: 'sample_logs/counter_2018-05-01.log'
+  touch sample_logs/counter_2018-05-$i.log
 done
-CONFIG_FILE=/dataverse/scripts/vagrant/counter-processor-config.yaml
-LOG_GLOB="sample_logs/counter_2018-05-*.log"
-START_DATE="2018-05-08"
-END_DATE="2018-05-09"
-python36 main.py
+#LOG_GLOB="sample_logs/counter_2018-05-*.log"
+#START_DATE="2018-05-08"
+#END_DATE="2018-05-09"
+CONFIG_FILE=/dataverse/scripts/vagrant/counter-processor-config.yaml python36 main.py
