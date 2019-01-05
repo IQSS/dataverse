3e94330549c2c847d412fdccfe3b98010396ae27
Jim Myers <myersjd@umich.edu>
2019-01-05 17:01:27 -0500

    increase to max limit

--- src/main/java/edu/harvard/iq/dataverse/EditDatafilesPage.java
+++ src/main/java/edu/harvard/iq/dataverse/EditDatafilesPage.java
@@ -1742,7 +1742,7 @@ public class EditDatafilesPage implements java.io.Serializable {
         //Assume file is not over the limit
 
         GetMethod hypothesisMethod = new GetMethod("https://hypothes.is/api/search?group="
-            + hypothesisGroupSelection + "&uri=" + hypothesisUrlSelection);
+            + hypothesisGroupSelection + "&uri=" + hypothesisUrlSelection + "&limit=200");
         String apiKey = getHypothesisKey();
         hypothesisMethod.addRequestHeader("Authorization","Bearer " + apiKey);
         
