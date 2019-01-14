f6cc0898d63579201c5f418f77d626e2ab52b689
Jim Myers <myersjd@umich.edu>
2019-01-14 15:18:33 -0500

    moving db upgrade command

--- scripts/database/upgrades/upgrade_v4.9.4_to_v4.10.sql
+++ scripts/database/upgrades/upgrade_v4.9.4_to_v4.10.sql
@@ -3,7 +3,6 @@ ADD requestor_id BIGINT;
 ALTER TABLE datasetfieldtype ADD COLUMN uri text;
 ALTER TABLE metadatablock ADD COLUMN namespaceuri text;
 ALTER TABLE pendingworkflowinvocation ADD COLUMN datasetexternallyreleased BOOLEAN;
-ALTER TABLE datasetversion ADD COLUMN archivalcopylocation text;
 
 INSERT INTO setting(
            name, content)
