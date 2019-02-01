cd744e527b0f38e8a648d240c48ad99a12b85d1b
Jim Myers <myersjd@umich.edu>
2019-02-01 11:53:17 -0500

    reorder

--- src/main/java/edu/harvard/iq/dataverse/engine/command/impl/CuratePublishedDatasetVersionCommand.java
+++ src/main/java/edu/harvard/iq/dataverse/engine/command/impl/CuratePublishedDatasetVersionCommand.java
@@ -144,10 +144,15 @@ public class CuratePublishedDatasetVersionCommand extends AbstractDatasetCommand
             logger.log(Level.WARNING, "Curate Published DatasetVersion: exception while exporting metadata files:{0}", ex.getMessage());
         }
         
+
+        // Update so that getDataset() in updateDatasetUser will get the up-to-date copy
+        // (with no draft version)
+        setDataset(savedDataset);
+        updateDatasetUser(ctxt);
+        
         //Update archive copy as well
         //Delete the record of any existing copy since it is now out of date/incorrect
         updateVersion.setArchivalCopyLocation(null);
-        ctxt.em().flush();
         //Then try to generate and submit an archival copy. If it fails the location being null signals the failure
         String className = ctxt.settings().get(SettingsServiceBean.Key.ArchiverClassName.toString());
         AbstractSubmitToArchiveCommand archiveCommand = ArchiverUtil.createSubmitToArchiveCommand(className, getRequest(), updateVersion);
@@ -155,10 +160,6 @@ public class CuratePublishedDatasetVersionCommand extends AbstractDatasetCommand
             updateVersion = ctxt.engine().submit(archiveCommand);
         }
 
-        // Update so that getDataset() in updateDatasetUser will get the up-to-date copy
-        // (with no draft version)
-        setDataset(savedDataset);
-        updateDatasetUser(ctxt);
 
         return savedDataset;
     }
