dc69bc75bad28e770edc7fe5e8e0c69ebefb0b25
matthew-a-dunlap <matthew.a.dunlap@gmail.com>
2019-02-12 13:32:49 -0500

    Move metrics db changes to correct upgrade script #5445

--- scripts/database/upgrades/upgrade_v4.10.1_to_v4.10.2.sql
+++
@@ -1,6 +0,0 @@
-TRUNCATE metric;
-ALTER TABLE metric ADD COLUMN dayString text;
-ALTER TABLE metric ADD COLUMN dataLocation text;
-ALTER TABLE metric DROP CONSTRAINT "metric_metricname_key";
-ALTER TABLE metric RENAME COLUMN metricValue TO valueJson;
-ALTER TABLE metric RENAME COLUMN metricName TO name;
