c69473a3ec3246a7d8839dbcd200f75092185137
Victoria Lubitch <victoria.lubitch@utoronto.ca>
2019-02-13 10:38:45 -0500

    Syntax error in upgrade comments fixed

--- scripts/database/upgrades/upgrade_v4.10.1_to_4.11.sql
+++ scripts/database/upgrades/upgrade_v4.10.1_to_4.11.sql
@@ -2,7 +2,7 @@ ALTER TABLE datasetversion ADD COLUMN archivalcopylocation text;
 ALTER TABLE externaltool ADD COLUMN contenttype text NOT NULL default 'text/tab-separated-values';
 
 -- universe and weighted are dropped since they are empty in the dataverse
--- this columns will be moved to variablemetadata table
+-- these columns will be moved to variablemetadata table
 -- issue 5513
 ALTER TABLE datavariable
 DROP COLUMN  if exists universe,
