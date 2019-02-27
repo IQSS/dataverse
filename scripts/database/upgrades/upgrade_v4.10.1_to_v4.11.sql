671a795c7570104f6ff542cf9a2e8f5bc9a14b94
Victoria Lubitch <victoria.lubitch@utoronto.ca>
2019-02-27 16:32:08 -0500

    renamed upgrade script

--- scripts/database/upgrades/upgrade_v4.10.1_to_v4.11.sql
+++ scripts/database/upgrades/upgrade_v4.10.1_to_v4.11.sql
@@ -1,16 +1,15 @@
 ALTER TABLE datasetversion ADD COLUMN archivalcopylocation text;
 ALTER TABLE externaltool ADD COLUMN contenttype text NOT NULL default 'text/tab-separated-values';
-
--- universe is dropped since it is empty in the dataverse
--- this column will be moved to variablemetadata table
--- issue 5513
-ALTER TABLE datavariable
-DROP COLUMN  if exists universe;
-
 TRUNCATE metric;
 ALTER TABLE metric ADD COLUMN dayString text;
 ALTER TABLE metric ADD COLUMN dataLocation text;
 ALTER TABLE metric DROP CONSTRAINT "metric_metricname_key";
 ALTER TABLE metric RENAME COLUMN metricValue TO valueJson;
 ALTER TABLE metric RENAME COLUMN metricName TO name;
+
+-- universe is dropped since it is empty in the dataverse
+-- this column will be moved to variablemetadata table
+-- issue 5513
+ALTER TABLE datavariable
+DROP COLUMN  if exists universe;
 
