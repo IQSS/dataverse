71a13e30a771f22abc5805586bb88651c1b2f9aa
Juan Corrales <juancorr@gmail.com>
2019-03-01 13:08:16 +0100

    Pruebas de formateo para buscar error en la fecha en la v4.9.11

--- src/main/java/edu/harvard/iq/dataverse/util/DateUtil.java
+++ src/main/java/edu/harvard/iq/dataverse/util/DateUtil.java
@@ -7,12 +7,16 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author jchengan
  */
 public class DateUtil {
+
+    private static final Logger logger = Logger.getLogger(DateUtil.class.getCanonicalName());
 
     public static String formatDate(Date dateToformat) {
         String formattedDate;
@@ -21,6 +25,9 @@ public class DateUtil {
             Locale currentLocale = BundleUtil.getCurrentLocale();
             dateFormatter = DateFormat.getDateInstance(DateFormat.DEFAULT, currentLocale);
             formattedDate = dateFormatter.format(dateToformat);
+            logger.log(Level.SEVERE, "******** JUAN ******: Fecha Formateada en formatDate: {0}", formattedDate);
+            logger.log(Level.SEVERE, "******** JUAN ******: Locale en formatDate: {0}", currentLocale.getDisplayName());
+
             return formattedDate;
         } catch(Exception e) {
             return null;
@@ -28,10 +35,19 @@ public class DateUtil {
     }
 
     public static String formatDate(String dateToformat, String format) {
+
+        
+        
         String formattedDate = "";
         DateFormat inputFormat = new SimpleDateFormat(format);
         Date _date = null;
+        logger.log(Level.SEVERE, "******** JUAN ******: Fecha A formatear en formatDate instancia 2: {0}", dateToformat);
+        logger.log(Level.SEVERE, "******** JUAN ******: Formato en formatDate instancia 2: {0}", format);
         try {
+            Locale currentLocale = BundleUtil.getCurrentLocale();
+            DateFormat dateFormatter = DateFormat.getDateInstance(DateFormat.DEFAULT, currentLocale);
+            
+            
             _date = inputFormat.parse(dateToformat);
             formattedDate = formatDate(_date);
             return formattedDate;
