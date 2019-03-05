a1e92b74a667c7cfc696a8e9e7bff2d750b30e7f
Jim Myers <myersjd@umich.edu>
2019-03-05 18:38:27 -0500

    Revert "quicktime video preview"
    This reverts commit e91276f1a71254e43815ed213c6407d209a233a1.

--- src/main/webapp/resources/previewers/js/qtvideo.js
+++
@@ -1,4 +0,0 @@
-function writeContent(fileUrl, label, creationDate, title, authors, parentUrl) {
-addStandardPreviewHeader(label, creationDate, title, authors, parentUrl);
-          $(".preview").append($("<embed/>").attr("CONTROLLER","true").attr("loop","false").attr("PLUGINSPAGE","http://www.apple.com/quicktime").attr("src",fileUrl));
-}
