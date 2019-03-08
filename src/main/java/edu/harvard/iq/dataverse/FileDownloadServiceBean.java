4914c5a1b5410e243233df5f45e93f5a2281c154
Jim Myers <myersjd@umich.edu>
2019-03-08 17:24:23 -0500

    try js open

--- src/main/java/edu/harvard/iq/dataverse/FileDownloadServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/FileDownloadServiceBean.java
@@ -29,6 +29,9 @@ import javax.persistence.PersistenceContext;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 
+import org.primefaces.PrimeFaces;
+import org.primefaces.context.RequestContext;
+
 /**
  *
  * @author skraffmi
@@ -249,11 +252,12 @@ public class FileDownloadServiceBean implements java.io.Serializable {
         guestbookResponse.setDownloadtype(externalTool.getDisplayName());
         String toolUrl = externalToolHandler.getToolUrlWithQueryParams();
         logger.fine("Exploring with " + toolUrl);
-        try {
-            FacesContext.getCurrentInstance().getExternalContext().redirect(toolUrl);
-        } catch (IOException ex) {
-            logger.info("Problem exploring with " + toolUrl + " - " + ex);
-        }
+       // try {
+            //FacesContext.getCurrentInstance().getExternalContext().redirect(toolUrl);
+            PrimeFaces.current().executeScript("window.open("+toolUrl + ", target='_blank');");
+      //  } catch (IOException ex) {
+     //       logger.info("Problem exploring with " + toolUrl + " - " + ex);
+     //   }
         // This is the old logic from TwoRavens, null checks and all.
         if (guestbookResponse != null && guestbookResponse.isWriteResponse()
                 && ((fmd != null && fmd.getDataFile() != null) || guestbookResponse.getDataFile() != null)) {
