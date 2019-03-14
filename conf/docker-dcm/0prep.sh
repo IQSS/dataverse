4b0a0956f48cca0dda7c2f63960eefe13fd98077
Pete Meyer <pameyer@crystal.harvard.edu>
2019-03-14 13:25:27 -0400

    update rsal rpm url to release

--- conf/docker-dcm/0prep.sh
+++ conf/docker-dcm/0prep.sh
@@ -7,5 +7,5 @@ if [ ! -e dcm-${DCM_VERSION}-0.noarch.rpm ]; then
 fi
 
 if [ ! -e rsal-${RSAL_VERSION}-0.noarch.rpm ] ;then
-	echo "TODO - rsal release url not configured yet"
+	wget https://github.com/sbgrid/rsal/releases/download/${RSAL_VERSION}/rsal-${RSAL_VERSION}-0.noarch.rpm
 fi
