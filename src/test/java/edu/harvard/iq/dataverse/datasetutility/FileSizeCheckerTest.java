8b3fbbf07763e5092dd4ef5a1ed8b6ba77fdd87e
Roland Schlaefli <rolandschlaefli@gmail.com>
2019-03-16 18:32:42 +0100

    Add test cases for FileSizeChecker.isAllowedFileSize #5634

--- src/test/java/edu/harvard/iq/dataverse/datasetutility/FileSizeCheckerTest.java
+++ src/test/java/edu/harvard/iq/dataverse/datasetutility/FileSizeCheckerTest.java
@@ -6,18 +6,39 @@
 package edu.harvard.iq.dataverse.datasetutility;
 
 import static edu.harvard.iq.dataverse.datasetutility.FileSizeChecker.bytesToHumanReadable;
+
+import edu.harvard.iq.dataverse.datasetutility.FileSizeChecker.FileSizeResponse;
 import edu.harvard.iq.dataverse.util.BundleUtil;
+import edu.harvard.iq.dataverse.util.SystemConfig;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  *
  * @author oscardssmith
  */
 public class FileSizeCheckerTest {
+
+    private FileSizeChecker fileSizeChecker;
+
+    @Before
+    public void setUp() {
+        // initialize a system config and instantiate a file size checker
+        // override the max file upload side to allow for testing
+        fileSizeChecker = new FileSizeChecker(new SystemConfig() {
+            @Override
+            public Long getMaxFileUploadSize() {
+                return 1000L;
+            }
+        });
+    }
 
     @Test
     public void testBytesToHumanReadable() {
@@ -35,4 +56,40 @@ public class FileSizeCheckerTest {
         assertEquals(expLongAns, longAns);
     }
 
+    @Test(expected = NullPointerException.class)
+    public void testIsAllowedFileSize_throwsOnNull() {
+        fileSizeChecker.isAllowedFileSize(null);
+    }
+
+    @Test
+    public void testIsAllowedFileSize_allowsSmallerFileSize() {
+        FileSizeResponse response = fileSizeChecker.isAllowedFileSize(999L);
+        assertTrue(response.fileSizeOK);
+    }
+
+    @Test
+    public void testIsAllowedFileSize_allowsEqualFileSize() {
+        FileSizeResponse response = fileSizeChecker.isAllowedFileSize(1000L);
+        assertTrue(response.fileSizeOK);
+    }
+
+    @Test
+    public void testIsAllowedFileSize_rejectsBiggerFileSize() {
+        FileSizeResponse response = fileSizeChecker.isAllowedFileSize(1001L);
+        assertFalse(response.fileSizeOK);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testIsAllowedFileSize_allowsOnUnboundedFileSize() {
+        // initialize a system config and instantiate a file size checker
+        // ensure that a max filesize is not set
+        FileSizeChecker unboundedFileSizeChecker = new FileSizeChecker(new SystemConfig() {
+            @Override
+            public Long getMaxFileUploadSize() {
+                return null;
+            }
+        });
+        FileSizeResponse response = unboundedFileSizeChecker.isAllowedFileSize(Long.MAX_VALUE);
+        assertTrue(response.fileSizeOK);
+    }
 }
