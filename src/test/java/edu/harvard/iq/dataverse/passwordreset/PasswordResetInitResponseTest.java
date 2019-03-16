46dd8ce9116032c545c82d1c3a411db714e41ddb
Roland Schlaefli <rolandschlaefli@gmail.com>
2019-03-16 18:53:09 +0100

    Add junit 5 stubs for password reset init response test

--- src/test/java/edu/harvard/iq/dataverse/passwordreset/PasswordResetInitResponseTest.java
+++ src/test/java/edu/harvard/iq/dataverse/passwordreset/PasswordResetInitResponseTest.java
@@ -5,16 +5,27 @@ import edu.harvard.iq.dataverse.authorization.providers.oauth2.impl.GitHubOAuth2
 import edu.harvard.iq.dataverse.authorization.providers.oauth2.impl.GoogleOAuth2AP;
 import edu.harvard.iq.dataverse.authorization.providers.oauth2.impl.OrcidOAuth2AP;
 import edu.harvard.iq.dataverse.authorization.providers.shib.ShibAuthenticationProvider;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
-import org.junit.Test;
-import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.Test;
 
 public class PasswordResetInitResponseTest {
 
+    private PasswordResetData passwordResetData = new PasswordResetData() {
+        @Override
+        public String getToken() {
+            return "abcd";
+        }
+    };
+
     @Test
-    public void testIsNonLocalLoginEnabled() {
+    public void testGeneratesValidResetUrl() {
         // TODO:
+        fail();
     }
 }
