7b0fe8265f086f4691187e80c5c24ba25b62f6c3
Roland Schlaefli <rolandschlaefli@gmail.com>
2019-03-18 22:12:23 +0100

    Remove unused variable and tests

--- src/test/java/edu/harvard/iq/dataverse/dataaccess/DataAccessTest.java
+++ src/test/java/edu/harvard/iq/dataverse/dataaccess/DataAccessTest.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 public class DataAccessTest {
   DataFile datafile;
   Dataset dataset;
-  DataAccess dataAccess;
 
   @BeforeEach
   void setUp() {
@@ -54,25 +53,4 @@ public class DataAccessTest {
     StorageIO<Dataset> storageIo = DataAccess.createNewStorageIO(dataset, "valid-tag");
     assertTrue(storageIo.getClass().equals(FileAccessIO.class));
   }
-
-  /*
-  TODO: testing these branches requires the possibility to inject swift and s3 client mocks
-  @Test
-  void testCreateNewStorageIO_updatesStorageIdentifier() throws IOException {
-    StorageIO<Dataset> storageIo = DataAccess.createNewStorageIO(dataset, "valid-tag");
-    DvObject obj = storageIo.getDvObject();
-    assertEquals("valid-tag", obj.getStorageIdentifier());
-  }
-
-  @Test
-  void testCreateNewStorageIO_createsSwiftAccessIO() throws IOException {
-    StorageIO<Dataset> storageIo = DataAccess.createNewStorageIO(dataset, "valid-tag", "swift");
-    assertTrue(storageIo.getClass().equals(SwiftAccessIO.class));
-  }
-
-  @Test
-  void testCreateNewStorageIO_createsS3AccessIO() throws IOException {
-    StorageIO<Dataset> storageIo = DataAccess.createNewStorageIO(dataset, "valid-tag", "s3");
-    assertTrue(storageIo.getClass().equals(S3AccessIO.class));
-  } */
 }
