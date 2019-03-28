99d469d934c15b618dee9242ce374d7df88c328f
Pete Meyer <pameyer@crystal.harvard.edu>
2019-03-28 17:17:27 -0400

    glassfish seems happier with less ttys from outside

--- conf/docker-aio/prep_it.bash
+++ conf/docker-aio/prep_it.bash
@@ -14,61 +14,45 @@ cd conf/docker-aio
 ./0prep_deps.sh
 ./1prep.sh
 docker build -t dv0 -f c7.dockerfile .
-i_retry=0
-setup_ok=0
-while [ $i_retry -lt $n_retries ]
+# cleanup from previous runs if necessary
+docker rm -f dv
+# start container
+docker run -d -p 8084:80 -p 8083:8080 -p 9010:9009 --name dv dv0
+# wait for glassfish to be healthy
+i_wait=0
+d_wait=10
+while [ $i_wait -lt $n_wait ]
 do
-	# cleanup from previous runs if necessary
-	docker rm -f dv
-	# start container
-	docker run -d -p 8084:80 -p 8083:8080 -p 9010:9009 --name dv dv0
-
-	# wait for glassfish to be healthy
-	i_wait=0
-	d_wait=10
-	while [ $i_wait -lt $n_wait ]
-	do
-		h=`docker inspect -f "{{.State.Health.Status}}" dv`
-		if [ "healthy" == "${h}" ]; then
-			break
-		else
-			sleep $d_wait
-		fi
-		i_wait=$(( $i_wait + 1 ))
-		
-	done
-
-	# try setupIT.bash
-	docker exec -it dv /opt/dv/setupIT.bash
-	err=$?
-	if [ $err -eq 0 ]
-	then
-		setup_ok=1
-		echo "setupIT worked ($i_retry tries)"
-		break # success!
+	h=`docker inspect -f "{{.State.Health.Status}}" dv`
+	if [ "healthy" == "${h}" ]; then
+		break
+	else
+		sleep $d_wait
 	fi
-	echo "$i_retry try in loop"
-	i_retry=$(( $i_retry + 1 ))
+	i_wait=$(( $i_wait + 1 ))
+	
 done
-if [ $setup_ok -ne 1 ]
-then
-	echo "failed setupIT after $i_retry tries ; bailing out"
-	exit 1
-fi
-
-# handle config for the private url test
-./seturl.bash
+# try setupIT.bash
+docker exec dv /opt/dv/setupIT.bash
 err=$?
 if [ $err -ne 0 ]; then
-	echo "seturl fail; bailing out"
+	echo "error - setupIT failure"
 	exit 1
 fi
 # configure DOI provider based on docker build arguments / environmental variables
-docker exec -it dv /opt/dv/configure_doi.bash
+docker exec dv /opt/dv/configure_doi.bash
+err=$?
+if [ $err -ne 0 ]; then
+	echo "error - DOI configuration failure"
+	exit 1
+fi
+# handle config for the private url test (and things like publishing...)
+./seturl.bash
 
 
 cd ../..
-echo "docker-aio ready to run integration tests ($i_retry)"
+#echo "docker-aio ready to run integration tests ($i_retry)"
+echo "docker-aio ready to run integration tests"
 curl http://localhost:8084/api/info/version
 echo $?
 
