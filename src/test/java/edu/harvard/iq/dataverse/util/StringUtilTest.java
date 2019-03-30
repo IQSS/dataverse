e6cec9b65f8e40bf2bf84761d7119c39b28ee243
Roland Schlaefli <rolandschlaefli@gmail.com>
2019-03-30 18:53:35 +0100

    Add draft for data retriever api test

--- src/test/java/edu/harvard/iq/dataverse/util/StringUtilTest.java
+++ src/test/java/edu/harvard/iq/dataverse/util/StringUtilTest.java
@@ -3,7 +3,6 @@ package edu.harvard.iq.dataverse.util;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.util.Arrays;
 import java.util.Collection;
