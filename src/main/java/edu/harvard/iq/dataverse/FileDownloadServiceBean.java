c7c8555f1355216d97ce48862d9d962d4ba980d0
Jim Myers <myersjd@umich.edu>
2019-04-11 15:14:51 -0400

    Revert "try alt tool launch"
    This reverts commit 70223e15d59a5c37b203d6a9c9c97a7035a392fb.

--- src/main/java/edu/harvard/iq/dataverse/FileDownloadServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/FileDownloadServiceBean.java
@@ -268,8 +268,7 @@ public class FileDownloadServiceBean implements java.io.Serializable {
         guestbookResponse.setDownloadtype(externalTool.getDisplayName());
         String toolUrl = externalToolHandler.getToolUrlWithQueryParams();
         logger.fine("Exploring with " + toolUrl);
-        //PrimeFaces.current().executeScript("window.open('"+toolUrl + "', target='_blank');");
-        PrimeFaces.current().ajax().addCallbackParam("url", toolUrl);
+        PrimeFaces.current().executeScript("window.open('"+toolUrl + "', target='_blank');");
         // This is the old logic from TwoRavens, null checks and all.
         if (guestbookResponse != null && guestbookResponse.isWriteResponse()
                 && ((fmd != null && fmd.getDataFile() != null) || guestbookResponse.getDataFile() != null)) {
