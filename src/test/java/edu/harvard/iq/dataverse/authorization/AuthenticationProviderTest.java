6150c7d7ecf053f01b86c4a4af6fad6a7ef1c6b5
Roland Schlaefli <rolandschlaefli@gmail.com>
2019-04-18 19:42:22 +0200

    Perform initial refactoring to parameterized and separate test cases

--- src/test/java/edu/harvard/iq/dataverse/authorization/AuthenticationProviderTest.java
+++ src/test/java/edu/harvard/iq/dataverse/authorization/AuthenticationProviderTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.harvard.iq.dataverse.authorization;
 
 import edu.harvard.iq.dataverse.util.BundleUtil;
@@ -11,84 +6,58 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
 
-/**
- *
- * @author rmp553
- */
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AuthenticationProviderTest {
-    
-    public AuthenticationProviderTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
-    /**
-     * Create a map used to test known AuthenticationProvider ids
-     * 
-     * @return 
-     */
-    private Map<String, String> getBundleTestMap(){
-        return Collections.unmodifiableMap(Stream.of(
+    private static Map<String, String> bundleTestMap;
+
+    @BeforeAll
+    static void setup() {
+        /*
+         * Create a map used to test known AuthenticationProvider ids
+         * @author rmp553
+         */
+        bundleTestMap = Collections.unmodifiableMap(Stream.of(
                 new SimpleEntry<>("builtin", "authenticationProvider.name.builtin"),
                 new SimpleEntry<>("github", "authenticationProvider.name.github"),
                 new SimpleEntry<>("google", "authenticationProvider.name.google"),
                 new SimpleEntry<>("orcid", "authenticationProvider.name.orcid"),
                 new SimpleEntry<>("orcid-sandbox", "authenticationProvider.name.orcid-sandbox"),
                 new SimpleEntry<>("shib", "authenticationProvider.name.shib"))
-                .collect(Collectors.toMap((e) -> e.getKey(), (e) -> e.getValue())));
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
     }
-    
-    /**
-     * Test of getFriendlyName method, of class AuthenticationProvider.
-     */
+
     @Test
-    public void testGetFriendlyName() {
-        System.out.println("getFriendlyName");
-        
-        Map<String, String> bundleTestMap = this.getBundleTestMap();
-        
-        // ------------------------------------------
-        // Test a null
-        // ------------------------------------------
+    void testGetFriendlyName_withNullInput() {
         String expResult = BundleUtil.getStringFromBundle("authenticationProvider.name.null");
         assertEquals(expResult, AuthenticationProvider.getFriendlyName(null));
-        
-        // ------------------------------------------
-        // Test an id w/o a bundle entry--should default to id
-        // ------------------------------------------
+    }
+
+    @Test
+    void getFriendlyName_withoutMatchingId() {
         String idNotInBundle = "id-not-in-bundle-so-use-id";
-        String expResult2 = AuthenticationProvider.getFriendlyName(idNotInBundle);
-        assertEquals(expResult2, idNotInBundle);
-        
-        // ------------------------------------------
-        // Iterate through the map and test each item
-        // ------------------------------------------
-        bundleTestMap.forEach((authProviderId, bundleName)->{
-            String expectedResult = BundleUtil.getStringFromBundle(bundleName);
-            assertEquals(expectedResult, AuthenticationProvider.getFriendlyName(authProviderId));
-        });    
-                   
+        assertEquals(idNotInBundle, AuthenticationProvider.getFriendlyName(idNotInBundle));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "builtin, authenticationProvider.name.builtin",
+            "github, authenticationProvider.name.github",
+            "google, authenticationProvider.name.google",
+            "orcid, authenticationProvider.name.orcid",
+            "orcid-sandbox, authenticationProvider.name.orcid-sandbox",
+            "shib, authenticationProvider.name.shib"
+    })
+    void testGetFriendlyName_withTestMap(String authProviderId, String bundleName) {
+        String expectedResult = BundleUtil.getStringFromBundle(bundleName);
+        assertEquals(expectedResult, AuthenticationProvider.getFriendlyName(authProviderId));
     }
 
 
