dd3a519a2a4fe8c17c7f9a086aa8ffb2085cd755
Roland Schlaefli <rolandschlaefli@gmail.com>
2019-04-18 20:17:19 +0200

    Extract BundleUtil logic into the setup method (no indirect testing) #5732

--- src/test/java/edu/harvard/iq/dataverse/authorization/AuthenticationProviderTest.java
+++ src/test/java/edu/harvard/iq/dataverse/authorization/AuthenticationProviderTest.java
@@ -2,7 +2,6 @@ package edu.harvard.iq.dataverse.authorization;
 
 import edu.harvard.iq.dataverse.util.BundleUtil;
 import java.util.AbstractMap.SimpleEntry;
-import java.util.Collections;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -10,33 +9,28 @@ import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AuthenticationProviderTest {
 
+    private final static String[] bundles = {"null", "builtin", "github", "google", "orcid", "orcid-sandbox", "shib"};
     private static Map<String, String> bundleTestMap;
 
     @BeforeAll
     static void setup() {
-        /*
-         * Create a map used to test known AuthenticationProvider ids
-         * @author rmp553
-         */
-        bundleTestMap = Collections.unmodifiableMap(Stream.of(
-                new SimpleEntry<>("builtin", "authenticationProvider.name.builtin"),
-                new SimpleEntry<>("github", "authenticationProvider.name.github"),
-                new SimpleEntry<>("google", "authenticationProvider.name.google"),
-                new SimpleEntry<>("orcid", "authenticationProvider.name.orcid"),
-                new SimpleEntry<>("orcid-sandbox", "authenticationProvider.name.orcid-sandbox"),
-                new SimpleEntry<>("shib", "authenticationProvider.name.shib"))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
+        bundleTestMap = Stream.of(bundles)
+                .map(bundleName -> new SimpleEntry<>(
+                        bundleName,
+                        BundleUtil.getStringFromBundle("authenticationProvider.name." + bundleName)
+                    )
+                ).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
     @Test
     void testGetFriendlyName_withNullInput() {
-        String expResult = BundleUtil.getStringFromBundle("authenticationProvider.name.null");
+        String expResult = bundleTestMap.get("null");
         assertEquals(expResult, AuthenticationProvider.getFriendlyName(null));
     }
 
@@ -47,19 +41,9 @@ public class AuthenticationProviderTest {
     }
 
     @ParameterizedTest
-    @CsvSource({
-            "builtin, authenticationProvider.name.builtin",
-            "github, authenticationProvider.name.github",
-            "google, authenticationProvider.name.google",
-            "orcid, authenticationProvider.name.orcid",
-            "orcid-sandbox, authenticationProvider.name.orcid-sandbox",
-            "shib, authenticationProvider.name.shib"
-    })
-    void testGetFriendlyName_withTestMap(String authProviderId, String bundleName) {
-        String expectedResult = BundleUtil.getStringFromBundle(bundleName);
+    @ValueSource(strings = {"builtin", "github", "google", "orcid", "orcid-sandbox", "shib"})
+    void testGetFriendlyName_withTestMap(String authProviderId) {
+        String expectedResult = bundleTestMap.get(authProviderId);
         assertEquals(expectedResult, AuthenticationProvider.getFriendlyName(authProviderId));
     }
-
-
-    
 }
