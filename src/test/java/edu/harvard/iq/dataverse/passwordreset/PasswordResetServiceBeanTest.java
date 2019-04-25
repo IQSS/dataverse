2bc1d2013ea4d635581192c3a592bb9d68b469d6
Roland Schlaefli <rolandschlaefli@gmail.com>
2019-04-25 22:38:23 +0200

    Add stub file for PasswordResetServiceBeanTest

---
+++ src/test/java/edu/harvard/iq/dataverse/passwordreset/PasswordResetServiceBeanTest.java
@@ -0,0 +1,11 @@
+package edu.harvard.iq.dataverse.passwordreset;
+
+import org.junit.jupiter.api.Test;
+
+public class PasswordResetServiceBeanTest {
+
+    @Test
+    void testAttemptPasswordReset() {
+
+    }
+}
