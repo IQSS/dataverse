d93a35e1261fd3916bf38105301f590dd64263e7
Karol Kulik <karol191pol@gmail.com>
2019-04-26 13:14:22 +0200

    Closes #102: Default language fix. (#111)
    * change the way the default locale is set.
    
    * change the way language is set in the system.
    
    * changed variable position
    
    * reverted the place where method is used.
    
    * updated java-docs.

--- src/main/java/edu/harvard/iq/dataverse/DataverseSession.java
+++ src/main/java/edu/harvard/iq/dataverse/DataverseSession.java
@@ -3,7 +3,6 @@ package edu.harvard.iq.dataverse;
 import edu.harvard.iq.dataverse.PermissionServiceBean.StaticPermissionQuery;
 import edu.harvard.iq.dataverse.actionlogging.ActionLogRecord;
 import edu.harvard.iq.dataverse.actionlogging.ActionLogServiceBean;
-import edu.harvard.iq.dataverse.authorization.providers.builtin.BuiltinUserServiceBean;
 import edu.harvard.iq.dataverse.authorization.users.GuestUser;
 import edu.harvard.iq.dataverse.authorization.users.User;
 import edu.harvard.iq.dataverse.settings.SettingsWrapper;
@@ -17,101 +16,70 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.Locale;
+import java.util.Set;
 import java.util.logging.Logger;
 
 /**
- *
  * @author gdurand
  */
 @Named
 @SessionScoped
-public class DataverseSession implements Serializable{
-    
-    /* Note that on logout, variables must be cleared manually in DataverseHeaderFragment*/
-    private User user;
+public class DataverseSession implements Serializable {
 
     @EJB
     PermissionServiceBean permissionsService;
 
     @EJB
-    BuiltinUserServiceBean usersSvc;
-	
-    @EJB 
     ActionLogServiceBean logSvc;
-    
+
     @Inject
     SettingsWrapper settingsWrapper;
-    
+
     private static final Logger logger = Logger.getLogger(DataverseSession.class.getCanonicalName());
-    
+
+    /* Note that on logout, variables must be cleared manually in DataverseHeaderFragment*/
+    private User user;
     private boolean statusDismissed = false;
-    
+    private String localeCode;
+
+    // -------------------- GETTERS --------------------
+
     public User getUser() {
-        if ( user == null ) {
+        if (user == null) {
             user = GuestUser.get();
         }
- 
-        return user;
-    }
 
-    public void setUser(User aUser) {
-        logSvc.log( 
-                new ActionLogRecord(ActionLogRecord.ActionType.SessionManagement,(aUser==null) ? "logout" : "login")
-                    .setUserIdentifier((aUser!=null) ? aUser.getIdentifier() : (user!=null ? user.getIdentifier() : "") ));
-        
-        this.user = aUser;
+        return user;
     }
 
     public boolean isStatusDismissed() {
         return statusDismissed;
     }
-    
-    public void setStatusDismissed(boolean status) {
-        statusDismissed = status; //MAD: Set to true to enable code!
-    }
-    
-    public StaticPermissionQuery on( Dataverse d ) {
-            return permissionsService.userOn(user, d);
-    }
-    
-    // Language Locale methods: 
-    
-    private String localeCode;
-    
+
     public String getLocaleCode() {
         if (localeCode == null) {
-            initLocale();
+            localeCode = initLocale();
+
+            logger.fine("init: locale set to " + localeCode);
         }
         return localeCode;
     }
 
-    public void setLocaleCode(String localeCode) {
-        this.localeCode = localeCode;
-    }
-
     public String getLocaleTitle() {
         if (localeCode == null) {
-            initLocale();
+            localeCode = initLocale();
+
+            logger.fine("init: locale set to " + localeCode);
         }
         return settingsWrapper.getConfiguredLocales().get(localeCode);
     }
-    
-    public void initLocale() {
-        
-        if(FacesContext.getCurrentInstance() == null) {
-            localeCode = "en";
-        }
-        else if (FacesContext.getCurrentInstance().getViewRoot() == null ) {
-            localeCode = FacesContext.getCurrentInstance().getExternalContext().getRequestLocale().getLanguage();
-        }
-        else if (FacesContext.getCurrentInstance().getViewRoot().getLocale().getLanguage().equals("en_US")) {
-            localeCode = "en";
-        }
-        else {
-            localeCode = FacesContext.getCurrentInstance().getViewRoot().getLocale().getLanguage();
-        }
-        
-        logger.fine("init: locale set to "+localeCode);
+
+    // -------------------- LOGIC --------------------
+
+    public String initLocale() {
+        Set<String> dataverseLanguages = settingsWrapper.getConfiguredLocales().keySet();
+
+        return dataverseLanguages.contains(getBrowserLanguage()) ? getBrowserLanguage() : "en";
     }
 
     public void updateLocaleInViewRootAndRedirect(String code) {
@@ -127,14 +95,42 @@ public class DataverseSession implements Serializable{
     }
 
     public void updateLocaleInViewRoot() {
-        if (localeCode != null 
-                && FacesContext.getCurrentInstance() != null 
-                && FacesContext.getCurrentInstance().getViewRoot() != null 
+        if (localeCode != null
+                && FacesContext.getCurrentInstance() != null
+                && FacesContext.getCurrentInstance().getViewRoot() != null
                 && !localeCode.equals(FacesContext.getCurrentInstance().getViewRoot().getLocale().getLanguage())) {
             FacesContext.getCurrentInstance().getViewRoot().setLocale(new Locale(localeCode));
-        } 
+        }
     }
-    
-    
 
+    public StaticPermissionQuery on(Dataverse d) {
+        return permissionsService.userOn(user, d);
+    }
+
+    // -------------------- PRIVATE --------------------
+
+    /**
+     * @return Top browser locale which is taken from 'Accept-Language header'.
+     */
+    private String getBrowserLanguage() {
+        return FacesContext.getCurrentInstance().getExternalContext().getRequestLocale().getLanguage();
+    }
+
+    // -------------------- SETTERS --------------------
+
+    public void setUser(User aUser) {
+        logSvc.log(
+                new ActionLogRecord(ActionLogRecord.ActionType.SessionManagement, (aUser == null) ? "logout" : "login")
+                        .setUserIdentifier((aUser != null) ? aUser.getIdentifier() : (user != null ? user.getIdentifier() : "")));
+
+        this.user = aUser;
+    }
+
+    public void setLocaleCode(String localeCode) {
+        this.localeCode = localeCode;
+    }
+
+    public void setStatusDismissed(boolean status) {
+        statusDismissed = status; //MAD: Set to true to enable code!
+    }
 }
