2d4f342905d9359cd3786e839e95c054bc3a6f48
Roland Schlaefli <rolandschlaefli@gmail.com>
2019-05-12 15:57:08 +0200

    Coverage for canDownloadFile

--- src/test/java/edu/harvard/iq/dataverse/FileDownloadHelperTest.java
+++ src/test/java/edu/harvard/iq/dataverse/FileDownloadHelperTest.java
@@ -1,19 +1,16 @@
 package edu.harvard.iq.dataverse;
 
 import edu.harvard.iq.dataverse.authorization.Permission;
-import edu.harvard.iq.dataverse.authorization.RoleAssignee;
-import edu.harvard.iq.dataverse.authorization.users.User;
-import jena.query;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -27,22 +24,16 @@ class FileDownloadHelperTest {
     private DataverseSession dataverseSession;
 
     @Mock
-    private PermissionServiceBean.StaticPermissionQuery staticPermissionQuery;
+    private DataverseRequestServiceBean dataverseRequestServiceBean;
 
     private FileDownloadHelper fileDownloadHelper;
-    private FileMetadata fileMetadata;
 
     @BeforeEach
     void setup() {
         fileDownloadHelper = new FileDownloadHelper();
         fileDownloadHelper.permissionService = permissionServiceBean;
         fileDownloadHelper.session = dataverseSession;
-
-        fileMetadata = new FileMetadata();
-        DatasetVersion datasetVersion = new DatasetVersion();
-        datasetVersion.setDataset(new Dataset());
-        fileMetadata.setDatasetVersion(datasetVersion);
-        fileMetadata.setDataFile(new DataFile());
+        fileDownloadHelper.dvRequestService = dataverseRequestServiceBean;
     }
 
     @Test
@@ -52,48 +43,177 @@ class FileDownloadHelperTest {
 
     @Test
     void testDoesSessionUserHavePermission_forEditDatasetPermission() {
-        // if the permission service is called with a Dataset, return a static permission query
-        when(permissionServiceBean.userOn(ArgumentMatchers.any(), ArgumentMatchers.any(Dataset.class))).thenReturn(staticPermissionQuery);
+        DatasetVersion datasetVersion = new DatasetVersion();
+        datasetVersion.setDataset(new Dataset());
+        DataFile dataFile = new DataFile();
 
-        when(staticPermissionQuery.has(Permission.EditDataset)).thenReturn(false);
-        assertFalse(fileDownloadHelper.doesSessionUserHavePermission(Permission.EditDataset, this.fileMetadata));
+        FileMetadata fileMetadata = new FileMetadata();
+        fileMetadata.setDatasetVersion(datasetVersion);
+        fileMetadata.setDataFile(dataFile);
 
-        when(staticPermissionQuery.has(Permission.EditDataset)).thenReturn(true);
-        assertTrue(fileDownloadHelper.doesSessionUserHavePermission(Permission.EditDataset, this.fileMetadata));
+        mockPermissionResponseUserOn(Permission.EditDataset, false);
+        assertFalse(fileDownloadHelper.doesSessionUserHavePermission(Permission.EditDataset, fileMetadata));
+
+        mockPermissionResponseUserOn(Permission.EditDataset, true);
+        assertTrue(fileDownloadHelper.doesSessionUserHavePermission(Permission.EditDataset, fileMetadata));
     }
 
     @Test
     void testDoesSessionUserHavePermission_forDownloadFilePermission() {
-        // if the permission service is called with a DataFile, return a static permission query
-        when(permissionServiceBean.userOn(ArgumentMatchers.any(), ArgumentMatchers.any(DataFile.class))).thenReturn(staticPermissionQuery);
+        DatasetVersion datasetVersion = new DatasetVersion();
+        datasetVersion.setDataset(new Dataset());
+        DataFile dataFile = new DataFile();
 
-        when(staticPermissionQuery.has(Permission.DownloadFile)).thenReturn(false);
-        assertFalse(fileDownloadHelper.doesSessionUserHavePermission(Permission.DownloadFile, this.fileMetadata));
+        FileMetadata fileMetadata = new FileMetadata();
+        fileMetadata.setDatasetVersion(datasetVersion);
+        fileMetadata.setDataFile(dataFile);
 
-        when(staticPermissionQuery.has(Permission.DownloadFile)).thenReturn(true);
-        assertTrue(fileDownloadHelper.doesSessionUserHavePermission(Permission.DownloadFile, this.fileMetadata));
+        mockPermissionResponseUserOn(Permission.DownloadFile, false);
+        assertFalse(fileDownloadHelper.doesSessionUserHavePermission(Permission.DownloadFile, fileMetadata));
+
+        mockPermissionResponseUserOn(Permission.DownloadFile, true);
+        assertTrue(fileDownloadHelper.doesSessionUserHavePermission(Permission.DownloadFile, fileMetadata));
     }
 
     @Test
     void testDoesSessionUserHavePermission_forAnyOtherPermission() {
-        assertFalse(fileDownloadHelper.doesSessionUserHavePermission(Permission.ManageDataversePermissions, this.fileMetadata));
+        DatasetVersion datasetVersion = new DatasetVersion();
+        datasetVersion.setDataset(new Dataset());
+        DataFile dataFile = new DataFile();
+
+        FileMetadata fileMetadata = new FileMetadata();
+        fileMetadata.setDatasetVersion(datasetVersion);
+        fileMetadata.setDataFile(dataFile);
+
+        assertFalse(fileDownloadHelper.doesSessionUserHavePermission(Permission.ManageDataversePermissions, fileMetadata));
     }
 
     @Test
     void testDoesSessionUserHavePermission_withNullDataset() {
         FileMetadata fileMetadata = new FileMetadata();
         fileMetadata.setDatasetVersion(new DatasetVersion());
+
         assertFalse(fileDownloadHelper.doesSessionUserHavePermission(Permission.EditDataset, fileMetadata));
     }
 
     @Test
     void testDoesSessionUserHavePermission_withNullDataFile() {
         FileMetadata fileMetadata = new FileMetadata();
+
         assertFalse(fileDownloadHelper.doesSessionUserHavePermission(Permission.DownloadFile, fileMetadata));
     }
 
     @Test
-    void testCanDownloadFile() {
+    void testCanDownloadFile_withoutFileMetadata() {
+        assertFalse(fileDownloadHelper.canDownloadFile(null));
+    }
 
+    @Test
+    void testCanDownloadFile_withNullMetadataId() {
+        FileMetadata fileMetadata = new FileMetadata();
+        fileMetadata.setId(null);
+
+        assertFalse(fileDownloadHelper.canDownloadFile(fileMetadata));
+    }
+
+    @Test
+    void testCanDownloadFile_withNullDataFileId() {
+        FileMetadata fileMetadata = new FileMetadata();
+        fileMetadata.setId(1L);
+        DataFile dataFile = new DataFile();
+        dataFile.setId(null);
+        fileMetadata.setDataFile(dataFile);
+
+        assertFalse(fileDownloadHelper.canDownloadFile(fileMetadata));
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "false", "true" })
+    void testCanDownloadFile_forDeaccessionedFile(boolean hasPermission) {
+        DataFile dataFile = new DataFile();
+        dataFile.setId(2L);
+
+        DatasetVersion datasetVersion = new DatasetVersion();
+        datasetVersion.setDataset(new Dataset());
+        datasetVersion.setVersionState(DatasetVersion.VersionState.DEACCESSIONED);
+
+        FileMetadata fileMetadata = new FileMetadata();
+        fileMetadata.setId(1L);
+        fileMetadata.setDataFile(dataFile);
+        fileMetadata.setDatasetVersion(datasetVersion);
+
+        mockPermissionResponseUserOn(Permission.EditDataset, hasPermission);
+
+        assertEquals(hasPermission, fileDownloadHelper.canDownloadFile(fileMetadata), "Initial response does not match expectation!");
+
+        // call again to exercise the cache and ensure it returns the same result
+        assertEquals(hasPermission, fileDownloadHelper.canDownloadFile(fileMetadata), "Cached response does not match initial response!");
+    }
+
+    @Test
+    void testCanDownloadFile_forUnrestrictedReleasedFile() {
+        DataFile dataFile = new DataFile();
+        dataFile.setId(2L);
+
+        DatasetVersion datasetVersion = new DatasetVersion();
+        datasetVersion.setVersionState(DatasetVersion.VersionState.RELEASED);
+
+        FileMetadata fileMetadata = new FileMetadata();
+        fileMetadata.setId(1L);
+        fileMetadata.setRestricted(false);
+        fileMetadata.setDataFile(dataFile);
+        fileMetadata.setDatasetVersion(datasetVersion);
+
+        assertTrue(fileDownloadHelper.canDownloadFile(fileMetadata));
+
+        // call again to exercise the cache and ensure it returns the same result
+        assertTrue(fileDownloadHelper.canDownloadFile(fileMetadata));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"false", "true"})
+    void testCanDownloadFile_forRestrictedReleasedFile(boolean hasPermission) {
+        DataFile dataFile = new DataFile();
+        dataFile.setId(2L);
+
+        DatasetVersion datasetVersion = new DatasetVersion();
+        datasetVersion.setVersionState(DatasetVersion.VersionState.RELEASED);
+
+        FileMetadata fileMetadata = new FileMetadata();
+        fileMetadata.setId(1L);
+        fileMetadata.setRestricted(true);
+        fileMetadata.setDataFile(dataFile);
+        fileMetadata.setDatasetVersion(datasetVersion);
+
+        mockPermissionResponseRequestOn(Permission.DownloadFile, hasPermission);
+
+        assertEquals(hasPermission, fileDownloadHelper.canDownloadFile(fileMetadata), "Initial response does not match expectation!");
+
+        // call again to exercise the cache and ensure it returns the same result
+        assertEquals(hasPermission, fileDownloadHelper.canDownloadFile(fileMetadata), "Cached response does not match initial response!");
+    }
+
+    private void mockPermissionResponseUserOn(Permission permission, boolean response) {
+        PermissionServiceBean.StaticPermissionQuery staticPermissionQuery = mock(PermissionServiceBean.StaticPermissionQuery.class);
+
+        if (permission.equals(Permission.EditDataset)) {
+            when(permissionServiceBean.userOn(ArgumentMatchers.any(), ArgumentMatchers.any(Dataset.class))).thenReturn(staticPermissionQuery);
+        } else {
+            when(permissionServiceBean.userOn(ArgumentMatchers.any(), ArgumentMatchers.any(DataFile.class))).thenReturn(staticPermissionQuery);
+        }
+
+        when(staticPermissionQuery.has(permission)).thenReturn(response);
+    }
+
+    private void mockPermissionResponseRequestOn(Permission permission, boolean response) {
+        PermissionServiceBean.RequestPermissionQuery requestPermissionQuery = mock(PermissionServiceBean.RequestPermissionQuery.class);
+
+        if (permission.equals(Permission.EditDataset)) {
+            when(permissionServiceBean.requestOn(ArgumentMatchers.any(), ArgumentMatchers.any(Dataset.class))).thenReturn(requestPermissionQuery);
+        } else {
+            when(permissionServiceBean.requestOn(ArgumentMatchers.any(), ArgumentMatchers.any(DataFile.class))).thenReturn(requestPermissionQuery);
+        }
+
+        when(requestPermissionQuery.has(permission)).thenReturn(response);
     }
 }
