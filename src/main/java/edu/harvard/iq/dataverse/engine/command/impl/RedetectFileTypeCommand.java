9cbf5ee13941153740a83a9b43dca07f12b5e3e4
Philip Durbin <philip_durbin@harvard.edu>
2019-05-16 13:56:20 -0400

    better error handling #2202

--- src/main/java/edu/harvard/iq/dataverse/engine/command/impl/RedetectFileTypeCommand.java
+++ src/main/java/edu/harvard/iq/dataverse/engine/command/impl/RedetectFileTypeCommand.java
@@ -11,11 +11,13 @@ import edu.harvard.iq.dataverse.engine.command.RequiredPermissions;
 import edu.harvard.iq.dataverse.engine.command.exception.CommandException;
 import edu.harvard.iq.dataverse.export.ExportException;
 import edu.harvard.iq.dataverse.export.ExportService;
+import edu.harvard.iq.dataverse.util.EjbUtil;
 import edu.harvard.iq.dataverse.util.FileTypeDetection;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.logging.Logger;
+import javax.ejb.EJBException;
 
 @RequiredPermissions(Permission.ManageDatasetPermissions)
 public class RedetectFileTypeCommand extends AbstractCommand<DataFile> {
@@ -34,33 +36,38 @@ public class RedetectFileTypeCommand extends AbstractCommand<DataFile> {
     @Override
     public DataFile execute(CommandContext ctxt) throws CommandException {
         DataFile filetoReturn = null;
+        Path path;
         try {
             // FIXME: Get this working with S3 and Swift.
-            Path path = DataAccess.getStorageIO(fileToRedetect).getFileSystemPath();
+            path = DataAccess.getStorageIO(fileToRedetect).getFileSystemPath();
             logger.fine("path: " + path);
             File file = path.toFile();
             String newlyDetectedContentType = FileTypeDetection.determineFileType(file);
             fileToRedetect.setContentType(newlyDetectedContentType);
-            filetoReturn = fileToRedetect;
-            if (!dryRun) {
-                filetoReturn = ctxt.files().save(fileToRedetect);
-                Dataset dataset = fileToRedetect.getOwner();
-                try {
-                    boolean doNormalSolrDocCleanUp = true;
-                    ctxt.index().indexDataset(dataset, doNormalSolrDocCleanUp);
-                } catch (Exception ex) {
-                    logger.info("Exception while reindexing files during file type redetection: " + ex.getLocalizedMessage());
-                }
-                try {
-                    ExportService instance = ExportService.getInstance(ctxt.settings());
-                    instance.exportAllFormats(dataset);
-                } catch (ExportException ex) {
-                    // Just like with indexing, a failure to export is not a fatal condition.
-                    logger.info("Exception while exporting metadata files during file type redetection: " + ex.getLocalizedMessage());
-                }
-            }
         } catch (IOException ex) {
-            throw new CommandException("Exception thrown redetecting file type: " + ex.getLocalizedMessage(), this);
+            throw new CommandException("Exception while attempting to get the bytes of the file during file type redetection: " + ex.getLocalizedMessage(), this);
+        }
+        filetoReturn = fileToRedetect;
+        if (!dryRun) {
+            try {
+                filetoReturn = ctxt.files().save(fileToRedetect);
+            } catch (EJBException ex) {
+                throw new CommandException("Exception while attempting to save the new file type: " + EjbUtil.ejbExceptionToString(ex), this);
+            }
+            Dataset dataset = fileToRedetect.getOwner();
+            try {
+                boolean doNormalSolrDocCleanUp = true;
+                ctxt.index().indexDataset(dataset, doNormalSolrDocCleanUp);
+            } catch (Exception ex) {
+                logger.info("Exception while reindexing files during file type redetection: " + ex.getLocalizedMessage());
+            }
+            try {
+                ExportService instance = ExportService.getInstance(ctxt.settings());
+                instance.exportAllFormats(dataset);
+            } catch (ExportException ex) {
+                // Just like with indexing, a failure to export is not a fatal condition.
+                logger.info("Exception while exporting metadata files during file type redetection: " + ex.getLocalizedMessage());
+            }
         }
         return filetoReturn;
     }
