9df8b753d5e5d6c1b7599c7cf7bb078c805de258
Stephen Kraffmiller <skraffmiller@hmdc.harvard.edu>
2019-05-29 15:34:35 -0400

    #5847 clean up references to the Dataset being updated

--- src/main/java/edu/harvard/iq/dataverse/engine/command/impl/UpdateDatasetVersionCommand.java
+++ src/main/java/edu/harvard/iq/dataverse/engine/command/impl/UpdateDatasetVersionCommand.java
@@ -77,21 +77,23 @@ public class UpdateDatasetVersionCommand extends AbstractDatasetCommand<Dataset>
             throw new IllegalCommandException("Only authenticated users can update datasets", this);
         }
         
-        ctxt.permissions().checkEditDatasetLock(getDataset(), getRequest(), this);
+        Dataset theDataset = getDataset();        
+        ctxt.permissions().checkEditDatasetLock(theDataset, getRequest(), this);
         Dataset savedDataset = null;
+        
         try {
             // Invariant: Dataset has no locks preventing the update
             String lockInfoMessage = "saving current edits";
             DatasetLock lock = ctxt.datasets().addDatasetLock(getDataset().getId(), DatasetLock.Reason.EditInProgress, ((AuthenticatedUser) getUser()).getId(), lockInfoMessage);
             if (lock != null) {
-                getDataset().addLock(lock);
+                theDataset.addLock(lock);
             } else {
                 logger.log(Level.WARNING, "Failed to lock the dataset (dataset id={0})", getDataset().getId());
             }
-            getDataset().getEditVersion().setDatasetFields(getDataset().getEditVersion().initDatasetFields());
-            validateOrDie(getDataset().getEditVersion(), isValidateLenient());
+            theDataset.getEditVersion().setDatasetFields(theDataset.getEditVersion().initDatasetFields());
+            validateOrDie(theDataset.getEditVersion(), isValidateLenient());
 
-            final DatasetVersion editVersion = getDataset().getEditVersion();
+            final DatasetVersion editVersion = theDataset.getEditVersion();
             tidyUpFields(editVersion);
 
             // Merge the new version into out JPA context, if needed.
@@ -101,7 +103,7 @@ public class UpdateDatasetVersionCommand extends AbstractDatasetCommand<Dataset>
                 ctxt.em().merge(editVersion);
             }
 
-            for (DataFile dataFile : getDataset().getFiles()) {
+            for (DataFile dataFile : theDataset.getFiles()) {
                 if (dataFile.getCreateDate() == null) {
                     dataFile.setCreateDate(getTimestamp());
                     dataFile.setCreator((AuthenticatedUser) getUser());
@@ -124,9 +126,9 @@ public class UpdateDatasetVersionCommand extends AbstractDatasetCommand<Dataset>
              */
             for (FileMetadata fmd : filesToDelete) {
                 // check if this file is being used as the default thumbnail
-                if (fmd.getDataFile().equals(getDataset().getThumbnailFile())) {
+                if (fmd.getDataFile().equals(theDataset.getThumbnailFile())) {
                     logger.fine("deleting the dataset thumbnail designation");
-                    getDataset().setThumbnailFile(null);
+                    theDataset.setThumbnailFile(null);
                 }
 
                 if (fmd.getDataFile().getUnf() != null) {
@@ -135,40 +137,40 @@ public class UpdateDatasetVersionCommand extends AbstractDatasetCommand<Dataset>
             }
             // we have to merge to update the database but not flush because
             // we don't want to create two draft versions!
-          //  Dataset tempDataset = ctxt.em().merge(getDataset());
-          //SEK 05/29/2019 not merging here while continuing to edit the current dataset
-          //to fix downstream merge issue 
+            //  Dataset tempDataset = ctxt.em().merge(getDataset());
+            //SEK 05/29/2019 not merging here while continuing to edit the current dataset
+            //to fix downstream merge issue 
             for (FileMetadata fmd : filesToDelete) {
                 if (!fmd.getDataFile().isReleased()) {
                     // if file is draft (ie. new to this version, delete; otherwise just remove
                     // filemetadata object)
                     ctxt.engine().submit(new DeleteDataFileCommand(fmd.getDataFile(), getRequest()));
-                    getDataset().getFiles().remove(fmd.getDataFile());
-                    getDataset().getEditVersion().getFileMetadatas().remove(fmd);
+                    theDataset.getFiles().remove(fmd.getDataFile());
+                    theDataset.getEditVersion().getFileMetadatas().remove(fmd);
                     // added this check to handle issue where you could not deleter a file that
                     // shared a category with a new file
                     // the relation ship does not seem to cascade, yet somehow it was trying to
                     // merge the filemetadata
                     // todo: clean this up some when we clean the create / update dataset methods
-                    for (DataFileCategory cat : getDataset().getCategories()) {
+                    for (DataFileCategory cat : theDataset.getCategories()) {
                         cat.getFileMetadatas().remove(fmd);
                     }
                 } else {
                     FileMetadata mergedFmd = ctxt.em().merge(fmd);
                     ctxt.em().remove(mergedFmd);
                     fmd.getDataFile().getFileMetadatas().remove(mergedFmd);
-                    getDataset().getEditVersion().getFileMetadatas().remove(mergedFmd);
+                    theDataset.getEditVersion().getFileMetadatas().remove(mergedFmd);
                 }
             }
 
             if (recalculateUNF) {
-                ctxt.ingest().recalculateDatasetVersionUNF(getDataset().getEditVersion());
+                ctxt.ingest().recalculateDatasetVersionUNF(theDataset.getEditVersion());
             }
 
-            getDataset().getEditVersion().setLastUpdateTime(getTimestamp());
-            getDataset().setModificationTime(getTimestamp());
+            theDataset.getEditVersion().setLastUpdateTime(getTimestamp());
+            theDataset.setModificationTime(getTimestamp());
 
-            savedDataset = ctxt.em().merge(getDataset());
+            savedDataset = ctxt.em().merge(theDataset);
             ctxt.em().flush();
 
             updateDatasetUser(ctxt);
