95010f93105ac98eef4b06ab949c3e417e6b4ab2
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2019-06-06 16:13:31 +0200

    test remaining methods of LruCache

--- src/test/java/edu/harvard/iq/dataverse/util/LruCacheTest.java
+++ src/test/java/edu/harvard/iq/dataverse/util/LruCacheTest.java
@@ -24,15 +24,23 @@ public class LruCacheTest {
     }
 
     /**
-     * Test of get method, of class LruCache.
+     * Test of put and get method, of class LruCache.
      */
     @Test
     public void testGetSet() {
-        sut.put(1l, "hello");
-        sut.put(2l, "world");
+        String value1 = "hello";
+        String value2 = "hello";
+
+        assertEquals(value1, sut.put(1l, value1));
+        assertEquals(value2, sut.put(2l, value2));
         
-        assertEquals("hello", sut.get(1l));
-        assertEquals("world", sut.get(2l));
+        assertEquals(value1, sut.get(1l));
+        assertEquals(value2, sut.get(2l));
+    }
+
+    @Test
+    public void testGetMaxSize() {
+        assertEquals(128, sut.getMaxSize());
     }
   
     /**
@@ -57,6 +65,44 @@ public class LruCacheTest {
         
     }
 
-    
-    
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetMaxSizeWithException() {
+        sut.setMaxSize(0l);
+    }
+
+    @Test
+    public void testInvalidateWholeCache() {
+        // prepare cache
+        Long key = 0l;
+        String value = "x";
+        assertEquals("put value", value, sut.put(key, value));
+        assertEquals("get value", value, sut.get(key));
+
+        // invalidate cache
+        sut.invalidate();
+
+        // verify invalidation
+        assertEquals("verify that value is no longer here", null, sut.get(key));
+    }
+
+    @Test
+    public void testInvalidateOneKeyOfCache() {
+        // prepare cache
+        Long key1 = 0l;
+        String value1 = "x";
+        assertEquals("put value 1", value1, sut.put(key1, value1));
+        assertEquals("get value 1", value1, sut.get(key1));
+
+        Long key2 = 1l;
+        String value2 = "y";
+        assertEquals("put value 2", value2, sut.put(key2, value2));
+        assertEquals("get value 2", value2, sut.get(key2));
+
+        // invalidate cache
+        sut.invalidate(key1);
+
+        // verify invalidation
+        assertEquals("verify that value 1 is no longer here", null, sut.get(key1));
+        assertEquals("verify that value 2 still exists", value2, sut.get(key2));
+    }
 }
