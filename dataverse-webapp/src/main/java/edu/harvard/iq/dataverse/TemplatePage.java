9147ee32d0599f50120e397c40fdcdbb9e8f60c9
Karol Kulik <karol191pol@gmail.com>
2019-06-12 10:21:13 +0200

    Closes #155: removed terms of use from dataset templates. (#165)

--- dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/TemplatePage.java
+++ dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/TemplatePage.java
@@ -7,24 +7,21 @@ import edu.harvard.iq.dataverse.engine.command.impl.UpdateDataverseCommand;
 import edu.harvard.iq.dataverse.engine.command.impl.UpdateDataverseTemplateCommand;
 import edu.harvard.iq.dataverse.util.BundleUtil;
 import edu.harvard.iq.dataverse.util.JsfHelper;
-import static edu.harvard.iq.dataverse.util.JsfHelper.JH;
-import java.sql.Timestamp;
-import java.util.Date;
-import java.util.Set;
+import org.apache.commons.lang.StringUtils;
+
 import javax.ejb.EJB;
 import javax.ejb.EJBException;
 import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.logging.Logger;
+
+import static edu.harvard.iq.dataverse.util.JsfHelper.JH;
 
 /**
- *
  * @author skraffmiller
  */
 @ViewScoped
@@ -39,23 +36,24 @@ public class TemplatePage implements java.io.Serializable {
 
     @EJB
     EjbDataverseEngine commandEngine;
-    
+
     @EJB
-    DataverseFieldTypeInputLevelServiceBean dataverseFieldTypeInputLevelService; 
-    
+    DataverseFieldTypeInputLevelServiceBean dataverseFieldTypeInputLevelService;
+
     @Inject
     DataverseRequestServiceBean dvRequestService;
-    
+
     @Inject
     PermissionsWrapper permissionsWrapper;
-    
+
     @Inject
     DataverseSession session;
 
-    public enum EditMode {
+    private static final Logger logger = Logger.getLogger(TemplatePage.class.getCanonicalName());
 
-        CREATE, METADATA, LICENSE, LICENSEADD
-    };
+    public enum EditMode {
+        CREATE, METADATA
+    }
 
     private Template template;
     private Dataverse dataverse;
@@ -102,7 +100,7 @@ public class TemplatePage implements java.io.Serializable {
     public void setOwnerId(Long ownerId) {
         this.ownerId = ownerId;
     }
-    
+
     private int selectedTabIndex;
 
     public int getSelectedTabIndex() {
@@ -114,20 +112,20 @@ public class TemplatePage implements java.io.Serializable {
     }
 
     public String init() {
- 
+
         dataverse = dataverseService.find(ownerId);
         if (dataverse == null) {
             return permissionsWrapper.notFound();
         }
         if (!permissionsWrapper.canIssueCommand(dataverse, UpdateDataverseCommand.class)) {
             return permissionsWrapper.notAuthorized();
-        } 
+        }
         if (templateId != null) { // edit or view existing for a template  
 
             template = templateService.find(templateId);
             template.setDataverse(dataverse);
             template.setMetadataValueBlocks();
-            
+
             if (template.getTermsOfUseAndAccess() != null) {
 
             } else {
@@ -150,23 +148,23 @@ public class TemplatePage implements java.io.Serializable {
             updateDatasetFieldInputLevels();
         } else {
             throw new RuntimeException("On Template page without id or ownerid."); // improve error handling
-        }       
-        return null;        
+        }
+        return null;
     }
-    
-    private void updateDatasetFieldInputLevels(){
-        Long dvIdForInputLevel = ownerId;        
-        if (!dataverseService.find(ownerId).isMetadataBlockRoot()){
+
+    private void updateDatasetFieldInputLevels() {
+        Long dvIdForInputLevel = ownerId;
+        if (!dataverseService.find(ownerId).isMetadataBlockRoot()) {
             dvIdForInputLevel = dataverseService.find(ownerId).getMetadataRootId();
-        }        
-        
-        for (DatasetField dsf: template.getFlatDatasetFields()){ 
-           DataverseFieldTypeInputLevel dsfIl = dataverseFieldTypeInputLevelService.findByDataverseIdDatasetFieldTypeId(dvIdForInputLevel, dsf.getDatasetFieldType().getId());
-           if (dsfIl != null){              
-               dsf.setInclude(dsfIl.isInclude());
-           } else {
-               dsf.setInclude(true);
-           } 
+        }
+
+        for (DatasetField dsf : template.getFlatDatasetFields()) {
+            DataverseFieldTypeInputLevel dsfIl = dataverseFieldTypeInputLevelService.findByDataverseIdDatasetFieldTypeId(dvIdForInputLevel, dsf.getDatasetFieldType().getId());
+            if (dsfIl != null) {
+                dsf.setInclude(dsfIl.isInclude());
+            } else {
+                dsf.setInclude(true);
+            }
         }
     }
 
@@ -174,64 +172,34 @@ public class TemplatePage implements java.io.Serializable {
         this.editMode = editMode;
     }
 
-    public String save(String redirectPage) {
+    public String save() {
 
-        //SEK - removed dead code 1/6/2015
-        
         boolean create = false;
         Command<Void> cmd;
-        Long createdId = new Long(0);
-        Template created;
         try {
             if (editMode == EditMode.CREATE) {
                 template.setCreateTime(new Timestamp(new Date().getTime()));
-                template.setUsageCount(new Long(0));
+                template.setUsageCount(0L);
                 dataverse.getTemplates().add(template);
-                created = commandEngine.submit(new CreateTemplateCommand(template, dvRequestService.getDataverseRequest(), dataverse));
-                createdId = created.getId();
-                //cmd = new UpdateDataverseCommand(dataverse, null, null, dvRequestService.getDataverseRequest(), null);
+                commandEngine.submit(new CreateTemplateCommand(template, dvRequestService.getDataverseRequest(), dataverse));
                 create = true;
-                //commandEngine.submit(cmd);
             } else {
                 cmd = new UpdateDataverseTemplateCommand(dataverse, template, dvRequestService.getDataverseRequest());
                 commandEngine.submit(cmd);
             }
 
-        } catch (EJBException ex) {
-            StringBuilder error = new StringBuilder();
-            error.append(ex).append(" ");
-            error.append(ex.getMessage()).append(" ");
-            Throwable cause = ex;
-            while (cause.getCause() != null) {
-                cause = cause.getCause();
-                error.append(cause).append(" ");
-                error.append(cause.getMessage()).append(" ");
-            }
-            //
-            //FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Template Save Failed", " - " + error.toString()));
-            System.out.print("dataverse " + dataverse.getName());
-            System.out.print("Ejb exception");
-            System.out.print(error.toString());
+        } catch (EJBException | CommandException ex) {
+
+            logger.fine("There was a problem with creating template: " + ex.getMessage());
             JH.addMessage(FacesMessage.SEVERITY_FATAL, BundleUtil.getStringFromBundle("template.save.fail"));
-            return null;
-        } catch (CommandException ex) {
-            System.out.print("command exception");
-            System.out.print(ex.toString());
-            //FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Template Save Failed", " - " + ex.toString()));
-            JH.addMessage(FacesMessage.SEVERITY_FATAL, BundleUtil.getStringFromBundle("template.save.fail"));
-            return null;
-            //logger.severe(ex.getMessage());
+            return StringUtils.EMPTY;
         }
-        editMode = null;       
-        String msg = (create)? BundleUtil.getStringFromBundle("template.create"): BundleUtil.getStringFromBundle("template.save");
+
+        editMode = null;
+        String msg = (create) ? BundleUtil.getStringFromBundle("template.create") : BundleUtil.getStringFromBundle("template.save");
         JsfHelper.addFlashMessage(msg);
-        String retString = "";   
-        if (!redirectPage.isEmpty() && createdId.intValue() > 0) {
-            retString = "/template.xhtml?id=" + createdId + "&ownerId=" + dataverse.getId() + "&editMode=LICENSEADD&faces-redirect=true";
-        } else {
-            retString = "/manage-templates.xhtml?dataverseId=" + dataverse.getId() + "&faces-redirect=true";           
-        }
-        return retString;
+
+        return "/manage-templates.xhtml?dataverseId=" + dataverse.getId() + "&faces-redirect=true";
     }
 
     public void cancel() {
