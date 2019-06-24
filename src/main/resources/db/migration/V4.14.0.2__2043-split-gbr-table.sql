2fe199798507f414b4da306af5feb1423a196212
Leonid Andreev <leonid@hmdc.harvard.edu>
2019-06-24 15:51:42 -0400

    adds "if exists" to the "drop column" statements in the flyway script for the gbr split. #5964

--- src/main/resources/db/migration/V4.14.0.2__2043-split-gbr-table.sql
+++ src/main/resources/db/migration/V4.14.0.2__2043-split-gbr-table.sql
@@ -1,4 +1,4 @@
 begin;
 insert into filedownload(GUESTBOOKRESPONSE_ID,DOWNLOADTYPE,DOWNLOADTIMESTAMP,SESSIONID) select ID, DOWNLOADTYPE,RESPONSETIME,SESSIONID from guestbookresponse;
-alter table guestbookresponse drop column DOWNLOADTYPE, drop column SESSIONID;
+alter table guestbookresponse drop column if exists  DOWNLOADTYPE, drop column if exists SESSIONID;
 commit;
