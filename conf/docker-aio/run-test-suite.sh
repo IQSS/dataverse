fd37a6805af6c47ee2aa68d6d75f68259aa076b5
Don Sizemore <don.sizemore@gmail.com>
2019-08-22 14:20:59 -0400

    try with one test

--- conf/docker-aio/run-test-suite.sh
+++ conf/docker-aio/run-test-suite.sh
@@ -8,4 +8,5 @@ fi
 
 # Please note the "dataverse.test.baseurl" is set to run for "all-in-one" Docker environment.
 # TODO: Rather than hard-coding the list of "IT" classes here, add a profile to pom.xml.
-mvn test -Dtest=DataversesIT,DatasetsIT,SwordIT,AdminIT,BuiltinUsersIT,UsersIT,UtilIT,ConfirmEmailIT,FileMetadataIT,FilesIT,SearchIT,InReviewWorkflowIT,HarvestingServerIT,MoveIT,MakeDataCountApiIT,FileTypeDetectionIT -Ddataverse.test.baseurl=$dvurl
+#mvn test -Dtest=DataversesIT,DatasetsIT,SwordIT,AdminIT,BuiltinUsersIT,UsersIT,UtilIT,ConfirmEmailIT,FileMetadataIT,FilesIT,SearchIT,InReviewWorkflowIT -Ddataverse.test.baseurl='http://localhost:8083'
+mvn test -Dtest=DataversesIT -Ddataverse.test.baseurl='http://localhost:8083'
