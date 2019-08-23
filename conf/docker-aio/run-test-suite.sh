d42039c801a6b0d47492c07fdea9366f72abf30f
Don Sizemore <don.sizemore@gmail.com>
2019-08-23 11:27:10 -0400

    run only InReviewWorkflowIT

---
+++ conf/docker-aio/run-test-suite.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is the canonical list of which "IT" tests are expected to pass.
+
+dvurl=$1
+if [ -z "$dvurl" ]; then
+	dvurl="http://localhost:8084"
+fi
+
+# Please note the "dataverse.test.baseurl" is set to run for "all-in-one" Docker environment.
+# TODO: Rather than hard-coding the list of "IT" classes here, add a profile to pom.xml.
+#mvn test -Dtest=DataversesIT,DatasetsIT,SwordIT,AdminIT,BuiltinUsersIT,UsersIT,UtilIT,ConfirmEmailIT,FileMetadataIT,FilesIT,SearchIT,InReviewWorkflowIT,HarvestingServerIT,MoveIT,MakeDataCountApiIT,FileTypeDetectionIT,EditDDIIT -Ddataverse.test.baseurl=$dvurl
+mvn test -Dtest=InReviewWorkflowIT -Ddataverse.test.baseurl=$dvurl
