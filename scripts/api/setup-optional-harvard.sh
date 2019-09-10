8a2d9f5a58669fa81c07d63a60b8bd14c526896a
Oliver Bertuch <o.bertuch@fz-juelich.de>
2019-09-10 15:01:32 +0200

    Add updateSchemaMDB.sh to setup-optional-harvard.sh as a usage example. Relates to #6142.

--- scripts/api/setup-optional-harvard.sh
+++ scripts/api/setup-optional-harvard.sh
@@ -55,4 +55,6 @@ curl http://localhost:8080/api/admin/datasetfield/load -X POST --data-binary @da
 curl http://localhost:8080/api/admin/datasetfield/load -X POST --data-binary @data/metadatablocks/customCHIA.tsv -H "Content-type: text/tab-separated-values"
 curl http://localhost:8080/api/admin/datasetfield/load -X POST --data-binary @data/metadatablocks/customDigaai.tsv -H "Content-type: text/tab-separated-values"
 curl http://localhost:8080/api/admin/datasetfield/load -X POST --data-binary @data/metadatablocks/custom_hbgdki.tsv -H "Content-type: text/tab-separated-values"
+# setup/update local solr instance with custom metadata block fields
+sudo -u solr ../../conf/solr/7.3.1/updateSchemaMDB.sh -t /usr/local/solr/server/solr/collection1/conf
 echo
