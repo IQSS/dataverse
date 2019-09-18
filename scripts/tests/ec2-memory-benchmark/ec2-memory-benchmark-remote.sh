a88574d89e838735724760929ed199a95f23ffcc
Leonid Andreev <leonid@hmdc.harvard.edu>
2019-09-18 17:50:40 -0400

    reorganized the benchmark script to stress-test both the dataverse and dataset pages, in two passes. (#5977)

--- scripts/tests/ec2-memory-benchmark/ec2-memory-benchmark-remote.sh
+++ scripts/tests/ec2-memory-benchmark/ec2-memory-benchmark-remote.sh
@@ -17,52 +17,121 @@ then
 fi
 
 
-# install gnuplot: 
+# install gnuplot, quietly: 
 
 yum -y install gnuplot >/dev/null 
 
 # install gplotpl if necessary:
 
-# 
+GPLOT=gplot-1.11/gplot.pl; export GPLOT
+GPLOTDIST=https://pilotfiber.dl.sourceforge.net/project/gplot/gplot/gplot-1.11.tar.gz; export GPLOTDIST
+(cd /tmp; ; curl ${GPLOTDIST} | tar xvzf - ${GPLOT})
 
-# bombard the root dataverse page with 20,000 GETs:
 
-for ((i = 0; i < 200; i++))
+# Bombard the application with some GET requests. 
+# We'll stress-test the dataverse and dataset pages. The latter is more memory- and 
+# cpu-intensive, so we'll be sending the requests at different rates, 
+# with different numbers of total requests: 20,000 for the dataverse page, 
+# and 1,000 for the dataset page. For the dataset page we'll also add 
+# some sleep interval between the calls. 
+# (we are using a very modest system configuration for this test; the heap 
+# size is set to 2GB only). 
+
+for page in dataverse dataset
 do
-    for ((j = 0; j < 20; j++))
+    date > ${DATAVERSE_APP_DIR}/memory-benchmark-raw-${page}.txt
+
+    if [ $page = "dataverse" ]
+    then
+	repeat_outer=200
+	repeat_inner=100
+    elif [ $page = "dataset" ]
+	repeat_outer=100
+	repeat_inner=10
+
+	# we'll use Gary King's dataset for the page test;
+	# let's find its id:
+	
+	dataset_id=`curl http://localhost:8080/api/dataverses/king/contents | jq '.data[0].id'`
+
+	if [[ ${dataset_id}"x" = "x" || ${dataset_id} = "null" ]]
+	then
+	    echo "Failed to obtain the id of Gary King's dataset! Did it fail to import?"
+	    exit 1
+	fi
+    fi
+
+    for ((i = 0; i < ${repeat_outer}; i++))
     do
-	# 5 GETs on the root dataverse, then sleep for 1 second:
-	for ((k = 0; k < 5; k++))
+	for ((j = 0; j < ${repeat_inner}; j++))
 	do
 	    # hide the output, standard and stderr:
-	    curl http://localhost:8080/dataverse/root 2>/dev/null > /dev/null
+	    if [ $page = "dataverse" ]
+	    then
+		curl http://localhost:8080/dataverse/root 2>/dev/null > /dev/null
+	    elif [ $page = "dataset" ]
+	    then
+		curl "http://localhost:8080/dataset.xhtml?id=${dataset_id}" 2>/dev/null > /dev/null
+		sleep 2
+	    fi	
 	done
 
-	sleep 1
-    done
+        # after every {repeat_outer} number of GETs, run jmap and save the output in a temp file:
 
-    # after every 100 GETs, run jmap and save the output in a temp file:
+	sudo -u glassfish jmap -histo ${GLASSFISH_PID} > /tmp/jmap.histo.out
+	
+        # select the dataverse classes from the histo output: 
+	#grep '  edu\.harvard\.iq\.dataverse' /tmp/jmap.histo.out
+	# (this will be a lot of output! - up to 50KB per iteration)
 
-    sudo -u glassfish jmap -histo ${GLASSFISH_PID} > /tmp/jmap.histo.out
-
-    # select the dataverse classes from the histo output: 
-    grep '  edu\.harvard\.iq\.dataverse' /tmp/jmap.histo.out
-
-    echo
-
-    # plus the totals:
-    tail -1 /tmp/jmap.histo.out
-
-    echo 
-
-    # run jstat to check on GC: 
-    sudo -u glassfish jstat -gcutil ${GLASSFISH_PID} 1000 1 2>/dev/null
-
-    echo 
-done > $DATAVERSE_APP_DIR/memory-benchmark-out.txt
+        # (alternatively, just select a few more interesting objects:)
+	grep '  edu\.harvard\.iq\.dataverse\.DataverseSession$' /tmp/jmap.histo.out
+	grep '  edu\.harvard\.iq\.dataverse\.Dataverse$' /tmp/jmap.histo.out
+	grep '  edu\.harvard\.iq\.dataverse\.Dataset$' /tmp/jmap.histo.out
+	grep '  edu\.harvard\.iq\.dataverse\.DataFile$' /tmp/jmap.histo.out
+	grep '  edu\.harvard\.iq\.dataverse\.FileMetadata$' /tmp/jmap.histo.out
 
 
-# Create a simple plot of page GETs vs. memory utilisation: 
+	echo
+	
+        # plus the totals:
+	tail -1 /tmp/jmap.histo.out
 
-grep '^  *[0-9][0-9]*\.[0-9]' ${DATAVERSE_APP_DIR}/memory-benchmark-out.txt | awk '{print (NR/10),$4}' > /tmp/dataverse.xhtml
-gplot.pl -type png -outfile ${DATAVERSE_APP_DIR}/benchmark.png -xlabel 'page GETs x1000' -ylabel 'oldgen (%, 2GB total)' -title 'Memory Utilisation, default GC' /tmp/dataverse.xhtml
+	echo 
+
+        # run jstat to check on GC: 
+	sudo -u glassfish jstat -gcutil ${GLASSFISH_PID} 1000 1 2>/dev/null
+
+	echo 
+
+	date
+
+	echo 
+    done >> $DATAVERSE_APP_DIR/memory-benchmark-out.txt
+
+    # Create simple plots of page GETs vs. memory utilisation: 
+
+    if [ $page = "dataverse" ] 
+    then
+	multiplier="0.1"
+	xlabel="page GETs x1000"
+    elif [ $page = "dataset" ] 
+    then
+	multiplier="10";
+	xlabel="page GETs"
+    fi
+
+    grep '^  *[0-9][0-9]*\.[0-9]' ${DATAVERSE_APP_DIR}/memory-benchmark-raw-${page}.txt | awk '{print (NR*'${multiplier}'),$4}' > /tmp/${page}.xhtml
+
+    if [ -f /tmp/${GPLOT} ]
+    then
+	/tmp/${GPLOT} -type png -outfile ${DATAVERSE_APP_DIR}/benchmark-${page}.png -xlabel "${xlabel}" -ylabel 'oldgen (%, 2GB total)' -title 'Memory Utilisation, default GC' /tmp/${page}.xhtml
+    else
+	echo "Skipping generating a plot, since gplotpl is not installed"
+	echo "You can still download the raw data, and generate plots locally"
+    fi
+
+    /bin/mv /tmp/${page}.xhtml ${DATAVERSE_APP_DIR}/plot-${page}.dat
+done
+
+
