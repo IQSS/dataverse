fce1289e89bf9ef6a0675f5cd627dbcccb2ab684
Philip Durbin <philip_durbin@harvard.edu>
2019-10-11 12:47:55 -0400

    add file data files (suggest 2018) #6238

--- doc/sphinx-guides/source/_static/api/file-parents.sql
+++ doc/sphinx-guides/source/_static/api/file-parents.sql
@@ -21,7 +21,12 @@ from datasetversion
 join dataset on dataset.id = datasetversion.dataset_id
 where versionstate='RELEASED'
 and dataset.harvestingclient_id is null
-group by dataset_id))
+group by dataset_id)), filedatefilter as (select id from dvobject where dtype='DataFile'
+-- Make sure the file is published.
+and publicationdate is not NULL
+-- Optionally subset by file publication date.
+--and publicationdate BETWEEN '2018-01-01' AND '2018-12-31'
+)
 select
     -- ID of the file
     dvo.id as fileid,
@@ -42,17 +47,17 @@ select
     level3dv.name as dataverse_level_3_friendly_name,
     -- Separate multiple subjects with a delimeter.
     string_agg(cvv.strvalue, ';') AS subjects,
-    -- File creation date.
-    dvo.createdate as file_creation_date,
-    -- File publication date.
-    dvo.publicationdate as file_publication_date,
     -- Dataset publication date.
-    dsv.releasetime as dataset_publication_date
+    --dsv.releasetime as dataset_publication_date,
+    -- File creation date.
+    --dvo.createdate as file_creation_date,
+    -- File publication date.
+    dvo.publicationdate as file_publication_date
 from filemetadata fmd, datasetversion dsv,
 datasetfieldvalue dsfv, datasetfield dsf,
 datasetfield dsfsub, datasetfield_controlledvocabularyvalue dsfcvv,
 controlledvocabularyvalue cvv, dvobject dvo,
-dataverse level1dv, dataset, maxversion, tree
+dataverse level1dv, dataset, maxversion, filedatefilter, tree
 left outer join dataverse level2dv on tree.id_path[2] = level2dv.id
 left outer join dataverse level3dv on tree.id_path[3] = level3dv.id
 where fmd.datasetversion_id = dsv.id
@@ -74,15 +79,10 @@ and cvv.datasetfieldtype_id = dsfsub.datasetfieldtype_id
 and dsfcvv.controlledvocabularyvalues_id = cvv.id
 -- Make sure the dataset is published.
 and dsv.releasetime is not NULL
--- Make sure the file is published.
-and dvo.publicationdate is not NULL
 -- Get the latest published version of the dataset...
 and dsv.id = maxversion.id
 -- ... done getting the latest published version of the dataset.
--- Uncomment the 3 lines below to show only 2018 data.
---and ( dvo.createdate BETWEEN '2018-01-01' AND '2018-12-31'
---or dvo.publicationdate BETWEEN '2018-01-01' AND '2018-12-31'
---or dsv.releasetime  BETWEEN '2018-01-01' AND '2018-12-31' )
+and dvo.id = filedatefilter.id
 GROUP BY
 -- Group by all the fields above *except* subject for the string_agg above.
 fileid,
@@ -97,6 +97,6 @@ dataverse_level_2_friendly_name,
 dataverse_level_3_id,
 dataverse_level_3_alias,
 dataverse_level_3_friendly_name,
-file_publication_date,
-dataset_publication_date
+--dataset_publication_date,
+file_publication_date
 order by file_publication_date desc;
