c6ee77f9eaac5e79e47bf08ac54c4a21e9ddaa58
mdmADA <mdmADA@users.noreply.github.com>
2019-10-14 12:25:45 +1100

    slight 'cosmetic' changes

--- src/main/java/edu/harvard/iq/dataverse/FileAccessRequestServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/FileAccessRequestServiceBean.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.harvard.iq.dataverse;
 
 import javax.ejb.Stateless;
@@ -16,7 +11,6 @@ import javax.persistence.PersistenceContext;
  */
 @Stateless
 @Named
-
 public class FileAccessRequestServiceBean {
 
     @PersistenceContext(unitName = "VDCNet-ejbPU")
