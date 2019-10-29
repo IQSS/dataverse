e438a27802e7d728f45d3781ef43a7e511d6f993
mdmADA <mdmADA@users.noreply.github.com>
2019-10-29 14:38:41 +1100

    removed em.refresh((AuthenticatedUser) session.getUser()); in writeGuestbookResponseAndRequestAccess as the AuthenticatedUser is not managed so doesn't work

--- src/main/java/edu/harvard/iq/dataverse/FileDownloadServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/FileDownloadServiceBean.java
@@ -185,7 +185,6 @@ public class FileDownloadServiceBean implements java.io.Serializable {
         if(countRequestAccessSuccess > 0){
             DataFile firstDataFile = selectedDataFiles.get(0);
             sendRequestFileAccessNotification(firstDataFile.getOwner(), firstDataFile.getId(), (AuthenticatedUser) session.getUser());
-            em.refresh((AuthenticatedUser) session.getUser()); //get the user to refresh in order to pick up the fileaccessrequests (I hope)
         }
        
     }
