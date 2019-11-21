c0d4f4ef8d721f0fa0b6ec8b117c2ab133e773df
Jim Myers <myersjd@umich.edu>
2019-11-21 14:13:52 -0500

    Handle expired or multiple tokens per user

--- src/main/java/edu/harvard/iq/dataverse/authorization/AuthenticationServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/authorization/AuthenticationServiceBean.java
@@ -434,11 +434,39 @@ public class AuthenticationServiceBean {
         }
         TypedQuery<ApiToken> typedQuery = em.createNamedQuery("ApiToken.findByUser", ApiToken.class);
         typedQuery.setParameter("user", au);
-        try {
-            return typedQuery.getSingleResult();
-        } catch (NoResultException | NonUniqueResultException ex) {
-            logger.log(Level.INFO, "When looking up API token for {0} caught {1}", new Object[]{au, ex});
+        List<ApiToken> tokens = typedQuery.getResultList();
+        Timestamp latest = new Timestamp(java.time.Instant.now().getEpochSecond());
+        if (tokens.isEmpty()) {
+            // Normal case - no token exists
             return null;
+        }
+        if (tokens.size() == 1) {
+            // Normal case - one token that may or may not have expired
+            ApiToken token = tokens.get(0);
+            if (token.getExpireTime().before(latest)) {
+                // Don't return an expired token which is unusable, delete it instead
+                em.remove(token);
+                return null;
+            } else {
+                return tokens.get(0);
+            }
+        } else {
+            // We have more than one due to https://github.com/IQSS/dataverse/issues/6389 or
+            // similar, so we should delete all but one token.
+            // Since having an expired token also makes no sense, if we only have an expired
+            // token, remove that as well
+            ApiToken goodToken = null;
+            for (ApiToken token : tokens) {
+                Timestamp time = token.getExpireTime();
+                if (time.before(latest)) {
+                    em.remove(token);
+                } else {
+                    latest = time;
+                    goodToken = token;
+                }
+            }
+            // Null if there are no un-expired ones
+            return goodToken;
         }
     }
     
