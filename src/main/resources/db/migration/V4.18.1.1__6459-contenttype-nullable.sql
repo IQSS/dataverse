72bc48819dbd6b570e0aad5b15e19a487dc8e5d8
Danny Brooke <danny_brooke@harvard.edu>
2019-12-16 11:27:44 -0500

    changing from non-null to null per standup discussion

--- src/main/resources/db/migration/V4.18.1.1__6459-contenttype-nullable.sql
+++ src/main/resources/db/migration/V4.18.1.1__6459-contenttype-nullable.sql
@@ -1,2 +1,2 @@
--- contenttype can be non-null because dataset tools do not require it
+-- contenttype can be null because dataset tools do not require it
 ALTER TABLE externaltool ALTER contenttype DROP NOT NULL;
