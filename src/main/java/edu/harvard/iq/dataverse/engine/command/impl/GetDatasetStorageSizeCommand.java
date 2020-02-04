88851575b79cbbdbc94f0af65fd89827b001850c
Stephen Kraffmiller <skraffmiller@hmdc.harvard.edu>
2020-02-04 10:14:42 -0500

    #6524 fix required permission for storage size

--- src/main/java/edu/harvard/iq/dataverse/engine/command/impl/GetDatasetStorageSizeCommand.java
+++ src/main/java/edu/harvard/iq/dataverse/engine/command/impl/GetDatasetStorageSizeCommand.java
@@ -72,7 +72,7 @@ public class GetDatasetStorageSizeCommand extends AbstractCommand<Long> {
     public Map<String, Set<Permission>> getRequiredPermissions() {
         // for data file check permission on owning dataset
         return Collections.singletonMap("",
-         mode != null &&  mode.equals("storage") ? Collections.singleton(Permission.ManageDatasetPermissions)
+         mode != null &&  mode.equals("storage") ? Collections.singleton(Permission.ViewUnpublishedDataset)
                 : Collections.<Permission>emptySet());
     }
 
