ad480d53a89eccf6bca5045d4693e7ee7d346c69
Marek Grzenkowicz <chopeen@gmail.com>
2020-02-28 14:04:05 +0100

    Closes #700: Make DatasetVersionDTOTest actually run and complete successfully (#702)

--- dataverse-webapp/src/test/java/edu/harvard/iq/dataverse/util/json/DatasetVersionDTOTest.java
+++ dataverse-webapp/src/test/java/edu/harvard/iq/dataverse/util/json/DatasetVersionDTOTest.java
@@ -5,32 +5,31 @@
  */
 package edu.harvard.iq.dataverse.util.json;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
-import edu.harvard.iq.dataverse.api.dto.DatasetVersionDTO;
-import edu.harvard.iq.dataverse.api.dto.FieldDTO;
-import edu.harvard.iq.dataverse.common.Util;
-import junit.framework.Assert;
+
+import org.apache.commons.io.IOUtils;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.DateFormat;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Scanner;
+import edu.harvard.iq.dataverse.api.dto.DatasetVersionDTO;
+import edu.harvard.iq.dataverse.api.dto.FieldDTO;
 
 /**
  * @author ellenk
  */
 public class DatasetVersionDTOTest {
-    private final DateFormat dateFormat = Util.getDateTimeFormat();
-
     public DatasetVersionDTOTest() {
     }
 
@@ -51,50 +50,43 @@ public class DatasetVersionDTOTest {
     public void tearDown() {
     }
 
-
     @Test
-    public void testReadDataSet() {
-        try {
+    public void testReadDataSet() throws IOException {
+        String text = readFileToString("txt/util/JsonDatasetVersion.txt");
+        Gson gson = new Gson();
+        DatasetVersionDTO dto = gson.fromJson(text, DatasetVersionDTO.class);
 
-            File file = new File("src/test/java/edu/harvard/iq/dataverse/util/json/JsonDatasetVersion.txt");
-            String text = new Scanner(file).useDelimiter("\\Z").next();
-            Gson gson = new Gson();
-            DatasetVersionDTO dto = gson.fromJson(text, DatasetVersionDTO.class);
+        HashSet<FieldDTO> author1Fields = new HashSet<>();
 
-            HashSet<FieldDTO> author1Fields = new HashSet<>();
+        author1Fields.add(FieldDTO.createPrimitiveFieldDTO("authorAffiliation", "Top"));
+        author1Fields.add(FieldDTO.createPrimitiveFieldDTO("authorIdentifier", "ellenid"));
+        author1Fields.add(FieldDTO.createVocabFieldDTO("authorIdentifierScheme", "ORCID"));
+        author1Fields.add(FieldDTO.createPrimitiveFieldDTO("authorName", "Privileged, Pete"));
 
-            author1Fields.add(FieldDTO.createPrimitiveFieldDTO("authorAffiliation", "Top"));
-            author1Fields.add(FieldDTO.createPrimitiveFieldDTO("authorIdentifier", "ellenid"));
-            author1Fields.add(FieldDTO.createVocabFieldDTO("authorIdentifierScheme", "ORCID"));
-            author1Fields.add(FieldDTO.createPrimitiveFieldDTO("authorName", "Privileged, Pete"));
+        HashSet<FieldDTO> author2Fields = new HashSet<>();
 
-            HashSet<FieldDTO> author2Fields = new HashSet<>();
+        author2Fields.add(FieldDTO.createPrimitiveFieldDTO("authorAffiliation", "Bottom"));
+        author2Fields.add(FieldDTO.createPrimitiveFieldDTO("authorIdentifier", "audreyId"));
+        author2Fields.add(FieldDTO.createVocabFieldDTO("authorIdentifierScheme", "DAISY"));
+        author2Fields.add(FieldDTO.createPrimitiveFieldDTO("authorName", "Awesome, Audrey"));
 
-            author2Fields.add(FieldDTO.createPrimitiveFieldDTO("authorAffiliation", "Bottom"));
-            author2Fields.add(FieldDTO.createPrimitiveFieldDTO("authorIdentifier", "audreyId"));
-            author2Fields.add(FieldDTO.createVocabFieldDTO("authorIdentifierScheme", "DAISY"));
-            author2Fields.add(FieldDTO.createPrimitiveFieldDTO("authorName", "Awesome, Audrey"));
+        List<HashSet<FieldDTO>> authorList = new ArrayList<>();
+        authorList.add(author1Fields);
+        authorList.add(author2Fields);
+        FieldDTO expectedDTO = new FieldDTO();
+        expectedDTO.setTypeName("author");
+        expectedDTO.setMultipleCompound(authorList);
 
-            List<HashSet<FieldDTO>> authorList = new ArrayList<>();
-            authorList.add(author1Fields);
-            authorList.add(author2Fields);
-            FieldDTO expectedDTO = new FieldDTO();
-            expectedDTO.setTypeName("author");
-            expectedDTO.setMultipleCompound(authorList);
+        FieldDTO authorDTO = dto.getMetadataBlocks().get("citation").getFields().get(1);
 
-            FieldDTO authorDTO = dto.getMetadataBlocks().get("citation").getFields().get(1);
+        // write both dto's to json to compare them with gson parser
+        JsonElement expected = gson.toJsonTree(expectedDTO, FieldDTO.class);
+        JsonElement result = gson.toJsonTree(authorDTO);
 
-            // write both dto's to json to compare them with gson parser
-
-            JsonElement expected = gson.toJsonTree(expectedDTO, FieldDTO.class);
-            JsonElement result = gson.toJsonTree(authorDTO);
-
-            Assert.assertEquals(expected, result);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        assertEquals(expected, result);
     }
 
-
+    private String readFileToString(String resourcePath) throws IOException {
+        return IOUtils.resourceToString(resourcePath, StandardCharsets.UTF_8, getClass().getClassLoader());
+    }
 }
