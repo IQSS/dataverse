347751cdbe9b611b01ab63698622a99e3c148707
Krzysztof Mądry <madryk@icm.edu.pl>
2020-05-04 15:41:05 +0200

    Closes #965: Fix flyway migration sql (#966)

--- dataverse-persistence/src/main/resources/db/migration/V25__fixGrantSuggestions.sql
+++ dataverse-persistence/src/main/resources/db/migration/V25__fixGrantSuggestions.sql
@@ -1,7 +1,4 @@
 ALTER TABLE IF EXISTS grantsuggestions RENAME TO grantsuggestion;
-ALTER TABLE IF EXISTS grantsuggestion RENAME COLUMN grantAgency TO grantagency;
-ALTER TABLE IF EXISTS grantsuggestion RENAME COLUMN grantAgencyAcronym TO grantagencyacronym;
-ALTER TABLE IF EXISTS grantsuggestion RENAME COLUMN fundingProgram TO fundingprogram;
 ALTER TABLE IF EXISTS grantsuggestion RENAME COLUMN foreignName TO suggestionname;
 ALTER TABLE IF EXISTS grantsuggestion RENAME COLUMN foreignNameLocale TO suggestionnamelocale;
 ALTER TABLE IF EXISTS grantsuggestion ALTER COLUMN fundingprogram DROP NOT NULL;
