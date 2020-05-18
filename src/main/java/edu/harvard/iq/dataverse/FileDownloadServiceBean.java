95d03c7935ca08981413fbac99b6743bbfe27141
Jim Myers <myersjd@umich.edu>
2020-05-18 15:49:32 -0400

    try to make an ajax call to launch previewer

--- src/main/java/edu/harvard/iq/dataverse/FileDownloadServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/FileDownloadServiceBean.java
@@ -275,7 +275,7 @@ public class FileDownloadServiceBean implements java.io.Serializable {
         String toolUrl = externalToolHandler.getToolUrlWithQueryParams();
         logger.fine("Exploring with " + toolUrl);
         PrimeFaces.current().executeScript("window.open('"+toolUrl + "', target='_blank');");
-        PrimeFaces.current().executeScript("window.open(window.location.href, '_self');");
+        //PrimeFaces.current().executeScript("window.open(window.location.href, '_self');");
         // This is the old logic from TwoRavens, null checks and all.
         if (guestbookResponse != null && guestbookResponse.isWriteResponse()
                 && ((fmd != null && fmd.getDataFile() != null) || guestbookResponse.getDataFile() != null)) {
