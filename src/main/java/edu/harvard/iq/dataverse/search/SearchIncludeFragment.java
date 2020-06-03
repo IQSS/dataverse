82f359eef1a80bcf8dc82778fba3480d3760dfbe
Jim Myers <myersjd@umich.edu>
2020-06-03 16:36:21 -0400

    use Java equals() :-)

--- src/main/java/edu/harvard/iq/dataverse/search/SearchIncludeFragment.java
+++ src/main/java/edu/harvard/iq/dataverse/search/SearchIncludeFragment.java
@@ -454,7 +454,7 @@ public class SearchIncludeFragment implements java.io.Serializable {
             //Sort Pub Year Chronologically (alphabetically descending - works until 10000 AD)
             for(FacetCategory fc: facetCategoryList) {
                 logger.info("Found facet: " + fc.getName());
-                if(fc.getName()==SearchFields.PUBLICATION_YEAR) {
+                if(fc.getName().equals(SearchFields.PUBLICATION_YEAR)) {
                     logger.info("Before: " + fc.getFacetLabel().toString());
                     Collections.sort(fc.getFacetLabel(), Collections.reverseOrder());     
                     logger.info("After: " + fc.getFacetLabel().toString());
