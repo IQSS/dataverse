7d575d2f1f248801eb9c392d14cfb2149f697e8f
Darek Kaczyński <kaczynskid@gmail.com>
2020-06-10 15:46:57 +0200

    Closes #1013: CIF file importer

--- dataverse-ext-common/src/main/java/edu/harvard/iq/dataverse/importer/metadata/ResultField.java
+++ dataverse-ext-common/src/main/java/edu/harvard/iq/dataverse/importer/metadata/ResultField.java
@@ -5,35 +5,34 @@ import org.apache.commons.lang3.StringUtils;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Objects;
+
+import static java.util.Collections.emptyList;
+import static java.util.Optional.ofNullable;
 
 /**
  * Container for name and value fields, but with the ability to contain also child elements, so
  * it can be used to build tree-like structures.
  */
 public class ResultField {
-    private String name;
-    private String value;
-    private List<ResultField> children;
+
+    private final String name;
+    private final String value;
+    private final List<ResultField> children;
 
     // -------------------- CONSTRUCTORS --------------------
 
     private ResultField(String name, String value) {
-        this.name = name;
-        this.value = value;
-        this.children = Collections.emptyList();
+        this(name, value, emptyList());
     }
 
     private ResultField(String name, ResultField... children) {
-        this.name = name;
-        this.value = StringUtils.EMPTY;
-        this.children = children == null || children.length == 0
-                ? Collections.emptyList()
-                : Arrays.stream(children).collect(Collectors.toList());
+        this(name, StringUtils.EMPTY, ofNullable(children).map(Arrays::asList).orElseGet(Collections::emptyList));
     }
 
     private ResultField(String name, String value, List<ResultField> children) {
-        this(name, value);
+        this.name = name;
+        this.value = value;
         this.children = children;
     }
 
@@ -78,5 +77,25 @@ public class ResultField {
                 StringUtils.isBlank(other.value) ? this.value : other.value,
                 other.children == null || other.children.isEmpty() ? this.children : other.children
          );
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ResultField that = (ResultField) o;
+
+        if (!Objects.equals(name, that.name)) return false;
+        if (!Objects.equals(value, that.value)) return false;
+        return Objects.equals(children, that.children);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (value != null ? value.hashCode() : 0);
+        result = 31 * result + (children != null ? children.hashCode() : 0);
+        return result;
     }
 }
