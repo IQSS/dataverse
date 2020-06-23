c10b516d4b77f8169e35f1d27d8f95b7e8935b35
Felker Tamás <felker.tomi@gmail.com>
2020-06-23 13:34:14 +0200

    Public ORCID login is available.

--- src/main/java/edu/harvard/iq/dataverse/authorization/providers/oauth2/impl/OrcidOAuth2AP.java
+++ src/main/java/edu/harvard/iq/dataverse/authorization/providers/oauth2/impl/OrcidOAuth2AP.java
@@ -55,7 +55,8 @@ public class OrcidOAuth2AP extends AbstractOAuth2AuthenticationProvider {
     public static final String PROVIDER_ID_SANDBOX = "orcid-sandbox";
     
     public OrcidOAuth2AP(String clientId, String clientSecret, String userEndpoint) {
-        scope = Arrays.asList("/read-limited");
+        String s = userEndpoint.startsWith("https://pub") ? "/authenticate" : "/read-limited";
+        scope = Arrays.asList(s);
         this.clientId = clientId;
         this.clientSecret = clientSecret;
         this.baseUserEndpoint = userEndpoint;
