55a081632cad25f64bbfe51e99dc46626b9f677c
Pete Meyer <pameyer@crystal.harvard.edu>
2020-07-22 14:16:54 -0400

    more cleanup

--- conf/docker-aio/0prep_deps.sh
+++ conf/docker-aio/0prep_deps.sh
@@ -3,19 +3,6 @@ if [ ! -d dv/deps ]; then
 	mkdir -p dv/deps
 fi
 wdir=`pwd`
-if [ ! -e dv/deps/glassfish4dv.tgz ]; then
-	echo "glassfish dependency prep"
-	mkdir -p /tmp/dv-prep/gf
-	cd /tmp/dv-prep/gf
-	wget http://download.java.net/glassfish/4.1/release/glassfish-4.1.zip
-	wget https://search.maven.org/remotecontent?filepath=org/jboss/weld/weld-osgi-bundle/2.2.10.Final/weld-osgi-bundle-2.2.10.Final-glassfish4.jar -O weld-osgi-bundle-2.2.10.Final-glassfish4.jar
-	unzip glassfish-4.1.zip
-	rm glassfish4/glassfish/modules/weld-osgi-bundle.jar
-	mv weld-osgi-bundle-2.2.10.Final-glassfish4.jar glassfish4/glassfish/modules
-	tar zcf $wdir/dv/deps/glassfish4dv.tgz glassfish4
-	cd $wdir
-	# assuming that folks usually have /tmp auto-clean as needed
-fi
 
 if [ ! -e dv/deps/payara-5.2020.2.zip ]; then
 	echo "payara dependency prep"
