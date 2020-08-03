795b02f7c69c698a26ba91f4e4c41032f0ae92f9
Oliver Bertuch <o.bertuch@fz-juelich.de>
2020-08-03 12:34:39 +0200

    Make the DataSourceDefinition configureable using MicroProfile Config variables in annotation options. #6819

--- src/main/java/edu/harvard/iq/dataverse/util/DataSourceProducer.java
+++ src/main/java/edu/harvard/iq/dataverse/util/DataSourceProducer.java
@@ -11,11 +11,12 @@ import javax.sql.DataSource;
     name = "java:global/jdbc/dataverse",
     // Using PGXADataSource instead of deprecated PGPoolingDataSource
     className = "org.postgresql.xa.PGXADataSource",
-    user = "dataverse",
-    password = "${ALIAS=db_password_alias}",
-    serverName = "postgresql",
-    portNumber = 5432,
-    databaseName = "dataverse",
+    user = "${MPCONFIG=dataverse.db.user}",
+    // Has to be using ALIAS for at least Payara 5.2020.3, as MPCONFIG does not seem to read properly
+    // from alias store while deploying. See upstream https://github.com/payara/Payara/issues/4487 and https://github.com/payara/Payara/issues/4709
+    password = "${ALIAS=dataverse.db.password}",
+    serverName = "${MPCONFIG=dataverse.db.host}",
+    url = "jdbc:postgresql://${MPCONFIG=dataverse.db.host}:${MPCONFIG=dataverse.db.port}/${MPCONFIG=dataverse.db.name}",
     minPoolSize = 10,
     maxPoolSize = 200)
 public class DataSourceProducer {
