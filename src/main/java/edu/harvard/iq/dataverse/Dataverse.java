80f3df064c7fab78fc273b58b28dc5a4eb78dd18
Gustavo Durand <scolapasta+github@gmail.com>
2020-08-13 14:37:01 -0400

    Update Dataverse.java

--- src/main/java/edu/harvard/iq/dataverse/Dataverse.java
+++ src/main/java/edu/harvard/iq/dataverse/Dataverse.java
@@ -226,6 +226,7 @@ public class Dataverse extends DvObjectContainer {
 
     public void setDataverseFeaturingDataverses(List<DataverseFeaturedDataverse> dataverseFeaturingDataverses) {
         this.dataverseFeaturingDataverses = dataverseFeaturingDataverses;
+	    //test
     }
     
     @OneToMany(mappedBy="dataverse", cascade={CascadeType.REMOVE, CascadeType.MERGE, CascadeType.PERSIST})
