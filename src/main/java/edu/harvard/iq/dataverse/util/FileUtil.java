9b3d3c371dea2f0b6feae8218ce5516e5ad70437
Jim Myers <myersjd@umich.edu>
2020-08-19 15:07:29 -0400

    convert int to string

--- src/main/java/edu/harvard/iq/dataverse/util/FileUtil.java
+++ src/main/java/edu/harvard/iq/dataverse/util/FileUtil.java
@@ -1869,7 +1869,7 @@ public class FileUtil implements java.io.Serializable  {
             JsonObject jo = (JsonObject) jv;
             String[] values = new String[headers.length];
             for (int i = 0; i < headers.length; i++) {
-                values[i] = jo.getString(headers[i]);
+                values[i] = jo.getJsonObject(headers[i]).toString();
             }
             csvSB.append("\n").append(String.join(",", values));
         });
