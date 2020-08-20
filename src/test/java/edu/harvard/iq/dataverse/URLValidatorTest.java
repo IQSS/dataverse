76c0b3c00e211e83f7c0e2d035fc1eb52066c4c3
Robert Treacy <rtreacy@hmdc.harvard.edu>
2020-08-20 12:48:16 -0400

    The version pf hibernate-validator that we were using, 5.0.3.Final was reported to allow an attacker to escalate permissions and access private values and create invalid instances - see CVE-2017-7536. It is reported to be fixed in versions 5.2.5.Final and greater.
    The upgraded library had changes to the api for constructing ConstaintValidatorContextImpl, used in URLValidatorTest.java. In investigating the changes, it was found that there were further changes to the api in recent versions and it was decided to adapt the code to the latest changes and use the latest available stable hibernate-validator library - 6.1.5.Final.
    
    It was also necessary to add a dependency to javax.el due to changes in the library starting with version 5.3.1.Final and later.

--- src/test/java/edu/harvard/iq/dataverse/URLValidatorTest.java
+++ src/test/java/edu/harvard/iq/dataverse/URLValidatorTest.java
@@ -7,6 +7,10 @@ import javax.validation.ConstraintValidatorContext;
 
 import org.hibernate.validator.internal.engine.constraintvalidation.ConstraintValidatorContextImpl;
 import org.hibernate.validator.internal.engine.path.PathImpl;
+//import org.hibernate.validator.internal.engine.time.DefaultTimeProvider;
+import javax.validation.Validation;
+import javax.validation.ValidatorFactory;
+import javax.validation.ClockProvider;
 import org.junit.Test;
 
 /**
@@ -14,6 +18,9 @@ import org.junit.Test;
  * @author skraffmi
  */
 public class URLValidatorTest {
+    //static DefaultTimeProvider timeProvider = DefaultTimeProvider.getInstance();
+    ValidatorFactory vFac = Validation.buildDefaultValidatorFactory();
+    
 
     @Test
     public void testIsURLValid() {
@@ -35,7 +42,7 @@ public class URLValidatorTest {
     @Test
     public void testIsValidWithContextAndValidURL() {
         String value = "https://twitter.com/";
-        ConstraintValidatorContext context = new ConstraintValidatorContextImpl(null, PathImpl.createPathFromString(""), null);
+        ConstraintValidatorContext context = new ConstraintValidatorContextImpl(vFac.getClockProvider(), PathImpl.createPathFromString(""),null, null);
 
         assertEquals(true, new URLValidator().isValid(value, context));
     }
@@ -43,7 +50,7 @@ public class URLValidatorTest {
     @Test
     public void testIsValidWithContextButInvalidURL() {
         String value = "cnn.com";
-        ConstraintValidatorContext context = new ConstraintValidatorContextImpl(null, PathImpl.createPathFromString(""), null);
+        ConstraintValidatorContext context = new ConstraintValidatorContextImpl(vFac.getClockProvider(), PathImpl.createPathFromString(""),null, null);
 
         assertEquals(false, new URLValidator().isValid(value, context));
     }
