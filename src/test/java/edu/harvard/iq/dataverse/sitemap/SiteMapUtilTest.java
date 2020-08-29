d409e8e6ef537163f7cb79172f8bcdca180f93e9
Jim Myers <myersjd@umich.edu>
2020-08-29 10:49:18 -0400

    missed import

--- src/test/java/edu/harvard/iq/dataverse/sitemap/SiteMapUtilTest.java
+++ src/test/java/edu/harvard/iq/dataverse/sitemap/SiteMapUtilTest.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Timestamp;
 import java.text.ParseException;
