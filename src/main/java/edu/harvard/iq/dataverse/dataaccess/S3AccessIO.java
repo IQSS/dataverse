1fda2b5441cb46dfe0ec4872b80591b0efa878d9
Jim Myers <myersjd@umich.edu>
2020-10-07 11:38:10 -0400

    configurable pool size with 256 default

--- src/main/java/edu/harvard/iq/dataverse/dataaccess/S3AccessIO.java
+++ src/main/java/edu/harvard/iq/dataverse/dataaccess/S3AccessIO.java
@@ -1044,7 +1044,11 @@ public class S3AccessIO<T extends DvObject> extends StorageIO<T> {
     		AmazonS3ClientBuilder s3CB = AmazonS3ClientBuilder.standard();
 
     		ClientConfiguration cc = new ClientConfiguration();
-    		cc.setMaxConnections(4096);
+    		Integer poolSize = Integer.getInteger("dataverse.files." + driverId + ".connection-pool-size");
+    		if(poolSize==null) {
+    		    poolSize = 256;
+    		}
+    		cc.setMaxConnections(poolSize);
     		s3CB.setClientConfiguration(cc);
     		
     		/**
