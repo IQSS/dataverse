38b04f8c4acb54c89df95cbfc5567f356f396a5a
lubitchv <victoria.lubitch@utoronto.ca>
2020-10-22 12:05:01 -0400

    New checksum test

--- src/main/java/edu/harvard/iq/dataverse/globus/GlobusServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/globus/GlobusServiceBean.java
@@ -724,7 +724,9 @@ public class GlobusServiceBean implements java.io.Serializable{
                 if (t.indexOf(".") > 0) {
                     long totalSize = s3ObjectSummary.getSize();
                     String filePath = s3ObjectKey;
-                    String checksumVal = s3ObjectSummary.getETag();
+                    logger.info("File Path " + filePath);
+                    String checksumVal = FileUtil.calculateChecksum(filePath, DataFile.ChecksumType.MD5);
+                    //String checksumVal = s3ObjectSummary.getETag();
 
                     if ((checksumMapOld.get(checksumVal) != null)) {
                         logger.info("datasetId :" + dataset.getId() + "======= filename ==== " + filePath + " == file already exists ");
