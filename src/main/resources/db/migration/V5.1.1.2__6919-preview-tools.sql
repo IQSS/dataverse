3e71d9609e8fd228c4d2b385019a60e68553277a
Philip Durbin <philip_durbin@harvard.edu>
2020-10-28 16:03:26 -0400

    always attempt to drop columns #6919
    On a new installation, the logic wasn't running and
    haspreviewmode is being added by a previous SQL script.

--- src/main/resources/db/migration/V5.1.1.2__6919-preview-tools.sql
+++ src/main/resources/db/migration/V5.1.1.2__6919-preview-tools.sql
@@ -5,8 +5,8 @@
 -- "externaltooltype".
 DO $$
 BEGIN
--- Only run these statements if "type" (and "haspreviewmode") still exist on the
--- externaltool table.
+-- Migrate existing external tools into to new schema. This is for upgrades,
+-- which have a "type" column from when the entity had a "type" field.
 IF EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name='externaltool' AND column_name='type') THEN
   -- For each explore tool that has preview, make an EXPLORE row.
   insert into externaltooltype(externaltool_id,type) select id,'EXPLORE' from externaltool where haspreviewmode = true and type = 'EXPLORE';
@@ -14,9 +14,9 @@ IF EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name='externalto
   insert into externaltooltype(externaltool_id,type) select id,'PREVIEW' from externaltool where haspreviewmode = true and type = 'EXPLORE';
   -- For all the other tools, make a row.
   insert into externaltooltype(externaltool_id,type) select id,type from externaltool where haspreviewmode = false or type != 'EXPLORE';
-  -- Drop "type" and "haspreviewmode" from externaltool.
-  alter table externaltool drop column if exists type;
-  alter table externaltool drop column if exists haspreviewmode;
 END IF;
+-- Drop "type" and "haspreviewmode" from externaltool.
+alter table externaltool drop column if exists type;
+alter table externaltool drop column if exists haspreviewmode;
 END
 $$
