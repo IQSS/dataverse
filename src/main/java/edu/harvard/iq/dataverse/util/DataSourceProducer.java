9dfe7b82f16c006abe6b12e94d55e667fdec48ca
Oliver Bertuch <o.bertuch@fz-juelich.de>
2020-11-18 12:46:28 +0100

    Provide @DataSourceDefinition for app-level DB config. #7418
    The JNDI names have been changed to be conform with Java EE 7.
    See https://github.com/javaee-samples/javaee7-samples/tree/master/jpa/datasourcedefinition-applicationxml-pu
    and others for working examples. (Staying with the old name was not successfull.)

---
+++ src/main/java/edu/harvard/iq/dataverse/util/DataSourceProducer.java
@@ -0,0 +1,30 @@
+package edu.harvard.iq.dataverse.util;
+
+import javax.annotation.Resource;
+import javax.annotation.sql.DataSourceDefinition;
+import javax.enterprise.inject.Produces;
+import javax.inject.Singleton;
+import javax.sql.DataSource;
+
+@Singleton
+@DataSourceDefinition(
+    name = "java:app/jdbc/dataverse",
+    // Using PGXADataSource instead of deprecated PGPoolingDataSource
+    className = "org.postgresql.xa.PGXADataSource",
+    user = "dataverse",
+    password = "${ALIAS=db_password_alias}",
+    serverName = "postgresql",
+    portNumber = 5432,
+    databaseName = "dataverse",
+    minPoolSize = 10,
+    maxPoolSize = 200)
+public class DataSourceProducer {
+    
+    @Resource(lookup="java:app/jdbc/dataverse")
+    DataSource ds;
+    
+    @Produces
+    public DataSource getDatasource() {
+        return ds;
+    }
+}
