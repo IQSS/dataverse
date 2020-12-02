1c0959b29da894d058814624b7ea9e53baebf400
Oliver Bertuch <o.bertuch@fz-juelich.de>
2020-12-02 11:25:03 +0100

    Switch to a javax.sql.ConnectionPoolDataSource class for DataSourceDefinition plus add some more inline docs. #7418

--- src/main/java/edu/harvard/iq/dataverse/util/DataSourceProducer.java
+++ src/main/java/edu/harvard/iq/dataverse/util/DataSourceProducer.java
@@ -9,9 +9,13 @@ import javax.sql.DataSource;
 @Singleton
 @DataSourceDefinition(
     name = "java:app/jdbc/dataverse",
-    // App server provides its own pooling, so go with simple data source class.
-    // PGXADataSource is unnecessary (no distributed transactions) and breaks ingest.
-    className = "org.postgresql.ds.PGSimpleDataSource",
+    //
+    // The app server (Payara) deploys a managed pool for this data source for us.
+    // We don't need to deal with this on our own.
+    //
+    // HINT: PGSimpleDataSource would work too, but as we use a connection pool, go with a javax.sql.ConnectionPoolDataSource
+    // HINT: PGXADataSource is unnecessary (no distributed transactions used) and breaks ingest.
+    className = "org.postgresql.ds.PGConnectionPoolDataSource",
     user = "${MPCONFIG=dataverse.db.user}",
     password = "${MPCONFIG=dataverse.db.password}",
     url = "jdbc:postgresql://${MPCONFIG=dataverse.db.host}:${MPCONFIG=dataverse.db.port}/${MPCONFIG=dataverse.db.name}",
