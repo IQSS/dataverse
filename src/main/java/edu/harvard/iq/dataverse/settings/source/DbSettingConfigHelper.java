00b95335e0fd312bc4649c116a06ca6470391e8e
Oliver Bertuch <o.bertuch@fz-juelich.de>
2020-12-09 00:18:33 +0100

    Add javadoc about what is DbSettingConfigHelper. #7457

--- src/main/java/edu/harvard/iq/dataverse/settings/source/DbSettingConfigHelper.java
+++ src/main/java/edu/harvard/iq/dataverse/settings/source/DbSettingConfigHelper.java
@@ -7,6 +7,13 @@ import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 
+/**
+ * This is a small helper bean for the MPCONFIG DbSettingConfigSource.
+ * As it is a singleton and built at application start (=deployment), it will inject the (stateless)
+ * settings service into the MPCONFIG POJO once it's ready.
+ *
+ * MPCONFIG requires it's sources to be POJOs. No direct dependency injection possible.
+ */
 @Singleton
 @Startup
 public class DbSettingConfigHelper {
