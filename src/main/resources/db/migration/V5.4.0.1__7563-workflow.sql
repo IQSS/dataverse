87e89a2fc222fda152a9e0e440ff80c344bd8a68
qqmyers <qqmyers@hotmail.com>
2021-02-05 12:30:41 -0500

    spaces

--- src/main/resources/db/migration/V5.4.0.1__7563-workflow.sql
+++ src/main/resources/db/migration/V5.4.0.1__7563-workflow.sql
@@ -1,4 +1,4 @@
 ALTER TABLE pendingworkflowinvocation
-ADD COLUMN IFNOTEXISTS lock_id bigint,
+ADD COLUMN IF NOT EXISTS lock_id bigint,
 ADD CONSTRAINT fk_pendingworkflowinvocation_lock_id FOREIGN KEY (lock_id)
       REFERENCES datasetlock (id);
