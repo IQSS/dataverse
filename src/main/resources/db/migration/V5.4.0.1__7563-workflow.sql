911de9a4817aa66670414409202d78611126e278
qqmyers <qqmyers@hotmail.com>
2021-02-05 12:51:03 -0500

    make idempotent/fix syntax

--- src/main/resources/db/migration/V5.4.0.1__7563-workflow.sql
+++ src/main/resources/db/migration/V5.4.0.1__7563-workflow.sql
@@ -1,4 +1,10 @@
 ALTER TABLE pendingworkflowinvocation
 ADD COLUMN IF NOT EXISTS lock_id bigint,
-ADD CONSTRAINT fk_pendingworkflowinvocation_lock_id FOREIGN KEY (lock_id)
-      REFERENCES datasetlock (id);
+      
+DO $$
+BEGIN
+IF NOT EXISTS (SELECT 1 FROM information_schema.constraint_column_usage WHERE constraint_name='fk_pendingworkflowinvocation_lock_id')
+THEN
+ALTER TABLE pendingworkflowinvocation ADD CONSTRAINT fk_pendingworkflowinvocation_lock_id FOREIGN KEY (lock_id) REFERENCES datasetlock (id);
+END IF;
+END$$;
