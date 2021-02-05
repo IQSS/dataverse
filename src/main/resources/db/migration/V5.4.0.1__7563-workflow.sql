77677625ed83b0ccb1bd0eb5fc75369cc7c1b394
qqmyers <qqmyers@hotmail.com>
2021-02-05 11:59:25 -0500

    one more

--- src/main/resources/db/migration/V5.4.0.1__7563-workflow.sql
+++ src/main/resources/db/migration/V5.4.0.1__7563-workflow.sql
@@ -1,4 +1,4 @@
 ALTER TABLE pendingworkflowinvocation
 ADD COLUMN IFNOTEXISTS lock_id bigint,
-ADD CONSTRAINT fk_pendingworkflowinvocation_lock_id FOREIGN KEY (datasetlock_id)
+ADD CONSTRAINT fk_pendingworkflowinvocation_lock_id FOREIGN KEY (lock_id)
       REFERENCES datasetlock (id);
