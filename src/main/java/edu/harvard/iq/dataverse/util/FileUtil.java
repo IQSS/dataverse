93225708c7a94d2dcd4d8fbff3ee2f063e90596f
qqmyers <qqmyers@hotmail.com>
2021-02-11 11:40:48 -0500

    set default checksumtype, add support via UI
    note - the javascript for direct upload does not yet use anything but
    md5, but this commit adds the java code to pass other types if it is
    sent (or if dropbox/other sources send it, etc.)

--- src/main/java/edu/harvard/iq/dataverse/util/FileUtil.java
+++ src/main/java/edu/harvard/iq/dataverse/util/FileUtil.java
@@ -732,6 +732,11 @@ public class FileUtil implements java.io.Serializable  {
     public static List<DataFile> createDataFiles(DatasetVersion version, InputStream inputStream, String fileName, String suppliedContentType, String newStorageIdentifier, String newCheckSum, ChecksumType newCheckSumType, SystemConfig systemConfig) throws IOException {
         List<DataFile> datafiles = new ArrayList<>();
 
+        //When there is no checksum/checksumtype being sent (normal upload, needs to be calculated), set the type to the current default
+        if(newCheckSumType == null) {
+            newCheckSumType = systemConfig.getFileFixityChecksumAlgorithm();
+        }
+
         String warningMessage = null;
 
         // save the file, in the temporary location for now: 
