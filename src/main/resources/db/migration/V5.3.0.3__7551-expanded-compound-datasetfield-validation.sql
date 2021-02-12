2ba85c2ffc72ad24bdc809169acc204928257802
Gustavo Durand <scolapasta+github@gmail.com>
2021-02-12 15:52:14 -0500

    Update V5.3.0.3__7551-expanded-compound-datasetfield-validation.sql

--- src/main/resources/db/migration/V5.3.0.3__7551-expanded-compound-datasetfield-validation.sql
+++ src/main/resources/db/migration/V5.3.0.3__7551-expanded-compound-datasetfield-validation.sql
@@ -1,13 +1,16 @@
--- update parents of compound fields where a subfield is required
-
-UPDATE datasetfieldtype SET required=true WHERE id IN 
+-- update required attribute of parents of compound fields where a subfield is required
+UPDATE datasetfieldtype SET required=true 
+WHERE id IN 
 (SELECT parentdatasetfieldtype_id FROM datasetfieldtype WHERE required=true);
 
 
 -- add input level rows for parents of compound fields  where a subfield has required input level
-
-INSERT INTO DataverseFieldTypeInputLevel (include,required,datasetfieldtype_id,dataverse_id)
+-- (first run a delete to clear out any possible existing parent values)
+DELETE FROM dataversefieldtypeinputlevel WHERE datasetfieldtype_id in (
+SELECT parentdatasetfieldtype_id FROM datasetfieldtype dsft);
+  
+INSERT INTO dataversefieldtypeinputlevel (include,required,datasetfieldtype_id,dataverse_id)
 SELECT true, true, parentdatasetfieldtype_id, dataverse_id FROM
-DataverseFieldTypeInputLevel dsftil, datasetfieldtype dsft
+dataversefieldtypeinputlevel dsftil, datasetfieldtype dsft
 WHERE dsftil.datasetfieldtype_id = dsft.id
 AND parentdatasetfieldtype_id IS NOT null;
