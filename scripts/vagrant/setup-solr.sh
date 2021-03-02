cb74b81012d634acb1b1b36e586d2c4fd8a5dd9f
Stephen Kraffmiller <skraffmiller@hmdc.harvard.edu>
2021-03-02 10:10:24 -0500

    #7373 another typo

--- scripts/vagrant/setup-solr.sh
+++ scripts/vagrant/setup-solr.sh
@@ -6,7 +6,7 @@ SOLR_HOME=/usr/local/solr
 mkdir $SOLR_HOME
 chown $SOLR_USER:$SOLR_USER $SOLR_HOME
 su $SOLR_USER -s /bin/sh -c "cp /dataverse/downloads/solr-8.8.1.tgz $SOLR_HOME"
-su $SOLR_USER -s /bin/sh -c "cd $SOLR_HOME && tar xfz solr-78.8.1.tgz"
+su $SOLR_USER -s /bin/sh -c "cd $SOLR_HOME && tar xfz solr-8.8.1.tgz"
 su $SOLR_USER -s /bin/sh -c "cd $SOLR_HOME/solr-8.8.1/server/solr && cp -r configsets/_default . && mv _default collection1"
 su $SOLR_USER -s /bin/sh -c "cp /dataverse/conf/solr/8.8.1/schema*.xml $SOLR_HOME/solr-8.8.1/server/solr/collection1/conf/"
 su $SOLR_USER -s /bin/sh -c "cp /dataverse/conf/solr/8.8.1/solrconfig.xml $SOLR_HOME/solr-8.8.1/server/solr/collection1/conf/solrconfig.xml"
