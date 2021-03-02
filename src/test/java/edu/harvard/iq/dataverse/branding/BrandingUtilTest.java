a8cb744deee43ff1215689ebc4c4a347da7f1179
Danny Brooke <danny_brooke@harvard.edu>
2021-03-02 18:08:41 -0500

    updating unit tests

--- src/test/java/edu/harvard/iq/dataverse/branding/BrandingUtilTest.java
+++ src/test/java/edu/harvard/iq/dataverse/branding/BrandingUtilTest.java
@@ -52,7 +52,7 @@ public class BrandingUtilTest {
         System.out.println("message: " + message);
         assertEquals("Welcome to LibraScholar! Get started by adding or finding data. "
                 + "Have questions? Check out the <a href=\"http://guides.dataverse.org/en/4.3/user/index.html\">User Guide</a>."
-                + " Want to test out Dataverse features? Use our <a href=\"https://demo.dataverse.org\">Demo Site</a>."
+                + " Want to test out Dataverse Software features? Use our <a href=\"https://demo.dataverse.org\">Demo Site</a>."
                 + " Also, check for your welcome email to verify your address.",
                 message);
     }
