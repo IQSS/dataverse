d52634d879dc2d3b68807f760c5d7926db02aa88
qqmyers <qqmyers@hotmail.com>
2021-03-06 13:09:02 -0500

    remove imports

--- src/main/java/edu/harvard/iq/dataverse/branding/BrandingUtil.java
+++ src/main/java/edu/harvard/iq/dataverse/branding/BrandingUtil.java
@@ -3,13 +3,10 @@ package edu.harvard.iq.dataverse.branding;
 import edu.harvard.iq.dataverse.DataverseServiceBean;
 import edu.harvard.iq.dataverse.util.BundleUtil;
 import java.util.Arrays;
-import java.util.NoSuchElementException;
-
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.mail.internet.InternetAddress;
 
-import org.apache.commons.lang3.StringUtils;
 import org.eclipse.microprofile.config.ConfigProvider;
 
 @Singleton
