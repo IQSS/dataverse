010962adba56828da7733e234b22273b8f0c3da5
Leonid Andreev <leonid@hmdc.harvard.edu>
2021-03-16 12:46:17 -0400

    removing an import that's not needed (#7638)

--- src/main/java/edu/harvard/iq/dataverse/api/DatasetFieldServiceApi.java
+++ src/main/java/edu/harvard/iq/dataverse/api/DatasetFieldServiceApi.java
@@ -20,7 +20,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Iterator; 
 import javax.ejb.EJB;
 import javax.ejb.EJBException;
 import javax.json.Json;
