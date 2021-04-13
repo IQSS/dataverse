25d772543da4cad24892e78c881cca2d4f86d941
Peter Kiraly <peter.kiraly@gwdg.de>
2021-04-13 22:25:59 +0200

    #7784 fixing test with Mockito

--- src/test/java/edu/harvard/iq/dataverse/MailServiceBeanTest.java
+++ src/test/java/edu/harvard/iq/dataverse/MailServiceBeanTest.java
@@ -1,7 +1,13 @@
 package edu.harvard.iq.dataverse;
 
+import edu.harvard.iq.dataverse.branding.BrandingUtil;
+import edu.harvard.iq.dataverse.settings.SettingsServiceBean;
 import edu.harvard.iq.dataverse.util.MailUtil;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.mail.internet.InternetAddress;
 
@@ -9,10 +15,18 @@ import java.io.UnsupportedEncodingException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class MailServiceBeanTest {
+
+    @Mock
+    DataverseServiceBean dataverseService;
+    @Mock
+    SettingsServiceBean settingsService;
 
     @Test
     void setContactDelegation_withName() {
+        initBrandingUtilWithRootDataverse(null, null);
+
         InternetAddress fromAddress = MailUtil.parseSystemAddress("Foo Bar <foo@bar.org>");
         MailServiceBean mailServiceBean = new MailServiceBean();
         try {
@@ -27,7 +41,43 @@ class MailServiceBeanTest {
     }
 
     @Test
-    void setContactDelegation_withoutName() {
+    void setContactDelegation_withoutName_fromInstallationName() {
+        initBrandingUtilWithRootDataverse("LibraScholar Dataverse", null);
+
+        InternetAddress fromAddress = MailUtil.parseSystemAddress("dataverse@dataverse.org");
+        MailServiceBean mailServiceBean = new MailServiceBean();
+        try {
+            mailServiceBean.setContactDelegation("user@example.edu", fromAddress);
+            assertEquals(
+                "LibraScholar Dataverse on behalf of user@example.edu",
+                fromAddress.getPersonal()
+            );
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void setContactDelegation_withoutName_fromBranding() {
+        initBrandingUtilWithRootDataverse(null, "LibraScholar");
+
+        InternetAddress fromAddress = MailUtil.parseSystemAddress("dataverse@dataverse.org");
+        MailServiceBean mailServiceBean = new MailServiceBean();
+        try {
+            mailServiceBean.setContactDelegation("user@example.edu", fromAddress);
+            assertEquals(
+                "LibraScholar on behalf of user@example.edu",
+                fromAddress.getPersonal()
+            );
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void setContactDelegation_withoutName_fromBundle() {
+        initBrandingUtilWithRootDataverse(null, null);
+
         InternetAddress fromAddress = MailUtil.parseSystemAddress("dataverse@dataverse.org");
         MailServiceBean mailServiceBean = new MailServiceBean();
         try {
@@ -39,5 +89,15 @@ class MailServiceBeanTest {
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
+    }
+
+    private void initBrandingUtilWithRootDataverse(String installationName, String rootDataverseName) {
+        Mockito.lenient()
+            .when(settingsService.getValueForKey(SettingsServiceBean.Key.InstallationName))
+            .thenReturn(installationName);
+        Mockito.lenient()
+            .when(dataverseService.getRootDataverseName())
+            .thenReturn(rootDataverseName);
+        BrandingUtil.injectServices(dataverseService, settingsService);
     }
 }
