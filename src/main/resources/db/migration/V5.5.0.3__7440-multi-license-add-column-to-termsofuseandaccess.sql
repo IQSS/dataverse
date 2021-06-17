5b68e53dd6e83704e3dd6a3b00913f5e26f728fb
Jan van Mansum <janvanmansum@users.noreply.github.com>
2021-06-17 12:30:40 +0200

    DD-511 Open links to license in new tab

--- src/main/resources/db/migration/V5.5.0.3__7440-multi-license-add-column-to-termsofuseandaccess.sql
+++ src/main/resources/db/migration/V5.5.0.3__7440-multi-license-add-column-to-termsofuseandaccess.sql
@@ -1,4 +1,13 @@
 
 ALTER TABLE termsofuseandaccess ADD COLUMN IF NOT EXISTS license_id BIGINT;
 
-ALTER TABLE termsofuseandaccess ADD CONSTRAINT fk_termsofuseandcesss_license_id foreign key (license_id) REFERENCES license(id);
+DO $$
+BEGIN
+
+  BEGIN
+    ALTER TABLE termsofuseandaccess ADD CONSTRAINT fk_termsofuseandcesss_license_id foreign key (license_id) REFERENCES license(id);
+  EXCEPTION
+    WHEN duplicate_object THEN RAISE NOTICE 'Table constraint fk_termsofuseandcesss_license_id already exists';
+  END;
+
+END $$;
