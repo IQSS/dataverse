4e2e6a56b6a6aea4009cba0d11c115a567f02847
Philip Durbin <philip_durbin@harvard.edu>
2021-06-30 14:29:16 -0400

    get "create dataset" working again #7986

--- src/main/java/edu/harvard/iq/dataverse/DatasetPage.java
+++ src/main/java/edu/harvard/iq/dataverse/DatasetPage.java
@@ -1851,6 +1851,11 @@ public class DatasetPage implements java.io.Serializable {
                 JsfHelper.addWarningMessage(retrieveDatasetVersionResponse.getDifferentVersionMessage());//BundleUtil.getStringFromBundle("dataset.message.metadataSuccess"));
             }
 
+            // init the citation
+            // TODO: Need to do this after privateUrl is initialized?
+            displayCitation = dataset.getCitation(true, workingVersion, isAnonymizedAccess());
+            logger.fine("Citation: " + displayCitation);
+
             if(workingVersion.isPublished()) {
                 MakeDataCountEntry entry = new MakeDataCountEntry(FacesContext.getCurrentInstance(), dvRequestService, workingVersion);
                 mdcLogService.logEntry(entry);
@@ -1984,11 +1989,6 @@ public class DatasetPage implements java.io.Serializable {
                         BundleUtil.getStringFromBundle("dataset.privateurl.infoMessageReviewer"));
             }
         }
-
-        // init the citation
-        //Need to do this after privateUrl is initialized (
-        displayCitation = dataset.getCitation(true, workingVersion, isAnonymizedAccess());
-        logger.fine("Citation: " + displayCitation);
 
         displayLockInfo(dataset);
 
