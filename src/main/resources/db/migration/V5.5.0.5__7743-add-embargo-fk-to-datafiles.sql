f1922afea69455ab69e7ca180f5b7deee86c8948
Jan van Mansum <janvanmansum@users.noreply.github.com>
2021-07-19 15:55:55 +0200

    Corrected syntax

--- src/main/resources/db/migration/V5.5.0.5__7743-add-embargo-fk-to-datafiles.sql
+++ src/main/resources/db/migration/V5.5.0.5__7743-add-embargo-fk-to-datafiles.sql
@@ -3,7 +3,7 @@ ALTER TABLE datafile ADD COLUMN IF NOT EXISTS embargo_id BIGINT;
 DO $$
 BEGIN
 
-  ALTER TABLE datafile ADD COLUMN embargo_id bigint IF NOT EXISTS;
+  ALTER TABLE datafile ADD COLUMN IF NOT EXISTS embargo_id bigint;
   BEGIN
     ALTER TABLE datafile ADD CONSTRAINT fk_datafiles_embargo_id foreign key (embargo_id) REFERENCES embargo(id);
   EXCEPTION
