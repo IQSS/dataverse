6a429f54cff68bac6d5689c0f638d866cf18c912
Jan van Mansum <janvanmansum@users.noreply.github.com>
2021-07-20 14:24:05 +0200

    Removed unnecessary part as indicated by @qqmyers

--- src/main/resources/db/migration/V5.5.0.5__7743-add-embargo-fk-to-datafiles.sql
+++ src/main/resources/db/migration/V5.5.0.5__7743-add-embargo-fk-to-datafiles.sql
@@ -1,13 +1,1 @@
 ALTER TABLE datafile ADD COLUMN IF NOT EXISTS embargo_id BIGINT;
-
-DO $$
-BEGIN
-
-  ALTER TABLE datafile ADD COLUMN IF NOT EXISTS embargo_id bigint;
-  BEGIN
-    ALTER TABLE datafile ADD CONSTRAINT fk_datafiles_embargo_id foreign key (embargo_id) REFERENCES embargo(id);
-  EXCEPTION
-    WHEN duplicate_object THEN RAISE NOTICE 'Table constraint fk_datafiles_embargo_id already exists';
-  END;
-
-END $$;
