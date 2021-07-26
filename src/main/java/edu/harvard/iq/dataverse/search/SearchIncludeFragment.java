47637629a959bb0ece02eb13a02e215de556a34d
Jim Myers <myersjd@umich.edu>
2021-07-26 16:11:53 -0400

    restrict ext status label visibility to those who can publish

--- src/main/java/edu/harvard/iq/dataverse/search/SearchIncludeFragment.java
+++ src/main/java/edu/harvard/iq/dataverse/search/SearchIncludeFragment.java
@@ -1249,6 +1249,10 @@ public class SearchIncludeFragment implements java.io.Serializable {
 
     }
 
+    public boolean canPublishDataset(long datasetId){
+        return permissionsWrapper.canIssuePublishDatasetCommand(dvObjectService.findDvObject(datasetId));
+    }
+    
     public void setDisplayCardValues() {
 
         Set<Long> harvestedDatasetIds = null;
