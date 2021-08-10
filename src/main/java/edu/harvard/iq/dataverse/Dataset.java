99e0b9fd5aba71c97ff4e0943f22b1ae5b1295aa
qqmyers <qqmyers@hotmail.com>
2021-08-10 10:54:24 -0400

    use super

--- src/main/java/edu/harvard/iq/dataverse/Dataset.java
+++ src/main/java/edu/harvard/iq/dataverse/Dataset.java
@@ -661,7 +661,7 @@ public class Dataset extends DvObjectContainer {
         List<DatasetVersion> versions = this.versions;
         // TODo - is this ever not version 1.0 (or draft if not published yet)
         DatasetVersion oldest = versions.get(versions.size() - 1);
-        Timestamp pubDate = ((DvObject) this).getPublicationDate();
+        Timestamp pubDate = super.getPublicationDate();
         if (oldest.isPublished()) {
             List<FileMetadata> fms = oldest.getFileMetadatas();
             for (FileMetadata fm : fms) {
