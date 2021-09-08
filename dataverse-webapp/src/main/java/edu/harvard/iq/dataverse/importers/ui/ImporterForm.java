ba13342b93f05554f6e6e1e36aeb57f3a5320a66
wtr-icm <58430746+wtr-icm@users.noreply.github.com>
2021-09-08 13:03:27 +0200

    Closes #MX-94: Fix message for cbg importer (#1957)

--- dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/importers/ui/ImporterForm.java
+++ dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/importers/ui/ImporterForm.java
@@ -129,7 +129,7 @@ public class ImporterForm {
         Path tempPath = prepareTempPath(file);
         Files.copy(file.getInputStream(), tempPath, StandardCopyOption.REPLACE_EXISTING);
         component.setValue(tempPath.toFile());
-        FacesContext.getCurrentInstance().addMessage(component.getClientId(), 
+        FacesContext.getCurrentInstance().addMessage(component.getClientId(),
                 new FacesMessage(FacesMessage.SEVERITY_INFO,
                 BundleUtil.getStringFromBundle(UPLOAD_SUCCESSFUL, file.getFileName()),
                 StringUtils.EMPTY));
@@ -223,7 +223,7 @@ public class ImporterForm {
                 .map(i -> new ValidationResult(i.getViewId(), fetchEmptyInputMessage(i)))
                 .collect(Collectors.toSet());
         return validated.entrySet().stream()
-                .map(e -> new ValidationResult(keyToItem.get(e.getKey()).getViewId(), e.getValue()))
+                .map(e -> new ValidationResult(keyToItem.get(e.getKey()).getViewId(), bundleWrapper.getString(e.getValue())))
                 .collect(() -> validationResults, Set::add, Set::addAll);
     }
 
@@ -247,6 +247,7 @@ public class ImporterForm {
 
     private void showValidationMessages(Set<ValidationResult> validationResults) {
         FacesContext fctx = FacesContext.getCurrentInstance();
+        JsfHelper.addFlashErrorMessage(BundleUtil.getStringFromBundle("metadata.import.form.validation.error"), "");
         for (ValidationResult result : validationResults) {
             Optional<UIComponent> soughtComponent =
                     JsfHelper.findComponent(Optional.ofNullable(fctx.getViewRoot()), result.viewId, String::endsWith);
@@ -258,7 +259,7 @@ public class ImporterForm {
                 ((UIInput) component).setValid(false);
             }
             fctx.addMessage(component.getClientId(),
-                    new FacesMessage(FacesMessage.SEVERITY_ERROR, StringUtils.EMPTY, bundleWrapper.getString(result.message)));
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, StringUtils.EMPTY, result.message));
         }
     }
 
