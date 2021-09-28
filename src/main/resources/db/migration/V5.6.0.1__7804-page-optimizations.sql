9c653fffb7eefd7fa330c7137e572d54b945e720
Leonid Andreev <leonid@hmdc.harvard.edu>
2021-09-27 20:17:02 -0400

    A flyway script that creates a custom function to replace a query that becomes unreasonably expensive for an installation with a lot of download/access activity. This solution is very efficient, but very PostgresQL-specific, hence the use of a stored function. (#7804)

---
+++ src/main/resources/db/migration/V5.6.0.1__7804-page-optimizations.sql
@@ -0,0 +1,22 @@
+-- This creates a function that ESTIMATES the size of the
+-- GuestbookResponse table (for the metrics display), instead
+-- of relying on straight "SELECT COUNT(*) ..."
+-- Significant potential savings for an active installation.
+
+CREATE OR REPLACE FUNCTION estimateGuestBookResponseTableSize()
+RETURNS bigint AS $$
+DECLARE
+  estimatedsize bigint;
+BEGIN
+  SELECT ((reltuples / relpages)
+  		* (pg_relation_size('public.guestbookresponse') / current_setting('block_size')::int)
+		)::bigint
+		FROM   pg_class
+		WHERE  oid = 'public.guestbookresponse'::regclass INTO estimatedsize;
+
+  RETURN estimatedsize;
+END;
+$$ LANGUAGE plpgsql IMMUTABLE;
+
+
+
