414f8e09bc07fde3871da32585ee7fcd1c4f7363
Oliver Bertuch <o.bertuch@fz-juelich.de>
2021-11-16 15:43:04 +0100

    feat(api-test): add JUnit5 Tags into helpers package #8251
    This will allow for a standardized collection of Strings
    thus avoid typos

---
+++ src/test/java/edu/harvard/iq/dataverse/api/helpers/Tags.java
@@ -0,0 +1,7 @@
+package edu.harvard.iq.dataverse.api.helpers;
+
+public class Tags {
+    public static final String SLOW = "slow";
+    public static final String STD_API_SET = "standard-api-test-set";
+    public static final String EXT_API_SET = "extended-api-test-set";
+}
