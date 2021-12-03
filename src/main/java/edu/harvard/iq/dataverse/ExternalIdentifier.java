bfdd70e944bc8dadf5eb044606cc0956194954cb
Jim Myers <myersjd@umich.edu>
2021-12-03 17:42:20 -0500

    also allow orcid URL form for displaying as link

--- src/main/java/edu/harvard/iq/dataverse/ExternalIdentifier.java
+++ src/main/java/edu/harvard/iq/dataverse/ExternalIdentifier.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public enum ExternalIdentifier {
-    ORCID("ORCID", "https://orcid.org/%s", "^\\d{4}-\\d{4}-\\d{4}-(\\d{4}|\\d{3}X)$"),
+    ORCID("ORCID", "https://orcid.org/%s", "^(https:\\/\\/orcid.org\\/)?\\d{4}-\\d{4}-\\d{4}-(\\d{4}|\\d{3}X)$"),
     ISNI("ISNI", "http://www.isni.org/isni/%s", "^\\d*$"),
     LCNA("LCNA", "http://id.loc.gov/authorities/names/%s", "^[a-z]+\\d+$"),
     VIAF("VIAF", "https://viaf.org/viaf/%s", "^\\d*$"),
