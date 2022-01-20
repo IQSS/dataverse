8d66d73a97a3c58f204a8fb1229a6e8535dda730
wtr-icm <58430746+wtr-icm@users.noreply.github.com>
2022-01-20 11:36:25 +0100

    Closes #2113: Fix consent cookie path (#2122)

--- dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/CookieConsentBean.java
+++ dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/CookieConsentBean.java
@@ -6,7 +6,8 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
-import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 @ManagedBean
 @RequestScoped
@@ -15,10 +16,12 @@ public class CookieConsentBean {
     private static int ONE_YEAR_IN_SECONDS = 365 * 24 * 60 * 60;
 
     public void addCookie() {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("maxAge", ONE_YEAR_IN_SECONDS);
+        properties.put("path", "/");
+
         FacesContext.getCurrentInstance()
                 .getExternalContext()
-                .addResponseCookie("ConsentCookie",
-                        StringUtils.EMPTY,
-                        Collections.singletonMap("maxAge", ONE_YEAR_IN_SECONDS));
+                .addResponseCookie("ConsentCookie", StringUtils.EMPTY, properties);
     }
 }
