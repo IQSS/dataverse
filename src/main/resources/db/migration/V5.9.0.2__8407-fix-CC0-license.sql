8dfb5d5ae0091d4d49d230a5b429d32c54eddd0f
qqmyers <qqmyers@hotmail.com>
2022-02-09 18:14:58 -0500

    #8407 fix - update CC0 1.0 info in db/align json with other licenses

---
+++ src/main/resources/db/migration/V5.9.0.2__8407-fix-CC0-license.sql
@@ -0,0 +1,3 @@
+UPDATE license SET shortdescription='Creative Commons CC0 1.0 Universal Public Domain Dedication.' WHERE name='CC0';
+UPDATE license SET iconurl='https://licensebuttons.net/p/zero/1.0/88x31.png' WHERE name='CC0';
+UPDATE license SET name='CC0 1.0' WHERE name='CC0';
