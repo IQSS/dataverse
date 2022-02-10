591c688a8f1aebb42fa874a569461fdfd06387b0
qqmyers <qqmyers@hotmail.com>
2022-02-10 08:19:18 -0500

    repackaging as one script

--- src/main/resources/db/migration/V5.9.0.1__7440-configurable-license-list.sql
+++ src/main/resources/db/migration/V5.9.0.1__7440-configurable-license-list.sql
@@ -71,6 +71,12 @@ BEGIN
   EXCEPTION
     WHEN undefined_column THEN RAISE NOTICE 'license is not in table - new instance';
   END;
+  
+  BEGIN
+    UPDATE license SET shortdescription='Creative Commons CC0 1.0 Universal Public Domain Dedication.' WHERE name='CC0';
+    UPDATE license SET iconurl='https://licensebuttons.net/p/zero/1.0/88x31.png' WHERE name='CC0';
+    UPDATE license SET name='CC0 1.0' WHERE name='CC0';
+  END;
 
 END $$;
 ALTER TABLE termsofuseandaccess DROP COLUMN IF EXISTS license;
