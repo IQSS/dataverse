4764b8e5501e0ba1b4c5bf791e2dfc7533bc52fd
qqmyers <qqmyers@hotmail.com>
2022-02-15 15:19:19 -0500

    read metadatalanauge if sent, set if exists and is allowed
    not sent - use default
    not allowed - throw exception to return bad request

--- src/main/java/edu/harvard/iq/dataverse/util/json/JsonParser.java
+++ src/main/java/edu/harvard/iq/dataverse/util/json/JsonParser.java
@@ -301,7 +301,13 @@ public class JsonParser {
         dataset.setAuthority(obj.getString("authority", null) == null ? settingsService.getValueForKey(SettingsServiceBean.Key.Authority) : obj.getString("authority"));
         dataset.setProtocol(obj.getString("protocol", null) == null ? settingsService.getValueForKey(SettingsServiceBean.Key.Protocol) : obj.getString("protocol"));
         dataset.setIdentifier(obj.getString("identifier",null));
-
+        String mdl = obj.getString("metadatalanguage",null);
+        if(mdl==null || settingsService.getBaseMetadataLanguageMap(new HashMap<String,String>(), true).containsKey(mdl)) {
+          dataset.setMetadataLanguage(mdl);
+        }else {
+            throw new JsonParseException("Specified metadatalangauge not allowed.");
+        }
+        
         DatasetVersion dsv = new DatasetVersion(); 
         dsv.setDataset(dataset);
         dsv = parseDatasetVersion(obj.getJsonObject("datasetVersion"), dsv);
