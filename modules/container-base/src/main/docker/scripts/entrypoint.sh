139f0f7d5c762533e4fdcec5dc65e487170e570f
Oliver Bertuch <o.bertuch@fz-juelich.de>
2022-03-08 17:27:51 +0100

    feat(ct-base): add new base container image in submodule

---
+++ modules/container-base/src/main/docker/scripts/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+##########################################################################################################
+#
+#  This script is a fork of https://github.com/payara/Payara/blob/master/appserver/extras/docker-images/
+#  server-full/src/main/docker/bin/entrypoint.sh and licensed under CDDL 1.1 by the Payara Foundation.
+#
+##########################################################################################################
+
+for f in "${SCRIPT_DIR}"/init_* "${SCRIPT_DIR}"/init.d/*; do
+      case "$f" in
+        *.sh)  echo "[Entrypoint] running $f"; . "$f" ;;
+        *)     echo "[Entrypoint] ignoring $f" ;;
+      esac
+      echo
+done
+
+exec "${SCRIPT_DIR}"/startInForeground.sh "${PAYARA_ARGS}"
