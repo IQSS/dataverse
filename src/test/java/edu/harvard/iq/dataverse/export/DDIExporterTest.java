ddae50f916dc550b6dd5488bf609afd5c855035e
ErykKul <eryk.kulikowski@kuleuven.be>
2022-03-22 12:07:07 +0100

    expanded wildcard in gson imports

--- src/test/java/edu/harvard/iq/dataverse/export/DDIExporterTest.java
+++ src/test/java/edu/harvard/iq/dataverse/export/DDIExporterTest.java
@@ -1,6 +1,10 @@
 package edu.harvard.iq.dataverse.export;
 
-import com.google.gson.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
 import edu.harvard.iq.dataverse.ControlledVocabularyValue;
 import edu.harvard.iq.dataverse.DatasetFieldType;
 import edu.harvard.iq.dataverse.DatasetFieldType.FieldType;
