097053338ac86fbd043ea9f11423a46682a30e80
Oliver Bertuch <o.bertuch@fz-juelich.de>
2022-04-01 17:49:47 +0200

    refactor(solr): move CompileSolrConfig to pretty sub #7662
    A pretty subcommand is used to compile the solr configset.
    The functionality will be split up to make the subcommand concentrate
    on the solrconfig.xml handling.

--- modules/solr-configset/src/main/scripts/cli/cmd/CompileSolrConfig.java
+++ modules/solr-configset/src/main/scripts/cli/cmd/CompileSolrConfig.java
@@ -1,5 +1,5 @@
-///usr/bin/env jbang "$0" "$@" ; exit $?
 //DEPS net.sf.saxon:Saxon-HE:10.6
+
 package cli.cmd;
 
 import net.sf.saxon.s9api.Processor;
@@ -29,89 +29,65 @@ import java.util.concurrent.Callable;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-@Command(name = "CompileSolrConfig",
-        mixinStandardHelpOptions = true,
-        version = "CompileSolrConfig 0.1",
-        description = "CompileSolrConfig made with jbang")
-public class CompileSolrConfig implements Callable<Integer> {
-    
-    /**
-     * A wrapper for Throwables to create a checked exception that leads to aborting the execution
-     */
-    static final class AbortScriptException extends Throwable {
-        private AbortScriptException() {}
-        public AbortScriptException(String msg, Throwable cause) {
-            super(msg, cause);
-        }
-    }
-    
-    /**
-     * Static inner logging wrapper for convenience.
-     * This is here because we don't want to add more clutter of a logging framework
-     * to the Maven output where we use this from.
-     */
-    static final class Logger {
-        public static void log(String message) {
-            System.out.println(message);
-        }
-        public static void log(AbortScriptException ex) {
-            System.out.println(ex.getMessage());
-            ex.getCause().printStackTrace();
-        }
-    }
+import cli.solrteur.AbortScriptException;
+import cli.solrteur.Logger;
+
+@Command(name = "solrconfig",
+    mixinStandardHelpOptions = true,
+    usageHelpAutoWidth = true,
+    showDefaultValues = true,
+    sortOptions = false,
+    description = "Compile the solrconfig.xml from a source and XSLT files%n")
+public
+class CompileSolrConfig implements Callable<Integer> {
     
     @Option(required = true,
         names = {"--solr-version"},
+        paramLabel = "x.y.z",
         description = "The release version of Solr (must match with the ZIP files content)")
     private String solrVersion;
     
     @Option(required = true,
-        names = {"--solr-zipfile"},
+        names = {"--zip"},
+        paramLabel = "<file>",
         description = "Path to local Solr ZIP distribution file")
     private Path solrZipFile;
     
     @Option(required = false,
-        names = {"--solr-configset-zippath"},
-        description = "Path within ZIP to _default configset",
+        names = {"--zip-subpath"},
+        paramLabel = "<path>",
+        description = "Relative path within ZIP to _default configset",
         defaultValue = "solr-{{ solr.version }}/server/solr/configsets/_default")
     private String solrConfigSetZipPath;
     
     @Option(required = true,
-        names = {"--solr-configset-targetdir"},
-        description = "Path to the new configset directory")
-    private String solrConfigSetTargetDir;
+        names = {"--xslts"},
+        paramLabel = "<dir>",
+        description = "Path to the directory with XSLTs to adapt solrconfig.xml")
+    private String solrConfigXSLTDir;
     
     @Option(required = true,
-        names = {"--solr-schemaxml-sourcepath"},
+        names = {"--schemaxml"},
+        paramLabel = "<file>",
         description = "Path to the schema.xml to include")
     private String solrSchemaXmlSourcePath;
     
     @Option(required = true,
-        names = {"--solr-config-xsltdir"},
-        description = "Path to the directory with XSLTs to adapt solrconfig.xml")
-    private String solrConfigXSLTDir;
-    
-    public static void main(String... args) {
-        int exitCode = new CommandLine(new CompileSolrConfig()).execute(args);
-        System.exit(exitCode);
-    }
+        names = {"--configset"},
+        paramLabel = "<dir>",
+        description = "Path to the new configset directory")
+    private String solrConfigSetTargetDir;
     
     /**
      * Business logic routine, calling all the execution steps.
      * @return The exit code
      */
     @Override
-    public Integer call() throws Exception { // TODO: remove Exception signature
-        try {
-            replaceVariables();
-            extractConfigSet();
-            replaceSchemaXML();
-            applySolrConfigXSLT();
-        } catch (AbortScriptException e) {
-            Logger.log(e);
-            // this might be nicely refactored with exit codes stored within the exception
-            return CommandLine.ExitCode.SOFTWARE;
-        }
+    public Integer call() throws Exception {
+        replaceVariables();
+        extractConfigSet();
+        replaceSchemaXML();
+        applySolrConfigXSLT();
         return CommandLine.ExitCode.OK;
     }
     
@@ -144,7 +120,7 @@ public class CompileSolrConfig implements Callable<Integer> {
                     
                     return FileVisitResult.CONTINUE;
                 }
-    
+                
                 // Copy & replace files already present (which makes any run idempotent by deleting the former run)
                 @Override
                 public FileVisitResult visitFile(Path zippedFile, BasicFileAttributes attrs) throws IOException {
@@ -172,7 +148,7 @@ public class CompileSolrConfig implements Callable<Integer> {
         } catch (IOException e) {
             throw new AbortScriptException("Could not delete managed-schema", e);
         }
-            
+        
         // Copy schema.xml in place
         try {
             Path sourceSchema = Path.of(solrSchemaXmlSourcePath);
@@ -188,14 +164,14 @@ public class CompileSolrConfig implements Callable<Integer> {
         
         final Path solrConfig = Path.of(solrConfigSetTargetDir, "conf", "solrconfig.xml");
         final Path xsltDir = Path.of(solrConfigXSLTDir);
-    
+        
         // Find all the XSLT files
         final List<Path> xsltFiles;
         try (Stream<Path> walk =  Files.walk(xsltDir, 2)) {
             xsltFiles = walk.filter(path -> path.toFile().isFile())
-                            .filter(path -> path.toString().endsWith("xslt"))
-                            .sorted()
-                            .collect(Collectors.toList());
+                .filter(path -> path.toString().endsWith("xslt"))
+                .sorted()
+                .collect(Collectors.toList());
         } catch (IOException e) {
             throw new AbortScriptException("Could not walk over XSLT files at " + xsltDir, e);
         }
@@ -211,28 +187,28 @@ public class CompileSolrConfig implements Callable<Integer> {
         final XsltCompiler compiler = processor.newXsltCompiler();
         
         try {
-    
+            
             // First iteration uses initial solrconfig.xml as input source
             StreamSource source = new StreamSource(Files.newInputStream(solrConfig));
-    
+            
             // For every XSLT, we need to do the transformation and rotate the input source afterwards,
             // so we apply the next transformation to the already transformed content.
             for (Path xsltFile : xsltFiles) {
                 final XsltExecutable stylesheet = compiler.compile(new StreamSource(Files.newInputStream(xsltFile)));
-    
+                
                 // Prepare a fresh temporary file (which will be deleted when we read it back for the next iteration)
                 final Path tmpFile = Files.createTempFile(null, null);
                 Serializer out = processor.newSerializer(Files.newOutputStream(tmpFile));
-    
+                
                 // Actual transformation happens
                 Xslt30Transformer transformer = stylesheet.load30();
                 transformer.transform(source, out);
-    
+                
                 // Read back the transformed config and rotate the source. The opening option makes the old temp file
                 // go away after it has been read.
                 source = new StreamSource(Files.newInputStream(tmpFile, StandardOpenOption.DELETE_ON_CLOSE));
             }
-    
+            
             // The final transformation still reads back the final result, so we need to push InputStream content somewhere
             Files.copy(source.getInputStream(),
                 solrConfig,
