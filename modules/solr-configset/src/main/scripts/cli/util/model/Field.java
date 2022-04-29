52dc5a90a3cf1692486c277820c89e3eda0772d2
Oliver Bertuch <o.bertuch@fz-juelich.de>
2022-04-29 19:18:51 +0200

    feat(solrteur): add all headers to Field #7662
    Includes all the predicates according to spec and test for them.

--- modules/solr-configset/src/main/scripts/cli/util/model/Field.java
+++ modules/solr-configset/src/main/scripts/cli/util/model/Field.java
@@ -1,5 +1,6 @@
 package cli.util.model;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -7,6 +8,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Predicate;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public final class Field {
@@ -14,15 +16,80 @@ public final class Field {
     public static final String KEYWORD = "datasetField";
     
     /**
+     * Currently, the spec says we need to comply with Solr and DDI rules.
+     * De-facto commonly used are camel-cased names.
+     * See also https://solr.apache.org/guide/8_11/defining-fields.html#field-properties
+     *
+     * This regex matches the strict Solr name spec plus adds "." as valid chars
+     * (we need it for comptability with astrophysics.tsv)
+     */
+    public static final String NAME_PATTERN = "^(?=_[\\w.]+[^_]$|[^_][\\w.]+_?$)[a-zA-Z_][\\w.]+$";
+    public static final String WHITESPACE_ONLY = "^\\s+$";
+    
+    public static final Predicate<String> matchBoolean = s -> "TRUE".equals(s) || "FALSE".equals(s);
+    
+    /**
      * Programmatic variant of the spec of a #datasetField. List all the column headers and associated restrictions
      * on the values of a column.
      */
     public enum Header {
-        // TODO: extend!
         KEYWORD(
             Field.KEYWORD,
             String::isEmpty,
             "must have no value (be empty)"
+        ),
+        
+        NAME(
+            "name",
+            Predicate.not(String::isBlank).and(Pattern.compile(Field.NAME_PATTERN).asMatchPredicate()),
+            "must not be blank and match regex pattern " + Field.NAME_PATTERN
+        ),
+        TITLE("title"),
+        DESCRIPTION("description"),
+        WATERMARK(
+            "watermark",
+            Predicate.not(Pattern.compile(WHITESPACE_ONLY).asMatchPredicate()),
+            "must not be whitespace only"
+        ),
+        
+        FIELDTYPE(
+            "fieldType",
+            Types.matchesTypes(),
+            "must be one of [" + String.join(", ", Types.getTypesList()) + "]"
+        ),
+        
+        DISPLAY_ORDER(
+            "displayOrder",
+            Pattern.compile("\\d+").asMatchPredicate(),
+            "must be a non-negative integer"
+        ),
+        DISPLAY_FORMAT(
+            "displayFormat",
+            Predicate.not(Pattern.compile(WHITESPACE_ONLY).asMatchPredicate()),
+            "must not be whitespace only"
+        ),
+        
+        ADVANCED_SEARCH_FIELD("advancedSearchField", matchBoolean, "must be 'TRUE' or 'FALSE"),
+        ALLOW_CONTROLLED_VOCABULARY("allowControlledVocabulary", matchBoolean, "must be 'TRUE' or 'FALSE"),
+        ALLOW_MULTIPLES("allowmultiples", matchBoolean, "must be 'TRUE' or 'FALSE"),
+        FACETABLE("facetable", matchBoolean, "must be 'TRUE' or 'FALSE"),
+        DISPLAY_ON_CREATE("displayoncreate", matchBoolean, "must be 'TRUE' or 'FALSE"),
+        REQUIRED("required", matchBoolean, "must be 'TRUE' or 'FALSE"),
+        
+        PARENT(
+            "parent",
+            Pattern.compile(Field.NAME_PATTERN).asMatchPredicate().or(String::isEmpty),
+            "must be either empty or match regex pattern " + Field.NAME_PATTERN
+        ),
+        METADATABLOCK_ID(
+            "metadatablock_id",
+            Predicate.not(String::isBlank).and(Pattern.compile(Block.NAME_PATTERN).asMatchPredicate()),
+            "must not be blank and match regex pattern " + Block.NAME_PATTERN
+        ),
+        TERM_URI(
+            "termURI",
+            s -> s.isEmpty() || Validator.isValidUrl(s),
+            "must be empty or a valid URL"
         );
         
         private final String value;
--- modules/solr-configset/src/test/java/cli/util/model/FieldTest.java
+++ modules/solr-configset/src/test/java/cli/util/model/FieldTest.java
@@ -2,7 +2,9 @@ package cli.util.model;
 
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.function.Predicate;
@@ -32,4 +34,64 @@ class FieldTest {
         }
     }
     
+    @Nested
+    class HeaderTest {
+        @ParameterizedTest
+        @NullAndEmptySource
+        @ValueSource(strings = {"   ", "\t", "_foobar_", "_foo_bar_"})
+        void invalidNames(String subject) {
+            assertFalse(Field.Header.NAME.isValid(subject));
+        }
+    
+        @ParameterizedTest
+        @ValueSource(strings = {"foobar_", "foo_bar_", "_foobar", "_foo_bar", "foobar", "foobar1234", "foo_bar_1234"})
+        void validNames(String subject) {
+            assertTrue(Field.Header.NAME.isValid(subject));
+            assertTrue(Field.Header.PARENT.isValid(subject));
+        }
+    
+        @ParameterizedTest
+        @EmptySource
+        void validParentName(String subject) {
+            assertTrue(Field.Header.PARENT.isValid(subject));
+        }
+    
+        @ParameterizedTest
+        @NullSource
+        @ValueSource(strings = {"   ", "\t"})
+        void invalidEmptyOrText(String subject) {
+            assertFalse(Field.Header.WATERMARK.isValid(subject));
+            assertFalse(Field.Header.DISPLAY_FORMAT.isValid(subject));
+        }
+    
+        @ParameterizedTest
+        @ValueSource(strings = { "", "foobar", "My name is Hase, I know about nothing."})
+        void validEmptyOrText(String subject) {
+            assertTrue(Field.Header.WATERMARK.isValid(subject));
+            assertTrue(Field.Header.DISPLAY_FORMAT.isValid(subject));
+        }
+    
+        @ParameterizedTest
+        @NullAndEmptySource
+        @ValueSource(strings = { "true", "false", "0", "1", "foobar"})
+        void invalidBoolean(String subject) {
+            assertFalse(Field.Header.ADVANCED_SEARCH_FIELD.isValid(subject));
+            assertFalse(Field.Header.ALLOW_CONTROLLED_VOCABULARY.isValid(subject));
+            assertFalse(Field.Header.ALLOW_MULTIPLES.isValid(subject));
+            assertFalse(Field.Header.FACETABLE.isValid(subject));
+            assertFalse(Field.Header.DISPLAY_ON_CREATE.isValid(subject));
+            assertFalse(Field.Header.REQUIRED.isValid(subject));
+        }
+    
+        @ParameterizedTest
+        @ValueSource(strings = { "TRUE", "FALSE" })
+        void validBoolean(String subject) {
+            assertTrue(Field.Header.ADVANCED_SEARCH_FIELD.isValid(subject));
+            assertTrue(Field.Header.ALLOW_CONTROLLED_VOCABULARY.isValid(subject));
+            assertTrue(Field.Header.ALLOW_MULTIPLES.isValid(subject));
+            assertTrue(Field.Header.FACETABLE.isValid(subject));
+            assertTrue(Field.Header.DISPLAY_ON_CREATE.isValid(subject));
+            assertTrue(Field.Header.REQUIRED.isValid(subject));
+        }
+    }
 }
