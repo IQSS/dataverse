436cd6625f09e852803530d9d18fda1fead07e3c
Oliver Bertuch <o.bertuch@fz-juelich.de>
2022-06-20 17:36:03 +0200

    feat(settings): start new enum JvmSettings #7000
    To be able to have more control over JVM settings names,
    avoid typos, maybe create lists of settings and so on,
    this enum will provide the place to add any old and new
    settings that are destined to be made at the JVM level.
    
    Further extensions of this class include adding aliases
    when renaming settings, adding predicates for validation
    and offering injecting parameters into keys (as used with
    the files subsystem).

---
+++ src/main/java/edu/harvard/iq/dataverse/settings/JvmSettings.java
@@ -0,0 +1,52 @@
+package edu.harvard.iq.dataverse.settings;
+
+/**
+ * Enum to store each and every JVM-based setting as a reference,
+ * much like the enum {@link SettingsServiceBean.Key} for DB settings.
+ *
+ * To be able to have more control over JVM settings names,
+ * avoid typos, maybe create lists of settings and so on,
+ * this enum will provide the place to add any old and new
+ * settings that are destined to be made at the JVM level.
+ *
+ * Further extensions of this class include
+ * - adding aliases when renaming settings,
+ * - adding predicates for validation and
+ * - offering injecting parameters into keys (as used with the file access subsystem).
+ */
+public enum JvmSettings {
+    // the upmost root scope - every setting shall start with it.
+    PREFIX("dataverse"),
+    
+    // GENERAL SETTINGS
+    VERSION(PREFIX, "version"),
+    BUILD(PREFIX, "build")
+    
+    ;
+    
+    private final String key;
+    private final JvmSettings parent;
+    
+    JvmSettings(String key) {
+        this.key = key;
+        this.parent = null;
+    }
+    
+    JvmSettings(JvmSettings scope, String key) {
+        this.key = key;
+        this.parent = scope;
+    }
+    
+    /**
+     * Retrieve the scoped key for this setting. Scopes are separated by dots.
+     *
+     * @return The scoped key (or the key if no scope). Example: dataverse.subscope.subsubscope.key
+     */
+    public String getScopedKey() {
+        if (this.parent != null) {
+            return parent.getScopedKey() + "." + this.key;
+        } else {
+            return this.key;
+        }
+    }
+}
