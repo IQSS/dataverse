0e52e07e5cedeb1ea72e69cb0cc4052ca6611959
qqmyers <qqmyers@hotmail.com>
2022-07-25 15:38:58 -0400

    remove old script

--- src/main/resources/db/migration/V5.10.1.3__8605-support-archival-status.sql
+++
@@ -1,2 +0,0 @@
-UPDATE datasetversion SET archivalCopyLocation = CONCAT('{"status":"success", "message":"', archivalCopyLocation,'"}') where archivalCopyLocation is not null and not archivalCopyLocation='Attempted';
-UPDATE datasetversion SET archivalCopyLocation = CONCAT('{"status":"failure", "message":"Attempted"}') where archivalCopyLocation='Attempted';
