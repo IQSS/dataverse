53dc116ea49c548c4dfdc379f131bb08e06ace1c
qqmyers <qqmyers@hotmail.com>
2022-07-29 14:26:23 -0400

    typo

--- src/main/java/edu/harvard/iq/dataverse/DatasetVersion.java
+++ src/main/java/edu/harvard/iq/dataverse/DatasetVersion.java
@@ -64,7 +64,7 @@ import org.apache.commons.lang3.StringUtils;
  */
 
 @NamedQueries({
-    @NamedQuery(name = "DatasetVersion.findUnarchivedReleasedVersions",
+    @NamedQuery(name = "DatasetVersion.findUnarchivedReleasedVersion",
                query = "SELECT OBJECT(o) FROM DatasetVersion AS o WHERE o.releaseTime IS NOT NULL and o.archivalCopyLocation IS NULL"
     )})
     
