ffc81a047269aa5fddacf511ea459daf41d79c2c
kuhlaid <41551429+kuhlaid@users.noreply.github.com>
2022-08-01 18:33:56 -0400

    Update 1prep.sh
    Trying to set Maven Path

--- conf/docker-aio/1prep.sh
+++ conf/docker-aio/1prep.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# @todo - need code to install `make` since the Docker build does not automatically include it
+
 # move things necessary for integration tests into build context.
 # this was based off the phoenix deployment; and is likely uglier and bulkier than necessary in a perfect world
 
@@ -17,17 +19,13 @@ tar xfz apache-maven-3.8.6-bin.tar.gz
 mkdir maven
 mv apache-maven-3.8.6/* maven/
 echo "export JAVA_HOME=/usr/lib/jvm/jre-openjdk" > maven/maven.sh
-echo "export M2_HOME=../maven" >> maven/maven.sh
-echo "export MAVEN_HOME=../maven" >> maven/maven.sh
+echo "export M2_HOME=$(pwd)/maven" >> maven/maven.sh
+echo "export MAVEN_HOME=$(pwd)/maven" >> maven/maven.sh
 echo "export PATH=$PATH:$(pwd)/maven/bin" >> maven/maven.sh
-# echo "export PATH=../maven/bin:${PATH}" >> maven/maven.sh
 chmod 0755 maven/maven.sh
-echo "export PATH=\$PATH:$(pwd)/maven/bin"
 
 # not using dvinstall.zip for setupIT.bash; but still used in install.bash for normal ops
-source maven/maven.sh
-# need to set Maven path before we can use Maven
-mvn clean
+source maven/maven.sh && mvn clean
 ./scripts/installer/custom-build-number
 source maven/maven.sh && mvn package
 cd scripts/installer
