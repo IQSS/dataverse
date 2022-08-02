0f2c994e301d5c31af5479564fbb31d7b65e096d
kuhlaid <41551429+kuhlaid@users.noreply.github.com>
2022-08-02 15:18:13 -0400

    Update 0prep_deps.sh
    Adding back Solr since it cannot be installed via a package manager

--- conf/docker-aio/0prep_deps.sh
+++ conf/docker-aio/0prep_deps.sh
@@ -5,10 +5,11 @@ if [ ! -d dv/deps ]; then
 	mkdir -p dv/deps
 fi
 
-if [ ! -e dv/deps/payara-5.2021.6.zip ]; then
-	echo "payara dependency prep"
-	wget https://s3-eu-west-1.amazonaws.com/payara.fish/Payara+Downloads/5.2021.6/payara-5.2021.6.zip  -O dv/deps/payara-5.2021.6.zip
-fi
+# installing payara using 
+# if [ ! -e dv/deps/payara-5.2021.6.zip ]; then
+# 	echo "payara dependency prep"
+# 	wget https://s3-eu-west-1.amazonaws.com/payara.fish/Payara+Downloads/5.2021.6/payara-5.2021.6.zip  -O dv/deps/payara-5.2021.6.zip
+# fi
 
 if [ ! -e dv/deps/solr-8.11.1dv.tgz ]; then
 	echo "solr dependency prep"	
