2967874976afc85c37dba0a8540af033bcabc855
kuhlaid <41551429+kuhlaid@users.noreply.github.com>
2022-08-02 09:56:13 -0400

    Update 1prep.sh
    Trying to fix Maven install

--- conf/docker-aio/1prep.sh
+++ conf/docker-aio/1prep.sh
@@ -18,8 +18,8 @@ cd ../../
 cp -r scripts conf/docker-aio/testdata/
 cp doc/sphinx-guides/source/_static/util/createsequence.sql conf/docker-aio/testdata/doc/sphinx-guides/source/_static/util/
 
-wget -q https://downloads.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz
-tar xfz apache-maven-3.8.6-bin.tar.gz
+wget -q https://downloads.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz -O dv/deps/apache-maven-3.8.6-bin.tar.gz
+tar xfz dv/deps/apache-maven-3.8.6-bin.tar.gz
 mkdir maven
 mv apache-maven-3.8.6/* maven/
 echo "export JAVA_HOME=/usr/local/openjdk-11/bin/java" > maven/maven.sh                 # if getting error about where java is installed try running `whereis java` to find the directory
@@ -27,6 +27,8 @@ echo "export M2_HOME=$(pwd)/maven" >> maven/maven.sh
 echo "export MAVEN_HOME=$(pwd)/maven" >> maven/maven.sh
 echo "export PATH=$PATH:$(pwd)/maven/bin" >> maven/maven.sh
 chmod 0755 maven/maven.sh
+
+echo $PATH
 
 # not using dvinstall.zip for setupIT.bash; but still used in install.bash for normal ops
 source maven/maven.sh && mvn clean
