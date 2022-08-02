426fab936feb24dac8dc21e3b0a3dfc2b184be06
kuhlaid <41551429+kuhlaid@users.noreply.github.com>
2022-08-02 15:48:36 -0400

    Update entrypoint.bash
    Reverting back

--- conf/docker-aio/entrypoint.bash
+++ conf/docker-aio/entrypoint.bash
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 export LANG=en_US.UTF-8
-sudo -u postgres /usr/pgsql-13/bin/pg_ctl start -D /var/lib/pgsql/13/data &
-cd /usr/share/solr                # hard-coding this path is not ideal
-echo "starting Solr"
+sudo -u postgres /usr/bin/pg_ctl start -D /var/lib/pgsql/data &
+cd /opt/solr-8.8.1/
 # TODO: Run Solr as non-root and remove "-force".
 bin/solr start -force
 bin/solr create_core -c collection1 -d server/solr/collection1/conf -force
@@ -10,8 +9,7 @@ bin/solr create_core -c collection1 -d server/solr/collection1/conf -force
 # start apache, in both foreground and background...
 apachectl -DFOREGROUND &
 
-# TODO: Run Payara as non-root.
-cd /opt/payara5
+# TODO: Run Glassfish as non-root.
+cd /opt/glassfish4
 bin/asadmin start-domain --debug
 sleep infinity
-
