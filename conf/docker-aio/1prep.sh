55f90a5e8bea16d17713c2a8bf1a6cccfa89cc23
kuhlaid <41551429+kuhlaid@users.noreply.github.com>
2022-08-02 16:05:46 -0400

    Update 1prep.sh
    Trying to fix Maven and Make install

--- conf/docker-aio/1prep.sh
+++ conf/docker-aio/1prep.sh
@@ -3,7 +3,9 @@
 # move things necessary for integration tests into build context.
 # this was based off the phoenix deployment; and is likely uglier and bulkier than necessary in a perfect world
         
-yum install -y make maven         # install `maven` and `make` since they are not automatically included in the OS
+sudo apt-get update                  # update the packages so `make` can be included
+sudo apt-get -y install make         # install `make` since it is not automatically included in the OS
+sudo apt-get install maven        # install `maven` since it is not automatically included in the OS
 
 mkdir -p testdata/doc/sphinx-guides/source/_static/util/
 cp ../solr/8.8.1/schema*.xml testdata/
