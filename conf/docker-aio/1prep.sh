d3d4e67bd6c933eb05acc30415d8e2e4fc5f6d98
kuhlaid <41551429+kuhlaid@users.noreply.github.com>
2022-08-02 15:47:52 -0400

    Update 1prep.sh
    Reverting back

--- conf/docker-aio/1prep.sh
+++ conf/docker-aio/1prep.sh
@@ -1,43 +1,37 @@
 #!/bin/bash
 
-# @todo - /usr/lib/jvm/jre-openjdk does not exist
-
 # move things necessary for integration tests into build context.
 # this was based off the phoenix deployment; and is likely uglier and bulkier than necessary in a perfect world
 
-sudo apt-get update                  # update the packages so `make` can be included
-sudo apt-get -y install make         # install `make` since it is not automatically included in the OS
-sudo apt-get -y install maven        # install `maven` since it is not automatically included in the OS
-# sudo apt-get -y install payara       # install `payara` since it is not automatically included in the OS
-# sudo apt-get -y update               # we are not able to install Solr using apt-get so we need to download it manually (not ideal)
-
-# insert test data configurations
 mkdir -p testdata/doc/sphinx-guides/source/_static/util/
-cp ../solr/8.11.1/schema*.xml testdata/
-cp ../solr/8.11.1/solrconfig.xml testdata/
+cp ../solr/8.8.1/schema*.xml testdata/
+cp ../solr/8.8.1/solrconfig.xml testdata/
+cp ../solr/8.8.1/updateSchemaMDB.sh testdata/
 cp ../jhove/jhove.conf testdata/
 cp ../jhove/jhoveConfig.xsd testdata/
 cd ../../
 cp -r scripts conf/docker-aio/testdata/
 cp doc/sphinx-guides/source/_static/util/createsequence.sql conf/docker-aio/testdata/doc/sphinx-guides/source/_static/util/
 
-# mkdir maven
-# mv apache-maven-3.8.6/* maven/
-# echo "export JAVA_HOME=/usr/local/openjdk-11/bin/java" > maven/maven.sh                 # if getting error about where java is installed try running `whereis java` to find the directory
-# echo "export M2_HOME=$(pwd)/maven" >> maven/maven.sh
-# echo "export MAVEN_HOME=$(pwd)/maven" >> maven/maven.sh
-# echo "export PATH=$PATH:$(pwd)/maven/bin" >> maven/maven.sh
-# chmod 0755 maven/maven.sh
-
+wget -q https://downloads.apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
+tar xfz apache-maven-3.6.3-bin.tar.gz
+mkdir maven
+mv apache-maven-3.6.3/* maven/
+echo "export JAVA_HOME=/usr/lib/jvm/jre-openjdk" > maven/maven.sh
+echo "export M2_HOME=../maven" >> maven/maven.sh
+echo "export MAVEN_HOME=../maven" >> maven/maven.sh
+echo "export PATH=../maven/bin:${PATH}" >> maven/maven.sh
+chmod 0755 maven/maven.sh
 
 # not using dvinstall.zip for setupIT.bash; but still used in install.bash for normal ops
-# source maven/maven.sh && mvn clean
-# ./scripts/installer/custom-build-number
-# source maven/maven.sh && 
-mvn package
-cd ./scripts/installer
+source maven/maven.sh && mvn clean
+./scripts/installer/custom-build-number
+source maven/maven.sh && mvn package
+cd scripts/installer
 make clean
 make
+mkdir -p ../../conf/docker-aio/dv/install
+cp dvinstall.zip ../../conf/docker-aio/dv/install/
 
 # ITs sometimes need files server-side
 # yes, these copies could be avoided by moving the build root here. but the build 
