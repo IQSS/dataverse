a7fe29c8e2e088fff71a13327e28c7cbb9595c15
Oliver Bertuch <o.bertuch@fz-juelich.de>
2022-09-16 10:32:10 +0200

    test(settings): make SystemConfigTest version testing use JvmSetting extension

--- src/test/java/edu/harvard/iq/dataverse/util/SystemConfigTest.java
+++ src/test/java/edu/harvard/iq/dataverse/util/SystemConfigTest.java
@@ -1,6 +1,7 @@
 package edu.harvard.iq.dataverse.util;
 
 import edu.harvard.iq.dataverse.settings.JvmSettings;
+import edu.harvard.iq.dataverse.util.testing.JvmSetting;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -26,22 +27,18 @@ class SystemConfigTest {
     }
     
     @Test
+    @JvmSetting(key = JvmSettings.VERSION, value = "100.100")
+    @JvmSetting(key = JvmSettings.BUILD, value = "FOOBAR")
     void testGetVersionWithBuild() {
-        // given
-        String version = "100.100";
-        String build = "FOOBAR";
-        System.setProperty(JvmSettings.VERSION.getScopedKey(), version);
-        System.setProperty(JvmSettings.BUILD.getScopedKey(), build);
-    
         // when
         String result = systemConfig.getVersion(true);
     
         // then
-        assertTrue(result.startsWith(version), "'" + result + "' not starting with " + version);
+        assertTrue(result.startsWith("100.100"), "'" + result + "' not starting with 100.100");
         assertTrue(result.contains("build"));
         
         // Cannot test this here - there might be the bundle file present which is not under test control
-        //assertTrue(result.endsWith(build), "'" + result + "' not ending with " + build);
+        //assertTrue(result.endsWith("FOOBAR"), "'" + result + "' not ending with FOOBAR");
     }
     
     @Test
