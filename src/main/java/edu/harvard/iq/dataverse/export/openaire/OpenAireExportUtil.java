43c0681e568856f330f183b4585c2d4535cc2a99
cstr <shmilyfeng3@163.com>
2022-09-21 17:55:37 +0800

    Update OpenAireExportUtil.java

--- src/main/java/edu/harvard/iq/dataverse/export/openaire/OpenAireExportUtil.java
+++ src/main/java/edu/harvard/iq/dataverse/export/openaire/OpenAireExportUtil.java
@@ -964,7 +964,7 @@ public class OpenAireExportUtil {
             relatedIdentifierTypeMap.put("URL".toLowerCase(), "URL");
             relatedIdentifierTypeMap.put("URN".toLowerCase(), "URN");
             relatedIdentifierTypeMap.put("WOS".toLowerCase(), "WOS");
-            relatedIdentifierTypeMap.put("CSTR".toLowerCase(), "CSTR");
+            relatedIdentifierTypeMap.put("CSTR".toLowerCase(), "cstr");
         }
 
         for (Map.Entry<String, MetadataBlockDTO> entry : datasetVersionDTO.getMetadataBlocks().entrySet()) {
