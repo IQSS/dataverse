9d0812b37e3fecfe89687ca356d8d2689ce144a0
qqmyers <qqmyers@hotmail.com>
2022-10-09 13:33:21 -0400

    update Perma alreadyExists and urlPrefix

--- src/main/java/edu/harvard/iq/dataverse/pidproviders/PermaLinkPidProviderServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/pidproviders/PermaLinkPidProviderServiceBean.java
@@ -34,16 +34,17 @@ public class PermaLinkPidProviderServiceBean extends AbstractGlobalIdServiceBean
     private static final Logger logger = Logger.getLogger(PermaLinkPidProviderServiceBean.class.getCanonicalName());
 
     public static final String PERMA_PROTOCOL = "perma";
+    public static final String PERMA_PROVIDER_NAME = "PERMA";
 
     //ToDo - handle dataset/file defaults for local system
     public static final String PERMA_RESOLVER_URL = System.getProperty("pid.baseurlstring", SystemConfig.getDataverseSiteUrlStatic());
     
     String authority = null; 
-    String separator = "";
+    private String separator = "";
     
     @PostConstruct
     private void init() {
-        if("PERMA".equals(settingsService.getValueForKey(Key.DoiProvider))){
+        if(PERMA_PROVIDER_NAME.equals(settingsService.getValueForKey(Key.DoiProvider))){
             authority = settingsService.getValueForKey(Key.Authority);
             isConfigured=true;
         };
@@ -51,42 +52,15 @@ public class PermaLinkPidProviderServiceBean extends AbstractGlobalIdServiceBean
     }
     
     @Override
-    public boolean alreadyExists(GlobalId globalId) throws Exception {
-        /*
-         * This method is called in cases where the 'right' answer can be true or false:
-         * 
-         * When called via CreateNewDatasetCommand (direct upload case), we expect
-         * 'false' as the response, whereas when called from ImportDatasetCommand or
-         * DeleteDataFileCommand, we expect 'true' as a confirmation that the expected
-         * PID exists.
-         * 
-         * This method now checks the stack and can send true/false as expected by the
-         * calling command as the right default/normal case.
-         *
-         * Alternately, this method could check the database as is done in
-         * DatasetServiceBean.isIdentifierLocallyUnique() (needs a similar method for
-         * DataFiles and could be refactored to only have one query for both).
-         */
-        StackWalker walker = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
-        if (walker.walk(this::getCallingClass)) {
-            logger.fine("Called from CreateNewDatasetCommand");
-            return false;
-        }
-        return true;
-        
-    }
-
-    private boolean getCallingClass(Stream<StackFrame> stackFrameStream) {
-        /*
-         * If/when other cases require a true response from the alreadyExists method,
-         * add those class names to the test below.
-         */
-        return stackFrameStream
-                .filter(frame -> frame.getDeclaringClass().getSimpleName()
-                        .equals(CreateNewDatasetCommand.class.getSimpleName()))
-                .findFirst().map(f -> true).orElse(false);
+    public String getSeparator() {
+        //The perma default
+        return separator;
     }
     
+    @Override
+    public boolean alreadyExists(GlobalId globalId) throws Exception {
+        return ! dvObjectService.isGlobalIdLocallyUnique(globalId);
+    }
 
     @Override
     public boolean registerWhenPublished() {
@@ -96,7 +70,7 @@ public class PermaLinkPidProviderServiceBean extends AbstractGlobalIdServiceBean
     @Override
     public List<String> getProviderInformation() {
         ArrayList<String> providerInfo = new ArrayList<>();
-        providerInfo.add(PERMA_PROTOCOL);
+        providerInfo.add(PERMA_PROVIDER_NAME);
         providerInfo.add(PERMA_RESOLVER_URL);
         return providerInfo;
     }
@@ -140,7 +114,9 @@ public class PermaLinkPidProviderServiceBean extends AbstractGlobalIdServiceBean
         return super.parsePersistentId(pidString);
     }
 
+    @Override
     public GlobalId parsePersistentId(String protocol, String identifierString) {
+        logger.info("Checking identifier: " + identifierString);
         if (!PERMA_PROTOCOL.equals(protocol)) {
             return null;
         }
@@ -154,22 +130,19 @@ public class PermaLinkPidProviderServiceBean extends AbstractGlobalIdServiceBean
         if (GlobalIdServiceBean.testforNullTerminator(identifier)) {
             return null;
         }
-        return new GlobalId(PERMA_PROTOCOL, authority, identifier, separator, getUrlPrefix(), PERMA_PROTOCOL);
+        return new GlobalId(PERMA_PROTOCOL, authority, identifier, separator, getUrlPrefix(), PERMA_PROVIDER_NAME);
     }
     
-    
-    @Override
-    //No slash between authority and identifier
-    public String asString(GlobalId globalId) {
-        if (globalId.getProtocol() == null || globalId.getAuthority() == null || globalId.getIdentifier() == null) {
-            return "";
+    public GlobalId parsePersistentId(String protocol, String authority, String identifier) {
+        if (!PERMA_PROTOCOL.equals(protocol)) {
+            return null;
         }
-        return globalId.getProtocol() + ":" + globalId.getAuthority() + globalId.getIdentifier();
+        return super.parsePersistentId(protocol, authority, identifier);
     }
 
     @Override
     public String getUrlPrefix() {
-        // TODO Auto-generated method stub
-        return null;
+        
+        return PERMA_RESOLVER_URL + "/citation?persistentId=" + PERMA_PROTOCOL + ":";
     }
 }
