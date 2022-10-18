abbd7fd6733a63d4e946200487c85efb7bc87c9f
Jim Myers <myersjd@umich.edu>
2022-10-18 18:22:06 -0400

    update test

--- src/test/java/edu/harvard/iq/dataverse/export/SchemaDotOrgExporterTest.java
+++ src/test/java/edu/harvard/iq/dataverse/export/SchemaDotOrgExporterTest.java
@@ -137,11 +137,11 @@ public class SchemaDotOrgExporterTest {
         assertEquals("https://doi.org/10.5072/FK2/IMK5A4", json2.getString("identifier"));
         assertEquals("Darwin's Finches", json2.getString("name"));
         assertEquals("Finch, Fiona", json2.getJsonArray("creator").getJsonObject(0).getString("name"));
-        assertEquals("Birds Inc.", json2.getJsonArray("creator").getJsonObject(0).getString("affiliation"));
+        assertEquals("Birds Inc.", json2.getJsonArray("creator").getJsonObject(0).getJsonObject("parentOrganization").getString("name"));
         assertEquals("https://orcid.org/0000-0002-1825-0097", json2.getJsonArray("creator").getJsonObject(0).getString("@id"));
         assertEquals("https://orcid.org/0000-0002-1825-0097", json2.getJsonArray("creator").getJsonObject(0).getString("identifier"));
         assertEquals("Finch, Fiona", json2.getJsonArray("author").getJsonObject(0).getString("name"));
-        assertEquals("Birds Inc.", json2.getJsonArray("author").getJsonObject(0).getString("affiliation"));
+        assertEquals("Birds Inc.", json2.getJsonArray("author").getJsonObject(0).getJsonObject("parentOrganization").getString("name"));
         assertEquals("https://orcid.org/0000-0002-1825-0097", json2.getJsonArray("author").getJsonObject(0).getString("@id"));
         assertEquals("https://orcid.org/0000-0002-1825-0097", json2.getJsonArray("author").getJsonObject(0).getString("identifier"));
         assertEquals("1955-11-05", json2.getString("datePublished"));
