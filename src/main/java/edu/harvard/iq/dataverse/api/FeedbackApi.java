67ecb2dcd2299e34502b668f961e8fe421351322
Jim Myers <myersjd@umich.edu>
2022-11-22 14:46:35 -0500

    minimal feedback API method

--- src/main/java/edu/harvard/iq/dataverse/api/FeedbackApi.java
+++ src/main/java/edu/harvard/iq/dataverse/api/FeedbackApi.java
@@ -2,11 +2,14 @@ package edu.harvard.iq.dataverse.api;
 
 import edu.harvard.iq.dataverse.DataverseSession;
 import edu.harvard.iq.dataverse.DvObject;
+import edu.harvard.iq.dataverse.MailServiceBean;
 import edu.harvard.iq.dataverse.branding.BrandingUtil;
 import edu.harvard.iq.dataverse.feedback.Feedback;
 import edu.harvard.iq.dataverse.feedback.FeedbackUtil;
 import edu.harvard.iq.dataverse.settings.SettingsServiceBean;
+import edu.harvard.iq.dataverse.util.MailUtil;
 
+import javax.ejb.EJB;
 import javax.json.Json;
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonNumber;
@@ -16,21 +19,40 @@ import javax.mail.internet.InternetAddress;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 @Path("admin/feedback")
 public class FeedbackApi extends AbstractApiBean {
 
+    @EJB MailServiceBean mailService;
+    
+    /**
+     * This method mimics the contact form and sends an email to the contacts of the
+     * specified Collection/Dataset/DataFile, optionally ccing the support email
+     * address, or to the support email address when there is no target object.
+     * 
+     * !!!!! This should not be moved outside the /admin path unless/until some form of
+     * captcha or other spam-prevention mechanism is added. As is, it allows an
+     * unauthenticated user (with access to the /admin api path) to send email from
+     * anyone to any contacts in Dataverse.!!!!
+     **/ 
     @POST
     public Response submitFeedback(JsonObject jsonObject) throws AddressException {
         JsonNumber jsonNumber = jsonObject.getJsonNumber("id");
         DvObject feedbackTarget = null;
         if (jsonNumber != null) {
             feedbackTarget =  dvObjSvc.findDvObject(jsonNumber.longValue());
+            if(feedbackTarget==null) {
+                return error(Status.BAD_REQUEST, "Feedback target object not found");
+            }
         }
         DataverseSession dataverseSession = null;
         String userMessage = jsonObject.getString("body");
-        String systemEmail = "support@librascholar.edu";
-        InternetAddress systemAddress = new InternetAddress(systemEmail);
+        String systemEmail = settingsSvc.getValueForKey(SettingsServiceBean.Key.SupportEmail);
+        if(systemEmail==null) {
+            systemEmail = settingsSvc.getValueForKey(SettingsServiceBean.Key.SystemEmail);
+        }
+        InternetAddress systemAddress = MailUtil.parseSystemAddress(systemEmail);
         String userEmail = jsonObject.getString("fromEmail");
         String messageSubject = jsonObject.getString("subject");
         String baseUrl = systemConfig.getDataverseSiteUrl();
@@ -40,7 +62,7 @@ public class FeedbackApi extends AbstractApiBean {
         boolean ccSupport=feedbackTarget!=null &&settingsSvc.isTrueForKey(SettingsServiceBean.Key.CCSupportOnContactEmails, false);
         Feedback feedback = FeedbackUtil.gatherFeedback(feedbackTarget, dataverseSession, messageSubject, userMessage, systemAddress, userEmail, baseUrl, installationBrandName, supportTeamName, ccSupport);
         jab.add(feedback.toJsonObjectBuilder());
-        
+        mailService.sendMail(feedback.getFromEmail(), feedback.getToEmail(), feedback.getCcEmail(), feedback.getSubject(), feedback.getBody(), null);
         return ok(jab);
     }
 }
