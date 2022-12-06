16b4c3c69b7e69b0e773d3a7292abe4da04ddef6
Jim Myers <myersjd@umich.edu>
2022-12-06 16:22:26 -0500

    add constraint directly

--- src/main/resources/db/migration/V5.13.0.3__guestbook-on-request.sql
+++ src/main/resources/db/migration/V5.13.0.3__guestbook-on-request.sql
@@ -1,2 +1,3 @@
 ALTER TABLE fileaccessrequests ADD COLUMN IF NOT EXISTS request_state VARCHAR(64);
-ALTER TABLE fileaccessrequests ADD COLUMN IF NOT EXISTS id INT PRIMARY_KEY;
+ALTER TABLE fileaccessrequests ADD COLUMN IF NOT EXISTS id INT;
+ALTER TABLE fileaccessrequests ADD CONSTRAINT fileaccessrequests_pkey PRIMARY KEY (id);
