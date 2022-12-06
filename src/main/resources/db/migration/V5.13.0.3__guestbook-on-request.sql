3658b8ec4e49ad7dae7daa9501dc89e0656c07e4
Jim Myers <myersjd@umich.edu>
2022-12-06 17:17:28 -0500

    remove old constraint, update id col

--- src/main/resources/db/migration/V5.13.0.3__guestbook-on-request.sql
+++ src/main/resources/db/migration/V5.13.0.3__guestbook-on-request.sql
@@ -1,3 +1,5 @@
 ALTER TABLE fileaccessrequests ADD COLUMN IF NOT EXISTS request_state VARCHAR(64);
 ALTER TABLE fileaccessrequests ADD COLUMN IF NOT EXISTS id INT;
+ALTER TABLE fileaccessrequests DROP CONSTRAINT fileaccessrequests_pkey PRIMARY KEY (datafile_id, authenticated_user_id);
+UPDATE fileaccessrequests SET id = DEFAULT;
 ALTER TABLE fileaccessrequests ADD CONSTRAINT fileaccessrequests_pkey PRIMARY KEY (id);
