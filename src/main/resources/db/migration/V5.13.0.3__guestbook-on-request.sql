b858cd325e1b933a49b91c39d68b4620fe2b2c93
Jim Myers <myersjd@umich.edu>
2022-12-06 17:52:37 -0500

    another variant

--- src/main/resources/db/migration/V5.13.0.3__guestbook-on-request.sql
+++ src/main/resources/db/migration/V5.13.0.3__guestbook-on-request.sql
@@ -1,5 +1,5 @@
 ALTER TABLE fileaccessrequests ADD COLUMN IF NOT EXISTS request_state VARCHAR(64);
 ALTER TABLE fileaccessrequests ADD COLUMN IF NOT EXISTS id INT;
 ALTER TABLE fileaccessrequests DROP CONSTRAINT IF EXISTS fileaccessrequests_pkey;
-UPDATE fileaccessrequests SET id = DEFAULT;
+ALTER TABLE fileaccessrequests ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY;
 ALTER TABLE fileaccessrequests ADD CONSTRAINT fileaccessrequests_pkey PRIMARY KEY (id);
