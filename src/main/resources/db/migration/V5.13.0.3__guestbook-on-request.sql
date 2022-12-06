6fda50c5ad3a39d98b6d0e1c38096c58d708a28e
Jim Myers <myersjd@umich.edu>
2022-12-06 17:27:33 -0500

    syntax err

--- src/main/resources/db/migration/V5.13.0.3__guestbook-on-request.sql
+++ src/main/resources/db/migration/V5.13.0.3__guestbook-on-request.sql
@@ -1,5 +1,5 @@
 ALTER TABLE fileaccessrequests ADD COLUMN IF NOT EXISTS request_state VARCHAR(64);
 ALTER TABLE fileaccessrequests ADD COLUMN IF NOT EXISTS id INT;
-ALTER TABLE fileaccessrequests DROP CONSTRAINT fileaccessrequests_pkey PRIMARY KEY (datafile_id, authenticated_user_id);
+ALTER TABLE fileaccessrequests DROP CONSTRAINT IF EXISTS fileaccessrequests_pkey;
 UPDATE fileaccessrequests SET id = DEFAULT;
 ALTER TABLE fileaccessrequests ADD CONSTRAINT fileaccessrequests_pkey PRIMARY KEY (id);
