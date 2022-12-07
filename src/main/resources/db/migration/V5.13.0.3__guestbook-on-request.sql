76448e7b8fcaca4ad6e25e453189e75d610b133c
Jim Myers <myersjd@umich.edu>
2022-12-07 09:49:28 -0500

    add fk constraint

--- src/main/resources/db/migration/V5.13.0.3__guestbook-on-request.sql
+++ src/main/resources/db/migration/V5.13.0.3__guestbook-on-request.sql
@@ -2,3 +2,5 @@ ALTER TABLE fileaccessrequests ADD COLUMN IF NOT EXISTS request_state VARCHAR(64
 ALTER TABLE fileaccessrequests ADD COLUMN IF NOT EXISTS id SERIAL;
 ALTER TABLE fileaccessrequests DROP CONSTRAINT IF EXISTS fileaccessrequests_pkey;
 ALTER TABLE fileaccessrequests ADD CONSTRAINT fileaccessrequests_pkey PRIMARY KEY (id);
+ALTER TABLE fileaccessrequests ADD COLUMN IF NOT EXISTS guestbook_id;
+ALTER TABLE fileaccessrequests ADD CONSTRAINT fk_fileaccessrequests_guestbookrequest FOREIGN KEY (guestbookrequest_id) REFERENCES guestbookrequest(id);
