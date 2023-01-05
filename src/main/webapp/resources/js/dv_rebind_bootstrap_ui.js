22370ad197c38375425d2effa7381f4f2429f6d8
qqmyers <qqmyers@hotmail.com>
2023-01-05 17:01:04 -0500

    Re-use contentTruncate in dataverse page
    Fix + sign in collapse
    Fix only truncate first description
    Use cset to avoid duplication (efficiency?)

--- src/main/webapp/resources/js/dv_rebind_bootstrap_ui.js
+++ src/main/webapp/resources/js/dv_rebind_bootstrap_ui.js
@@ -166,7 +166,7 @@ function sharrre(){
  */
 function contentTruncate(truncSelector, truncMoreBtn, truncMoreTip, truncLessBtn, truncLessTip){
     // SELECTOR ID FROM PARAMETERS
-    $('#' + truncSelector + ' td > div:first-child').each(function () {
+    $('#' +  truncSelector).each(function () {
         
         // add responsive img class to limit width to that of container
         $(this).find('img').attr('class', 'img-responsive');
