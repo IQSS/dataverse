2885dab20a6a8f5e0cb4b8a0f90c0e331064709e
Oliver Bertuch <o.bertuch@fz-juelich.de>
2023-04-21 11:06:56 +0200

    fix(pid,sql): correct SQL default for version pid conduct in migration script

--- src/main/resources/db/migration/V5.13.0.1__4499-dataset-version-pids.sql
+++ src/main/resources/db/migration/V5.13.0.1__4499-dataset-version-pids.sql
@@ -1,4 +1,4 @@
-ALTER TABLE dataverse ADD COLUMN IF NOT EXISTS datasetVersionPidConduct varchar(16) NOT NULL DEFAULT 'inherit';
+ALTER TABLE dataverse ADD COLUMN IF NOT EXISTS datasetVersionPidConduct varchar(16) NOT NULL DEFAULT 'INHERIT';
 
 ALTER TABLE datasetVersion ADD COLUMN IF NOT EXISTS persistentIdentifier varchar(255);
 /* As Postgres does not support "if not exists" when adding a constraint, must remove first to make this not bail */
