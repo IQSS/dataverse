63e98b3b60a4baae98f1f88a282b97694929c443
Jim Myers <qqmyers@hotmail.com>
2023-05-17 14:16:47 -0400

    make clearer

--- src/main/java/edu/harvard/iq/dataverse/DataFileServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/DataFileServiceBean.java
@@ -1149,11 +1149,9 @@ public class DataFileServiceBean implements java.io.Serializable {
            file.setPreviewImageAvailable(true);
            this.save(file); 
            return true;
-       } else {
-           file.setPreviewsHaveFailed(true);
-           this.save(file);
        }
-
+       file.setPreviewsHaveFailed(true);
+       this.save(file);
        return false;
     }
 
