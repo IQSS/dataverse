10f8a0a1470fd62e75b84defc0ad752de6175658
qqmyers <qqmyers@hotmail.com>
2023-05-19 12:36:24 -0400

    update script naming and content

--- src/main/resources/db/migration/V5.13.0.2__guestbook-on-request.sql
+++ src/main/resources/db/migration/V5.13.0.2__guestbook-on-request.sql
@@ -3,4 +3,5 @@ ALTER TABLE fileaccessrequests ADD COLUMN IF NOT EXISTS id SERIAL;
 ALTER TABLE fileaccessrequests DROP CONSTRAINT IF EXISTS fileaccessrequests_pkey;
 ALTER TABLE fileaccessrequests ADD CONSTRAINT fileaccessrequests_pkey PRIMARY KEY (id);
 ALTER TABLE fileaccessrequests ADD COLUMN IF NOT EXISTS guestbookresponse_id INT;
+ALTER TABLE fileaccessrequests DROP CONSTRAINT IF EXISTS fk_fileaccessrequests_guestbookresponse;
 ALTER TABLE fileaccessrequests ADD CONSTRAINT fk_fileaccessrequests_guestbookresponse FOREIGN KEY (guestbookresponse_id) REFERENCES guestbookresponse(id);
