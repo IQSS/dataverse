82e91f204e01875ad95af4522ec61c544749f45e
Leonid Andreev <leonid@hmdc.harvard.edu>
2023-05-23 16:32:00 -0400

    a simple restassured test for disabling file pid registration within a collection (#8889)

--- src/test/java/edu/harvard/iq/dataverse/api/FilesIT.java
+++ src/test/java/edu/harvard/iq/dataverse/api/FilesIT.java
@@ -2018,4 +2018,73 @@ public class FilesIT {
                 .body("data.files[0]", equalTo(null))
                 .statusCode(OK.getStatusCode());
     }
+    
+    @Test
+    public void testFilePIDsBehavior() {
+        // Create user
+        String apiToken = createUserGetToken();
+
+        // Create Dataverse
+        String collectionAlias = createDataverseGetAlias(apiToken);
+
+        // Create Initial Dataset with 1 file:
+        Integer datasetId = createDatasetGetId(collectionAlias, apiToken);
+        String pathToFile = "scripts/search/data/replace_test/003.txt";
+        Response addResponse = UtilIT.uploadFileViaNative(datasetId.toString(), pathToFile, apiToken);
+
+        addResponse.then().assertThat()
+                .body("data.files[0].dataFile.contentType", equalTo("text/plain"))
+                .body("data.files[0].label", equalTo("003.txt"))
+                .statusCode(OK.getStatusCode());
+        
+        Long origFileId = JsonPath.from(addResponse.body().asString()).getLong("data.files[0].dataFile.id");
+        
+        // -------------------------
+        // Publish dataverse and dataset
+        // -------------------------
+        msg("Publish dataverse and dataset");
+        Response publishDataversetResp = UtilIT.publishDataverseViaSword(collectionAlias, apiToken);
+        publishDataversetResp.then().assertThat()
+                .statusCode(OK.getStatusCode());
+        
+        Response publishDatasetResp = UtilIT.publishDatasetViaNativeApi(datasetId, "major", apiToken);
+        publishDatasetResp.then().assertThat()
+                .statusCode(OK.getStatusCode());
+        
+        // The file in this dataset should have been assigned a PID when it was published:
+        Response fileInfoResponse = UtilIT.getFileData(origFileId.toString(), apiToken);
+        fileInfoResponse.then().assertThat().statusCode(OK.getStatusCode());
+        String fileInfoResponseString = fileInfoResponse.body().asString();
+        msg(fileInfoResponseString);
+        
+        assertNotNull(JsonPath.from(fileInfoResponseString).getString("data.dataFile.persistentId"));
+
+        // Now change the file PIDs registration configuration for the collection:
+        
+        Response changeAttributeResp = UtilIT.setCollectionAttribute(collectionAlias, "filePIDsEnabled", "false", apiToken);
+        
+        // ... And do the whole thing with creating another dataset with a file:
+        
+        datasetId = createDatasetGetId(collectionAlias, apiToken);
+        addResponse = UtilIT.uploadFileViaNative(datasetId.toString(), pathToFile, apiToken);
+        addResponse.then().assertThat().statusCode(OK.getStatusCode());                
+        Long newFileId = JsonPath.from(addResponse.body().asString()).getLong("data.files[0].dataFile.id");
+        
+        // And publish this dataset:
+        msg("Publish second dataset");
+        
+        publishDatasetResp = UtilIT.publishDatasetViaNativeApi(datasetId, "major", apiToken);
+        publishDatasetResp.then().assertThat()
+                .statusCode(OK.getStatusCode());
+        
+        // And confirm that the file didn't get a PID:
+        
+        fileInfoResponse = UtilIT.getFileData(origFileId.toString(), apiToken);
+        fileInfoResponse.then().assertThat().statusCode(OK.getStatusCode());
+        fileInfoResponseString = fileInfoResponse.body().asString();
+        msg(fileInfoResponseString);
+        
+        assertNull(JsonPath.from(fileInfoResponseString).getString("data.dataFile.persistentId"));
+      
+    }
 }
