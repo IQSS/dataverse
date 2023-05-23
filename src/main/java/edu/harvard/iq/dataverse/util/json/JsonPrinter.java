ff34b4666cc2649be476f581b705461dee1ad945
Leonid Andreev <leonid@hmdc.harvard.edu>
2023-05-23 15:18:55 -0400

    Added the handling for the file pids attribute to the json parser/printer #8889

--- src/main/java/edu/harvard/iq/dataverse/util/json/JsonPrinter.java
+++ src/main/java/edu/harvard/iq/dataverse/util/json/JsonPrinter.java
@@ -294,6 +294,9 @@ public class JsonPrinter {
         if(dv.getStorageDriverId() != null) {
         	bld.add("storageDriverLabel", DataAccess.getStorageDriverLabelFor(dv.getStorageDriverId()));
         }
+        if (dv.getFilePIDsEnabled() != null) {
+            bld.add("filePIDsEnabled", dv.getFilePIDsEnabled());
+        }
 
         return bld;
     }
