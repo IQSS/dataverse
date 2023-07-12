5ce3eb35015342966005ccd63becce96a9d91608
qqmyers <qqmyers@hotmail.com>
2023-07-12 16:44:40 -0400

    report default/inherited setting as well.

--- src/main/java/edu/harvard/iq/dataverse/api/Datasets.java
+++ src/main/java/edu/harvard/iq/dataverse/api/Datasets.java
@@ -3893,8 +3893,11 @@ public class Datasets extends AbstractApiBean {
         } catch (WrappedResponse ex) {
             return error(Response.Status.NOT_FOUND, "No such dataset");
         }
-        
-        return response(req -> ok(dataset.getEffectiveGuestbookEntryAtRequest()), getRequestUser(crc));
+        String gbAtRequest = dataset.getGuestbookEntryAtRequest();
+        if(gbAtRequest == null || gbAtRequest.equals(DvObjectContainer.UNDEFINED_CODE)) {
+            return ok("Not set on dataset, using the default: " + dataset.getEffectiveGuestbookEntryAtRequest());
+        }
+        return ok(dataset.getEffectiveGuestbookEntryAtRequest());
     }
     
     @PUT
