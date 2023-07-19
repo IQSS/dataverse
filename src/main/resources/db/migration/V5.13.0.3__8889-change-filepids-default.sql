a0ac9e3ce23d6ae94ffff8f21de9ac2b901790a4
qqmyers <qqmyers@hotmail.com>
2023-07-19 16:50:21 -0400

    no need to delete since we aren't using the null/unset val now

--- src/main/resources/db/migration/V5.13.0.3__8889-change-filepids-default.sql
+++ src/main/resources/db/migration/V5.13.0.3__8889-change-filepids-default.sql
@@ -1,4 +1,3 @@
 INSERT INTO setting(content, lang, name) VALUES
               ('true', '', ':FilePIDsEnabled')
        ON CONFLICT DO NOTHING;
-DELETE FROM setting where name=':FilePIDsEnabled' AND content='false';
