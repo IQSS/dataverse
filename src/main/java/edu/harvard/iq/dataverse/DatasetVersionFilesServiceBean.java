22f1cf0a19868b9db192a2241bc42de4e385ab7c
GPortas <hey@gportas.me>
2023-08-14 09:21:39 +0100

    Refactor: DatasetVersionFilesServiceBean

--- src/main/java/edu/harvard/iq/dataverse/DatasetVersionFilesServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/DatasetVersionFilesServiceBean.java
@@ -27,6 +27,9 @@ public class DatasetVersionFilesServiceBean implements Serializable {
     @PersistenceContext(unitName = "VDCNet-ejbPU")
     private EntityManager em;
 
+    private final QFileMetadata fileMetadata = QFileMetadata.fileMetadata;
+    private final QDvObject dvObject = QDvObject.dvObject;
+
     /**
      * Different criteria to sort the results of FileMetadata queries used in {@link DatasetVersionFilesServiceBean#getFileMetadatas}
      */
@@ -44,7 +47,7 @@ public class DatasetVersionFilesServiceBean implements Serializable {
      * @return a FileMetadata list of the specified DatasetVersion
      */
     public List<FileMetadata> getFileMetadatas(DatasetVersion datasetVersion, Integer limit, Integer offset, String fileType, String fileAccess, String fileTag, FileMetadatasOrderCriteria orderCriteria) {
-        JPAQuery<FileMetadata> query = createQueryFromCriteria(datasetVersion, orderCriteria);
+        JPAQuery<FileMetadata> query = createQueryFromOrderCriteria(datasetVersion, orderCriteria);
         if (limit != null) {
             query.limit(limit);
         }
@@ -54,33 +57,38 @@ public class DatasetVersionFilesServiceBean implements Serializable {
         return query.fetch();
     }
 
-    // TODO: Refactor
-    private JPAQuery<FileMetadata> createQueryFromCriteria(DatasetVersion datasetVersion, FileMetadatasOrderCriteria orderCriteria) {
-        QFileMetadata fileMetadata = QFileMetadata.fileMetadata;
-        QDvObject dvObject = QDvObject.dvObject;
+    private JPAQuery<FileMetadata> createQueryFromOrderCriteria(DatasetVersion datasetVersion, FileMetadatasOrderCriteria orderCriteria) {
         DateTimeExpression<Timestamp> orderByLifetimeExpression = new CaseBuilder().when(dvObject.publicationDate.isNotNull()).then(dvObject.publicationDate).otherwise(dvObject.createDate);
         JPAQueryFactory queryFactory = new JPAQueryFactory(em);
         JPAQuery<FileMetadata> query;
         switch (orderCriteria) {
             case NameZA:
-                query = queryFactory.selectFrom(fileMetadata).where(fileMetadata.datasetVersion.id.eq(datasetVersion.getId())).orderBy(fileMetadata.label.desc());
+                query = createFileMetadataBaseQuery(datasetVersion, queryFactory).orderBy(fileMetadata.label.desc());
                 break;
             case Newest:
-                query = queryFactory.select(fileMetadata).from(fileMetadata, dvObject).where(fileMetadata.datasetVersion.id.eq(datasetVersion.getId())).where(dvObject.id.eq(fileMetadata.dataFile.id)).orderBy(orderByLifetimeExpression.desc());
+                query = createFileMetadataDvObjectBaseQuery(datasetVersion, queryFactory).orderBy(orderByLifetimeExpression.desc());
                 break;
             case Oldest:
-                query = queryFactory.select(fileMetadata).from(fileMetadata, dvObject).where(fileMetadata.datasetVersion.id.eq(datasetVersion.getId())).where(dvObject.id.eq(fileMetadata.dataFile.id)).orderBy(orderByLifetimeExpression.asc());
+                query = createFileMetadataDvObjectBaseQuery(datasetVersion, queryFactory).orderBy(orderByLifetimeExpression.asc());
                 break;
             case Size:
-                query = queryFactory.selectFrom(fileMetadata).where(fileMetadata.datasetVersion.id.eq(datasetVersion.getId())).orderBy(fileMetadata.dataFile.filesize.asc());
+                query = createFileMetadataBaseQuery(datasetVersion, queryFactory).orderBy(fileMetadata.dataFile.filesize.asc());
                 break;
             case Type:
-                query = queryFactory.selectFrom(fileMetadata).where(fileMetadata.datasetVersion.id.eq(datasetVersion.getId())).orderBy(fileMetadata.dataFile.contentType.asc());
+                query = createFileMetadataBaseQuery(datasetVersion, queryFactory).orderBy(fileMetadata.dataFile.contentType.asc());
                 break;
             default:
-                query = queryFactory.selectFrom(fileMetadata).where(fileMetadata.datasetVersion.id.eq(datasetVersion.getId())).orderBy(fileMetadata.label.asc());
+                query = createFileMetadataBaseQuery(datasetVersion, queryFactory).orderBy(fileMetadata.label.asc());
                 break;
         }
         return query;
     }
-} // end class
+
+    private JPAQuery<FileMetadata> createFileMetadataDvObjectBaseQuery(DatasetVersion datasetVersion, JPAQueryFactory queryFactory) {
+        return queryFactory.select(fileMetadata).from(fileMetadata, dvObject).where(fileMetadata.datasetVersion.id.eq(datasetVersion.getId())).where(dvObject.id.eq(fileMetadata.dataFile.id));
+    }
+
+    private JPAQuery<FileMetadata> createFileMetadataBaseQuery(DatasetVersion datasetVersion, JPAQueryFactory queryFactory) {
+        return queryFactory.selectFrom(fileMetadata).where(fileMetadata.datasetVersion.id.eq(datasetVersion.getId()));
+    }
+}
