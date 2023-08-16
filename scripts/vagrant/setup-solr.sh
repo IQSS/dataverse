10cb29440f52a19d745e9bae300580d7b6bf6402
Philip Durbin <philip_durbin@harvard.edu>
2023-08-16 10:52:57 -0400

    fix XSLTResponseWriter namespace and enable scripting module #9260
    "To improve security, XSLTResponseWriter has been moved
    to the scripting Module instead of shipping as part of
    Solr core. This module needs to be enabled explicitly."
    
    -- https://github.com/apache/solr/blob/main/solr/solr-ref-guide/modules/upgrade-notes/pages/major-changes-in-solr-9.adoc
    
    See also https://stackoverflow.com/questions/74118537/solr-9-org-apache-solr-common-solrexceptionorg-apache-solr-common-solrexcepti

--- scripts/vagrant/setup-solr.sh
+++ scripts/vagrant/setup-solr.sh
@@ -10,7 +10,7 @@ su $SOLR_USER -s /bin/sh -c "cd $SOLR_HOME && tar xfz solr-9.3.0.tgz"
 su $SOLR_USER -s /bin/sh -c "cd $SOLR_HOME/solr-9.3.0/server/solr && cp -r configsets/_default . && mv _default collection1"
 su $SOLR_USER -s /bin/sh -c "cp /dataverse/conf/solr/9.3.0/schema*.xml $SOLR_HOME/solr-9.3.0/server/solr/collection1/conf/"
 su $SOLR_USER -s /bin/sh -c "cp /dataverse/conf/solr/9.3.0/solrconfig.xml $SOLR_HOME/solr-9.3.0/server/solr/collection1/conf/solrconfig.xml"
-su $SOLR_USER -s /bin/sh -c "cd $SOLR_HOME/solr-9.3.0 && bin/solr start && bin/solr create_core -c collection1 -d server/solr/collection1/conf/"
+su $SOLR_USER -s /bin/sh -c "cd $SOLR_HOME/solr-9.3.0 && bin/solr start -Dsolr.modules=scripting && bin/solr create_core -c collection1 -d server/solr/collection1/conf/"
 cp /dataverse/doc/sphinx-guides/source/_static/installation/files/etc/init.d/solr /etc/init.d/solr
 chmod 755 /etc/init.d/solr
 /etc/init.d/solr stop
