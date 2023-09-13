3d2255b963f869028b68576075462664f67a5888
Jérôme ROUCOU <jerome.roucou@inrae.fr>
2023-09-13 18:35:40 +0200

    Assign roles from email address Give a user a role from email address of the user's account

--- src/main/java/edu/harvard/iq/dataverse/authorization/users/AuthenticatedUser.java
+++ src/main/java/edu/harvard/iq/dataverse/authorization/users/AuthenticatedUser.java
@@ -64,7 +64,8 @@ import org.hibernate.validator.constraints.NotBlank;
     @NamedQuery( name="AuthenticatedUser.filter",
                 query="select au from AuthenticatedUser au WHERE ("
                         + "LOWER(au.userIdentifier) like LOWER(:query) OR "
-                        + "lower(concat(au.firstName,' ',au.lastName)) like lower(:query))"),
+                        + "lower(concat(au.firstName,' ',au.lastName)) like lower(:query) or "
+                        + "lower(au.email) like lower(:query))"),
     @NamedQuery( name="AuthenticatedUser.findAdminUser",
                 query="select au from AuthenticatedUser au WHERE "
                         + "au.superuser = true "
