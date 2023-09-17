87bfe16c6a4390075bd0b481071ebddf2f1c1e5d
bencomp <ben@companjen.name>
2023-09-18 00:25:52 +0200

    Test DatasetField identities

--- src/test/java/edu/harvard/iq/dataverse/DatasetFieldTest.java
+++ src/test/java/edu/harvard/iq/dataverse/DatasetFieldTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 
 class DatasetFieldTest {
@@ -21,12 +22,17 @@ class DatasetFieldTest {
 
     @Test
     void testNotEqualDatasetFields() {
-        DatasetField field1 = DatasetField.createNewEmptyDatasetField(new DatasetFieldType("subject", FieldType.TEXT, false), new Template());
+        DatasetFieldType type1 = new DatasetFieldType("subject", FieldType.TEXT, false);
+        Template template1 = new Template();
+        DatasetField field1 = DatasetField.createNewEmptyDatasetField(type1, template1);
         field1.setId(MocksFactory.nextId());
-        DatasetField field2 = DatasetField.createNewEmptyDatasetField(new DatasetFieldType("subject", FieldType.TEXT, false), new Template());
+        DatasetFieldType type2 = new DatasetFieldType("subject", FieldType.TEXT, false);
+        Template template2 = new Template();
+        DatasetField field2 = DatasetField.createNewEmptyDatasetField(type2, template2);
         field2.setId(MocksFactory.nextId());
 
         assertNotEquals(field1, field2);
+        assertNotEquals(field1, template2);
     }
 
     @Test
@@ -34,8 +40,25 @@ class DatasetFieldTest {
         DatasetField field1 = DatasetField.createNewEmptyDatasetField(new DatasetFieldType("subject", FieldType.TEXT, false), new Template());
         field1.setId(100L);
         DatasetField field2 = DatasetField.createNewEmptyDatasetField(new DatasetFieldType("subject", FieldType.TEXT, false), new Template());
+
+        // Fields are not equal before both have IDs set
+        assertNotEquals(field1, field2);
+        
         field2.setId(100L);
 
         assertEquals(field1, field2);
+    }
+
+    @Test
+    void testCopyDatasetFields() {
+        DatasetField field1 = DatasetField.createNewEmptyDatasetField(new DatasetFieldType("subject", FieldType.TEXT, false), new Template());
+        field1.setId(100L);
+        DatasetField field2 = field1.copy(field1.getTemplate());
+
+        assertNull(field2.getId());
+        // A copy of a field should not be equal
+        assertNotEquals(field1, field2);
+
+        assertEquals(field2.getDatasetFieldType(), field1.getDatasetFieldType());
     }
 }
