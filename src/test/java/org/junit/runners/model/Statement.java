600d209fbd7b70893fe29f7601ba8049cb415d8f
Oliver Bertuch <o.bertuch@fz-juelich.de>
2023-09-20 13:53:29 +0200

    build(test): enable using GenericContainer without JUnit4 around
    As a hack to work around testcontainers/testcontainers-java#970,
    we add these fake, empty classes. Copied from Spring project.
    
    See also: https://github.com/testcontainers/testcontainers-java/issues/970

---
+++ src/test/java/org/junit/runners/model/Statement.java
@@ -0,0 +1,11 @@
+package org.junit.runners.model;
+
+/**
+ * "Fake" class used as a replacement for Junit4-dependent classes.
+ * See more at: <a href="https://github.com/testcontainers/testcontainers-java/issues/970">
+ * GenericContainer run from Jupiter tests shouldn't require JUnit 4.x library on runtime classpath
+ * </a>.
+ */
+@SuppressWarnings("unused")
+public class Statement {
+}
