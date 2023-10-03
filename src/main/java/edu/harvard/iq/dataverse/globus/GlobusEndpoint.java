c724094dcfffaa83c61f415d572e2e5a8958cef0
qqmyers <qqmyers@hotmail.com>
2023-10-03 17:03:57 -0400

    fix constructor, reformat

--- src/main/java/edu/harvard/iq/dataverse/globus/GlobusEndpoint.java
+++ src/main/java/edu/harvard/iq/dataverse/globus/GlobusEndpoint.java
@@ -5,27 +5,34 @@ public class GlobusEndpoint {
     private String id;
     private String clientToken;
     private String basePath;
- 
-    
+
     public GlobusEndpoint(String id, String clientToken, String basePath) {
-        
+        this.id = id;
+        this.clientToken = clientToken;
+        this.basePath = basePath;
     }
-        public String getId() {
+
+    public String getId() {
         return id;
     }
-        public void setId(String id) {
+
+    public void setId(String id) {
         this.id = id;
     }
-        public String getClientToken() {
+
+    public String getClientToken() {
         return clientToken;
     }
-        public void setClientToken(String clientToken) {
+
+    public void setClientToken(String clientToken) {
         this.clientToken = clientToken;
     }
-        public String getBasePath() {
+
+    public String getBasePath() {
         return basePath;
     }
-        public void setBasePath(String basePath) {
+
+    public void setBasePath(String basePath) {
         this.basePath = basePath;
     }
 }
