7fc613f7a870951a088ebbf7238895318fb057c5
Oliver Bertuch <o.bertuch@fz-juelich.de>
2023-10-06 18:10:14 +0200

    refactor(mail): remove unused MailUtil.parseSystemAddress #7424
    As we replaced the lookups and parsing with a streamlined version of it all
    in MailServiceBean, we don't need this helper function anymore.

--- src/main/java/edu/harvard/iq/dataverse/util/MailUtil.java
+++ src/main/java/edu/harvard/iq/dataverse/util/MailUtil.java
@@ -5,31 +5,13 @@ import edu.harvard.iq.dataverse.Dataset;
 import edu.harvard.iq.dataverse.DatasetVersion;
 import edu.harvard.iq.dataverse.UserNotification;
 import edu.harvard.iq.dataverse.branding.BrandingUtil;
-import static edu.harvard.iq.dataverse.settings.SettingsServiceBean.Key.SystemEmail;
 import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
-import jakarta.mail.internet.AddressException;
-import jakarta.mail.internet.InternetAddress;
 
 public class MailUtil {
 
     private static final Logger logger = Logger.getLogger(MailUtil.class.getCanonicalName());
-
-    public static InternetAddress parseSystemAddress(String systemEmail) {
-        if (systemEmail != null) {
-            try {
-                InternetAddress parsedSystemEmail = new InternetAddress(systemEmail);
-                logger.fine("parsed system email: " + parsedSystemEmail);
-                return parsedSystemEmail;
-            } catch (AddressException ex) {
-                logger.info("Email will not be sent due to invalid value in " + SystemEmail + " setting: " + ex);
-                return null;
-            }
-        }
-        logger.fine("Email will not be sent because the " + SystemEmail + " setting is null.");
-        return null;
-    }
 
     public static String getSubjectTextBasedOnNotification(UserNotification userNotification, Object objectOfNotification) {
         List<String> rootDvNameAsList = Arrays.asList(BrandingUtil.getInstallationBrandName());
