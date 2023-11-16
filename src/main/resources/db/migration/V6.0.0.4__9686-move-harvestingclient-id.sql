5c045120d6660ee0b07501cadfb06aaf9f083f6b
Stephen Kraffmiller <skraffmiller@hmdc.harvard.edu>
2023-11-16 13:42:51 -0500

    #9686 rename migration script

--- src/main/resources/db/migration/V6.0.0.4__9686-move-harvestingclient-id.sql
+++ src/main/resources/db/migration/V6.0.0.4__9686-move-harvestingclient-id.sql
@@ -1,7 +1,7 @@
 ALTER TABLE dvobject ADD COLUMN IF NOT EXISTS harvestingclient_id BIGINT;
 
 update dvobject dvo set harvestingclient_id = s.harvestingclient_id from
-(select id, harvestingclient_id from dataset d) s
+(select id, harvestingclient_id from dataset d where d.harvestingclient_id is not null) s
 where s.id = dvo.id; 
 
 --ALTER TABLE dataset drop COLUMN IF EXISTS harvestingclient_id;
