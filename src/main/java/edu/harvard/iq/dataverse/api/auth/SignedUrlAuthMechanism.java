1c2a20d940632da96c9d56c1bb40367c7902b398
Philip Durbin <philip_durbin@harvard.edu>
2023-11-21 14:25:26 -0500

    fix formatting (indentation) #10093

--- src/main/java/edu/harvard/iq/dataverse/api/auth/SignedUrlAuthMechanism.java
+++ src/main/java/edu/harvard/iq/dataverse/api/auth/SignedUrlAuthMechanism.java
@@ -61,9 +61,9 @@ public class SignedUrlAuthMechanism implements AuthMechanism {
         String userId = uriInfo.getQueryParameters().getFirst(SIGNED_URL_USER);
         User targetUser = null; 
         ApiToken userApiToken = null;
-        if(!userId.startsWith(PrivateUrlUser.PREFIX)) {
-        targetUser = authSvc.getAuthenticatedUser(userId);
-        userApiToken = authSvc.findApiTokenByUser((AuthenticatedUser)targetUser);
+        if (!userId.startsWith(PrivateUrlUser.PREFIX)) {
+            targetUser = authSvc.getAuthenticatedUser(userId);
+            userApiToken = authSvc.findApiTokenByUser((AuthenticatedUser) targetUser);
         } else {
             PrivateUrl privateUrl = privateUrlSvc.getPrivateUrlFromDatasetId(Long.parseLong(userId.substring(PrivateUrlUser.PREFIX.length())));
             userApiToken = new ApiToken();
