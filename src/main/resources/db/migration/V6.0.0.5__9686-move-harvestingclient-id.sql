a6fd3f5a715f3717941d1997813b9b4f7313bab1
Stephen Kraffmiller <skraffmiller@hmdc.harvard.edu>
2023-11-30 09:49:56 -0500

    #9686 changes from CR

--- src/main/resources/db/migration/V6.0.0.5__9686-move-harvestingclient-id.sql
+++ src/main/resources/db/migration/V6.0.0.5__9686-move-harvestingclient-id.sql
@@ -4,5 +4,5 @@ update dvobject dvo set harvestingclient_id = s.harvestingclient_id from
 (select id, harvestingclient_id from dataset d where d.harvestingclient_id is not null) s
 where s.id = dvo.id; 
 
---ALTER TABLE dataset drop COLUMN IF EXISTS harvestingclient_id;
+ALTER TABLE dataset drop COLUMN IF EXISTS harvestingclient_id;
 
