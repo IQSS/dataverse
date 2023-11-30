54d228aca37bc8d53c975617a61dcec12d5f2099
qqmyers <qqmyers@hotmail.com>
2023-11-30 15:11:56 -0500

    fix remote case

--- src/main/java/edu/harvard/iq/dataverse/globus/GlobusServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/globus/GlobusServiceBean.java
@@ -660,12 +660,15 @@ public class GlobusServiceBean implements java.io.Serializable {
     public JsonObject getFilesMap(ArrayList<DataFile> dataFiles, Dataset d) {
         JsonObjectBuilder filesBuilder = Json.createObjectBuilder();
         for (DataFile df : dataFiles) {
-            String[] fileInfo = DataAccess.getDriverIdAndStorageLocation(DataAccess.getLocationFromStorageId(df.getStorageIdentifier(), d));
-            String driverId = fileInfo[0];
-            String fileLocation= fileInfo[1]; 
-            if(GlobusAccessibleStore.isDataverseManaged(driverId)) {
+            String storageId = df.getStorageIdentifier();
+            String[] parts = DataAccess.getDriverIdAndStorageLocation(DataAccess.getLocationFromStorageId(storageId, d));
+            String driverId =  parts[0];
+            String fileLocation = parts[1];
+            if (GlobusAccessibleStore.isDataverseManaged(driverId)) {
                 String endpointWithBasePath = GlobusAccessibleStore.getTransferEnpointWithPath(driverId);
                 fileLocation = endpointWithBasePath + "/" + fileLocation;
+            } else {
+                fileLocation = storageId.substring(storageId.lastIndexOf("//") + 2);
             }
             filesBuilder.add(df.getId().toString(), fileLocation);
         }
