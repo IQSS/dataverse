b0c639a79f8450ac7f5acb619754c91326a69a8c
sbondka <sabrine.bondka@open-groupe.com>
2024-01-03 18:30:31 +0100

    fix sql

--- src/main/resources/db/migration/V6.1.0.1__2622-update-controlledvocabulary.sql
+++ src/main/resources/db/migration/V6.1.0.1__2622-update-controlledvocabulary.sql
@@ -1,9 +1,13 @@
-UPDATE controlledvocabularyvalue
-SET strvalue = 
+-- Fix Text Spacing in Astronomy Metadata Fields
+UPDATE controlledvocabularyvalue cvv
+SET strvalue =
   CASE
-    WHEN strvalue = 'LightCurve' THEN 'Light Curve'
-    WHEN strvalue = 'EventList' THEN 'Event List'
-    WHEN strvalue = 'PrettyPicture' THEN 'Pretty Picture'
-    WHEN strvalue = 'ValuePair' THEN 'Value Pair'
-    ELSE strvalue
-  END;
+    WHEN cvv.strvalue = 'LightCurve' THEN 'Light Curve'
+    WHEN cvv.strvalue = 'EventList' THEN 'Event List'
+    WHEN cvv.strvalue = 'PrettyPicture' THEN 'Pretty Picture'
+    WHEN cvv.strvalue = 'ValuePair' THEN 'Value Pair'
+    ELSE cvv.strvalue
+  END
+FROM datasetfieldtype dft
+WHERE dft.name = 'astroType'
+ AND cvv.datasetfieldtype_id = dft.id;
