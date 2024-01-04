06eb4fc789756167a3071a0daa4473d822f0155b
sbondka <sabrine.bondka@open-groupe.com>
2024-01-04 17:30:18 +0100

    Fix sql

--- src/main/resources/db/migration/V6.1.0.1__2622-update-controlledvocabulary.sql
+++ src/main/resources/db/migration/V6.1.0.1__2622-update-controlledvocabulary.sql
@@ -10,4 +10,4 @@ SET strvalue =
   END
 FROM datasetfieldtype dft
 WHERE dft.name = 'astroType' AND cvv.datasetfieldtype_id = dft.id 
-and (cvv.strvalue = 'LightCurve' or cvv.strvalue = 'EventList' or cvv.strvalue = 'PrettyPicture' or cvv.strvalue = 'ValuePair') ;
+AND cvv.strvalue IN ('LightCurve', 'EventList', 'PrettyPicture', 'ValuePair') ;
