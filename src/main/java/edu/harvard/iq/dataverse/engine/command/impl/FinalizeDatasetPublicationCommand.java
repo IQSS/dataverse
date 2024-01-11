571c166dfe98f1569cc56e01a9be1e9fc70e5efb
qqmyers <qqmyers@hotmail.com>
2024-01-11 17:39:10 -0500

    cleanup

--- src/main/java/edu/harvard/iq/dataverse/engine/command/impl/FinalizeDatasetPublicationCommand.java
+++ src/main/java/edu/harvard/iq/dataverse/engine/command/impl/FinalizeDatasetPublicationCommand.java
@@ -386,56 +386,52 @@ public class FinalizeDatasetPublicationCommand extends AbstractPublishDatasetCom
     }
     
     private void publicizeExternalIdentifier(Dataset dataset, CommandContext ctxt) throws CommandException {
-        String protocol = getDataset().getProtocol();
-        String authority = getDataset().getAuthority();
-        PidProvider pidProvider = PidUtil.getPidProvider(getDataset().getGlobalId().getProviderId());
- 
-        if (pidProvider != null) {
-            
-            try {
-                boolean isFilePIDsEnabled = ctxt.systemConfig().isFilePIDsEnabledForCollection(getDataset().getOwner());
-                // We will skip trying to register the global identifiers for datafiles 
-                // if "dependent" file-level identifiers are requested, AND the naming 
-                // protocol, or the authority of the dataset global id is different from 
-                // what's currently configured for the Dataverse. In other words
-                // we can't get "dependent" DOIs assigned to files in a dataset  
-                // with the registered id that is a handle; or even a DOI, but in 
-                // an authority that's different from what's currently configured.
-                // Additionaly in 4.9.3 we have added a system variable to disable 
-                // registering file PIDs on the installation level.
-                boolean registerGlobalIdsForFiles = 
-                        ctxt.systemConfig().isFilePIDsEnabledForCollection(getDataset().getOwner()) &&
-                                ctxt.dvObjects().getEffectivePidGenerator(dataset).canCreatePidsLike(dataset.getGlobalId());
-                
-                if (registerGlobalIdsForFiles
-                        && isFilePIDsEnabled
-                        && dataset.getLatestVersion().getMinorVersionNumber() != null
-                        && dataset.getLatestVersion().getMinorVersionNumber().equals((long) 0)) {
-                    //A false return value indicates a failure in calling the service
-                    for (DataFile df : dataset.getFiles()) {
-                        logger.log(Level.FINE, "registering global id for file {0}", df.getId());
-                        //A false return value indicates a failure in calling the service
-                        if (!pidProvider.publicizeIdentifier(df)) {
-                            throw new Exception();
-                        }
-                        df.setGlobalIdCreateTime(getTimestamp());
-                        df.setIdentifierRegistered(true);
+        PidProvider pidProvider = ctxt.dvObjects().getEffectivePidGenerator(dataset);
+        try {
+            // We will skip trying to register the global identifiers for datafiles
+            // if "dependent" file-level identifiers are requested, AND the naming
+            // protocol, or the authority of the dataset global id is different from
+            // what's currently configured for the Dataverse. In other words
+            // we can't get "dependent" DOIs assigned to files in a dataset
+            // with the registered id that is a handle; or even a DOI, but in
+            // an authority that's different from what's currently configured.
+            // Additionaly in 4.9.3 we have added a system variable to disable
+            // registering file PIDs on the installation level.
+            boolean registerGlobalIdsForFiles = ctxt.systemConfig().isFilePIDsEnabledForCollection(
+                    getDataset().getOwner()) 
+                    && pidProvider.canCreatePidsLike(dataset.getGlobalId());
+
+            if (registerGlobalIdsForFiles 
+                    && dataset.getLatestVersion().getMinorVersionNumber() != null
+                    && dataset.getLatestVersion().getMinorVersionNumber().equals((long) 0)) {
+                // A false return value indicates a failure in calling the service
+                for (DataFile df : dataset.getFiles()) {
+                    logger.log(Level.FINE, "registering global id for file {0}", df.getId());
+                    // A false return value indicates a failure in calling the service
+                    if (!pidProvider.publicizeIdentifier(df)) {
+                        throw new Exception();
                     }
+                    df.setGlobalIdCreateTime(getTimestamp());
+                    df.setIdentifierRegistered(true);
                 }
-                if (!pidProvider.publicizeIdentifier(dataset)) {
-                    throw new Exception();
-                }
-                dataset.setGlobalIdCreateTime(new Date()); // TODO these two methods should be in the responsibility of the pidProvider.
-                dataset.setIdentifierRegistered(true);
-            } catch (Throwable e) {
-                logger.warning("Failed to register the identifier "+dataset.getGlobalId().asString()+", or to register a file in the dataset; notifying the user(s), unlocking the dataset");
-                
-                // Send failure notification to the user: 
-                notifyUsersDatasetPublishStatus(ctxt, dataset, UserNotification.Type.PUBLISHFAILED_PIDREG);
-                
-                ctxt.datasets().removeDatasetLocks(dataset, DatasetLock.Reason.finalizePublication);
-                throw new CommandException(BundleUtil.getStringFromBundle("dataset.publish.error", pidProvider.getProviderInformation()), this);
             }
+            if (!pidProvider.publicizeIdentifier(dataset)) {
+                throw new Exception();
+            }
+            dataset.setGlobalIdCreateTime(new Date()); // TODO these two methods should be in the responsibility of the
+                                                       // pidProvider.
+            dataset.setIdentifierRegistered(true);
+        } catch (Throwable e) {
+            logger.warning("Failed to register the identifier " + dataset.getGlobalId().asString()
+                    + ", or to register a file in the dataset; notifying the user(s), unlocking the dataset");
+
+            // Send failure notification to the user:
+            notifyUsersDatasetPublishStatus(ctxt, dataset, UserNotification.Type.PUBLISHFAILED_PIDREG);
+
+            ctxt.datasets().removeDatasetLocks(dataset, DatasetLock.Reason.finalizePublication);
+            throw new CommandException(
+                    BundleUtil.getStringFromBundle("dataset.publish.error", pidProvider.getProviderInformation()),
+                    this);
         }
     }
     
