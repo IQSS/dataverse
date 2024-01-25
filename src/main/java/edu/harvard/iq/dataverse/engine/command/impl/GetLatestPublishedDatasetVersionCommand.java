9d124e760bba83b7baa46bb1f88ec453a6bf6e6a
GPortas <hey@gportas.me>
2024-01-25 11:51:12 +0000

    Refactor: GetLatestPublishedDatasetVersionCommand

--- src/main/java/edu/harvard/iq/dataverse/engine/command/impl/GetLatestPublishedDatasetVersionCommand.java
+++ src/main/java/edu/harvard/iq/dataverse/engine/command/impl/GetLatestPublishedDatasetVersionCommand.java
@@ -17,7 +17,7 @@ import edu.harvard.iq.dataverse.engine.command.exception.CommandException;
 public class GetLatestPublishedDatasetVersionCommand extends AbstractCommand<DatasetVersion> {
     private final Dataset ds;
     private final boolean includeDeaccessioned;
-    private boolean checkPermsWhenDeaccessioned;
+    private final boolean checkPermsWhenDeaccessioned;
 
     public GetLatestPublishedDatasetVersionCommand(DataverseRequest aRequest, Dataset anAffectedDataset) {
         this(aRequest, anAffectedDataset, false, false);
@@ -31,37 +31,35 @@ public class GetLatestPublishedDatasetVersionCommand extends AbstractCommand<Dat
     }
 
     /*
-    * This command depending on the requested parameters will return:
-    * 
-    * If the user requested to include a deaccessioned dataset with the files, the command will return the deaccessioned version if the user has permissions to view the files. Otherwise, it will return null. 
-    * If the user requested to include a deaccessioned dataset but did not request the files, the command will return the deaccessioned version. 
-    * If the user did not request to include a deaccessioned dataset, the command will return the latest published version.
-    *  
-    */
+     * This command depending on the requested parameters will return:
+     *
+     * If the user requested to include a deaccessioned dataset with the files, the command will return the deaccessioned version if the user has permissions to view the files. Otherwise, it will return null.
+     * If the user requested to include a deaccessioned dataset but did not request the files, the command will return the deaccessioned version.
+     * If the user did not request to include a deaccessioned dataset, the command will return the latest published version.
+     *
+     */
     @Override
     public DatasetVersion execute(CommandContext ctxt) throws CommandException {
-
-        DatasetVersion dsv = null;
-
-        //We search of a released or deaccessioned version if it is requested.
-        for (DatasetVersion next : ds.getVersions()) {
-            if (next.isReleased() || (includeDeaccessioned && next.isDeaccessioned())){
-                dsv = next;
-                break;
-            }
+        DatasetVersion dsVersionResult = getReleaseOrDeaccessionedVersion();
+        if (dsVersionResult != null && userHasPermissionsOnDatasetVersion(dsVersionResult, checkPermsWhenDeaccessioned, ctxt, ds)) {
+            return dsVersionResult;
         }
-
-        //Checking permissions if the deaccessionedVersion was found and we are checking permissions because files were requested.
-        if(dsv != null && (dsv.isDeaccessioned() && checkPermsWhenDeaccessioned)){
-            //If the user has no permissions we return null
-            if(!ctxt.permissions().requestOn(getRequest(), ds).has(Permission.EditDataset)){
-                dsv = null;
-            }
-        }
-
-        return dsv;
+        return null;
     }
 
+    private DatasetVersion getReleaseOrDeaccessionedVersion() {
+        for (DatasetVersion dsVersion : ds.getVersions()) {
+            if (dsVersion.isReleased() || (includeDeaccessioned && dsVersion.isDeaccessioned())) {
+                return dsVersion;
+            }
+        }
+        return null;
+    }
 
-
+    private boolean userHasPermissionsOnDatasetVersion(DatasetVersion dsVersionResult, boolean checkPermsWhenDeaccessioned, CommandContext ctxt, Dataset ds) {
+        if (dsVersionResult.isDeaccessioned() && checkPermsWhenDeaccessioned) {
+            return ctxt.permissions().requestOn(getRequest(), ds).has(Permission.EditDataset);
+        }
+        return true;
+    }
 }
