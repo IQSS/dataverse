2e41b9e42f9f8c74eadf124538080827836a39c4
qqmyers <jim.myers@computer.org>
2024-02-09 14:56:56 -0500

    Apply suggestions from code review
    Co-authored-by: Oliver Bertuch <poikilotherm@users.noreply.github.com>

--- src/main/java/edu/harvard/iq/dataverse/pidproviders/PidProviderFactory.java
+++ src/main/java/edu/harvard/iq/dataverse/pidproviders/PidProviderFactory.java
@@ -5,5 +5,11 @@ public interface PidProviderFactory {
     public String getType();
     
     public PidProvider createPidProvider(String id);
-
+    
+    /**
+     * Get ordinal value to ensure loading order of provider factories is coherent
+     */
+    default int getOrdinal() {
+        return 100;
+    }
 }
