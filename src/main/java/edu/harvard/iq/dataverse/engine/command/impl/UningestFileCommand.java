262fb267a2025872d8f537e937ad31dc0a25a156
qqmyers <qqmyers@hotmail.com>
2024-02-13 17:49:09 -0500

    superuser only in command, add docs

--- src/main/java/edu/harvard/iq/dataverse/engine/command/impl/UningestFileCommand.java
+++ src/main/java/edu/harvard/iq/dataverse/engine/command/impl/UningestFileCommand.java
@@ -33,7 +33,7 @@ import jakarta.persistence.Query;
  * @author skraffmi
  * @author Leonid Andreev 
  */
-@RequiredPermissions(Permission.PublishDataset)
+@RequiredPermissions({})
 public class UningestFileCommand extends AbstractVoidCommand  {
 
     private static final Logger logger = Logger.getLogger(UningestFileCommand.class.getCanonicalName());
@@ -47,10 +47,10 @@ public class UningestFileCommand extends AbstractVoidCommand  {
     @Override
     protected void executeImpl(CommandContext ctxt) throws CommandException {
         
-        // first check if  user is a superuser
-        if (!(getUser() instanceof AuthenticatedUser)) {
-            throw new PermissionException("Uningest File can only be called by User with the PublishDataset permission.",
-                this,  Collections.singleton(Permission.EditDataset), uningest);                
+        // first check if user is a superuser
+        if ((!(getUser() instanceof AuthenticatedUser) || !getUser().isSuperuser())) {
+            throw new PermissionException("Uningest File can only be called by Superusers.", this,
+                    Collections.singleton(Permission.EditDataset), uningest);
         }
         
         // is this actually a tabular data file?
