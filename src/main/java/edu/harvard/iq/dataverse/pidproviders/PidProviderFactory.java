7b83f3e1b38a4e8e929de32d2f5b5777a51869bb
qqmyers <qqmyers@hotmail.com>
2024-02-13 12:32:24 -0500

    revert 2e41b9e

--- src/main/java/edu/harvard/iq/dataverse/pidproviders/PidProviderFactory.java
+++ src/main/java/edu/harvard/iq/dataverse/pidproviders/PidProviderFactory.java
@@ -1,15 +1,8 @@
 package edu.harvard.iq.dataverse.pidproviders;
 
 public interface PidProviderFactory {
-    
-    public String getType();
-    
-    public PidProvider createPidProvider(String id);
-    
-    /**
-     * Get ordinal value to ensure loading order of provider factories is coherent
-     */
-    default int getOrdinal() {
-        return 100;
-    }
+
+    String getType();
+
+    PidProvider createPidProvider(String id);
 }
