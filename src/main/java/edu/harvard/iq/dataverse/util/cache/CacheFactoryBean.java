a9b2514620a6e4f1fb1377936423c2527800400c
Steven Winship <steven_winship@iq.harvard.edu>
2024-03-20 15:54:03 -0400

    add check for existing cache before creating a new one

--- src/main/java/edu/harvard/iq/dataverse/util/cache/CacheFactoryBean.java
+++ src/main/java/edu/harvard/iq/dataverse/util/cache/CacheFactoryBean.java
@@ -32,10 +32,13 @@ public class CacheFactoryBean implements java.io.Serializable {
 
     @PostConstruct
     public void init() {
-        CompleteConfiguration<String, String> config =
-                new MutableConfiguration<String, String>()
-                        .setTypes( String.class, String.class );
-        rateLimitCache = manager.createCache(RATE_LIMIT_CACHE, config);
+        rateLimitCache = manager.getCache(RATE_LIMIT_CACHE);
+        if (rateLimitCache == null) {
+            CompleteConfiguration<String, String> config =
+                    new MutableConfiguration<String, String>()
+                            .setTypes( String.class, String.class );
+            rateLimitCache = manager.createCache(RATE_LIMIT_CACHE, config);
+        }
     }
 
     /**
