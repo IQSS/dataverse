d714a1a2b68ff77eaa01f07422bcd64c95595c4b
qqmyers <qqmyers@hotmail.com>
2024-04-23 17:15:29 -0400

    merge issues

--- src/main/java/edu/harvard/iq/dataverse/workflow/internalspi/LDNAnnounceDatasetVersionStep.java
+++ src/main/java/edu/harvard/iq/dataverse/workflow/internalspi/LDNAnnounceDatasetVersionStep.java
@@ -35,6 +35,7 @@ import jakarta.json.JsonArray;
 import jakarta.json.JsonArrayBuilder;
 import jakarta.json.JsonObject;
 import jakarta.json.JsonObjectBuilder;
+import jakarta.json.JsonString;
 import jakarta.json.JsonValue;
 
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -177,7 +178,7 @@ public class LDNAnnounceDatasetVersionStep implements WorkflowStep {
             Map<String, String> localContext) {
         String id = getBestId(dft, jval);
         return Json.createObjectBuilder().add("object", id).add("relationship", dft.getJsonLDTerm().getUrl())
-                .add("subject", d.getGlobalId().toURL().toString()).add("id", "urn:uuid:" + UUID.randomUUID().toString()).add("type","Relationship").build();
+                .add("subject", d.getGlobalId().asURL().toString()).add("id", "urn:uuid:" + UUID.randomUUID().toString()).add("type","Relationship").build();
     }
 
     HttpPost buildAnnouncement(Dataset d, JsonObject rel, JsonObject target) throws URISyntaxException {
