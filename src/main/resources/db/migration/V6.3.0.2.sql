bf668a94bc7461c95097dd0ffc29297cba4108cb
Philip Durbin <philip_durbin@harvard.edu>
2024-07-29 11:33:12 -0400

    fix sql script, add column etc #10517

--- src/main/resources/db/migration/V6.3.0.2.sql
+++ src/main/resources/db/migration/V6.3.0.2.sql
@@ -1,9 +1,16 @@
 -- Dataset types have been added. See #10517 and #10694
 --
--- First, insert some types (dataset is the default).
+-- Insert some types (dataset is the default).
 INSERT INTO datasettype (name) VALUES ('dataset');
 INSERT INTO datasettype (name) VALUES ('software');
 INSERT INTO datasettype (name) VALUES ('workflow');
 --
--- Then, give existing datasets a type of "dataset".
+-- Add the new column (if it doesn't exist) and foreign key.
+ALTER TABLE dataset ADD COLUMN IF NOT EXISTS datasettype_id bigint;
+ALTER TABLE dataset ADD CONSTRAINT fk_dataset_datasettype_id FOREIGN KEY (datasettype_id) REFERENCES datasettype(id);
+--
+-- Give existing datasets a type of "dataset".
 UPDATE dataset SET datasettype_id = (SELECT id FROM datasettype WHERE name = 'dataset');
+--
+-- Make the column non-null
+ALTER TABLE dataset ALTER COLUMN datasettype_id SET NOT NULL;
