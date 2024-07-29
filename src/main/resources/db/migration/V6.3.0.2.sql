78a3c1a27199abb1b99b61b508c3439c1be4f342
Philip Durbin <philip_durbin@harvard.edu>
2024-07-29 15:46:16 -0400

    fix sql script when adding foreign key constraint #10517

--- src/main/resources/db/migration/V6.3.0.2.sql
+++ src/main/resources/db/migration/V6.3.0.2.sql
@@ -5,12 +5,21 @@ INSERT INTO datasettype (name) VALUES ('dataset');
 INSERT INTO datasettype (name) VALUES ('software');
 INSERT INTO datasettype (name) VALUES ('workflow');
 --
--- Add the new column (if it doesn't exist) and foreign key.
+-- Add the new column (if it doesn't exist).
 ALTER TABLE dataset ADD COLUMN IF NOT EXISTS datasettype_id bigint;
-ALTER TABLE dataset ADD CONSTRAINT fk_dataset_datasettype_id FOREIGN KEY (datasettype_id) REFERENCES datasettype(id);
+--
+-- Add the foreign key.
+DO $$
+BEGIN
+  BEGIN
+    ALTER TABLE dataset ADD CONSTRAINT fk_dataset_datasettype_id FOREIGN KEY (datasettype_id) REFERENCES datasettype(id);
+  EXCEPTION
+    WHEN duplicate_object THEN RAISE NOTICE 'Table constraint fk_dataset_datasettype_id already exists';
+  END;
+END $$;
 --
 -- Give existing datasets a type of "dataset".
 UPDATE dataset SET datasettype_id = (SELECT id FROM datasettype WHERE name = 'dataset');
 --
--- Make the column non-null
+-- Make the column non-null.
 ALTER TABLE dataset ALTER COLUMN datasettype_id SET NOT NULL;
