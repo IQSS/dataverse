c8cdb8bf46b9c4a9a6836cab418d8012911689be
Vera Clemens <clemens@zbmed.de>
2024-08-22 13:22:59 +0200

    fix: remove doi property for perma PIDs in BibTeX citation

--- src/main/java/edu/harvard/iq/dataverse/DataCitation.java
+++ src/main/java/edu/harvard/iq/dataverse/DataCitation.java
@@ -293,11 +293,13 @@ public class DataCitation {
         out.write("version = {");
         out.write(version);
         out.write("},\r\n");
-        out.write("doi = {");
-        out.write(persistentId.getAuthority());
-        out.write("/");
-        out.write(persistentId.getIdentifier());
-        out.write("},\r\n");
+        if("doi".equals(persistentId.getProtocol())) {
+            out.write("doi = {");
+            out.write(persistentId.getAuthority());
+            out.write("/");
+            out.write(persistentId.getIdentifier());
+            out.write("},\r\n");
+        }
         out.write("url = {");
         out.write(persistentId.asURL());
         out.write("}\r\n");
