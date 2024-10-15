6baff905f7005ced2235a39217dd5985d6b7f758
Jim Myers <qqmyers@hotmail.com>
2024-10-15 17:45:17 -0400

    adding a space in log message - per review

--- src/main/java/edu/harvard/iq/dataverse/search/SolrIndexServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/search/SolrIndexServiceBean.java
@@ -433,7 +433,7 @@ public class SolrIndexServiceBean {
          * files, see https://github.com/IQSS/dataverse/issues/2421
          */
         reindexFilesInBatches(filesToReindexAsBatch);
-        logger.fine("Reindexed permissions for " + i + " files and " + numObjects + "datasets/collections");
+        logger.fine("Reindexed permissions for " + i + " files and " + numObjects + " datasets/collections");
         return new IndexResponse("Number of dvObject permissions indexed for " + definitionPoint
                 + ": " + numObjects);
     }
