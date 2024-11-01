fd68cd225082ab27a7723d7980223f5bfbad0853
GPortas <hey@gportas.me>
2024-11-01 19:53:18 +0000

    Refactor: error message string extracted to const

--- src/main/java/edu/harvard/iq/dataverse/engine/command/impl/RegisterOidcUserCommand.java
+++ src/main/java/edu/harvard/iq/dataverse/engine/command/impl/RegisterOidcUserCommand.java
@@ -16,6 +16,8 @@ public class RegisterOidcUserCommand extends AbstractVoidCommand {
     private final String bearerToken;
     private final UserDTO userDTO;
 
+    private static final String ERROR_USER_ALREADY_REGISTERED_WITH_TOKEN = "User is already registered with this token";
+
     public RegisterOidcUserCommand(DataverseRequest aRequest, String bearerToken, UserDTO userDTO) {
         super(aRequest, (DvObject) null);
         this.bearerToken = bearerToken;
@@ -28,7 +30,7 @@ public class RegisterOidcUserCommand extends AbstractVoidCommand {
             UserRecordIdentifier userRecordIdentifier = ctxt.authentication().verifyOidcBearerTokenAndGetUserIdentifier(bearerToken);
             User user = ctxt.authentication().lookupUser(userRecordIdentifier);
             if (user != null) {
-                throw new IllegalCommandException("User is already registered with this token", this);
+                throw new IllegalCommandException(ERROR_USER_ALREADY_REGISTERED_WITH_TOKEN, this);
             }
             AuthenticatedUserDisplayInfo authenticatedUserDisplayInfo = new AuthenticatedUserDisplayInfo(
                     userDTO.firstName,
