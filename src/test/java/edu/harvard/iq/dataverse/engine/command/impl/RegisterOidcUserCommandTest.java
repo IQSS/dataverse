753f6ebcfa54618579f14f244103a964290867fd
GPortas <hey@gportas.me>
2024-11-08 10:25:31 +0000

    Added: unit tests for RegisterOidcUserCommand

---
+++ src/test/java/edu/harvard/iq/dataverse/engine/command/impl/RegisterOidcUserCommandTest.java
@@ -0,0 +1,166 @@
+package edu.harvard.iq.dataverse.engine.command.impl;
+
+import edu.harvard.iq.dataverse.api.dto.UserDTO;
+import edu.harvard.iq.dataverse.authorization.AuthenticatedUserDisplayInfo;
+import edu.harvard.iq.dataverse.authorization.AuthenticationServiceBean;
+import edu.harvard.iq.dataverse.authorization.UserRecordIdentifier;
+import edu.harvard.iq.dataverse.authorization.exceptions.AuthorizationException;
+import edu.harvard.iq.dataverse.authorization.users.AuthenticatedUser;
+import edu.harvard.iq.dataverse.engine.command.CommandContext;
+import edu.harvard.iq.dataverse.engine.command.exception.CommandException;
+import edu.harvard.iq.dataverse.engine.command.exception.IllegalCommandException;
+import edu.harvard.iq.dataverse.engine.command.exception.InvalidFieldsCommandException;
+import edu.harvard.iq.dataverse.engine.command.exception.PermissionException;
+import edu.harvard.iq.dataverse.util.BundleUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import static edu.harvard.iq.dataverse.mocks.MocksFactory.makeRequest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
+
+class RegisterOidcUserCommandTest {
+
+    private static final String TEST_BEARER_TOKEN = "Bearer test";
+
+    private UserDTO userDTO;
+
+    @Mock
+    private CommandContext context;
+
+    @Mock
+    private AuthenticationServiceBean authServiceMock;
+
+    @InjectMocks
+    private RegisterOidcUserCommand sut;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        setUpDefaultUserDTO();
+        when(context.authentication()).thenReturn(authServiceMock);
+        sut = new RegisterOidcUserCommand(makeRequest(), TEST_BEARER_TOKEN, userDTO);
+    }
+
+    private void setUpDefaultUserDTO() {
+        userDTO = new UserDTO();
+        userDTO.setTermsAccepted(true);
+        userDTO.setFirstName("FirstName");
+        userDTO.setLastName("LastName");
+        userDTO.setUsername("username");
+        userDTO.setEmailAddress("user@example.com");
+    }
+
+    @Test
+    public void execute_unacceptedTerms_availableEmailAndUsername() {
+        userDTO.setTermsAccepted(false);
+        when(authServiceMock.getAuthenticatedUserByEmail(userDTO.getEmailAddress())).thenReturn(null);
+        when(authServiceMock.getAuthenticatedUser(userDTO.getUsername())).thenReturn(null);
+
+        assertThatThrownBy(() -> sut.execute(context))
+                .isInstanceOf(InvalidFieldsCommandException.class)
+                .satisfies(exception -> {
+                    InvalidFieldsCommandException ex = (InvalidFieldsCommandException) exception;
+                    assertThat(ex.getFieldErrors())
+                            .containsEntry("termsAccepted", BundleUtil.getStringFromBundle("registerOidcUserCommand.errors.userShouldAcceptTerms"))
+                            .doesNotContainEntry("emailAddress", BundleUtil.getStringFromBundle("registerOidcUserCommand.errors.emailAddressInUse"))
+                            .doesNotContainEntry("username", BundleUtil.getStringFromBundle("registerOidcUserCommand.errors.usernameInUse"));
+                });
+    }
+
+    @Test
+    public void execute_acceptedTerms_availableEmailAndUsername() {
+        AuthenticatedUser existingUser = new AuthenticatedUser();
+        when(authServiceMock.getAuthenticatedUserByEmail(userDTO.getEmailAddress())).thenReturn(existingUser);
+        when(authServiceMock.getAuthenticatedUser(userDTO.getUsername())).thenReturn(existingUser);
+
+        assertThatThrownBy(() -> sut.execute(context))
+                .isInstanceOf(InvalidFieldsCommandException.class)
+                .satisfies(exception -> {
+                    InvalidFieldsCommandException ex = (InvalidFieldsCommandException) exception;
+                    assertThat(ex.getFieldErrors())
+                            .containsEntry("emailAddress", BundleUtil.getStringFromBundle("registerOidcUserCommand.errors.emailAddressInUse"))
+                            .containsEntry("username", BundleUtil.getStringFromBundle("registerOidcUserCommand.errors.usernameInUse"))
+                            .doesNotContainKey("termsAccepted");
+                });
+    }
+
+    @Test
+    void execute_throwsPermissionException_onAuthorizationException() throws AuthorizationException {
+        String testAuthorizationExceptionMessage = "Authorization failed";
+        when(context.authentication().verifyOidcBearerTokenAndGetUserIdentifier(TEST_BEARER_TOKEN))
+                .thenThrow(new AuthorizationException(testAuthorizationExceptionMessage));
+
+        assertThatThrownBy(() -> sut.execute(context))
+                .isInstanceOf(PermissionException.class)
+                .hasMessageContaining(testAuthorizationExceptionMessage);
+
+        Mockito.verify(context.authentication(), times(1))
+                .verifyOidcBearerTokenAndGetUserIdentifier(TEST_BEARER_TOKEN);
+    }
+
+    @Test
+    void execute_throwsIllegalCommandException_ifUserAlreadyRegisteredWithToken() throws AuthorizationException {
+        UserRecordIdentifier userRecordIdentifierMock = Mockito.mock(UserRecordIdentifier.class);
+        when(context.authentication().verifyOidcBearerTokenAndGetUserIdentifier(TEST_BEARER_TOKEN))
+                .thenReturn(userRecordIdentifierMock);
+        when(context.authentication().lookupUser(userRecordIdentifierMock)).thenReturn(new AuthenticatedUser());
+
+        assertThatThrownBy(() -> sut.execute(context))
+                .isInstanceOf(IllegalCommandException.class)
+                .hasMessageContaining(BundleUtil.getStringFromBundle("registerOidcUserCommand.errors.userAlreadyRegisteredWithToken"));
+
+        Mockito.verify(context.authentication(), times(1))
+                .lookupUser(userRecordIdentifierMock);
+    }
+
+    @Test
+    void execute_happyPath_withoutAffiliationAndPosition() throws AuthorizationException, CommandException {
+        UserRecordIdentifier userRecordIdentifierMock = mock(UserRecordIdentifier.class);
+        when(authServiceMock.verifyOidcBearerTokenAndGetUserIdentifier(TEST_BEARER_TOKEN)).thenReturn(userRecordIdentifierMock);
+
+        sut.execute(context);
+
+        verify(authServiceMock, times(1)).createAuthenticatedUser(
+                eq(userRecordIdentifierMock),
+                eq(userDTO.getUsername()),
+                eq(new AuthenticatedUserDisplayInfo(
+                        userDTO.getFirstName(),
+                        userDTO.getLastName(),
+                        userDTO.getEmailAddress(),
+                        "",
+                        "")
+                ),
+                eq(true)
+        );
+    }
+
+    @Test
+    void execute_happyPath_withAffiliationAndPosition() throws AuthorizationException, CommandException {
+        userDTO.setPosition("test position");
+        userDTO.setAffiliation("test affiliation");
+
+        UserRecordIdentifier userRecordIdentifierMock = mock(UserRecordIdentifier.class);
+        when(authServiceMock.verifyOidcBearerTokenAndGetUserIdentifier(TEST_BEARER_TOKEN)).thenReturn(userRecordIdentifierMock);
+
+        sut.execute(context);
+
+        verify(authServiceMock, times(1)).createAuthenticatedUser(
+                eq(userRecordIdentifierMock),
+                eq(userDTO.getUsername()),
+                eq(new AuthenticatedUserDisplayInfo(
+                        userDTO.getFirstName(),
+                        userDTO.getLastName(),
+                        userDTO.getEmailAddress(),
+                        userDTO.getAffiliation(),
+                        userDTO.getPosition())
+                ),
+                eq(true)
+        );
+    }
+}
