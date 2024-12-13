ffc71d85abcd8c953e5cfaab70536b5ffd48c61b
Jim Myers <qqmyers@hotmail.com>
2024-12-13 14:07:06 -0500

    use do

--- src/main/resources/db/migration/V6.5.0.2.sql
+++ src/main/resources/db/migration/V6.5.0.2.sql
@@ -10,14 +10,11 @@ UPDATE datasetversion set deaccessionnote = versionnote;
 UPDATE datasetversion set versionnote = null;
 
 -- Move/merge archivenote contents and remove archivenote column (on existing DBs that have this column)
---
-IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'datasetversion'' AND COLUMN_NAME = 'archivenote'')
-
+DO $$
 BEGIN
-
+IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'datasetversion' AND COLUMN_NAME = 'archivenote') THEN
 UPDATE datasetversion set deaccessionlink = CONCAT_WS(' ', deaccessionlink, archivenote);
-
 ALTER TABLE datasetversion DROP COLUMN archivenote;
-
+END IF;
 END
-
+$$
