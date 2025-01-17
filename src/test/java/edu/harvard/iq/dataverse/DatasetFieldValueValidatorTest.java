ac1a881ff9d6a69f748235d3445e4b5a8bcfb52d
Philip Durbin <philip_durbin@harvard.edu>
2025-01-17 15:15:19 -0500

    add test asserting current ROR regex behavior #11075

--- src/test/java/edu/harvard/iq/dataverse/DatasetFieldValueValidatorTest.java
+++ src/test/java/edu/harvard/iq/dataverse/DatasetFieldValueValidatorTest.java
@@ -157,6 +157,14 @@ public class DatasetFieldValueValidatorTest {
         assertFalse(validator.isValidAuthorIdentifier("junk", pattern));
     }
 
+    @Test
+    public void testIsValidAuthorIdentifierRor() {
+        DatasetFieldValueValidator validator = new DatasetFieldValueValidator();
+        Pattern pattern = ExternalIdentifier.valueOf("ROR").getPattern();
+        assertTrue(validator.isValidAuthorIdentifier("https://ror.org/03vek6s52", pattern));
+        assertFalse(validator.isValidAuthorIdentifier("junk", pattern));
+    }
+
     final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
     
     @ParameterizedTest
