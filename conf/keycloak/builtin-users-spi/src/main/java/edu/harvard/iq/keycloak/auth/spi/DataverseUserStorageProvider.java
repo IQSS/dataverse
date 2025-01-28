fa72703b13ce9f4e81fefe299035c07ce1b9961b
GPortas <hey@gportas.me>
2025-01-28 15:12:30 +0000

    Added: Keycloak SPI base logic to support searching dv builtin users through kc admin console

--- conf/keycloak/builtin-users-spi/src/main/java/edu/harvard/iq/keycloak/auth/spi/DataverseUserStorageProvider.java
+++ conf/keycloak/builtin-users-spi/src/main/java/edu/harvard/iq/keycloak/auth/spi/DataverseUserStorageProvider.java
@@ -4,7 +4,6 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 
 import org.jboss.logging.Logger;
-
 import org.keycloak.component.ComponentModel;
 import org.keycloak.connections.jpa.JpaConnectionProvider;
 import org.keycloak.credential.CredentialInput;
@@ -13,12 +12,10 @@ import org.keycloak.models.GroupModel;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserModel;
-import org.keycloak.models.cache.CachedUserModel;
-import org.keycloak.models.cache.OnUserCache;
+import org.keycloak.models.credential.PasswordCredentialModel;
 import org.keycloak.storage.UserStorageProvider;
 import org.keycloak.storage.user.UserLookupProvider;
-import org.keycloak.storage.user.UserQueryProvider;
-import org.keycloak.storage.user.UserRegistrationProvider;
+import org.keycloak.storage.user.UserQueryMethodsProvider;
 
 import java.util.List;
 import java.util.Map;
@@ -27,10 +24,8 @@ import java.util.stream.Stream;
 public class DataverseUserStorageProvider implements
         UserStorageProvider,
         UserLookupProvider,
-        UserRegistrationProvider,
-        UserQueryProvider,
         CredentialInputValidator,
-        OnUserCache {
+        UserQueryMethodsProvider {
 
     private static final Logger logger = Logger.getLogger(DataverseUserStorageProvider.class);
 
@@ -46,7 +41,13 @@ public class DataverseUserStorageProvider implements
 
     @Override
     public UserModel getUserById(RealmModel realmModel, String id) {
-        return null;
+        logger.info("getUserById: " + id);
+        DataverseUser user = em.find(DataverseUser.class, id);
+        if (user == null) {
+            logger.info("could not find user by id: " + id);
+            return null;
+        }
+        return new DataverseUserAdapter(session, realmModel, model, user);
     }
 
     @Override
@@ -56,36 +57,53 @@ public class DataverseUserStorageProvider implements
         query.setParameter("username", username);
         List<DataverseUser> result = query.getResultList();
         if (result.isEmpty()) {
-            logger.info("could not find username: " + username);
+            logger.info("User not found: " + username);
             return null;
         }
-
+        logger.info("User found: " + result.get(0).getUsername());
         return new DataverseUserAdapter(session, realmModel, model, result.get(0));
     }
 
     @Override
     public UserModel getUserByEmail(RealmModel realmModel, String email) {
+        logger.info("getUserByEmail is not supported in DataverseUserStorageProvider");
         return null;
     }
 
     @Override
-    public boolean supportsCredentialType(String s) {
-        return false;
+    public boolean supportsCredentialType(String credentialType) {
+        return credentialType.equals(PasswordCredentialModel.TYPE);
     }
 
     @Override
-    public boolean isConfiguredFor(RealmModel realmModel, UserModel userModel, String s) {
+    public boolean isConfiguredFor(RealmModel realmModel, UserModel userModel, String credentialType) {
+        logger.info("isConfiguredFor called for user: " + userModel.getUsername() + " and credentialType: " + credentialType);
         return false;
     }
 
     @Override
     public boolean isValid(RealmModel realmModel, UserModel userModel, CredentialInput credentialInput) {
+        logger.info("isValid is not supported in DataverseUserStorageProvider");
         return false;
     }
 
     @Override
-    public Stream<UserModel> searchForUserStream(RealmModel realmModel, Map<String, String> map, Integer integer, Integer integer1) {
-        return Stream.empty();
+    public void close() {
+        logger.info("Closing DataverseUserStorageProvider");
+        if (em != null) {
+            em.close();
+        }
+    }
+
+    @Override
+    public Stream<UserModel> searchForUserStream(RealmModel realm, Map<String, String> params, Integer firstResult, Integer maxResults) {
+        // TODO search by email too
+        String search = params.get(UserModel.SEARCH);
+        logger.info("searchForUserStream: " + search);
+        String lower = search != null ? search.toLowerCase() : "";
+        TypedQuery<DataverseUser> query = em.createNamedQuery("DataverseUser.findByUsername", DataverseUser.class);
+        query.setParameter("username", lower);
+        return query.getResultStream().map(entity -> new DataverseUserAdapter(session, realm, model, entity));
     }
 
     @Override
@@ -96,24 +114,5 @@ public class DataverseUserStorageProvider implements
     @Override
     public Stream<UserModel> searchForUserByUserAttributeStream(RealmModel realmModel, String s, String s1) {
         return Stream.empty();
-    }
-
-    @Override
-    public UserModel addUser(RealmModel realmModel, String s) {
-        return null;
-    }
-
-    @Override
-    public boolean removeUser(RealmModel realmModel, UserModel userModel) {
-        return false;
-    }
-
-    @Override
-    public void close() {
-    }
-
-    @Override
-    public void onCache(RealmModel realmModel, CachedUserModel cachedUserModel, UserModel userModel) {
-
     }
 }
