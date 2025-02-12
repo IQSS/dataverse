653a43f75a04240a0ed7b5bf0365fb87a79a85fb
Jim Myers <qqmyers@hotmail.com>
2025-02-12 09:07:52 -0500

    add tests

--- src/test/java/edu/harvard/iq/dataverse/ExternalIdentifierTest.java
+++ src/test/java/edu/harvard/iq/dataverse/ExternalIdentifierTest.java
@@ -9,16 +9,46 @@ public class ExternalIdentifierTest {
 
   @Test
   public void testIsValidAuthorIdentifierOrcid() {
-    ExternalIdentifier identifier = ExternalIdentifier.valueOf("ORCID");
-    assertTrue(identifier.isValidIdentifier("0000-0002-1825-0097"));
-    // An "X" at the end of an ORCID is less common but still valid.
-    assertTrue(identifier.isValidIdentifier("0000-0002-1694-233X"));
-    assertFalse(identifier.isValidIdentifier("0000 0002 1825 0097"));
-    assertFalse(identifier.isValidIdentifier(" 0000-0002-1825-0097"));
-    assertFalse(identifier.isValidIdentifier("0000-0002-1825-0097 "));
-    assertFalse(identifier.isValidIdentifier("junk"));
+      ExternalIdentifier identifier = ExternalIdentifier.valueOf("ORCID");
+      assertTrue(identifier.isValidIdentifier("0000-0002-1825-0097"));
+      // An "X" at the end of an ORCID is less common but still valid.
+      assertTrue(identifier.isValidIdentifier("0000-0002-1694-233X"));
+      assertFalse(identifier.isValidIdentifier("0000 0002 1825 0097"));
+      assertFalse(identifier.isValidIdentifier(" 0000-0002-1825-0097"));
+      assertFalse(identifier.isValidIdentifier("0000-0002-1825-0097 "));
+      assertFalse(identifier.isValidIdentifier("junk"));
+      
+      // Test ORCID with https://orcid.org/ prefix
+      assertTrue(identifier.isValidIdentifier("https://orcid.org/0000-0002-1825-0097"));
+      assertTrue(identifier.isValidIdentifier("https://orcid.org/0000-0002-1694-233X"));
+      
+      // Test format command
+      assertEquals("https://orcid.org/0000-0002-1825-0097", identifier.format("0000-0002-1825-0097"));
+      assertEquals("https://orcid.org/0000-0002-1694-233X", identifier.format("0000-0002-1694-233X"));
+      assertEquals("https://orcid.org/0000-0002-1825-0097", identifier.format("https://orcid.org/0000-0002-1825-0097"));
   }
 
+  @Test
+  public void testIsValidAuthorIdentifierRor() {
+      ExternalIdentifier identifier = ExternalIdentifier.valueOf("ROR");
+      assertTrue(identifier.isValidIdentifier("01h6qyw18"));
+      assertTrue(identifier.isValidIdentifier("02mhbdp49"));
+      assertFalse(identifier.isValidIdentifier("01h6qyw1"));  // Too short
+      assertFalse(identifier.isValidIdentifier("01h6qyw18a")); // Too long
+      assertFalse(identifier.isValidIdentifier(" 01h6qyw18"));
+      assertFalse(identifier.isValidIdentifier("01h6qyw18 "));
+      assertFalse(identifier.isValidIdentifier("junk"));
+      
+      // Test ROR with https://ror.org/ prefix
+      assertTrue(identifier.isValidIdentifier("https://ror.org/01h6qyw18"));
+      assertTrue(identifier.isValidIdentifier("https://ror.org/02mhbdp49"));
+      
+      // Test format command
+      assertEquals("https://ror.org/01h6qyw18", identifier.format("01h6qyw18"));
+      assertEquals("https://ror.org/02mhbdp49", identifier.format("02mhbdp49"));
+      assertEquals("https://ror.org/01h6qyw18", identifier.format("https://ror.org/01h6qyw18"));
+  }
+  
   @Test
   public void testIsValidAuthorIdentifierIsni() {
     ExternalIdentifier identifier = ExternalIdentifier.valueOf("ISNI");
