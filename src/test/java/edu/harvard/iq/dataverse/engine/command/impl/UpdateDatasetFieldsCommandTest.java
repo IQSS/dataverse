585bd61ea7554e65aeec51ffae9c54a54c4d5320
GPortas <hey@gportas.me>
2025-02-25 14:51:07 +0000

    Added: test cases to UpdateDatasetFieldsCommandTest

--- src/test/java/edu/harvard/iq/dataverse/engine/command/impl/UpdateDatasetFieldsCommandTest.java
+++ src/test/java/edu/harvard/iq/dataverse/engine/command/impl/UpdateDatasetFieldsCommandTest.java
@@ -59,23 +59,123 @@ public class UpdateDatasetFieldsCommandTest {
 
         CommandException exception = assertThrows(InvalidCommandArgumentsException.class, () -> sut.execute(commandContextMock));
 
-        assertEquals(
-                BundleUtil.getStringFromBundle("updateDatasetFieldsCommand.api.processDatasetUpdate.parseError", List.of("validation error")),
-                exception.getMessage()
-        );
+        assertEquals(BundleUtil.getStringFromBundle("updateDatasetFieldsCommand.api.processDatasetUpdate.parseError", List.of("validation error")),
+                exception.getMessage());
     }
 
     @Test
-    public void execute_withValidFields_updatesFields() throws CommandException {
-        List<DatasetField> testUpdatedFields = new ArrayList<>();
-        sut = new UpdateDatasetFieldsCommand(datasetMock, testUpdatedFields, true, dataverseRequestStub, updateDatasetVersionCommandStub);
+    public void execute_withSingleDatasetField_replaceDataTrue_updatesField() throws CommandException {
+        DatasetFieldType fieldType = new DatasetFieldType("test", DatasetFieldType.FieldType.TEXT, false);
+        DatasetVersion datasetVersion = prepareDatasetVersionWithSingleValueField(fieldType, "original");
+        List<DatasetField> updatedFields = createUpdatedSingleValueField(fieldType, "updated");
 
-        when(datasetFieldsValidatorMock.validateFields(testUpdatedFields, datasetVersionMock)).thenReturn("");
-        when(datasetVersionMock.getDatasetFields()).thenReturn(new ArrayList<>());
+        sut = new UpdateDatasetFieldsCommand(datasetMock, updatedFields, true, dataverseRequestStub, updateDatasetVersionCommandStub);
+        when(datasetFieldsValidatorMock.validateFields(updatedFields, datasetVersion)).thenReturn("");
 
         Dataset result = sut.execute(commandContextMock);
-
         verify(commandContextMock).engine();
-        assertNotNull(result);
+
+        assertEquals("updated", result.getLatestVersion().getDatasetField(fieldType).getValue());
+    }
+
+    @Test
+    public void execute_withSingleEmptyDatasetField_replaceDataTrue_updatesFieldToEmpty() throws CommandException {
+        DatasetFieldType fieldType = new DatasetFieldType("test", DatasetFieldType.FieldType.TEXT, false);
+        DatasetVersion datasetVersion = prepareDatasetVersionWithSingleValueField(fieldType, "original");
+        List<DatasetField> updatedFields = createUpdatedSingleValueField(fieldType, "");
+
+        sut = new UpdateDatasetFieldsCommand(datasetMock, updatedFields, true, dataverseRequestStub, updateDatasetVersionCommandStub);
+        when(datasetFieldsValidatorMock.validateFields(updatedFields, datasetVersion)).thenReturn("");
+
+        Dataset result = sut.execute(commandContextMock);
+        verify(commandContextMock).engine();
+
+        assertTrue(result.getLatestVersion().getDatasetField(fieldType).isEmpty());
+    }
+
+    @Test
+    public void execute_withSingleDatasetField_replaceDataFalse_doesNotUpdateField() throws CommandException {
+        DatasetFieldType fieldType = new DatasetFieldType("test", DatasetFieldType.FieldType.TEXT, false);
+        DatasetVersion datasetVersion = prepareDatasetVersionWithSingleValueField(fieldType, "original");
+        List<DatasetField> updatedFields = createUpdatedSingleValueField(fieldType, "updated");
+
+        sut = new UpdateDatasetFieldsCommand(datasetMock, updatedFields, false, dataverseRequestStub, updateDatasetVersionCommandStub);
+        when(datasetFieldsValidatorMock.validateFields(updatedFields, datasetVersion)).thenReturn("");
+
+        Dataset result = sut.execute(commandContextMock);
+        verify(commandContextMock).engine();
+
+        assertEquals("original", result.getLatestVersion().getDatasetField(fieldType).getValue());
+    }
+
+    @Test
+    public void execute_withMultipleDatasetField_replaceDataFalse_updatesField() throws CommandException {
+        DatasetFieldType fieldType = new DatasetFieldType("test", DatasetFieldType.FieldType.TEXT, true);
+        DatasetVersion datasetVersion = prepareDatasetVersionWithSingleValueField(fieldType, "original");
+        List<DatasetField> updatedFields = createUpdatedSingleValueField(fieldType, "updated");
+
+        sut = new UpdateDatasetFieldsCommand(datasetMock, updatedFields, false, dataverseRequestStub, updateDatasetVersionCommandStub);
+        when(datasetFieldsValidatorMock.validateFields(updatedFields, datasetVersion)).thenReturn("");
+
+        Dataset result = sut.execute(commandContextMock);
+        verify(commandContextMock).engine();
+
+        assertEquals(2, result.getLatestVersion().getDatasetField(fieldType).getDatasetFieldValues().size());
+    }
+
+    @Test
+    public void execute_withMultipleDatasetField_replaceDataTrue_updatesField() throws CommandException {
+        DatasetFieldType fieldType = new DatasetFieldType("test", DatasetFieldType.FieldType.TEXT, true);
+        DatasetVersion datasetVersion = prepareDatasetVersionWithSingleValueField(fieldType, "original");
+        List<DatasetField> updatedFields = createUpdatedSingleValueField(fieldType, "updated");
+
+        sut = new UpdateDatasetFieldsCommand(datasetMock, updatedFields, true, dataverseRequestStub, updateDatasetVersionCommandStub);
+        when(datasetFieldsValidatorMock.validateFields(updatedFields, datasetVersion)).thenReturn("");
+
+        Dataset result = sut.execute(commandContextMock);
+        verify(commandContextMock).engine();
+
+        assertEquals(1, result.getLatestVersion().getDatasetField(fieldType).getDatasetFieldValues().size());
+    }
+
+    @Test
+    public void execute_withMultipleEmptyDatasetField_replaceDataTrue_updatesFieldToEmpty() throws CommandException {
+        DatasetFieldType fieldType = new DatasetFieldType("test", DatasetFieldType.FieldType.TEXT, true);
+        DatasetVersion datasetVersion = prepareDatasetVersionWithSingleValueField(fieldType, "original");
+        List<DatasetField> updatedFields = createUpdatedSingleValueField(fieldType, "");
+
+        sut = new UpdateDatasetFieldsCommand(datasetMock, updatedFields, true, dataverseRequestStub, updateDatasetVersionCommandStub);
+        when(datasetFieldsValidatorMock.validateFields(updatedFields, datasetVersion)).thenReturn("");
+
+        Dataset result = sut.execute(commandContextMock);
+        verify(commandContextMock).engine();
+
+        assertTrue(result.getLatestVersion().getDatasetField(fieldType).isEmpty());
+    }
+
+    private DatasetVersion prepareDatasetVersionWithSingleValueField(DatasetFieldType fieldType, String originalValue) {
+        DatasetVersion datasetVersion = new DatasetVersion();
+        datasetVersion.setTermsOfUseAndAccess(new TermsOfUseAndAccess());
+
+        List<DatasetField> fields = new ArrayList<>();
+        DatasetField field = new DatasetField();
+        field.setDatasetFieldType(fieldType);
+        field.setSingleValue(originalValue);
+        fields.add(field);
+        datasetVersion.setDatasetFields(fields);
+
+        when(datasetMock.getOrCreateEditVersion()).thenReturn(datasetVersion);
+        when(datasetMock.getLatestVersion()).thenReturn(datasetVersion);
+
+        return datasetVersion;
+    }
+
+    private List<DatasetField> createUpdatedSingleValueField(DatasetFieldType fieldType, String updatedValue) {
+        List<DatasetField> updatedFields = new ArrayList<>();
+        DatasetField updatedField = new DatasetField();
+        updatedField.setDatasetFieldType(fieldType);
+        updatedField.setSingleValue(updatedValue);
+        updatedFields.add(updatedField);
+        return updatedFields;
     }
 }
