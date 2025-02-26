9c22b583805eadd45de33af8fda703473a3abc12
GPortas <hey@gportas.me>
2025-02-26 16:45:22 +0000

    Added: controlled vocabulary test cases to UpdateDatasetFieldsCommandTest

--- src/test/java/edu/harvard/iq/dataverse/engine/command/impl/UpdateDatasetFieldsCommandTest.java
+++ src/test/java/edu/harvard/iq/dataverse/engine/command/impl/UpdateDatasetFieldsCommandTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -67,7 +68,7 @@ public class UpdateDatasetFieldsCommandTest {
     public void execute_withSingleDatasetField_replaceDataTrue_updatesField() throws CommandException {
         DatasetFieldType fieldType = new DatasetFieldType("test", DatasetFieldType.FieldType.TEXT, false);
         DatasetVersion datasetVersion = prepareDatasetVersionWithSingleValueField(fieldType, "original");
-        List<DatasetField> updatedFields = createUpdatedSingleValueField(fieldType, "updated");
+        List<DatasetField> updatedFields = createUpdatedSingleValueFieldList(fieldType, "updated");
 
         sut = new UpdateDatasetFieldsCommand(datasetMock, updatedFields, true, dataverseRequestStub, updateDatasetVersionCommandStub);
         when(datasetFieldsValidatorMock.validateFields(updatedFields, datasetVersion)).thenReturn("");
@@ -82,7 +83,7 @@ public class UpdateDatasetFieldsCommandTest {
     public void execute_withSingleEmptyDatasetField_replaceDataTrue_updatesFieldToEmpty() throws CommandException {
         DatasetFieldType fieldType = new DatasetFieldType("test", DatasetFieldType.FieldType.TEXT, false);
         DatasetVersion datasetVersion = prepareDatasetVersionWithSingleValueField(fieldType, "original");
-        List<DatasetField> updatedFields = createUpdatedSingleValueField(fieldType, "");
+        List<DatasetField> updatedFields = createUpdatedSingleValueFieldList(fieldType, "");
 
         sut = new UpdateDatasetFieldsCommand(datasetMock, updatedFields, true, dataverseRequestStub, updateDatasetVersionCommandStub);
         when(datasetFieldsValidatorMock.validateFields(updatedFields, datasetVersion)).thenReturn("");
@@ -97,7 +98,7 @@ public class UpdateDatasetFieldsCommandTest {
     public void execute_withSingleDatasetField_replaceDataFalse_doesNotUpdateField() throws CommandException {
         DatasetFieldType fieldType = new DatasetFieldType("test", DatasetFieldType.FieldType.TEXT, false);
         DatasetVersion datasetVersion = prepareDatasetVersionWithSingleValueField(fieldType, "original");
-        List<DatasetField> updatedFields = createUpdatedSingleValueField(fieldType, "updated");
+        List<DatasetField> updatedFields = createUpdatedSingleValueFieldList(fieldType, "updated");
 
         sut = new UpdateDatasetFieldsCommand(datasetMock, updatedFields, false, dataverseRequestStub, updateDatasetVersionCommandStub);
         when(datasetFieldsValidatorMock.validateFields(updatedFields, datasetVersion)).thenReturn("");
@@ -112,7 +113,7 @@ public class UpdateDatasetFieldsCommandTest {
     public void execute_withMultipleDatasetField_replaceDataFalse_updatesField() throws CommandException {
         DatasetFieldType fieldType = new DatasetFieldType("test", DatasetFieldType.FieldType.TEXT, true);
         DatasetVersion datasetVersion = prepareDatasetVersionWithSingleValueField(fieldType, "original");
-        List<DatasetField> updatedFields = createUpdatedSingleValueField(fieldType, "updated");
+        List<DatasetField> updatedFields = createUpdatedSingleValueFieldList(fieldType, "updated");
 
         sut = new UpdateDatasetFieldsCommand(datasetMock, updatedFields, false, dataverseRequestStub, updateDatasetVersionCommandStub);
         when(datasetFieldsValidatorMock.validateFields(updatedFields, datasetVersion)).thenReturn("");
@@ -120,6 +121,8 @@ public class UpdateDatasetFieldsCommandTest {
         Dataset result = sut.execute(commandContextMock);
         verify(commandContextMock).engine();
 
+        assertEquals("original", result.getLatestVersion().getDatasetField(fieldType).getDatasetFieldValues().get(0).getValue());
+        assertEquals("updated", result.getLatestVersion().getDatasetField(fieldType).getDatasetFieldValues().get(1).getValue());
         assertEquals(2, result.getLatestVersion().getDatasetField(fieldType).getDatasetFieldValues().size());
     }
 
@@ -127,7 +130,7 @@ public class UpdateDatasetFieldsCommandTest {
     public void execute_withMultipleDatasetField_replaceDataTrue_updatesField() throws CommandException {
         DatasetFieldType fieldType = new DatasetFieldType("test", DatasetFieldType.FieldType.TEXT, true);
         DatasetVersion datasetVersion = prepareDatasetVersionWithSingleValueField(fieldType, "original");
-        List<DatasetField> updatedFields = createUpdatedSingleValueField(fieldType, "updated");
+        List<DatasetField> updatedFields = createUpdatedSingleValueFieldList(fieldType, "updated");
 
         sut = new UpdateDatasetFieldsCommand(datasetMock, updatedFields, true, dataverseRequestStub, updateDatasetVersionCommandStub);
         when(datasetFieldsValidatorMock.validateFields(updatedFields, datasetVersion)).thenReturn("");
@@ -135,6 +138,7 @@ public class UpdateDatasetFieldsCommandTest {
         Dataset result = sut.execute(commandContextMock);
         verify(commandContextMock).engine();
 
+        assertEquals("updated", result.getLatestVersion().getDatasetField(fieldType).getValue());
         assertEquals(1, result.getLatestVersion().getDatasetField(fieldType).getDatasetFieldValues().size());
     }
 
@@ -142,7 +146,7 @@ public class UpdateDatasetFieldsCommandTest {
     public void execute_withMultipleEmptyDatasetField_replaceDataTrue_updatesFieldToEmpty() throws CommandException {
         DatasetFieldType fieldType = new DatasetFieldType("test", DatasetFieldType.FieldType.TEXT, true);
         DatasetVersion datasetVersion = prepareDatasetVersionWithSingleValueField(fieldType, "original");
-        List<DatasetField> updatedFields = createUpdatedSingleValueField(fieldType, "");
+        List<DatasetField> updatedFields = createUpdatedSingleValueFieldList(fieldType, "");
 
         sut = new UpdateDatasetFieldsCommand(datasetMock, updatedFields, true, dataverseRequestStub, updateDatasetVersionCommandStub);
         when(datasetFieldsValidatorMock.validateFields(updatedFields, datasetVersion)).thenReturn("");
@@ -151,6 +155,73 @@ public class UpdateDatasetFieldsCommandTest {
         verify(commandContextMock).engine();
 
         assertTrue(result.getLatestVersion().getDatasetField(fieldType).isEmpty());
+    }
+
+    @Test
+    public void execute_withSingleControlledVocabularyDatasetField_replaceDataTrue_updatesField() throws CommandException {
+        DatasetFieldType fieldType = createControlledVocabularyDatasetFieldType(false);
+        List<ControlledVocabularyValue> controlledVocabularyValueList = setupControlledVocabularyValueList(fieldType);
+        DatasetVersion datasetVersion = prepareDatasetVersionWithSingleControlledVocabularyValueField(fieldType, controlledVocabularyValueList.get(0));
+        List<DatasetField> updatedFields = createUpdatedSingleControlledVocabularyValueFieldList(fieldType, controlledVocabularyValueList.get(1));
+
+        sut = new UpdateDatasetFieldsCommand(datasetMock, updatedFields, true, dataverseRequestStub, updateDatasetVersionCommandStub);
+        when(datasetFieldsValidatorMock.validateFields(updatedFields, datasetVersion)).thenReturn("");
+
+        Dataset result = sut.execute(commandContextMock);
+        verify(commandContextMock).engine();
+
+        assertEquals("law", result.getLatestVersion().getDatasetField(fieldType).getSingleControlledVocabularyValue().getStrValue());
+    }
+
+    @Test
+    public void execute_withSingleControlledVocabularyDatasetField_replaceDataFalse_doesNotUpdateField() throws CommandException {
+        DatasetFieldType fieldType = createControlledVocabularyDatasetFieldType(false);
+        List<ControlledVocabularyValue> controlledVocabularyValueList = setupControlledVocabularyValueList(fieldType);
+        DatasetVersion datasetVersion = prepareDatasetVersionWithSingleControlledVocabularyValueField(fieldType, controlledVocabularyValueList.get(0));
+        List<DatasetField> updatedFields = createUpdatedSingleControlledVocabularyValueFieldList(fieldType, controlledVocabularyValueList.get(1));
+
+        sut = new UpdateDatasetFieldsCommand(datasetMock, updatedFields, false, dataverseRequestStub, updateDatasetVersionCommandStub);
+        when(datasetFieldsValidatorMock.validateFields(updatedFields, datasetVersion)).thenReturn("");
+
+        Dataset result = sut.execute(commandContextMock);
+        verify(commandContextMock).engine();
+
+        assertEquals("mgmt", result.getLatestVersion().getDatasetField(fieldType).getSingleControlledVocabularyValue().getStrValue());
+    }
+
+    @Test
+    public void execute_withMultipleControlledVocabularyDatasetField_replaceDataTrue_updatesField() throws CommandException {
+        DatasetFieldType fieldType = createControlledVocabularyDatasetFieldType(true);
+        List<ControlledVocabularyValue> controlledVocabularyValueList = setupControlledVocabularyValueList(fieldType);
+        DatasetVersion datasetVersion = prepareDatasetVersionWithSingleControlledVocabularyValueField(fieldType, controlledVocabularyValueList.get(0));
+        List<DatasetField> updatedFields = createUpdatedSingleControlledVocabularyValueFieldList(fieldType, controlledVocabularyValueList.get(1));
+
+        sut = new UpdateDatasetFieldsCommand(datasetMock, updatedFields, true, dataverseRequestStub, updateDatasetVersionCommandStub);
+        when(datasetFieldsValidatorMock.validateFields(updatedFields, datasetVersion)).thenReturn("");
+
+        Dataset result = sut.execute(commandContextMock);
+        verify(commandContextMock).engine();
+
+        assertEquals(1, result.getLatestVersion().getDatasetField(fieldType).getControlledVocabularyValues().size());
+        assertEquals("law", result.getLatestVersion().getDatasetField(fieldType).getSingleControlledVocabularyValue().getStrValue());
+    }
+
+    @Test
+    public void execute_withMultipleControlledVocabularyDatasetField_replaceDataFalse_updatesField() throws CommandException {
+        DatasetFieldType fieldType = createControlledVocabularyDatasetFieldType(true);
+        List<ControlledVocabularyValue> controlledVocabularyValueList = setupControlledVocabularyValueList(fieldType);
+        DatasetVersion datasetVersion = prepareDatasetVersionWithSingleControlledVocabularyValueField(fieldType, controlledVocabularyValueList.get(0));
+        List<DatasetField> updatedFields = createUpdatedSingleControlledVocabularyValueFieldList(fieldType, controlledVocabularyValueList.get(1));
+
+        sut = new UpdateDatasetFieldsCommand(datasetMock, updatedFields, false, dataverseRequestStub, updateDatasetVersionCommandStub);
+        when(datasetFieldsValidatorMock.validateFields(updatedFields, datasetVersion)).thenReturn("");
+
+        Dataset result = sut.execute(commandContextMock);
+        verify(commandContextMock).engine();
+
+        assertEquals(2, result.getLatestVersion().getDatasetField(fieldType).getControlledVocabularyValues().size());
+        assertEquals("mgmt", result.getLatestVersion().getDatasetField(fieldType).getControlledVocabularyValues().get(0).getStrValue());
+        assertEquals("law", result.getLatestVersion().getDatasetField(fieldType).getControlledVocabularyValues().get(1).getStrValue());
     }
 
     private DatasetVersion prepareDatasetVersionWithSingleValueField(DatasetFieldType fieldType, String originalValue) {
@@ -170,12 +241,56 @@ public class UpdateDatasetFieldsCommandTest {
         return datasetVersion;
     }
 
-    private List<DatasetField> createUpdatedSingleValueField(DatasetFieldType fieldType, String updatedValue) {
+    private List<DatasetField> createUpdatedSingleValueFieldList(DatasetFieldType fieldType, String updatedValue) {
         List<DatasetField> updatedFields = new ArrayList<>();
         DatasetField updatedField = new DatasetField();
         updatedField.setDatasetFieldType(fieldType);
         updatedField.setSingleValue(updatedValue);
         updatedFields.add(updatedField);
         return updatedFields;
+    }
+
+    private DatasetVersion prepareDatasetVersionWithSingleControlledVocabularyValueField(DatasetFieldType fieldType, ControlledVocabularyValue controlledVocabularyValue) {
+        DatasetVersion datasetVersion = new DatasetVersion();
+        datasetVersion.setTermsOfUseAndAccess(new TermsOfUseAndAccess());
+
+        List<DatasetField> fields = new ArrayList<>();
+        DatasetField field = new DatasetField();
+        field.setDatasetFieldType(fieldType);
+        field.setSingleControlledVocabularyValue(controlledVocabularyValue);
+        fields.add(field);
+        datasetVersion.setDatasetFields(fields);
+
+        when(datasetMock.getOrCreateEditVersion()).thenReturn(datasetVersion);
+        when(datasetMock.getLatestVersion()).thenReturn(datasetVersion);
+
+        return datasetVersion;
+    }
+
+    private List<DatasetField> createUpdatedSingleControlledVocabularyValueFieldList(DatasetFieldType fieldType, ControlledVocabularyValue controlledVocabularyValue) {
+        List<DatasetField> updatedFields = new ArrayList<>();
+        DatasetField updatedField = new DatasetField();
+        updatedField.setDatasetFieldType(fieldType);
+        updatedField.setSingleControlledVocabularyValue(controlledVocabularyValue);
+        updatedFields.add(updatedField);
+        return updatedFields;
+    }
+
+    private List<ControlledVocabularyValue> setupControlledVocabularyValueList(DatasetFieldType fieldType) {
+        ControlledVocabularyValue controlledVocabularyValue1 = new ControlledVocabularyValue(1L, "mgmt", fieldType);
+        ControlledVocabularyValue controlledVocabularyValue2 = new ControlledVocabularyValue(2L, "law", fieldType);
+        List<ControlledVocabularyValue> controlledVocabularyValueList = Arrays.asList(
+                controlledVocabularyValue1,
+                controlledVocabularyValue2
+        );
+        fieldType.setControlledVocabularyValues(controlledVocabularyValueList);
+        return controlledVocabularyValueList;
+    }
+
+    private DatasetFieldType createControlledVocabularyDatasetFieldType(boolean allowMultiples) {
+        DatasetFieldType fieldType = new DatasetFieldType("test", DatasetFieldType.FieldType.TEXT, allowMultiples);
+        fieldType.setAllowControlledVocabulary(true);
+        fieldType.setMetadataBlock(new MetadataBlock());
+        return fieldType;
     }
 }
