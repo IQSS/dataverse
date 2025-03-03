736a2f076b4165b92d3053816278443ffb818101
qqmyers <qqmyers@hotmail.com>
2025-03-03 16:10:20 -0500

    add unique constraint

--- src/main/resources/db/migration/V6.5.0.7.sql
+++ src/main/resources/db/migration/V6.5.0.7.sql
@@ -1,6 +1,4 @@
 -- Store authenticated orcid in URL form
 ALTER TABLE authenticateduser ADD COLUMN IF NOT EXISTS authenticatedorcid VARCHAR(45);
--- To do - add unique constraint in Postgres 15+
--- ALTER TABLE authenticateduser DROP CONSTRAINT IF EXISTS orcid_unique;
--- ALTER TABLE authenticateduser
--- ADD CONSTRAINT orcid_unique UNIQUE NULLS NOT DISTINCT (authenticatedorcid);
+ALTER TABLE authenticateduser DROP CONSTRAINT IF EXISTS orcid_unique;
+ALTER TABLE authenticateduser ADD CONSTRAINT orcid_unique UNIQUE (authenticatedorcid);
