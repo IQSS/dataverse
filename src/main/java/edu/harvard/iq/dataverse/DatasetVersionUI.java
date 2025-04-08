138a62a687759dc237ed9d0cd9be741ba26e931f
Stephen Kraffmiller <skraffmiller@hmdc.harvard.edu>
2025-04-08 12:04:21 -0400

    #11391 fix mdb display on create

--- src/main/java/edu/harvard/iq/dataverse/DatasetVersionUI.java
+++ src/main/java/edu/harvard/iq/dataverse/DatasetVersionUI.java
@@ -458,6 +458,10 @@ public class DatasetVersionUI implements Serializable {
                         mdb.setEmpty(false);
                         datasetFieldsForView.add(dsf);
                     }
+                    //Seeting Local Display on Create on mdb when there are any set at dataverse level 
+                    if ( dsf.getDatasetFieldType().getLocalDisplayOnCreate() != null ){
+                       mdb.setLocalDisplayOnCreate(dsf.getDatasetFieldType().getLocalDisplayOnCreate());
+                    }
                 }
             }
 
