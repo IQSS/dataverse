4fca21b37d867aa20f31933c0c38144a3d85ca5f
Jim Myers <qqmyers@hotmail.com>
2025-07-07 14:32:00 -0400

    missing import

--- src/main/java/edu/harvard/iq/dataverse/DataverseRoleServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/DataverseRoleServiceBean.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
