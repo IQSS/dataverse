dd5dce9b40df935d65371a4f10a9e02a4df4af5f
Jim Myers <qqmyers@hotmail.com>
2025-07-08 10:41:09 -0400

    handle XMLReader

--- src/main/java/edu/harvard/iq/dataverse/util/xml/XmlUtil.java
+++ src/main/java/edu/harvard/iq/dataverse/util/xml/XmlUtil.java
@@ -2,9 +2,16 @@ package edu.harvard.iq.dataverse.util.xml;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
 
 /**
  * Utility class for XML processing with security settings to prevent XXE attacks.
@@ -74,5 +81,34 @@ public class XmlUtil {
         }
         
         return factory;
+    }
+    
+    /**
+     * Creates and returns a secure XMLReader with protection against XXE attacks.
+     * 
+     * @return A secure XMLReader instance
+     * @throws SAXException If there's an error creating the XMLReader
+     * @throws ParserConfigurationException If there's an error configuring the parser
+     */
+    public static XMLReader getSecureXMLReader() throws SAXException, ParserConfigurationException {
+        SAXParserFactory spf = SAXParserFactory.newInstance();
+        
+        // Configure the parser factory with security features
+        spf.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+        spf.setFeature("http://xml.org/sax/features/external-general-entities", false);
+        spf.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+        spf.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+        
+        // Create a secure parser
+        SAXParser parser = spf.newSAXParser();
+        
+        // Set additional security properties
+        parser.setProperty(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+        parser.setProperty(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
+        
+        // Get the XMLReader from the parser
+        XMLReader reader = parser.getXMLReader();
+        
+        return reader;
     }
 }
