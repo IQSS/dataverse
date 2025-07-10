2b476e3a8175805b4e24f12e97db3d8071dc13e9
Jim Myers <qqmyers@hotmail.com>
2025-07-10 14:44:29 -0400

    use LRU

--- src/main/java/edu/harvard/iq/dataverse/util/DataSourceProducer.java
+++ src/main/java/edu/harvard/iq/dataverse/util/DataSourceProducer.java
@@ -55,7 +55,7 @@ import javax.sql.DataSource;
             "fish.payara.statement-leak-reclaim=${MPCONFIG=dataverse.db.statement-leak-reclaim:false}",
             // LOGGING, SLOWNESS, PERFORMANCE
             "fish.payara.statement-cache-size=${MPCONFIG=dataverse.db.statement-cache-size:50}",
-            "fish.payara.statement-cache-type=${MPCONFIG=dataverse.db.statement-cache-type:com.sun.gjc.util.LRUCache}",
+            "fish.payara.statement-cache-type=${MPCONFIG=dataverse.db.statement-cache-type:LRU}",
             "fish.payara.statement-timeout-in-seconds=${MPCONFIG=dataverse.db.statement-timeout-in-seconds:-1}",
             "fish.payara.slow-query-threshold-in-seconds=${MPCONFIG=dataverse.db.slow-query-threshold-in-seconds:-1}",
             "fish.payara.log-jdbc-calls=${MPCONFIG=dataverse.db.log-jdbc-calls:false}"
