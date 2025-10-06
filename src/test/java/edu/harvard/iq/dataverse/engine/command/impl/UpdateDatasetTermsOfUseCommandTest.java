ca1536686566ba433d23b1221b3eafcdd39a5e5a
Stephen Kraffmiller <skraffmiller@hmdc.harvard.edu>
2025-10-06 15:15:13 -0400

    #11772 fix unit test

--- src/test/java/edu/harvard/iq/dataverse/engine/command/impl/UpdateDatasetTermsOfUseCommandTest.java
+++ src/test/java/edu/harvard/iq/dataverse/engine/command/impl/UpdateDatasetTermsOfUseCommandTest.java
@@ -1,27 +1,18 @@
-
 package edu.harvard.iq.dataverse.engine.command.impl;
 
 import edu.harvard.iq.dataverse.Dataset;
 import edu.harvard.iq.dataverse.DatasetVersion;
 import edu.harvard.iq.dataverse.TermsOfUseAndAccess;
-import edu.harvard.iq.dataverse.authorization.users.AuthenticatedUser;
-import edu.harvard.iq.dataverse.dataset.DatasetFieldsValidator;
 import edu.harvard.iq.dataverse.engine.DataverseEngine;
 import edu.harvard.iq.dataverse.engine.command.CommandContext;
 import edu.harvard.iq.dataverse.engine.command.DataverseRequest;
 import edu.harvard.iq.dataverse.engine.command.exception.CommandException;
-import edu.harvard.iq.dataverse.engine.command.exception.IllegalCommandException;
-import edu.harvard.iq.dataverse.engine.command.exception.InvalidCommandArgumentsException;
-import edu.harvard.iq.dataverse.util.BundleUtil;
-import java.util.ArrayList;
-import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
@@ -31,57 +22,62 @@ import org.mockito.MockitoAnnotations;
  * @author stephenkraffmiller
  */
 public class UpdateDatasetTermsOfUseCommandTest {
+
     @Mock
-    private UpdateDatasetVersionCommand updateDatasetVersionCommandStub;
+    private CommandContext ctxt;
+
     @Mock
     private DataverseEngine dataverseEngineMock;
+
+    @Mock
+    private UpdateDatasetVersionCommand updateDatasetVersionCommand;
+
     @Mock
     private Dataset datasetMock;
+
     @Mock
-    private DataverseRequest dataverseRequestStub;
-    @Mock
-    private TermsOfUseAndAccess termsOfUseAndAccessStub;
+    private DataverseRequest request;
+
     @Mock
     private CommandContext commandContextMock;
+
     @Mock
     private DatasetVersion datasetVersionMock;
-    @Mock
-    private DatasetFieldsValidator datasetFieldsValidatorMock;
-    @Mock
-    private AuthenticatedUser authenticatedUser;
 
-    
+    private Dataset dataset = new Dataset();
+    private TermsOfUseAndAccess terms = new TermsOfUseAndAccess();
+
+    private UpdateDatasetTermsOfUseCommand command;
+
     @BeforeEach
     public void setUp() throws CommandException {
         MockitoAnnotations.openMocks(this);
-
-        when(dataverseEngineMock.submit(updateDatasetVersionCommandStub)).thenReturn(datasetMock);
+        when(dataverseEngineMock.submit(updateDatasetVersionCommand)).thenReturn(datasetMock);
         when(commandContextMock.engine()).thenReturn(dataverseEngineMock);
-        when(commandContextMock.datasetFieldsValidator()).thenReturn(datasetFieldsValidatorMock);
         when(datasetMock.getOrCreateEditVersion()).thenReturn(datasetVersionMock);
-        when(datasetVersionMock.getTermsOfUseAndAccess()).thenReturn(termsOfUseAndAccessStub);
+        dataset.getOrCreateEditVersion();
+        dataset = new Dataset();
+        dataset.getOrCreateEditVersion();
+        terms = new TermsOfUseAndAccess();
 
+        command = new UpdateDatasetTermsOfUseCommand(dataset, terms, request, updateDatasetVersionCommand);
     }
-    
+
     @Test
-    public void latestVersionShouldBeDraft() {
-        // Arrange
+    public void testExecute_SubmitsUpdateCommandAndUpdatesTerms() throws Exception {
+        when(ctxt.engine()).thenReturn(dataverseEngineMock);
+        Dataset expectedDataset = new Dataset();
+        when(dataverseEngineMock.submit(updateDatasetVersionCommand)).thenReturn(expectedDataset);
 
-        Dataset dataset = new Dataset();
-        TermsOfUseAndAccess toua = new TermsOfUseAndAccess();
-        UpdateDatasetTermsOfUseCommand sut = new UpdateDatasetTermsOfUseCommand(dataset, toua, dataverseRequestStub);
-        try {
-           Dataset updatedDataset = sut.execute(commandContextMock);
-            verify(commandContextMock).engine();
-            assertNotNull(datasetMock);
-        } catch (CommandException ce) {
+        Dataset result = command.execute(ctxt);
 
-        }
+        DatasetVersion version = dataset.getOrCreateEditVersion();
+        assertEquals(terms, version.getTermsOfUseAndAccess());
+        assertEquals(DatasetVersion.VersionState.DRAFT, version.getVersionState());
+        assertSame(version, terms.getDatasetVersion());
 
-        // Act & Assert
-        DatasetVersion retVal = dataset.getLatestVersion();
-
+        verify(dataverseEngineMock).submit(updateDatasetVersionCommand);
+        assertSame(expectedDataset, result);
     }
-    
 
 }
