f18dbadfcdc51f1c19976d94054b9696e9151f90
Steven Winship <39765413+stevenwinship@users.noreply.github.com>
2025-10-14 09:58:02 -0400

    adding index and other sql

---
+++ src/main/resources/db/migration/V6.7.1.3.sql
@@ -0,0 +1,10 @@
+CREATE INDEX IF NOT EXISTS  INDEX_GUESTBOOKRESPONSE_datasetversion_id ON GUESTBOOKRESPONSE (datasetversion_id);
+
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'datasetversion' AND column_name = 'versionstate') THEN
+UPDATE datasetversion SET versionstate = 'DRAFT' where versionstate IS NULL;
+ALTER TABLE datasetversion ALTER COLUMN versionstate SET NOT NULL;
+END IF;
+END
+$$;
