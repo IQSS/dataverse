371ef93cbc10a387c9325771279701208cc372ab
Leonid Andreev <leonid@hmdc.harvard.edu>
2025-10-17 20:01:27 -0400

    Adding an index on DATASETMETRICS/dataset_id. #11828

--- src/main/resources/db/migration/V6.8.0.1.sql
+++ src/main/resources/db/migration/V6.8.0.1.sql
@@ -1,5 +1,7 @@
 CREATE INDEX IF NOT EXISTS  INDEX_GUESTBOOKRESPONSE_datasetversion_id ON GUESTBOOKRESPONSE (datasetversion_id);
 
+CREATE INDEX IF NOT EXISTS INDEX_DATASETMETRICS_dataset_id ON DATASETMETRICS (dataset_id);
+
 DO $$
 BEGIN
     IF EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'datasetversion' AND column_name = 'versionstate') THEN
