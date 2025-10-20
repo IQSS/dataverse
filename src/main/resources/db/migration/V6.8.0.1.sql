91347cafd250f1be9660c52a8a7a370413aa18a5
Leonid Andreev <leonid@hmdc.harvard.edu>
2025-10-20 10:23:02 -0400

    Simplified the flyway script. #11828

--- src/main/resources/db/migration/V6.8.0.1.sql
+++ src/main/resources/db/migration/V6.8.0.1.sql
@@ -2,11 +2,5 @@ CREATE INDEX IF NOT EXISTS  INDEX_GUESTBOOKRESPONSE_datasetversion_id ON GUESTBO
 
 CREATE INDEX IF NOT EXISTS INDEX_DATASETMETRICS_dataset_id ON DATASETMETRICS (dataset_id);
 
-DO $$
-BEGIN
-    IF EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'datasetversion' AND column_name = 'versionstate') THEN
-        UPDATE datasetversion SET versionstate = 'DRAFT' where versionstate IS NULL;
-        ALTER TABLE datasetversion ALTER COLUMN versionstate SET NOT NULL;
-    END IF;
-END
-$$;
+UPDATE datasetversion SET versionstate = 'DRAFT' where versionstate IS NULL;
+ALTER TABLE datasetversion ALTER COLUMN versionstate SET NOT NULL;
