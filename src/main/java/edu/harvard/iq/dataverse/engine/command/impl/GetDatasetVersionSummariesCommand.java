b26a14d5182e2dfe5e77c1e092bacb786f65c27f
GPortas <hey@gportas.me>
2025-10-22 16:29:45 +0100

    Refactor: GetDatasetVersionSummariesCommand to include explanatory method doc and removed unused exception throws

--- src/main/java/edu/harvard/iq/dataverse/engine/command/impl/GetDatasetVersionSummariesCommand.java
+++ src/main/java/edu/harvard/iq/dataverse/engine/command/impl/GetDatasetVersionSummariesCommand.java
@@ -7,7 +7,6 @@ import edu.harvard.iq.dataverse.datasetversionsummaries.DatasetVersionSummary;
 import edu.harvard.iq.dataverse.engine.command.CommandContext;
 import edu.harvard.iq.dataverse.engine.command.DataverseRequest;
 import edu.harvard.iq.dataverse.engine.command.RequiredPermissions;
-import edu.harvard.iq.dataverse.engine.command.exception.CommandException;
 
 import java.util.EnumSet;
 import java.util.List;
@@ -26,8 +25,19 @@ public class GetDatasetVersionSummariesCommand extends AbstractPaginatedCommand<
         this.dataset = dataset;
     }
 
+    /**
+     * Executes the command to retrieve a paginated list of dataset version summaries.
+     * <p>
+     * This method first checks if the user has permission to view unpublished
+     * versions of the dataset. It then fetches the appropriate {@link DatasetVersion}s,
+     * respecting pagination parameters (limit and offset), and converts each
+     * version into a {@link DatasetVersionSummary}.
+     *
+     * @param ctxt The command context.
+     * @return A list of {@link DatasetVersionSummary} objects.
+     */
     @Override
-    public List<DatasetVersionSummary> executeCommand(CommandContext ctxt) throws CommandException {
+    public List<DatasetVersionSummary> executeCommand(CommandContext ctxt) {
         boolean canViewUnpublished = ctxt.permissions().hasPermissionsFor(
                 getRequest().getUser(),
                 dataset,
