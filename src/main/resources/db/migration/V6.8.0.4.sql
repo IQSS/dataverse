75eddab0362f434597469f9c2a29bd4b6f55a378
Steven Winship <39765413+stevenwinship@users.noreply.github.com>
2025-12-12 11:51:16 -0500

    remove flyway script

--- src/main/resources/db/migration/V6.8.0.4.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE metric DROP CONSTRAINT fk_metric_dataverse_id;
-ALTER TABLE metric ADD CONSTRAINT fk_metric_dataverse_id FOREIGN KEY (dataverse_id) REFERENCES dataverse(id) ON DELETE CASCADE;
