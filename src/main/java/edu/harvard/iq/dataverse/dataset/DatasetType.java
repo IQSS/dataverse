c3b4de6410b4c2040041b677c087f668fb3fa8e2
Philip Durbin <philip_durbin@harvard.edu>
2026-03-02 15:55:23 -0500

    let VS Code generate hashCode and equals

--- src/main/java/edu/harvard/iq/dataverse/dataset/DatasetType.java
+++ src/main/java/edu/harvard/iq/dataverse/dataset/DatasetType.java
@@ -213,4 +213,35 @@ public class DatasetType implements Serializable {
         }
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        DatasetType other = (DatasetType) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+
 }
