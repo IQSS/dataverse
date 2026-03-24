17e4829e26a818d6f187e7033bd653d4ae7022db
Vera Clemens <clemens@zbmed.de>
2026-03-24 10:48:27 +0100

    feat: show "Link" button for collections in UI to non-superusers

--- src/main/java/edu/harvard/iq/dataverse/DataversePage.java
+++ src/main/java/edu/harvard/iq/dataverse/DataversePage.java
@@ -218,7 +218,7 @@ public class DataversePage implements java.io.Serializable {
             testquery = query;
         }
 
-        return (session.getUser().isSuperuser() && (dataverse.getOwner() != null || !testquery.isEmpty()));
+        return (dataverse.getOwner() != null || !testquery.isEmpty());
     }
     
     public void setupLinkingPopup (String popupSetting){
