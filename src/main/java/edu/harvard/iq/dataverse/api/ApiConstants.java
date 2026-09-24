783059b8fa25a772524f65fb79346453b89e3d8f
Oliver Bertuch <o.bertuch@fz-juelich.de>
2026-09-24 16:37:28 +0200

    feat(api): introduce bulk dataset export endpoint
    Added datasets/export POST REST API endpoint to support bulk dataset version exports. Validates exporters, resolves datasets/versions, and processes requests with error aggregation.

--- src/main/java/edu/harvard/iq/dataverse/api/ApiConstants.java
+++ src/main/java/edu/harvard/iq/dataverse/api/ApiConstants.java
@@ -35,4 +35,7 @@ public final class ApiConstants {
     // addFiles call
     public static final String API_ADD_FILES_COUNT_PROCESSED = "Total number of files";
     public static final String API_ADD_FILES_COUNT_SUCCESSFUL = "Number of files successfully added";
+    
+    // Export
+    public static final String EXPORT_CORRELATION_ID = "X-Dataverse-Export-ID";
 }
