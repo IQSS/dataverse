64b72552d1427b184c61ec5d32ca462c8391910b
Philip Durbin <philip_durbin@harvard.edu>
2026-09-25 15:51:32 -0400

    get file upload working from the SPA
    The file store must start with "s3". This is currently
    hardcoded in the SPA:
    https://github.com/IQSS/dataverse-frontend/blob/v0.3.2/src/sections/dataset/dataset-files/dataset-upload-files-button/DatasetUploadFilesButton.tsx#L25

--- conf/localstack/buckets.sh
+++ conf/localstack/buckets.sh
@@ -2,3 +2,16 @@
 # https://stackoverflow.com/questions/53619901/auto-create-s3-buckets-on-localstack
 awslocal s3 mb s3://mybucket
 awslocal s3 mb s3://mybucket-noredirect
+# Allow direct uploads/downloads from browsers (e.g. the SPA) via presigned URLs
+awslocal s3api put-bucket-cors \
+  --bucket mybucket \
+  --cors-configuration '{
+    "CORSRules": [
+      {
+        "AllowedHeaders": ["*"],
+        "AllowedMethods": ["GET", "HEAD", "PUT", "POST", "DELETE"],
+        "AllowedOrigins": ["*"],
+        "ExposeHeaders": ["ETag"]
+      }
+    ]
+  }'
